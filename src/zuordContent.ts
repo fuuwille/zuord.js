dd3b062ec97f650666b946929c503b1ae7314a09
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 02:27:08 +0300

    Fix formatting in zuordContent.ts and ensure default export is correctly placed

--- src/zuordContent.ts
+++ src/zuordContent.ts
@@ -1,3 +1,5 @@
 export type ZuordContent<U extends object[]> = U extends [infer Head, ...infer Rest]
     ? Head & ZuordContent<Rest extends object[] ? Rest : []>
     : {};
+
+export default ZuordContent;
