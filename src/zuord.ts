2a3a3a6bda1d2fefcfa882b2f7009e0b06cdc7db
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 01:47:03 +0300

    Refactor zuord class to include static assign method for object merging

--- src/zuord.ts
+++ src/zuord.ts
@@ -1,5 +1,14 @@
+import { ZuordContent } from "./zuordChain";
+
 export class zuord {
     private constructor() {
+    }
+
+    //
+
+    public static assign<U extends object[]>(...rest: U): ZuordContent<U> {
+        const mergedRest = Object.assign({}, ...rest);
+        return mergedRest as ZuordContent<U>;
     }
 }
 
