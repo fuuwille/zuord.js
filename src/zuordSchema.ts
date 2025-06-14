3be96511e009e1c17d550a691a5f1a2584f0516f
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 14:54:30 +0300

    Add ZuordSchemaOf type definition for nested object structures

--- src/zuordSchema.ts
+++ src/zuordSchema.ts
@@ -4,5 +4,10 @@ export default ZuordSchema;
 
 //
 
+export type ZuordSchemaOf<T> = T extends object ? {
+    [K in keyof T]?: T[K] extends object
+        ? ZuordSchemaOf<T[K]> : true;
+} : never;
+
 export type IsZuordSchema<T> = T extends true 
     ? true : T extends object ? true : false;
