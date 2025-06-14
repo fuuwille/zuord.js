4d0ad70838ea0a892457e167026f3e3519456bd1
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 19:08:45 +0300

    Refactor ZuordMerge type to simplify type inference for rest parameters

--- src/zuordMerge.ts
+++ src/zuordMerge.ts
@@ -1,5 +1,6 @@
-export type ZuordMerge<U extends object[]> = U extends [infer Head, ...infer Rest]
-    ? Head & ZuordMerge<Rest extends object[] ? Rest : []>
+export type ZuordMerge<U extends object[]> = U extends [infer Head extends object, ...infer Rest extends object[]]
+    ? Head & ZuordMerge<Rest>
     : {};
+
 
 export default ZuordMerge;
