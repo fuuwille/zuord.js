07044fb031ab587df6f4e2b815a0e3ce1d8cf533
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 00:31:02 +0300

    Refactor zuord.ts to improve type exports and namespace organization

--- src/zuord.ts
+++ src/zuord.ts
@@ -3,3 +3,29 @@ export { zuordMerge, type ZuordMerge, type ZuordMergeRaw } from "./zuordMerge";
 export { zuordPick, type ZuordPick, type ZuordPickRaw, type ZuordPickOf } from "./zuordPick";
 export { zuordOmit, type ZuordOmit, type ZuordOmitRaw, type ZuordOmitOf } from "./zuordOmit";
 export { zuordSchema, type ZuordSchema, type IsZuordSchema } from "./zuordSchema";
+
+import { ZuordNormalize } from "./zuordNormalize";
+import { zuordMerge, type ZuordMerge, type ZuordMergeRaw } from "./zuordMerge";
+import { zuordPick, type ZuordPick, type ZuordPickRaw, type ZuordPickOf } from "./zuordPick";
+import { zuordOmit, type ZuordOmit, type ZuordOmitRaw, type ZuordOmitOf } from "./zuordOmit";
+import { zuordSchema, type ZuordSchema } from "./zuordSchema";
+
+export namespace Zuord {
+    export type Normalize<T> = ZuordNormalize<T>;
+    export type Merge<U extends object[]> = ZuordMerge<U>;
+    export type MergeRaw<U extends object[]> = ZuordMergeRaw<U>;
+    export type Pick<T, U> = ZuordPick<T, U>;
+    export type PickRaw<T, U> = ZuordPickRaw<T, U>;
+    export type PickOf<T, U> = ZuordPickOf<T, U>;
+    export type Omit<T, U> = ZuordOmit<T, U>;
+    export type OmitRaw<T, U> = ZuordOmitRaw<T, U>;
+    export type OmitOf<T, U> = ZuordOmitOf<T, U>;
+    export type Schema<T> = ZuordSchema<T>;
+};
+
+export const zuord = {
+  merge: zuordMerge,
+  pick: zuordPick,
+  omit: zuordOmit,
+  schema: zuordSchema,
+} as const;
