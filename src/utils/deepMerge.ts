81c12982c5eb777415be3a65549f33d84b94d35b
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 01:00:45 +0300

    Refactor DeepMerge type to enhance type checking for arrays and plain objects

--- src/utils/deepMerge.ts
+++ src/utils/deepMerge.ts
@@ -1,12 +1,27 @@
+import { IsArray } from "./isArray";
+import { IsPlainObject } from "./isPlainObject";
+
 export type DeepMerge<A, B> = {
-    [K in keyof A | keyof B]: K extends keyof B
-        ? K extends keyof A
-            ? A[K] extends object
-                ? B[K] extends object
-                    ? DeepMerge<A[K], B[K]>
-                    : B[K]
-                : B[K]
+  [K in keyof A | keyof B]:
+    K extends keyof B
+      ? K extends keyof A
+        ? IsArray<A[K]> extends true
+          ? IsArray<B[K]> extends true
+            ? Array<
+                A[K] extends readonly (infer U)[] 
+                  ? B[K] extends readonly (infer V)[]
+                    ? U | V
+                    : U
+                  : never
+              >
             : B[K]
-        : K extends keyof A
-        ? A[K] : never;
+          : IsPlainObject<A[K]> extends true
+          ? IsPlainObject<B[K]> extends true
+            ? DeepMerge<A[K], B[K]>
+            : B[K]
+          : B[K]
+        : B[K]
+      : K extends keyof A
+      ? A[K]
+      : never;
 };
