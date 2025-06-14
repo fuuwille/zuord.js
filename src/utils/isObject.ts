4b3828b166e59ce61c19b99cee13db060e741116
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 17:26:37 +0300

    Refactor isObject utility and add missing newlines for improved readability

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,3 +1,9 @@
 export type IsObject<T> = T extends object ? (T extends Function ? false : true) : false;
 
 export default IsObject;
+
+//
+
+export function isObject(item: any) : item is object {
+    return item !== null && typeof item === 'object' && !Array.isArray(item);
+}
