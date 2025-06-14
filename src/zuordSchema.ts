9cabe80ffb761729f8b9e6c72a0cc76a5ca792bd
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 14:35:04 +0300

    Add newline and formatting improvements to zuordSchema.ts

--- src/zuordSchema.ts
+++ src/zuordSchema.ts
@@ -1,3 +1,8 @@
 export type ZuordSchema = true | { [key: string]: ZuordSchema };
 
 export default ZuordSchema;
+
+//
+
+export type IsZuordSchema<T> = T extends true 
+    ? true : T extends object ? true : false;
