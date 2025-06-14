2a6a27578bd2a41c543ef6c067cc6d2356adffc3
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 23:32:59 +0300

    Add isFunction and isNever utility functions for type checking

--- src/utils/isFunction.ts
+++ src/utils/isFunction.ts
@@ -1,1 +1,5 @@
 export type IsFunction<T> = T extends (...args: any[]) => any ? true : false;
+
+export function isFunction(value: unknown): value is (...args: any[]) => unknown {
+    return typeof value === 'function';
+}
--- src/utils/isNever.ts
+++ src/utils/isNever.ts
@@ -1,1 +1,5 @@
 export type IsNever<T> = [T] extends [never] ? true : false;
+
+export function isNever<T>(value: T): value is never {
+    return value === undefined && typeof value === 'undefined';
+}
