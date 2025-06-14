58e19561809535b6869d2d07c6d64b5155815474
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 22:51:45 +0300

    Add export for zuordSchema and ensure proper type exports for ZuordSchema

--- src/zuord.ts
+++ src/zuord.ts
@@ -102,3 +102,4 @@ export type { ZuordMerge as ZuordContent } from "./zuordMerge";
 export type { ZuordPick, ZuordPickOf } from "./zuordPick";
 export type { ZuordOmit, ZuordOmitOf } from "./zuordOmit";
 export type { ZuordSchema, IsZuordSchema } from "./zuordSchema";
+export { zuordSchema } from "./zuordSchema";
