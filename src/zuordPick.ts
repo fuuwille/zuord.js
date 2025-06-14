9ed41e456db86f85cdea3bb9b91b0e2703ebed19
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 17:08:41 +0300

    Refactor ZuordPick type definition to remove unnecessary ZuordSchema constraint for improved flexibility

--- src/zuordPick.ts
+++ src/zuordPick.ts
@@ -1,6 +1,6 @@
 import { IsZuordSchema, ZuordSchema } from "./zuordSchema";
 
-export type ZuordPick<T, U extends ZuordSchema<T>> = {
+export type ZuordPick<T, U> = {
     [K in keyof T & keyof U as IsZuordSchema<U[K]> extends true ? K : never]:
         U[K] extends true
             ? T[K]
