549a99fb7ec1ca460a70b098b5409a73f3a2bbc4
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 22:13:18 +0300

    Refactor ZuordPick type to improve clarity and maintainability

--- src/zuordPick.ts
+++ src/zuordPick.ts
@@ -1,6 +1,13 @@
+import { ZuordNormalize } from "./zuordNormalize";
 import { IsZuordSchema, ZuordSchema } from "./zuordSchema";
 
-export type ZuordPick<T, U> = {
+export type ZuordPick<T, U> = ZuordNormalize<ZuordPickRaw<T, U>>;
+
+export default ZuordPick;
+
+//
+
+export type ZuordPickRaw<T, U> = {
     [K in keyof T & keyof U as IsZuordSchema<U[K]> extends true ? K : never]:
         U[K] extends true
             ? T[K]
