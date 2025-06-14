c3a41c9dc944ff91aa6feb7a378909a0c67913a3
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 19:49:58 +0300

    Refactor ZuordMerge type to utilize ZuordNormalize for improved type handling

--- src/zuordMerge.ts
+++ src/zuordMerge.ts
@@ -1,6 +1,11 @@
-export type ZuordMerge<U extends object[]> = U extends [infer Head extends object, ...infer Rest extends object[]]
-    ? Head & ZuordMerge<Rest>
-    : {};
+import { ZuordNormalize } from "./zuordNormalize";
 
+export type ZuordMerge<U extends object[]> = ZuordNormalize<ZuordMergeRaw<U>>
 
 export default ZuordMerge;
+
+//
+
+export type ZuordMergeRaw<U extends object[]> = U extends [infer Head extends object, ...infer Rest extends object[]]
+    ? Head & ZuordMerge<Rest>
+    : {};
