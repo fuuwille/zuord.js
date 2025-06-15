d4e0745766df52758e25b6125d30d0bf7a763cb7
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 11:58:20 +0300

    Refactor ZuordIsExists type for improved clarity and organization

--- src/util/exists.ts
+++ src/util/exists.ts
@@ -1,6 +1,12 @@
 import { ZuordIsExtends } from "./extends";
 
-export type ZuordIsExists<T, E> = (() => ZuordIsExtends<T, E>) extends (() => infer R) ? (
+//
+
+type IsExists<T, E> = (() => ZuordIsExtends<T, E>) extends (() => infer R) ? (
     [R] extends [never] ? false : 
     [R] extends [true] ? true : false
 ) : false;
+
+//
+
+export type ZuordIsExists<T, E> = IsExists<T, E>
