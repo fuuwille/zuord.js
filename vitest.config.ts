8f02e89559eb3e710f800ccc451e64a0ab6497de
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 22:15:58 +0300

    Refactor vitest configuration: restructure resolve alias for improved clarity

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+        resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
     test: {
         globals: true,
         environment: 'node',
         include: ['tests/**/*.test.ts', 'tests/**/*.spec.ts'],
         watch: false,
         testTimeout: 20000
-    },
+    }
 });
