03b1edcf6995a1c2102c20056a72f0d95d90dcf2
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 10:37:34 +0300

    Refactor ZuordIsExists type to improve conditional logic for existence checking

--- src/is/exists.ts
+++ src/is/exists.ts
@@ -1,3 +1,6 @@
 import { ZuordIsExtends } from "./extends";
 
-export type ZuordIsExists<T, H> = ZuordIsExtends<T, H> extends never ? false : true;
+export type ZuordIsExists<T, H> = (() => ZuordIsExtends<T, H>) extends (() => infer R) ? (
+    [R] extends [never] ? false : 
+    [R] extends [true] ? true : false
+) : false;
