d78c7bfe2e84a5207d83a2504d325b782afef3f7
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 11:56:17 +0300

    Refactor ZuordUtil namespace to include ArrayDepth and IsArray types for improved clarity

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,12 +2,22 @@ export { type ZuordUtilIntegrate } from "./integrate";
 export { type ZuordUtilNormalize } from "./normalize";
 export { type ZuordUtilPartial } from "./partial";
 
+import { type ZuordArrayDepth, type ZuordIsArray } from "./array";
 import { type ZuordUtilIntegrate } from "./integrate";
 import { type ZuordUtilNormalize } from "./normalize";
 import { type ZuordUtilPartial } from "./partial";
 
 export namespace ZuordUtil {
+    // Array
+    export type ArrayDepth<T> = ZuordArrayDepth<T>;
+    export type IsArray<T> = ZuordIsArray<T>;
+
+    // Integrate
     export type Integrate<A, B> = ZuordUtilIntegrate<A, B>;
+
+    // Normalize
     export type Normalize<T> = ZuordUtilNormalize<T>;
+
+    // Partial
     export type Partial<T> = ZuordUtilPartial<T>;
 };
