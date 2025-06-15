2428b13439a2505253702dd706eb63a4bc8a49cc
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 13:00:47 +0300

    Add exports for core functions in index.ts for Zuord module

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export * as zuord from "./core/alias.runtime";
+export * as Zuord from "./core/alias.compile";
 export * as zuordUtil from "./util/alias.runtime";
 export * as ZuordUtil from "./util/alias.compile";
