9734cadc7547baea9ba849f41f1da80472dc8b06
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 09:27:44 +0300

    Add is type exports for improved type utility consistency

---
+++ src/is/index.ts
@@ -0,0 +1,5 @@
+export { type ZuordIsArray } from "./isArray";
+export { type ZuordIsPlain } from "./isPlain";
+export { isObject, type ZuordIsObject } from "./isObject";
+export { isFunction, type ZuordIsFunction } from "./isFunction";
+export { isNever, type ZuordIsNever } from "./isNever";
