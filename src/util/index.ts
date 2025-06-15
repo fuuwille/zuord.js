d062272cf8324810881f4444dc4a1d5aaba87ca9
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 12:35:56 +0300

    Refactor exports in index.ts for improved clarity and organization

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,23 +1,10 @@
-export { type ZuordUtilIntegrate } from "./integrate";
-export { type ZuordNormalize as ZuordUtilNormalize } from "./normalize";
-export { type ZuordUtilPartial } from "./partial";
-
-import { type ZuordArrayDepth, type ZuordIsArray } from "./array";
-import { type ZuordUtilIntegrate } from "./integrate";
-import { type ZuordNormalize } from "./normalize";
-import { type ZuordUtilPartial } from "./partial";
-
-export namespace ZuordUtil {
-    // Array
-    export type ArrayDepth<T> = ZuordArrayDepth<T>;
-    export type IsArray<T> = ZuordIsArray<T>;
-
-    // Integrate
-    export type Integrate<A, B> = ZuordUtilIntegrate<A, B>;
-
-    // Normalize
-    export type Normalize<T> = ZuordNormalize<T>;
-
-    // Partial
-    export type Partial<T> = ZuordUtilPartial<T>;
-};
+export * from "./never";
+export * from "./extends";
+export * from "./exists";
+export * from "./function";
+export * from "./object";
+export * from "./array";
+export * from "./plain";
+export * from "./integrate";
+export * from "./normalize";
+export * from "./partial";
