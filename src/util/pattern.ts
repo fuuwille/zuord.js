dc842c65b1caace6142449a1830d8ca35662f004
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 20:48:35 +0300

    Refactor pattern types: move Pattern, PatternRaw, and IsPattern type definitions to pattern.types.ts for consistency

--- src/util/pattern.ts
+++ src/util/pattern.ts
@@ -1,27 +1,7 @@
 import * as ZuordUtil from "@/util/alias.types";
 
-type Pattern<T> = ZuordUtil.Normalize<PatternRaw<T>>
-
-type PatternRaw<T> = true | {
-    [K in keyof T]?: T[K] extends Array<unknown>
-        ? true
-        : T[K] extends object
-            ? PatternRaw<T[K]>
-            : T[K] extends undefined
-                ? never
-                : true;
-}
-
-type IsPattern<T> = T extends true 
-    ? true : T extends object ? true : false;
-
-function pattern<T extends object, S extends Pattern<T>>(_: T, sch: S): S {
+function pattern<T extends object, S extends ZuordUtil.Pattern<T>>(_: T, sch: S): S {
     return sch;
 }
 
-//
-
-export type { Pattern as ZuordPattern };
-export type { PatternRaw as ZuordPatternRaw };
-export type { IsPattern as ZuordIsPattern };
 export { pattern as zuordPattern };
