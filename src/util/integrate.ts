c6bb7177f0990d39ce3c44c8e405380edf0be018
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 11:01:50 +0300

    Refactor Integrate type to streamline array handling and improve readability

--- src/util/integrate.ts
+++ src/util/integrate.ts
@@ -10,11 +10,7 @@ type Integrate<A, B, O extends string = ""> = {
       Is.Exists<O, "concat"> extends true ? (
         Is.Array<A[K]> extends true ? (
           Is.Array<B[K]> extends true ? (
-            A[K] extends readonly (infer U)[] ? (
-              B[K] extends readonly (infer V)[] ? (
-                Array<U | V>
-              ): B[K]
-            ) : B[K]
+            IntegrateArray<A[K], B[K], O>
           ) : B[K]
         ) : B[K]
       ) : Is.Plain<A[K]> extends true
@@ -27,3 +23,11 @@ type Integrate<A, B, O extends string = ""> = {
       ? A[K]
       : never;
 };
+
+type IntegrateArray<A, B, O> = A extends readonly (infer U)[] ? (
+  B extends readonly (infer V)[] ? (
+    Is.Exists<O, "concat"> extends true ? (
+      Array<U | V>
+    ) : Array<V>
+  ) : B
+) : B;
