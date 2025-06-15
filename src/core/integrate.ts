6ecdce85043238c1ee00534228006706afc32f33
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 16:46:25 +0300

    Add Integrate and IntegrateArray types for enhanced type manipulation

---
+++ src/core/integrate.ts
@@ -0,0 +1,34 @@
+import { ZuordUtil } from "@";
+
+//
+
+type Integrate<A, B, Mode extends string = ""> = {
+  [K in keyof A | keyof B]: K extends keyof B ? (
+    K extends keyof A ? (
+      ZuordUtil.IsArray<A[K]> extends true ? (
+        ZuordUtil.IsArray<B[K]> extends true ? (
+          IntegrateArray<A[K], B[K], Mode>
+        ) : B[K]
+      ) : ZuordUtil.IsPlain<A[K]> extends true
+          ? ZuordUtil.IsPlain<B[K]> extends true
+            ? Integrate<A[K], B[K], Mode>
+            : B[K]
+          : B[K]
+    ) : B[K]
+  ) : K extends keyof A
+      ? A[K]
+      : never;
+};
+
+type IntegrateArray<A, B, Mode extends string = ""> = A extends readonly (infer U)[] ? (
+  B extends readonly (infer V)[] ? (
+    ZuordUtil.IsExists<Mode, "concat"> extends true ? (
+      Array<U | V>
+    ) : Array<V>
+  ) : never
+) : never;
+
+//
+
+export type { Integrate as ZuordIntegrate };
+export type { IntegrateArray as ZuordIntegrateArray };
