d5c13c1020002a2136642ec1649f0c6bd4ad7f3f
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 10:46:10 +0300

    Refactor ZuordUtilIntegrate type to simplify conditional logic for improved clarity

--- src/util/integrate.ts
+++ src/util/integrate.ts
@@ -1,26 +1,25 @@
 import { ZuordIs as Is } from "../is/index"
 
 export type ZuordUtilIntegrate<A, B, O extends string = ""> = {
-  [K in keyof A | keyof B]:
-    K extends keyof B
-      ? K extends keyof A
-        ? Is.Exists<O, "concat" | "Lola"> extends true
-          ? Is.Array<A[K]> extends true
-            ? Is.Array<B[K]> extends true
-              ? A[K] extends readonly (infer U)[]
-                ? B[K] extends readonly (infer V)[]
-                  ? Array<U | V>
-                  : B[K]
-                : B[K]
-              : B[K]
+  [K in keyof A | keyof B]: K extends keyof B ? (
+    K extends keyof A ? (
+      Is.Exists<O, "concat"> extends true ? (
+        Is.Array<A[K]> extends true ? (
+          Is.Array<B[K]> extends true ? (
+            A[K] extends readonly (infer U)[] ? (
+              B[K] extends readonly (infer V)[] ? (
+                Array<U | V>
+              ): B[K]
+            ) : B[K]
+          ) : B[K]
+        ) : B[K]
+      ) : Is.Plain<A[K]> extends true
+          ? Is.Plain<B[K]> extends true
+            ? ZuordUtilIntegrate<A[K], B[K], O>
             : B[K]
-          : Is.Plain<A[K]> extends true
-            ? Is.Plain<B[K]> extends true
-              ? ZuordUtilIntegrate<A[K], B[K], O>
-              : B[K]
-            : B[K]
-        : B[K]
-      : K extends keyof A
-        ? A[K]
-        : never;
+          : B[K]
+    ) : B[K]
+  ) : K extends keyof A
+      ? A[K]
+      : never;
 };
