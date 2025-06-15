8ad23353c504e1a8c7ec272db18bc5f7a603c72d
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 10:40:25 +0300

    Refactor ZuordUtilIntegrate type to include optional parameter for enhanced flexibility

--- src/util/integrate.ts
+++ src/util/integrate.ts
@@ -1,27 +1,28 @@
+import { ZuordIsExists } from "../is/exists"
 import { ZuordIsArray } from "../is/array";
 import { ZuordIsPlain } from "../is/plain";
 
-export type ZuordUtilIntegrate<A, B> = {
+export type ZuordUtilIntegrate<A, B, O extends string = ""> = {
   [K in keyof A | keyof B]:
     K extends keyof B
       ? K extends keyof A
-        ? ZuordIsArray<A[K]> extends true
-          ? ZuordIsArray<B[K]> extends true
-            ? Array<
-                A[K] extends readonly (infer U)[] 
-                  ? B[K] extends readonly (infer V)[]
-                    ? U | V
-                    : U
-                  : never
-              >
+        ? ZuordIsExists<O, "concat" | "Lola"> extends true
+          ? ZuordIsArray<A[K]> extends true
+            ? ZuordIsArray<B[K]> extends true
+              ? A[K] extends readonly (infer U)[]
+                ? B[K] extends readonly (infer V)[]
+                  ? Array<U | V> // ✅ concat ve her ikisi array
+                  : B[K]
+                : B[K]
+              : B[K]
             : B[K]
           : ZuordIsPlain<A[K]> extends true
-          ? ZuordIsPlain<B[K]> extends true
-            ? ZuordUtilIntegrate<A[K], B[K]>
+            ? ZuordIsPlain<B[K]> extends true
+              ? ZuordUtilIntegrate<A[K], B[K], O>
+              : B[K]
             : B[K]
-          : B[K]
         : B[K]
       : K extends keyof A
-      ? A[K]
-      : never;
+        ? A[K]
+        : never;
 };
