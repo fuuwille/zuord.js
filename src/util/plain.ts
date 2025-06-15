9b00207262775b36c60cc555f316a64efba96600
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 12:06:00 +0300

    Refactor IsPlain type and isPlain function for improved clarity and organization

--- src/util/plain.ts
+++ src/util/plain.ts
@@ -1,4 +1,14 @@
-export type ZuordIsPlain<T> = 
+type IsPlain<T> = 
     T extends Function ? false :
     T extends readonly any[] ? false :
     T extends object ? true : false;
+
+function isPlain(item: any): item is object {
+    return item !== null && typeof item === 'object' && !Array.isArray(item) && !(item instanceof Date);
+}
+
+//
+
+export type ZuordIsPlain<T> = IsPlain<T>;
+
+export { isPlain };
