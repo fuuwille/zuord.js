94e5222e4f8e48e2ae48d682924012fc36ccba30
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 11:56:14 +0300

    Refactor ZuordIsArray type for improved clarity and consistency

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,4 +1,10 @@
+export type ZuordIsArray<T> = IsArray<T>;
+
 export type ZuordArrayDepth<T> = ArrayDepth<T>;
+
+//
+
+type IsArray<T> = ArrayDepth<T> extends 0 ? false : true;
 
 type ArrayDepth<T, D extends unknown[] = []> = T extends readonly (infer U)[]
     ? ArrayDepth<U, [unknown, ...D]> : D['length'];
