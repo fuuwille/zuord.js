72bd9171c8d7f34984fb7f715ea10430c6976b6d
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 11:58:57 +0300

    Refactor ZuordIsExtends type for improved clarity and organization

--- src/util/extends.ts
+++ src/util/extends.ts
@@ -1,2 +1,6 @@
-export type ZuordIsExtends<T, E> = [T] extends [never]
+type IsExtends<T, E> = [T] extends [never]
     ? unknown : (T extends any ? (T extends E ? true : never) : never);
+
+//
+
+export type ZuordIsExtends<T, E> = IsExtends<T, E>;
