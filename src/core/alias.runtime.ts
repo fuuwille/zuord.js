c3dbfa57d43207429888726a0b057091fee438af
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 12:59:45 +0300

    Add alias.runtime.ts to export core functions for Zuord

---
+++ src/core/alias.runtime.ts
@@ -0,0 +1,4 @@
+export { zuordMerge as merge } from "./merge";
+export { zuordOmit as omit } from "./omit";
+export { zuordPick as pick } from "./pick";
+export { zuordPattern as pattern } from "./pattern";
