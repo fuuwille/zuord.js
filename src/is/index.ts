22ce3bc83da174476937e36370ddc63ec2f4a087
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 09:37:30 +0300

    Refactor is namespace to include all type exports for improved consistency and organization

--- src/is/index.ts
+++ src/is/index.ts
@@ -3,3 +3,23 @@ export { type ZuordIsPlain } from "./isPlain";
 export { isObject, type ZuordIsObject } from "./isObject";
 export { isFunction, type ZuordIsFunction } from "./isFunction";
 export { isNever, type ZuordIsNever } from "./isNever";
+
+import { type ZuordIsArray } from "./isArray";
+import { type ZuordIsPlain } from "./isPlain";
+import { isObject, type ZuordIsObject } from "./isObject";
+import { isFunction, type ZuordIsFunction } from "./isFunction";
+import { isNever, type ZuordIsNever } from "./isNever";
+
+export namespace ZuordIs {
+    export type Array<T> = ZuordIsArray<T>;
+    export type Plain<T> = ZuordIsPlain<T>;
+    export type Object<T> = ZuordIsObject<T>;
+    export type Function<T> = ZuordIsFunction<T>;
+    export type Never<T> = ZuordIsNever<T>;
+};
+
+export const zuordIs = {
+    object: isObject,
+    function: isFunction,
+    never: isNever,
+} as const;
