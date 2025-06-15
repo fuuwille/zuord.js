64668df370aff43ec5cf6af36ba5f557014402d1
k4yr2 <kyrzkaya@gmail.com>
2025-06-15 09:36:46 +0300

    Refactor type definitions in Zuord namespace for improved readability and consistency

--- src/zuord.ts
+++ src/zuord.ts
@@ -9,15 +9,15 @@ import { zuordOmit, type ZuordOmit, type ZuordOmitRaw, type ZuordOmitOf } from "
 import { zuordPattern, type ZuordPattern } from "./zuordPattern";
 
 export namespace Zuord {
-    export type Merge<U extends object[]> = ZuordMerge<U>;
-    export type MergeRaw<U extends object[]> = ZuordMergeRaw<U>;
-    export type Pick<T, U> = ZuordPick<T, U>;
-    export type PickRaw<T, U> = ZuordPickRaw<T, U>;
-    export type PickOf<T, U> = ZuordPickOf<T, U>;
-    export type Omit<T, U> = ZuordOmit<T, U>;
-    export type OmitRaw<T, U> = ZuordOmitRaw<T, U>;
-    export type OmitOf<T, U> = ZuordOmitOf<T, U>;
-    export type Pattern<T> = ZuordPattern<T>;
+  export type Merge<U extends object[]> = ZuordMerge<U>;
+  export type MergeRaw<U extends object[]> = ZuordMergeRaw<U>;
+  export type Pick<T, U> = ZuordPick<T, U>;
+  export type PickRaw<T, U> = ZuordPickRaw<T, U>;
+  export type PickOf<T, U> = ZuordPickOf<T, U>;
+  export type Omit<T, U> = ZuordOmit<T, U>;
+  export type OmitRaw<T, U> = ZuordOmitRaw<T, U>;
+  export type OmitOf<T, U> = ZuordOmitOf<T, U>;
+  export type Pattern<T> = ZuordPattern<T>;
 };
 
 export const zuord = {
