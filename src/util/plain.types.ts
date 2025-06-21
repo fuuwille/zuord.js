042b72fe3162b5f7c3a02c85800f058d970fce66
k4yr2 <kyrzkaya@gmail.com>
2025-06-21 13:22:54 +0300

    Refactor IsPlain type definition: change readonly array check to unknown array for consistency

--- src/util/plain.types.ts
+++ src/util/plain.types.ts
@@ -1,6 +1,6 @@
 type IsPlain<T> = 
     T extends Function ? false :
-    T extends readonly any[] ? false :
+    T extends unknown[] ? false :
     T extends object ? true : false;
     
 export type { IsPlain as ZuordIsPlain };
