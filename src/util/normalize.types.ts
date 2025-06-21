7bc324c21d5686311c1b021ab4cc736a4bf589a8
k4yr2 <kyrzkaya@gmail.com>
2025-06-21 21:57:40 +0300

    Refactor Normalize type definition: simplify structure by removing redundant conditions for improved clarity

--- src/util/normalize.types.ts
+++ src/util/normalize.types.ts
@@ -1,12 +1,6 @@
 import * as ZuordUtil from "@/util/alias.types";
 
-type Normalize<T> = (
-    ZuordUtil.IsArray<T> extends true ? (
-        NormalizeStructural<T> 
-    ) : T extends any ? (
-        NormalizeStructural<T> 
-    ) : never
-);
+type Normalize<T> = NormalizeStructural<T>;
 
 type NormalizeDistributed<T> = T extends any ? NormalizeStructural<T> : never;
 
@@ -14,7 +8,7 @@ type NormalizeStructural<T> = (
     ZuordUtil.IsObject<T> extends true ? (
         { [K in keyof T]: Normalize<T[K]> }
     ) :
-    ZuordUtil.IsArray<T> extends true ? (
+    true extends ZuordUtil.IsArray<T> ? (
         NormalizeDistributed<Exclude<T, unknown[]>> | NormalizeStructural<(T extends unknown[] ? T[number] : never)>[]
     ) : T
 )
