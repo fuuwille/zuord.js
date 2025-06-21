730abaa819c13e5f8514053ff31b859dcc1cdbcf
k4yr2 <kyrzkaya@gmail.com>
2025-06-21 21:59:18 +0300

    Refactor IsArray type definition: simplify structure for improved clarity

--- src/util/array.types.ts
+++ src/util/array.types.ts
@@ -1,4 +1,4 @@
-type IsArray<T> = T extends any[] ? true : false;
+type IsArray<T> = true extends (T extends any[] ? true : false) ? true : false;
 
 type ArrayDepth<T, D extends unknown[] = []> = T extends readonly (infer U)[]
     ? ArrayDepth<U, [unknown, ...D]> : D['length'];
