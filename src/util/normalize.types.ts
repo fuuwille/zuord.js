5cf7e3f33e7f9895db669af5da2c2aaab3630cc2
k4yr2 <kyrzkaya@gmail.com>
2025-06-21 20:56:26 +0300

    Refactor Normalize type definition: enhance structure for array handling and clarity

--- src/util/normalize.types.ts
+++ src/util/normalize.types.ts
@@ -1,5 +1,23 @@
 import * as ZuordUtil from "@/util/alias.types";
 
-type Normalize<T> = ZuordUtil.IsObject<T> extends true ? { [K in keyof T]: Normalize<T[K]> } : T;
+type Normalize<T> = ZuordUtil.IsArray<T> extends true ? NormalizeDirect<T> :T extends any ? NormalizeDirect<T> : never;
+
+type NormalizeAny<T> = T extends any ? NormalizeDirect<T> : never;
+
+type NormalizeDirect<T> = (
+    ZuordUtil.IsObject<T> extends true ? (
+        { [K in keyof T]: Normalize<T[K]> }
+    ) :
+    ZuordUtil.IsArray<T> extends true ? (
+        NormalizeAny<Exclude<T, unknown[]>> | NormalizeDirect<(T extends unknown[] ? T[number] : never)>[]
+    ) : T
+)
 
 export type { Normalize as ZuordNormalize };
+
+
+type A = number[] | string[][];
+
+type Result = Normalize<A>
+
+type TEST = ZuordUtil.IsArray<A> extends true ? true : false;
