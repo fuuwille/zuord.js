dc830d167e6505614420c4fe0ffef4f7ac9c1711
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 19:20:50 +0300

    Refactor Integrate type definition for improved readability and clarity

--- src/core/integrate.types.ts
+++ src/core/integrate.types.ts
@@ -3,17 +3,16 @@ import { ZuordUtil } from "@";
 type Integrate<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = {
     [K in keyof A | keyof B]: K extends keyof B ? (
         K extends keyof A ? (
-            ZuordUtil.HasArray<A[K]> extends true ? (
-                ZuordUtil.HasArray<B[K]> extends true ? (
+            A[K] extends readonly unknown[] ? (
+                B[K] extends readonly unknown[] ? (
                     IntegrateArray<A[K], B[K], Mode>
                 ) : B[K]
-            ) : (
-                ZuordUtil.IsPlain<A[K]> extends true ? (
-                    ZuordUtil.IsPlain<B[K]> extends true ? (
-                        Integrate<A[K], B[K], Mode>
-                    ) : B[K]
+            ) :                 
+            ZuordUtil.IsPlain<A[K]> extends true ? (
+                ZuordUtil.IsPlain<B[K]> extends true ? (
+                    Integrate<A[K], B[K], Mode>
                 ) : B[K]
-            )
+            ) : B[K]
         ) : B[K]
     ) : (
         K extends keyof A ? (
@@ -22,13 +21,15 @@ type Integrate<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = {
     )
 };
 
-type IntegrateArray<A, B, Mode extends ZuordUtil.Mode<IntegrateMode>  = ""> = A extends readonly (infer U)[] ? (
-    B extends readonly (infer V)[] ? (
-        ZuordUtil.IsExists<Mode, "no-concat"> extends true ? (
-            Array<V>
-        ) : Array<U | V>
+type IntegrateArray<A, B, Mode extends ZuordUtil.Mode<IntegrateMode>  = ""> = (
+    A extends readonly (infer U)[] ? (
+        B extends readonly (infer V)[] ? (
+            ZuordUtil.IsExists<Mode, "no-concat"> extends true ? (
+                Array<V>
+            ) : Array<U | V>
+        ) : never
     ) : never
-) : never;
+);
 
 type IntegrateMode = IntegrateNoConcatMode;
 
