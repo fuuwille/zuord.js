0340c50c2d420f54394039795f16b157aa44c8fc
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 20:58:19 +0300

    Refactor IsPlainObject type definition for improved clarity and structure

--- src/util/object.types.ts
+++ src/util/object.types.ts
@@ -1,3 +1,12 @@
+import { ZuordUtil } from "@/util/alias.types";
+
 type IsObject<T> = [T] extends [object] ? true : false;
 
+type IsPlainObject<T> = IsObject<T> extends true ? (
+  ZuordUtil.IsFunction<T> extends true ? false :
+  ZuordUtil.IsArray<T> extends true ? false :
+  true
+) : false;
+
 export type { IsObject as ZuordIsObject }
+export type { IsPlainObject as ZuordIsPlainObject }
