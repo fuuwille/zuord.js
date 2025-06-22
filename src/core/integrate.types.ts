0eb063eae3b021c254d687094211c796f82403e1
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 14:40:56 +0300

    Refactor Integrate type to utilize HasAllArray for improved type handling

--- src/core/integrate.types.ts
+++ src/core/integrate.types.ts
@@ -3,10 +3,8 @@ import { ZuordUtil } from "@";
 type Integrate<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = {
     [K in keyof A | keyof B]: K extends keyof B ? (
         K extends keyof A ? (
-            ZuordUtil.HasArray<A[K]> extends true ? (
-                ZuordUtil.HasArray<B[K]> extends true ? (
-                    IntegrateArray<A[K], B[K], Mode>
-                ) : B[K]
+            ZuordUtil.HasAllArray<[A[K], B[K]]> extends true ? (
+                IntegrateArray<A[K], B[K], Mode>
             ) : (
                 ZuordUtil.IsPlain<A[K]> extends true ? (
                     ZuordUtil.IsPlain<B[K]> extends true ? (
