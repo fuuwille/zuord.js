68c6a053d2cd69b256550e12529796cd494462f9
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 20:55:10 +0300

    Add IsArray type definition for enhanced type checking

--- src/util/array.types.ts
+++ src/util/array.types.ts
@@ -1,3 +1,5 @@
+type IsArray<T> = [T]extends [readonly unknown[]] ? true : false;
+
 type HasArray<T> = true extends (T extends readonly unknown[] ? true : false) ? true : false;
 
 type HasAnyArray<U extends readonly unknown[]> = U extends readonly [unknown[]] ? true : ([HasArray<U[number]>] extends [true] ? true : false);
@@ -7,6 +9,7 @@ type HasAllArray<U extends readonly unknown[]> = U extends readonly [unknown[]] 
 type ArrayDepth<T, D extends unknown[] = []> = T extends readonly (infer U)[]
     ? ArrayDepth<U, [unknown, ...D]> : D['length'];
 
+export type { IsArray as ZuordIsArray };
 export type { HasArray as ZuordHasArray };
 export type { HasAnyArray as ZuordHasAnyArray };
 export type { HasAllArray as ZuordHasAllArray };
