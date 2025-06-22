580a456eac7a4bb3edcd70ea20e52bcddd6e9893
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 15:30:15 +0300

    Add AllHasKey type definition for improved type handling

--- src/util/key.types.ts
+++ src/util/key.types.ts
@@ -4,6 +4,9 @@ type HasKey<T, K> = true extends (K extends keyof T ? true : false) ? true : fal
 
 type AnyHasKey<U extends readonly unknown[], K> = U extends readonly [unknown[]] ? true : ([HasKey<U[number], K>] extends [true] ? true : false);
 
+type AllHasKey<U extends readonly unknown[], K> = U extends readonly [unknown[]] ? true : ([HasKey<U[number], K>] extends [boolean] ? false : true);
+
 export type { IsKey as ZuordIsKey };
 export type { HasKey as ZuordHasKey };
 export type { AnyHasKey as ZuordAnyHasKey };
+export type { AllHasKey as ZuordAllHasKey };
