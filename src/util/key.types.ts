57673012e19eed31839609dff3e5b842fafcaca0
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 15:20:18 +0300

    Add HasKey type definition for improved type handling

--- src/util/key.types.ts
+++ src/util/key.types.ts
@@ -1,3 +1,6 @@
 type IsKey<T, K> = K extends keyof T ? true : false;
 
+type HasKey<T, K> = true extends (K extends keyof T ? true : false) ? true : false;
+
 export type { IsKey as ZuordIsKey };
+export type { HasKey as ZuordHasKey };
