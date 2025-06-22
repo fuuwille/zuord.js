c1f6c6dab78fd3cab68ddf679a55cbc65a116b83
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 14:37:39 +0300

    Add HasAllArray type definition for improved type handling

--- src/util/array.types.ts
+++ src/util/array.types.ts
@@ -2,9 +2,12 @@ type HasArray<T> = true extends (T extends readonly unknown[] ? true : false) ? 
 
 type HasAnyArray<U extends readonly unknown[]> =  [HasArray<U[number]>] extends [true] ? true : false;
 
+type HasAllArray<U extends unknown[] = []> = [HasArray<U[number]>] extends [boolean] ? false : true;
+
 type ArrayDepth<T, D extends unknown[] = []> = T extends readonly (infer U)[]
     ? ArrayDepth<U, [unknown, ...D]> : D['length'];
 
 export type { HasArray as ZuordHasArray };
 export type { HasAnyArray as ZuordHasAnyArray };
+export type { HasAllArray as ZuordHasAllArray };
 export type { ArrayDepth as ZuordArrayDepth };
