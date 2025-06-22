9b74780bb4856e48a370827cbbd0e3930326526c
k4yr2 <kyrzkaya@gmail.com>
2025-06-22 13:16:30 +0300

    Fix Normalize type definition: ensure correct type resolution in NormalizeDirect for improved type safety

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,11 +1,13 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T> = (
+type Normalize<T> = T extends any ? NormalizeDirect<T> : never;
+
+type NormalizeDirect<T> = (
     [ZuordUtil.IsObject<T>] extends [true] ? (
         { [K in keyof T]: Normalize<T[K]> }
     ) :
     [ZuordUtil.HasArray<T>] extends [true] ? (
-        Normalize<Exclude<T, unknown[]>> | Normalize<(T extends unknown[] ? T[number] : never)>[]
+        Normalize<Exclude<T, unknown[]>> | NormalizeDirect<(T extends unknown[] ? T[number] : never)>[]
     ) : T
 )
 
