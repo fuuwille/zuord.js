4301b7e1ae8d755d5fd4095fee6daf88edd3f7cf
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 02:56:01 +0300

    Add HasPlainObject type definition for improved type utility

--- src/util/object.types.ts
+++ src/util/object.types.ts
@@ -6,7 +6,10 @@ type IsPlainObject<T> = IsObject<T> extends true ? (
   ZuordUtil.IsFunction<T> extends true ? false :
   ZuordUtil.IsArray<T> extends true ? false :
   true
+
 ) : false;
+
+type HasPlainObject<T> = true extends (T extends any ? IsPlainObject<T> : false) ? true : false;
 
 type AsPlainObject<T> = T extends object
   ? T extends Function ? never 
@@ -16,4 +19,5 @@ type AsPlainObject<T> = T extends object
 
 export type { IsObject as ZuordIsObject }
 export type { IsPlainObject as ZuordIsPlainObject }
+export type { HasPlainObject as ZuordHasPlainObject }
 export type { AsPlainObject as ZuordAsPlainObject }
