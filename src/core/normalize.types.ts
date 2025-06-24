21282e136136578b71a7eb3b126f5783df843651
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 20:39:27 +0300

    Refactor Normalize and NormalizeDirect types to support additional generic parameters for improved flexibility

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,6 +1,6 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T> = (
+type Normalize<T, L extends object[] = []> = (
     ZuordUtil.HasPlain<T> extends true
     ? (
         (ZuordUtil.AsNonPlain<T> extends infer V ? V extends any ? NormalizeDirect<V> : never : never) | 
@@ -9,9 +9,9 @@ type Normalize<T> = (
     : T extends any ? NormalizeDirect<T> : never
 );
 
-type NormalizeDirect<T> = (
+type NormalizeDirect<T, L extends object[] = []> = (
     [ZuordUtil.IsPlain<T>] extends [true] ? (
-        { [K in keyof T]: Normalize<T[K]> }
+        { [K in keyof T]: Normalize<T[K], L> }
     ) :
     [ZuordUtil.IsArray<T>] extends [true] ? (
         NormalizeDirect<(T extends readonly unknown[] ? T[number] : never)>[]
