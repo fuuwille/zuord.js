d3800b0ab9be4d40b6cdd0727de0f544dbc76727
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 19:12:47 +0300

    Refactor Normalize type definitions for improved clarity and consistency

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,13 +1,13 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T> = T extends any ? NormalizeDirect<T> : never;
+type Normalize<T> = T extends any ? NormalizeObject<T> : never;
 
-type NormalizeDirect<T> = (
+type NormalizeObject<T> = (
     [ZuordUtil.IsPlainObject<T>] extends [true] ? (
         NormalizePlain<T>
     ) :
     [ZuordUtil.HasArray<T>] extends [true] ? (
-        Normalize<Exclude<T, readonly unknown[]>> | NormalizeDirect<(T extends readonly unknown[] ? T[number] : never)>[]
+        Normalize<Exclude<T, readonly unknown[]>> | NormalizeObject<(T extends readonly unknown[] ? T[number] : never)>[]
     ) : T
 )
 
@@ -16,5 +16,5 @@ type NormalizePlain<T> = (
 )
 
 export type { Normalize as ZuordNormalize };
-export type { NormalizeDirect as ZuordNormalizeDirect };
+export type { NormalizeObject as ZuordNormalizeDirect };
 export type { NormalizePlain as ZuordNormalizePlain };
