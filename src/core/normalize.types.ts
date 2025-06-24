7988d7df582f5a22ff1c26a12c346585388ce959
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 21:27:51 +0300

    Refactor Normalize and NormalizeDirect types to use BlackList as default for improved type safety

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,6 +1,6 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T, L extends object[] = []> = (
+type Normalize<T, L extends object[] = BlackList> = T extends ZuordUtil.UnionOf<L> ? T : (
     ZuordUtil.HasPlain<T> extends true
     ? (
         (ZuordUtil.AsNonPlain<T> extends infer V ? V extends any ? NormalizeDirect<V, L> : never : never) | 
@@ -9,7 +9,7 @@ type Normalize<T, L extends object[] = []> = (
     : T extends any ? NormalizeDirect<T, L> : never
 );
 
-type NormalizeDirect<T, L extends object[] = []> = (
+type NormalizeDirect<T, L extends object[] = BlackList> = (
     [ZuordUtil.IsPlain<T>] extends [true] ? (
         { [K in keyof T]: Normalize<T[K], L> }
     ) :
@@ -20,3 +20,29 @@ type NormalizeDirect<T, L extends object[] = []> = (
 
 export type { Normalize as ZuordNormalize };
 export type { NormalizeDirect as ZuordNormalizeDirect };
+
+type BlackList = [
+    Date,
+    RegExp,
+    Function,
+    Error,
+    Map<any, any>,
+    Set<any>,
+    WeakMap<any, any>,
+    WeakSet<any>,
+    ArrayBuffer,
+    SharedArrayBuffer,
+    DataView,
+    Promise<any>,
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array,
+    BigInt64Array,
+    BigUint64Array
+]
