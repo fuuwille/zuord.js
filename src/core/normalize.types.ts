80a0a41ade33992637817724bf172bd3672799e1
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 20:26:24 +0300

    Enhance Normalize type to handle plain and non-plain objects more effectively

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,6 +1,15 @@
 import { ZuordUtil } from "@/util/alias.types";
 
 type Normalize<T> = (
+    ZuordUtil.HasPlain<T> extends true
+    ? (
+        (ZuordUtil.AsNonPlain<T> extends infer V ? V extends any ? NormalizeRaw<V> : never : never) | 
+        (NormalizeRaw<ZuordUtil.MergeUnionObjects<ZuordUtil.AsPlain<T>>>)
+      )
+    : T extends any ? NormalizeRaw<T> : never
+);
+
+type NormalizeRaw<T> = (
     [ZuordUtil.IsPlain<T>] extends [true] ? (
         { [K in keyof T]: Normalize<T[K]> }
     ) :
