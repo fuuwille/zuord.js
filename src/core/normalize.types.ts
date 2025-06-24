978ec84634844810f4cd210ebb76f9ed279a6005
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 19:36:42 +0300

    Refactor NormalizeObject type to simplify handling of plain objects and arrays

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -4,17 +4,12 @@ type Normalize<T> = T extends any ? NormalizeObject<T> : never;
 
 type NormalizeObject<T> = (
     [ZuordUtil.IsPlainObject<T>] extends [true] ? (
-        NormalizePlain<T>
+        { [K in keyof T]: NormalizeObject<T[K]> }
     ) :
-    [ZuordUtil.HasArray<T>] extends [true] ? (
-        Normalize<Exclude<T, readonly unknown[]>> | NormalizeObject<(T extends readonly unknown[] ? T[number] : never)>[]
+    [ZuordUtil.IsArray<T>] extends [true] ? (
+        NormalizeObject<(T extends readonly unknown[] ? T[number] : never)>[]
     ) : T
-)
-
-type NormalizePlain<T> = (     
-    { [K in keyof T]: Normalize<T[K]> }
 )
 
 export type { Normalize as ZuordNormalize };
 export type { NormalizeObject as ZuordNormalizeDirect };
-export type { NormalizePlain as ZuordNormalizePlain };
