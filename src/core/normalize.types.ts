d98df7ea4d673248621ca972b176394e600bc16f
k4yr2 <kyrzkaya@gmail.com>
2025-06-24 19:12:23 +0300

    Rename NormalizePlainObject to NormalizePlain for consistency in type definitions

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -4,17 +4,17 @@ type Normalize<T> = T extends any ? NormalizeDirect<T> : never;
 
 type NormalizeDirect<T> = (
     [ZuordUtil.IsPlainObject<T>] extends [true] ? (
-        NormalizePlainObject<T>
+        NormalizePlain<T>
     ) :
     [ZuordUtil.HasArray<T>] extends [true] ? (
         Normalize<Exclude<T, readonly unknown[]>> | NormalizeDirect<(T extends readonly unknown[] ? T[number] : never)>[]
     ) : T
 )
 
-type NormalizePlainObject<T> = (     
+type NormalizePlain<T> = (     
     { [K in keyof T]: Normalize<T[K]> }
 )
 
 export type { Normalize as ZuordNormalize };
 export type { NormalizeDirect as ZuordNormalizeDirect };
-export type { NormalizePlainObject as ZuordNormalizePlainObject };
+export type { NormalizePlain as ZuordNormalizePlain };
