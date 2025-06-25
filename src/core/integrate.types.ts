9b3a144be4fc77573c76b938bcfa36235b6ce8bc
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 18:07:42 +0300

    Remove commented-out IntegrateRaw type definition for cleaner code

--- src/core/integrate.types.ts
+++ src/core/integrate.types.ts
@@ -24,29 +24,6 @@ type IntegrateType<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = Zuor
     ) : B
 ) : ZuordUtil.AsSomeNonNever<[A, B]>;
 
-/*type IntegrateRaw<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = {
-    [K in keyof A | keyof B]: K extends keyof B ? (
-        K extends keyof A ? (
-            A[K] extends readonly (infer AX)[] ? (
-                B[K] extends readonly (infer BX)[] ? (
-                    ZuordUtil.IsExists<Mode, IntegrateNoConcantMode> extends true 
-                    ? Array<BX>
-                    : Array<BX | AX>
-                ) : B[K]
-            ) :                 
-            ZuordUtil.IsPlainObject<A[K]> extends true ? (
-                ZuordUtil.IsPlainObject<B[K]> extends true ? (
-                    Integrate<A[K], B[K], Mode>
-                ) : B[K]
-            ) : B[K]
-        ) : B[K]
-    ) : (
-        K extends keyof A ? (
-            A[K]
-        ) : never
-    )
-};*/
-
 type IntegrateMode = IntegrateNoConcantMode;
 
 type IntegrateNoConcantMode= "no concat";
