442d95229c7c44ec45bcd3cbd1917a7bf1729cd2
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 22:57:02 +0300

    Add optional 'lite' property to Options and DefaultOptions types for enhanced configuration

--- src/core/options.types.ts
+++ src/core/options.types.ts
@@ -2,10 +2,12 @@ import { Zuord } from "@/core/alias.types";
 
 type Options = {
     ignored: Zuord.Ignored
+    lite? : boolean
 }
 
 type DefaultOptions = {
     ignored: Zuord.DefaultIgnored
+    lite: false
 }
 
 export type { Options as ZuordOptions };
