c97aa90cdc46ef92e02444ae304ee3220b5bd22b
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 16:31:01 +0300

    Refactor Normalize type for improved readability and consistency

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -3,9 +3,7 @@ import { ZuordUtil } from "@/util/alias.types";
 type Normalize<T, I extends object[] = ZuordUtil.Ignored> = ZuordUtil.HasIgnored<T, I> extends false ? (
     ZuordUtil.HasPlain<T> extends true ? (
         (ZuordUtil.AsNonPlain<T> extends infer V ? Normalize<V, I> : never) | 
-        (ZuordUtil.AsOnePlain<T> extends infer T ? {
-            [K in keyof T]: Normalize<T[K], I> 
-        } : never)
+        (ZuordUtil.AsOnePlain<T> extends infer T ? { [K in keyof T]: Normalize<T[K], I> } : never)
     ) : 
     ZuordUtil.HasArray<T> extends true ? (
         (Normalize<Exclude<T, readonly unknown[]>>) |
