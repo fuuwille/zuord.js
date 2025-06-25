31e418fa095979f2efff30f36e8add43f2ba96fe
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 16:18:27 +0300

    Remove unused UnionKeys type definition from ZuordUtil namespace

--- src/util/alias.types.ts
+++ src/util/alias.types.ts
@@ -98,8 +98,6 @@ export namespace ZuordUtil {
   export type MergeUnionObjects<U> = _MergeUnionObjects<U>;
 }
 
-type UnionKeys<U> = U extends object ? keyof U : never;
-
 type ValueAt<U, K extends PropertyKey> =
   U extends any ? (K extends keyof U ? U[K] : never) : never;
 
