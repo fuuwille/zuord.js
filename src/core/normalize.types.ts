e3972035cd988d78c21118312a407a8b4ba936c5
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 08:58:11 +0300

    Refactor Normalize type for improved handling of plain and array types

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,12 +1,15 @@
 import { ZuordUtil } from "@/util/alias.types";
 
 type Normalize<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgnore<T, L> extends true ? T : (
-    ZuordUtil.HasPlain<T> extends true
-    ? (
-        (ZuordUtil.AsNonPlain<T> extends infer V ? ZuordUtil.AsAny<NormalizeDirect<V, L>> : never) | 
-        (NormalizeDirect<ZuordUtil.MergeUnionObjects<ZuordUtil.AsPlain<T>>, L>)
-      )
-    : ZuordUtil.AsAny<NormalizeDirect<T, L>>
+    ZuordUtil.HasPlain<T> extends true ? (
+        (ZuordUtil.AsNonPlain<T> extends infer V ? Normalize<V, L> : never) | 
+        (ZuordUtil.MergeUnionObjects<ZuordUtil.AsPlain<T>> extends infer T ? {
+            [K in keyof T]: Normalize<T[K], L> 
+        } : never)
+    ) : ZuordUtil.HasArray<T> extends true ? (
+        Normalize<Exclude<T, readonly unknown[]>>|
+        Normalize<(T extends readonly unknown[] ? T[number] : never)>[]
+    ) : T
 );
 
 type NormalizeDirect<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgnore<T, L> extends true ? T: (
