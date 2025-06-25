e4a6b8e86e8aa194b419a609f1ea3ee2ea1f7645
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 22:06:27 +0300

    Add DefaultOptions type for improved type handling in options.types.ts

--- src/core/options.types.ts
+++ src/core/options.types.ts
@@ -1,5 +1,12 @@
+import { ZuordUtil } from "@/util/alias.types";
+
 type Options = {
     ignored: object[]
 }
 
+type DefaultOptions = {
+    ignored: ZuordUtil.DefaultIgnored
+}
+
 export type { Options as ZuordOptions };
+export type { DefaultOptions as ZuordDefaultOptions };
