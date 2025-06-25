85d1db6f0f35e2e616db5f8b453afa48748b2ba3
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 08:58:31 +0300

    Refactor Normalize type by removing NormalizeDirect for improved clarity and simplicity

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -12,14 +12,4 @@ type Normalize<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgno
     ) : T
 );
 
-type NormalizeDirect<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgnore<T, L> extends true ? T: (
-    [ZuordUtil.IsPlain<T>] extends [true] ? (
-        { [K in keyof T]: Normalize<T[K], L> }
-    ) :
-    [ZuordUtil.IsArray<T>] extends [true] ? (
-        Normalize<(T extends readonly unknown[] ? T[number] : never)>[]
-    ) : T
-);
-
 export type { Normalize as ZuordNormalize };
-export type { NormalizeDirect as ZuordNormalizeDirect };
