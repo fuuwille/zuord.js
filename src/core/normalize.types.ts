254b4e37dd73a591e0c4b015bf2369079854971f
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 09:54:51 +0300

    Refactor Normalize type for improved conditional handling and clarity

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,15 +1,15 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgnore<T, L> extends true ? T : (
+type Normalize<T, L extends object[] = ZuordUtil.Ignore > = ZuordUtil.ShouldIgnore<T, L> extends false ? (
     ZuordUtil.HasPlain<T> extends true ? (
         (ZuordUtil.AsNonPlain<T> extends infer V ? Normalize<V, L> : never) | 
         (ZuordUtil.MergeUnionObjects<ZuordUtil.AsPlain<T>> extends infer T ? {
             [K in keyof T]: Normalize<T[K], L> 
         } : never)
     ) : ZuordUtil.HasArray<T> extends true ? (
-        Normalize<Exclude<T, readonly unknown[]>>|
-        Normalize<(T extends readonly unknown[] ? T[number] : never)>[]
+        (Normalize<Exclude<T, readonly unknown[]>>) |
+        (Normalize<(T extends readonly unknown[] ? T[number] : never)>[])
     ) : T
-);
+) : Normalize<ZuordUtil.AsNonIgnore<T, L>> | ZuordUtil.AsIgnore<T, L>;
 
 export type { Normalize as ZuordNormalize };
