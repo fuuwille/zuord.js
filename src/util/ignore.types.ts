78cc74e9aa814b57b50c63c5e9b77a00fa27994f
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 08:40:24 +0300

    Refactor ShouldIgnore type for improved type inference and handling

--- src/util/ignore.types.ts
+++ src/util/ignore.types.ts
@@ -27,7 +27,7 @@ type Ignore<U extends object[] = []> = [
     BigUint64Array
 ]
 
-type ShouldIgnore<T, I extends object[] = Ignore> = T extends ZuordUtil.UnionOf<I> ? true : false;
+type ShouldIgnore<T, I extends object[] = Ignore> = T extends (ZuordUtil.UnionOf<I> extends infer T ? T | readonly T[] : never) ? true : false;
 
 export type { Ignore as ZuordIgnore };
 export type { ShouldIgnore as ZuordShouldIgnore };
