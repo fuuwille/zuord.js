b351cf1e04700563b21367bb7eb6d0a594b4fd2e
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 21:10:04 +0300

    Refactor IntegrateRaw type for improved handling of array and plain object cases

--- src/core/integrate.types.ts
+++ src/core/integrate.types.ts
@@ -6,18 +6,18 @@ type Integrate<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = Zuord.No
 type IntegrateRaw<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = [ZuordUtil.IsSomeNever<[A, B]>] extends [false] ? (
     [ZuordUtil.IsAllArray<[A, B]>] extends [true] ? (
         ZuordUtil.IsExists<Mode, IntegrateConcantMode> extends true 
-            ? Array<ZuordUtil.ArrayIn<B> | ZuordUtil.ArrayIn<A>>
-            : Array<ZuordUtil.ArrayIn<B>>
+            ? Array<ZuordUtil.ArrayIn<A> | ZuordUtil.ArrayIn<B>>
+            : Array<ZuordUtil.ArrayIn<A>>
     ) : 
     [ZuordUtil.IsAllPlain<[A, B]>] extends [true] ? ({
-        [K in keyof A | keyof B]: (
+        [K in (keyof A | keyof B)]: (
             ZuordUtil.IsExists<Mode, IntegrateShallowMode> extends false ? (
                 IntegrateRaw<
                     K extends keyof A ? A[K] : never,
                     K extends keyof B ? B[K] : never,
                     Mode
                 > 
-            ) : K extends keyof B ? B[K] : never
+            ) : K extends keyof A ? A[K] : never
         )
     }) : B
 ) : ZuordUtil.AsNonNever<[A, B]>;
