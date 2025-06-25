2603a3a3d9d77a38c977415a55ab0d9c2bda292b
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 21:47:53 +0300

    Add missing newline at end of options.types.ts for consistency

--- src/core/options.types.ts
+++ src/core/options.types.ts
@@ -1,5 +1,8 @@
+type Options<T extends OptionsData> = T;
+
 type OptionsData = {
     ignored: object[]
 }
 
+export type { Options as ZuordOptions };
 export type { OptionsData as ZuordOptionsData };
