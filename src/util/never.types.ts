31332f463c4a581f8ca47b4d38bf1dabc5446808
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 17:55:27 +0300

    Refactor never.types.ts to move SomeIsNever type definition above export statements for improved organization

--- src/util/never.types.ts
+++ src/util/never.types.ts
@@ -1,3 +1,8 @@
 type IsNever<T> = [T] extends [never] ? true : false;
 
+type SomeIsNever<U extends readonly unknown[]> = U extends readonly [infer First, ...infer Rest]
+    ? (IsNever<First> extends true ? true : SomeIsNever<Rest>) 
+    : false;
+
 export type { IsNever as ZuordIsNever };
+export type { SomeIsNever as ZuordSomeIsNever };
