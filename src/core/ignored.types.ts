249c8a7f999aa3b1a7d3811fdcd6ff967cc81950
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 22:23:25 +0300

    Add DefaultIgnored type for improved type handling in ignored.types.ts

--- src/core/ignored.types.ts
+++ src/core/ignored.types.ts
@@ -1,3 +1,31 @@
 type Ignored = object[];
 
+type DefaultIgnored = [
+    Date,
+    Function,
+    RegExp,
+    Function,
+    Error,
+    Map<any, any>,
+    Set<any>,
+    WeakMap<any, any>,
+    WeakSet<any>,
+    ArrayBuffer,
+    SharedArrayBuffer,
+    DataView,
+    Promise<any>,
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array,
+    BigInt64Array,
+    BigUint64Array
+]
+
 export type { Ignored as ZuordIgnored };
+export type { DefaultIgnored as ZuordDefaultIgnored };
