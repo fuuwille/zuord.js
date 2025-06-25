069f540081292780079cfb6e339445f040225aa3
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 08:05:59 +0300

    Add CanNormalize type to ZuordUtil for enhanced type checking

--- src/util/alias.types.ts
+++ src/util/alias.types.ts
@@ -1,4 +1,4 @@
-import type { ZuordNormalizeIgnore } from "./normalize.types";
+import type { ZuordCanNormalize, ZuordNormalizeIgnore } from "./normalize.types";
 import type { ZuordIsKey, ZuordHasKey, ZuordAnyHasKey, ZuordAllHasKey } from "./key.types";
 import type { ZuordIsNever } from "./never.types";
 import type { ZuordIsExtends } from "./extends.types";
@@ -14,6 +14,7 @@ import type { ZuordIsPattern } from "./pattern.types";
 import type { ZuordMode } from "./mode.types";
 
 export namespace ZuordUtil {
+    export type CanNormalize<T, I extends ZuordUtil.NormalizeIgnore = ZuordUtil.NormalizeIgnore> = ZuordCanNormalize<T, I>;
     export type NormalizeIgnore<U extends object[] = []> = ZuordNormalizeIgnore<U>;
     export type IsKey<T, K> = ZuordIsKey<T, K>;
     export type HasKey<T, K> = ZuordHasKey<T, K>;
