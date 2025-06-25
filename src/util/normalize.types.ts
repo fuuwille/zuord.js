2d758d4c29becd44bd6ed5467096a55da04a5dd6
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 07:59:16 +0300

    Add NormalizeIgnore type for improved type safety in normalization processes

---
+++ src/util/normalize.types.ts
@@ -0,0 +1,27 @@
+type NormalizeIgnore = [
+    Date,
+    RegExp,
+    Function,
+    Error,
+    Map<any, any>,
+    Set<any>,
+    WeakMap<any, any>,
+    WeakSet<any>,
+    ArrayBuffer,
+    SharedArrayBuffer,
+    DataView,
+    Promise<any>,
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array,
+    BigInt64Array,
+    BigUint64Array
+]
+
+export type { NormalizeIgnore as ZuordNormalizeIgnore };
