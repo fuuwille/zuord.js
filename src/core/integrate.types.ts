9ea9ca1257e478512e3aa0efebc7350ef6bc6878
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 20:09:09 +0300

    Improve type checks in IntegrateRaw for consistency and clarity

--- src/core/integrate.types.ts
+++ src/core/integrate.types.ts
@@ -4,12 +4,12 @@ import { ZuordUtil } from "@/util/alias.types";
 type Integrate<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = Zuord.Normalize<IntegrateRaw<A, B, Mode>>;
 
 type IntegrateRaw<A, B, Mode extends ZuordUtil.Mode<IntegrateMode> = ""> = ZuordUtil.IsSomeNever<[A, B]> extends false ? (
-    ZuordUtil.IsAllArray<[A, B]> extends true ? (
+    [ZuordUtil.IsAllArray<[A, B]>] extends [true] ? (
         ZuordUtil.IsExists<Mode, IntegrateConcantMode> extends true 
             ? Array<ZuordUtil.ArrayIn<B> | ZuordUtil.ArrayIn<A>>
             : Array<ZuordUtil.ArrayIn<B>>
     ) : 
-    ZuordUtil.IsAllPlain<[A, B]> extends true ? ({
+    [ZuordUtil.IsAllPlain<[A, B]>] extends [true] ? ({
         [K in keyof A | keyof B]: IntegrateRaw<
             K extends keyof A ? A[K] : never,
             K extends keyof B ? B[K] : never,
