cf6c3406600ae6df8857be93fcc98de7772b05e1
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 15:35:20 +0300

    Refactor Normalize type to improve parameter naming for clarity and consistency

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,15 +1,15 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T, L extends object[] = ZuordUtil.Ignored > = ZuordUtil.HasIgnored<T, L> extends false ? (
+type Normalize<T, I extends object[] = ZuordUtil.Ignored> = ZuordUtil.HasIgnored<T, I> extends false ? (
     ZuordUtil.HasPlain<T> extends true ? (
-        (ZuordUtil.AsNonPlain<T> extends infer V ? Normalize<V, L> : never) | 
+        (ZuordUtil.AsNonPlain<T> extends infer V ? Normalize<V, I> : never) | 
         (ZuordUtil.MergeUnionObjects<ZuordUtil.AsPlain<T>> extends infer T ? {
-            [K in keyof T]: Normalize<T[K], L> 
+            [K in keyof T]: Normalize<T[K], I> 
         } : never)
     ) : ZuordUtil.HasArray<T> extends true ? (
         (Normalize<Exclude<T, readonly unknown[]>>) |
         (Normalize<(T extends readonly unknown[] ? T[number] : never)>[])
     ) : T
-) : Normalize<ZuordUtil.AsNonIgnored<T, L>> | ZuordUtil.AsIgnored<T, L>;
+) : Normalize<ZuordUtil.AsNonIgnored<T, I>> | ZuordUtil.AsIgnored<T, I>;
 
 export type { Normalize as ZuordNormalize };
