4b9ff77594b18edc760403615783136cc210b9fb
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 23:11:57 +0300

    Refactor Normalize type to use NormalizeOptions for improved clarity

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,7 +1,7 @@
 import { Zuord } from "@/core/alias.types";
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T, O extends Zuord.Options = Zuord.DefaultOptions> = ZuordUtil.HasIgnored<T, O["ignored"]> extends false ? (
+type Normalize<T, O extends NormalizeOptions = Zuord.DefaultOptions> = ZuordUtil.HasIgnored<T, O["ignored"]> extends false ? (
     ZuordUtil.HasPlain<T> extends true ? (
         (ZuordUtil.AsNonPlain<T> extends infer T ? Normalize<T, O> : never) | 
         (([O["lite"]] extends [true] ? ZuordUtil.AsPlain<T> : ZuordUtil.AsOnePlain<T>) extends infer T ? { [K in keyof T]: Normalize<T[K], O> } : never)
@@ -12,4 +12,7 @@ type Normalize<T, O extends Zuord.Options = Zuord.DefaultOptions> = ZuordUtil.Ha
     ) : T
 ) : Normalize<ZuordUtil.AsNonIgnored<T, O["ignored"]>, O> | ZuordUtil.AsIgnored<T, O["ignored"]>;
 
+type NormalizeOptions = Zuord.Options
+
 export type { Normalize as ZuordNormalize };
+export type { NormalizeOptions as ZuordNormalizeOptions };
