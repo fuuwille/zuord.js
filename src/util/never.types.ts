6919731a5a70ad2a7470ee8e95bdea16f7d407d7
k4yr2 <kyrzkaya@gmail.com>
2025-06-25 18:01:56 +0300

    Refactor never.types.ts to add AsSomeNonNever type and improve organization

--- src/util/never.types.ts
+++ src/util/never.types.ts
@@ -1,8 +1,13 @@
+import { ZuordUtil } from "@/util/alias.types";
+
 type IsNever<T> = [T] extends [never] ? true : false;
 
 type SomeIsNever<U extends readonly unknown[]> = U extends readonly [infer First, ...infer Rest]
     ? (IsNever<First> extends true ? true : SomeIsNever<Rest>) 
     : false;
 
+type AsSomeNonNever<U extends unknown[]> = ZuordUtil.UnionOf<U>;
+
 export type { IsNever as ZuordIsNever };
 export type { SomeIsNever as ZuordSomeIsNever };
+export type { AsSomeNonNever as ZuordAsSomeNonNever };
