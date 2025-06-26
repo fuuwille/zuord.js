7d342b10f2ee5a19e2224f5cea7be8e7906b5c91
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 15:19:45 +0300

    Add InstanceTuple type definition for improved type handling

--- src/util/instance.types.ts
+++ src/util/instance.types.ts
@@ -1,3 +1,8 @@
 type InstanceOf<T> = T extends new (...args: any[]) => infer R ? R : never;
 
+type InstanceTuple<T extends readonly any[]> = {
+    [K in keyof T]: InstanceOf<T[K]>
+};
+
 export type { InstanceOf as ZuordInstanceOf };
+export type { InstanceTuple as ZuordInstanceTuple };
