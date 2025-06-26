45e97df658d489450b975ef8a67f0d92134d1748
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 15:14:50 +0300

    Update DataOf type definition to accept an array of Mode types for improved flexibility

--- src/core/data.types.ts
+++ src/core/data.types.ts
@@ -3,13 +3,13 @@ import { Zuord } from "@/core/alias.types";
 type Data = {
     content: object[];
     outcasts?: Zuord.OutcastConstructors;
-    mode?: Zuord.Mode;
+    mode?: Zuord.Mode[];
 };
 
-type DataOf<U extends object[], C extends Zuord.OutcastConstructors, M extends Zuord.Mode> = Data & {
+type DataOf<U extends object[], C extends Zuord.OutcastConstructors, M extends Zuord.Mode[]> = Data & {
     content: [...U];
     outcasts?: [...C];
-    mode?: M;
+    mode?: [...M];
 };
 
 export type { Data as ZuordData };
