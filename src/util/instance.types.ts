3a0dda2dd0620b9a4f4f4442527086dd9919d9ce
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 15:20:18 +0300

    Update InstanceTuple type definition to restrict to object arrays for improved type safety

--- src/util/instance.types.ts
+++ src/util/instance.types.ts
@@ -1,6 +1,6 @@
 type InstanceOf<T> = T extends new (...args: any[]) => infer R ? R : never;
 
-type InstanceTuple<T extends readonly any[]> = {
+type InstanceTuple<T extends readonly object[]> = {
     [K in keyof T]: InstanceOf<T[K]>
 };
 
