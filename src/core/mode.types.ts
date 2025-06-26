cb5e0bc8a65213e140d7c57bef61a870f9760d20
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 09:14:13 +0300

    Refactor Mode type for improved clarity and define LiteMode in mode.types.ts

--- src/core/mode.types.ts
+++ src/core/mode.types.ts
@@ -1,6 +1,9 @@
-type Mode = string | undefined;
+type Mode = LiteMode | undefined;
+
+type LiteMode = "lite";
 
 type DefaultMode = undefined;
 
 export type { Mode as ZuordMode };
+export type { LiteMode as ZuordLiteMode };
 export type { DefaultMode as ZuordDefaultMode };
