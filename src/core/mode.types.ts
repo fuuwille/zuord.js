a86c10ef47ad1d56115dad19e59e2a3a8f940bf8
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 15:27:21 +0300

    Add ShallowMode type definition for enhanced mode handling

--- src/core/mode.types.ts
+++ src/core/mode.types.ts
@@ -2,8 +2,11 @@ type Mode = string | undefined;
 
 type LiteMode = "lite";
 
+type ShallowMode = "shallow";
+
 type DefaultMode = undefined;
 
 export type { Mode as ZuordMode };
 export type { LiteMode as ZuordLiteMode };
+export type { ShallowMode as ZuordShallowMode };
 export type { DefaultMode as ZuordDefaultMode };
