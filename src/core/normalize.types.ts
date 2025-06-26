644aeccdd41921407f8680e7a33be747dd779cd6
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 17:57:14 +0300

    Refactor Normalize type to improve conditional checks for clarity and consistency

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -1,12 +1,12 @@
 import { Zuord } from "@/core/alias.types";
 import { ZuordUtil } from "@/util/alias.types";
 
-type Normalize<T, Options extends NormalizeOptions = NormalizeDefaultOptions> = ZuordUtil.HasOutcasts<T, Options["outcasts"]> extends false ? (
-    ZuordUtil.HasPlain<T> extends true ? (
+type Normalize<T, Options extends NormalizeOptions = NormalizeDefaultOptions> = [ZuordUtil.HasOutcasts<T, Options["outcasts"]>] extends [false]? (
+    [ZuordUtil.HasPlain<T>] extends [true] ? (
         (ZuordUtil.AsNonPlain<T> extends infer T ? Normalize<T, Options> : never) | 
-        (([Options["mode"]["lite"]] extends [true] ? ZuordUtil.AsPlain<T> : ZuordUtil.AsOnePlain<T>) extends infer T ? { [K in keyof T]: Normalize<T[K], Options> } : never)
+        ([Options["mode"]["lite"]] extends [true] ? ZuordUtil.AsPlain<T> : (ZuordUtil.AsOnePlain<T> extends infer T ? { [K in keyof T]: Normalize<T[K], Options> } : never))
     ) : 
-    ZuordUtil.HasArray<T> extends true ? (
+    [ZuordUtil.HasArray<T>] extends [true] ? (
         (ZuordUtil.AsNonArray<T> extends infer T ? Normalize<T, Options> : never) |
         ([Options["mode"]["lite"]] extends [true] ? ZuordUtil.AsArray<T>: Normalize<Extract<ZuordUtil.AsOneArray<T>, readonly unknown[]>[number], Options>[])
     ) : T
