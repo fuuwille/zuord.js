fdd95abe03811f1b8be53d4ece5372778b2e16d4
k4yr2 <kyrzkaya@gmail.com>
2025-06-26 16:19:30 +0300

    Add PartialMode type definition for enhanced mode handling

--- src/core/mode.types.ts
+++ src/core/mode.types.ts
@@ -4,9 +4,12 @@ type LiteMode = "lite";
 
 type ShallowMode = "shallow";
 
+type PartialMode = Partial<Mode>;
+
 type DefaultMode = undefined;
 
 export type { Mode as ZuordMode };
+export type { PartialMode as ZuordPartialMode };
 export type { LiteMode as ZuordLiteMode };
 export type { ShallowMode as ZuordShallowMode };
 export type { DefaultMode as ZuordDefaultMode };
