5f372f6b30cc04931a9644962efcc1756d962f83
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 17:30:07 +0300

    Add IsAllTuple type for comprehensive tuple type checking

--- src/util/tuple.types.ts
+++ src/util/tuple.types.ts
@@ -5,5 +5,9 @@ type IsTuple<T> = T extends readonly any[] ? (
 type IsSomeTuple<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
     ? (IsTuple<First> extends true ? true : IsSomeTuple<Rest>) : false;
 
+type IsAllTuple<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
+    ? (IsTuple<First> extends true ? IsAllTuple<Rest> : false) : true;
+
 export type { IsTuple as ZuordIsTuple };
 export type { IsSomeTuple as ZuordIsSomeTuple };
+export type { IsAllTuple as ZuordIsAllTuple };
