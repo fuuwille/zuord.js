2f007a100188996235d6ae04b86411755de2c5c0
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 17:28:47 +0300

    Add IsSomeTuple type for enhanced tuple type checking

--- src/util/tuple.types.ts
+++ src/util/tuple.types.ts
@@ -2,4 +2,8 @@ type IsTuple<T> = T extends readonly any[] ? (
     [number] extends [T['length']] ? false : true
 ) : false;
 
+type IsSomeTuple<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
+    ? (IsTuple<First> extends true ? true : IsSomeTuple<Rest>) : false;
+
 export type { IsTuple as ZuordIsTuple };
+export type { IsSomeTuple as ZuordIsSomeTuple };
