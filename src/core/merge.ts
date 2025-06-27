46e24cedbeffac346a41852245f162494f01a60c
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 21:21:04 +0300

    Fix type inference in mergeBy function for consistent handling of data options

--- src/core/merge.ts
+++ src/core/merge.ts
@@ -7,10 +7,10 @@ function merge<U extends object[]>(...content: U) : Zuord.Merge<U> {
         content
     }) as Zuord.Merge<U>;
 }
-function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[], const M extends Partial<Zuord.MergeMode> = Zuord.MergeDefaultMode>(data : Zuord.DataOf<U, C, M> ): Zuord.Merge<U, Zuord.OptionsOf<Required<typeof data>>> {
+function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[], const M extends Partial<Zuord.MergeMode> = Zuord.MergeDefaultMode>(data : Zuord.DataOf<U, C, M> ): Zuord.Merge<U, Zuord.OptionsOf<typeof data>> {
     if (data.content.length === 0) {
         // If no content is provided, return an empty object
-        return {} as Zuord.Merge<U, Zuord.OptionsOf<Required<typeof data>>>;
+        return {} as Zuord.Merge<U, Zuord.OptionsOf<typeof data>>;
     }
 
     const result: Record<string, unknown> = {};
@@ -40,7 +40,7 @@ function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[],
     }
 
     // Return the merged result as a normalized object
-    return result as Zuord.Merge<U, Zuord.OptionsOf<Required<typeof data>>>;
+    return result as Zuord.Merge<U, Zuord.OptionsOf<typeof data>>;
 }
 
 export { merge as zuordMerge };
