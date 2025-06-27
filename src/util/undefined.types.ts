e5c270801ef547f138b41dfb75b397f3a0d942fa
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 01:48:30 +0300

    Refactor AsNonUndefined type for improved clarity and consistency

--- src/util/undefined.types.ts
+++ src/util/undefined.types.ts
@@ -1,11 +1,11 @@
-type AsNonUndefined<T> = T extends undefined ? never : T;
-
 type IsUndefined<T> = [T] extends [undefined] ? true : false;
 
 type HasUndefined<T> = true extends (T extends any ? IsUndefined<T> : never) ? true : false;
 
-export type { AsNonUndefined as ZuordAsNonUndefined };
+type AsNonUndefined<T> = T extends any ? (IsUndefined<T> extends true ? never : T) : never;
 
 export type { IsUndefined as ZuordIsUndefined };
 
 export type { HasUndefined as ZuordHasUndefined };
+
+export type { AsNonUndefined as ZuordAsNonUndefined };
