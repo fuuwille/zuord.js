dc4aa8f846299facadd6e27bb60c8a3c18ea982b
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 15:12:54 +0300

    Refactor Impose type for improved readability and structure

--- src/core/impose.types.ts
+++ src/core/impose.types.ts
@@ -1,5 +1,10 @@
 import { ZuordUtil } from "@/util/alias.types";
+import { Zuord } from "./alias.types";
 
 type Impose<TBase, TPatch extends ZuordUtil.Optional<TBase>, TCurrent extends TBase = TBase> = {
-    
+    [K in keyof TCurrent]: K extends keyof TBase ? (
+        TCurrent[K] extends object ? (TPatch[K] extends object ? (
+            Impose<TBase[K], TPatch[K], TCurrent[K]>
+        ) : TCurrent[K] ) : K extends keyof TPatch ? TPatch[K] : TCurrent[K]
+    ) : unknown;
 };
