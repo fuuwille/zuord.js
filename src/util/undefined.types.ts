db62f07d8ef49a4bb93468ff60740bc44a52259d
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 23:11:33 +0300

    Add HasUndefined type for improved detection of undefined values in types

--- src/util/undefined.types.ts
+++ src/util/undefined.types.ts
@@ -2,6 +2,10 @@ type AsNonUndefined<T> = T extends undefined ? never : T;
 
 type IsUndefined<T> = [T] extends [undefined] ? true : false;
 
+type HasUndefined<T> = true extends (T extends any ? IsUndefined<T> : never) ? true : false;
+
 export type { AsNonUndefined as ZuordAsNonUndefined };
 
 export type { IsUndefined as ZuordIsUndefined };
+
+export type { HasUndefined as ZuordHasUndefined };
