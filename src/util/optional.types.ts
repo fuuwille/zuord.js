4af93e0ded749aaf668069b25eb990ee762baadb
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 09:07:15 +0300

    Refactor Optional type to explicitly include undefined for improved type handling

--- src/util/optional.types.ts
+++ src/util/optional.types.ts
@@ -1,5 +1,5 @@
 type Optional<T> = {
-    [K in keyof T]?: T[K] extends object ? Optional<T[K]> : T[K];
+    [K in keyof T]?: (T[K] extends object ? Optional<T[K]> : T[K]) | undefined;
 };
 
 export type { Optional as ZuordOptional };
