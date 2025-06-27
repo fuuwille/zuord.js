41b6f7bf43f514845e7baf889e04d90dfdbf26b7
k4yr2 <kyrzkaya@gmail.com>
2025-06-27 23:55:40 +0300

    Refactor ImposeRaw type to enhance readability and maintainability

--- src/core/impose.types.ts
+++ src/core/impose.types.ts
@@ -4,9 +4,9 @@ import { ZuordUtil } from "@/util/alias.types";
 type Impose<TBase, TPatch extends ZuordUtil.Optional<TBase>, TCurrent extends TBase = TBase> = Zuord.Normalize<ImposeRaw<TBase, TPatch, TCurrent>>;
 
 type ImposeRaw<TBase, TPatch extends ZuordUtil.Optional<TBase>, TCurrent extends TBase = TBase> = [ZuordUtil.IsNever<TPatch>] extends [false] ? (
-    ({
+    ZuordUtil.HasPlain<TBase> extends true ? ({
         [K in keyof TBase]: K extends keyof TPatch ? Required<Omit<TCurrent[K], keyof NonNullable<TPatch[K]>> & TPatch[K]> : ImposeRaw<TBase[K], TCurrent[K], TCurrent[K]>
-    })
+    }) : TCurrent
 ) : TCurrent;
 
 export type { Impose as ZuordImpose };
