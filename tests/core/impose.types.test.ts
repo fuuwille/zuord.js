ff761963c251c29bbb90522558c77c79a1c22022
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 09:13:54 +0300

    Refactor T101, T102, T103, and T104 type definitions for clarity and consistency in loose mode testing

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -5,38 +5,38 @@ import { ZuordTest } from "@/test";
  * Loose mode with primitive base:
  * Patch is `never`, should return current
  */
-export type T101 = ZuordTest.Assert<ZuordTest.IsEqual<T101S, T101E>>;
+export type T1010 = ZuordTest.Assert<ZuordTest.IsEqual<T1010S, T1010E>>;
 
-type T101S = Zuord.ImposeLoose<string, never, "test">;
+type T1010S = Zuord.ImposeLoose<string, never, "test">;
 
-type T101E = "test";
+type T1010E = "test";
 
 /**
  * Loose mode with primitive base:
  * Patch is `undefined`, should return current
  */
-export type T102 = ZuordTest.Assert<ZuordTest.IsEqual<T102S, T102E>>;
+export type T1020 = ZuordTest.Assert<ZuordTest.IsEqual<T1020S, T1020E>>;
 
-type T102S = Zuord.ImposeLoose<string, undefined, "test">;
+type T1020S = Zuord.ImposeLoose<string, undefined, "test">;
 
-type T102E = "test";
+type T1020E = "test";
 
 /**
  * Loose mode with primitive base:
  * Patch is `null`, should return patch
  */
-export type T103 = ZuordTest.Assert<ZuordTest.IsEqual<T103S, T103E>>;
+export type T1030 = ZuordTest.Assert<ZuordTest.IsEqual<T1030S, T1030E>>;
 
-type T103S = Zuord.ImposeLoose<string, null, "test">;
+type T1030S = Zuord.ImposeLoose<string, null, "test">;
 
-type T103E = null;
+type T1030E = null;
 
 /**
  * Loose mode with primitive base:
  * Patch is exact general primitive, should return patch
  */
-export type T104 = ZuordTest.Assert<ZuordTest.IsEqual<T104S, T104E>>;
+export type T1040 = ZuordTest.Assert<ZuordTest.IsEqual<T1040S, T1040E>>;
 
-type T104S = Zuord.ImposeLoose<string, string, "test">;
+type T1040S = Zuord.ImposeLoose<string, string, "test">;
 
-type T104E = string;
+type T1040E = string;
