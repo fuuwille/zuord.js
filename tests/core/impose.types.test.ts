f5676b264362591a2f2808cd3f925716f477eb44
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:08:13 +0300

    Refactor impose.types.test.ts to update type expectations and improve documentation

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -1,10 +1,11 @@
 import { Zuord } from "@";
 import { ZuordTest } from "@/test";
 
-// 1. Patch is never, should return TCurrent
-
+/**
+ * Patch is never, should return TCurrent
+ */
 export type Test1 = ZuordTest.Assert<ZuordTest.IsEqual<Test1Sample, Test1Expected>>;
 
-type Test1Sample = Zuord.ImposeRaw<{ a: number }, never, { a: number }>;
+type Test1Sample = Zuord.ImposeRaw<{ a: number }, never, { a: 222 }>;
 
-type Test1Expected = { a: number };
+type Test1Expected = { a: 222 };
