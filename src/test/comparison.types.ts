53a5c3dec10940f60d7e39aa35c6a286acc77ea7
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 21:17:15 +0300

    Add ComparisonSelfList type for handling arrays of ComparisonSelf

--- src/test/comparison.types.ts
+++ src/test/comparison.types.ts
@@ -8,6 +8,12 @@ type ComparisonSelf<T> = {
     expected : T;
 }
 
+type ComparisonSelfList<U extends unknown[]> = {
+    [K in keyof U]: ComparisonSelf<U[K]>;
+}
+
 export type { Comparison as ZuordComparison };
 
 export type { ComparisonSelf as ZuordComparisonSelf };
+
+export type { ComparisonSelfList as ZuordComparisonSelfList };
