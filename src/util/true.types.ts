72d85c7ee4e8bcad989c57145a1da94facde9b87
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:46:57 +0300

    Add IsSomeTrue type definition and export in ZuordUtil namespace

--- src/util/true.types.ts
+++ src/util/true.types.ts
@@ -1,3 +1,8 @@
 type IsTrue<T> = [T] extends [true] ? true : false;
 
+type IsSomeTrue<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
+    ? (IsTrue<First> extends true ? true : IsSomeTrue<Rest>) : false;
+
 export type { IsTrue as ZuordIsTrue };
+
+export type { IsSomeTrue as ZuordIsSomeTrue };
