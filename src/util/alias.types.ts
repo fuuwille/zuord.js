8aba856231955b4783176c24d6498bda0cf801bf
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:22:33 +0300

    Import ZuordHasBoolean type and add export for HasBoolean in ZuordUtil namespace

--- src/util/alias.types.ts
+++ src/util/alias.types.ts
@@ -2,7 +2,7 @@ import { Zuord } from "@/core/alias.types";
 import type { ZuordAsAny } from "./any.types";
 import type { ZuordIsUndefined, ZuordHasUndefined, ZuordAsNonUndefined } from "./undefined.types";
 import type { ZuordIsTrue } from "./true.types";
-import type { ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean } from "./boolean.types";
+import type { ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean, ZuordHasBoolean } from "./boolean.types";
 import type { ZuordHasOutcasts, ZuordExtractOutcasts, ZuordExcludeOutcasts } from "./outcasts.types";
 import type { ZuordIsKey, ZuordIsRequiredKey, ZuordHasKey, ZuordAnyHasKey, ZuordAllHasKey, ZuordKeysOf, ZuordRequiredKeysOf, ZuordOptionalKeysOf } from "./key.types";
 import type { ZuordValueAt } from "./value.types";
@@ -59,6 +59,9 @@ export namespace ZuordUtil {
 
   export type IsAllBoolean<U extends readonly unknown[]> = ZuordIsAllBoolean<U>;
 
+  export type HasBoolean<T> = ZuordHasBoolean<T>;
+
+  
   // BIGINT
 
   // NUMBER
