f07441e1a775278b1f873e185c45adb7bab4bbe5
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:49:36 +0300

    Add IsAllTrue type definition to evaluate if all elements in a tuple are true

--- src/util/true.types.ts
+++ src/util/true.types.ts
@@ -3,6 +3,11 @@ type IsTrue<T> = [T] extends [true] ? true : false;
 type IsSomeTrue<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
     ? (IsTrue<First> extends true ? true : IsSomeTrue<Rest>) : false;
 
+type IsAllTrue<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
+    ? (IsTrue<First> extends true ? IsAllTrue<Rest> : false) : true;
+
 export type { IsTrue as ZuordIsTrue };
 
 export type { IsSomeTrue as ZuordIsSomeTrue };
+
+export type { IsAllTrue as ZuordIsAllTrue };
