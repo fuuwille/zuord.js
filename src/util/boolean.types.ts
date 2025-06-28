ca4c0e002316ee917aea1ec74933a8040050017f
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:11:32 +0300

    Add IsBooleanTrue type definition and export as ZuordIsBooleanTrue

--- src/util/boolean.types.ts
+++ src/util/boolean.types.ts
@@ -1,3 +1,7 @@
 type IsBoolean<T> = [T] extends [boolean] ? true : false;
 
+type IsBooleanTrue<T> = [T] extends [true] ? true : false;
+
 export type { IsBoolean as ZuordIsBoolean };
+
+export type { IsBooleanTrue as ZuordIsBooleanTrue };
