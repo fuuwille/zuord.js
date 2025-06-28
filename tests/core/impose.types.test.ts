fcf761822e1d7c998acc47b3ee3840bb586ceb12
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:55:38 +0300

    Refactor T101 type definition for clarity and consistency in loose mode testing

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -1,3 +1,12 @@
 import { Zuord } from "@";
 import { ZuordTest } from "@/test";
 
+/**
+ * Loose mode with primitive base:
+ * Patch is `never`, should return current value
+ */
+export type T101 = ZuordTest.Assert<ZuordTest.IsEqual<T101S, T101E>>;
+
+type T101S = Zuord.ImposeLoose<string, never, "test">;
+
+type T101E = "test";
