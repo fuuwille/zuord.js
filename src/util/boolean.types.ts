878f5e854475423a676c26e9ab221c90991eccfc
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:19:10 +0300

    Add IsAllBoolean type definition and export as ZuordIsAllBoolean

--- src/util/boolean.types.ts
+++ src/util/boolean.types.ts
@@ -3,6 +3,11 @@ type IsBoolean<T> = [T] extends [boolean] ? true : false;
 type IsSomeBoolean<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
     ? (IsBoolean<First> extends true ? true : IsSomeBoolean<Rest>) : false;
 
+type IsAllBoolean<U extends readonly unknown[]> = U extends [infer First, ...infer Rest]
+    ? (IsBoolean<First> extends true ? IsAllBoolean<Rest> : false) : true;
+
 export type { IsBoolean as ZuordIsBoolean };
 
 export type { IsSomeBoolean as ZuordIsSomeBoolean };
+
+export type { IsAllBoolean as ZuordIsAllBoolean };
