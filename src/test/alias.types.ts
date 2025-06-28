f592faa51ced3a0843a300fabda78cad94b33d58
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 14:48:23 +0300

    Add Comparison type alias to ZuordTest namespace

--- src/test/alias.types.ts
+++ src/test/alias.types.ts
@@ -1,9 +1,12 @@
 import { ZuordAssert } from "./assert.types";
 import { ZuordIsEqual } from "./isEqual.types";
+import { ZuordComparison } from "./comparison.types";
 
 export namespace ZuordTest {
 
     export type Assert<T extends true> = ZuordAssert<T>;
 
     export type IsEqual<A, B> = ZuordIsEqual<A, B>;
+
+    export type Comparison = ZuordComparison;
 }
