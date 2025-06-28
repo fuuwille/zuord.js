b71a4c4434913c1812fff486205a055ab66f570e
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:26:49 +0300

    Refactor test types to use ImposeLoose naming convention for consistency

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -4,28 +4,28 @@ import { ZuordTest } from "@/test";
 /**
  * Patch is never, should return TCurrent
  */
-export type Test1 = ZuordTest.Assert<ZuordTest.IsEqual<Test1Sample, Test1Expected>>;
+export type ImposeLoose_Test1 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test1Sample, ImposeLoose_Test1Expected>>;
 
-type Test1Sample = Zuord.ImposeLoose<{ a: number }, never, { a: 222 }>;
+type ImposeLoose_Test1Sample = Zuord.ImposeLoose<{ a: number }, never, { a: 222 }>;
 
-type Test1Expected = { a: 222 };
+type ImposeLoose_Test1Expected = { a: 222 };
 
 
 /**
  * Patch is undefined, should return TCurrent
  */
-export type Test2 = ZuordTest.Assert<ZuordTest.IsEqual<Test2Sample, Test2Expected>>;
+export type ImposeLoose_Test2 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test2Sample, ImposeLoose_Test2Expected>>;
 
-type Test2Sample = Zuord.ImposeLoose<{ a: number }, undefined, { a: 222 }>;
+type ImposeLoose_Test2Sample = Zuord.ImposeLoose<{ a: number }, undefined, { a: 222 }>;
 
-type Test2Expected = { a: 222 };
+type ImposeLoose_Test2Expected = { a: 222 };
 
 
 /**
  * Patch is a plain object, should recursively impose
  */
-export type Test3 = ZuordTest.Assert<ZuordTest.IsEqual<Test3Sample, Test3Expected>>;
+export type ImposeLoose_Test3 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test3Sample, ImposeLoose_Test3Expected>>;
 
-type Test3Sample = Zuord.ImposeLoose<{ a: number; b: string }, { a: 7 }, { a: 222; b: "test" }>;
+type ImposeLoose_Test3Sample = Zuord.ImposeLoose<{ a: number; b: string }, { a: 7 }, { a: 222; b: "test" }>;
 
-type Test3Expected = { a: 7; b: "test" };
+type ImposeLoose_Test3Expected = { a: 7; b: "test" };
