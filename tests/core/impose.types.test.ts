f8560559932080b39c37b2658c48778b9bcaa6fe
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:40:11 +0300

    Refactor impose types in tests for consistency and clarity

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -4,28 +4,28 @@ import { ZuordTest } from "@/test";
 /**
  * Patch is never, should return TCurrent
  */
-export type ImposeLoose_Test1 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test1Sample, ImposeLoose_Test1Expected>>;
+export type T101 = ZuordTest.Assert<ZuordTest.IsEqual<T101_Sample, T101_Expected>>;
 
-type ImposeLoose_Test1Sample = Zuord.ImposeLoose<{ a: number }, never, { a: 222 }>;
+type T101_Sample = Zuord.ImposeLoose<{ a: number }, never, { a: 222 }>;
 
-type ImposeLoose_Test1Expected = { a: 222 };
+type T101_Expected = { a: 222 };
 
 
 /**
  * Patch is undefined, should return TCurrent
  */
-export type ImposeLoose_Test2 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test2Sample, ImposeLoose_Test2Expected>>;
+export type T102 = ZuordTest.Assert<ZuordTest.IsEqual<T102_Sample, T102_Expected>>;
 
-type ImposeLoose_Test2Sample = Zuord.ImposeLoose<{ a: number }, undefined, { a: 222 }>;
+type T102_Sample = Zuord.ImposeLoose<{ a: number }, undefined, { a: 222 }>;
 
-type ImposeLoose_Test2Expected = { a: 222 };
+type T102_Expected = { a: 222 };
 
 
 /**
  * Patch is an empty object, should return TCurrent
  */
-export type ImposeLoose_Test3 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose_Test3Sample, ImposeLoose_Test3Expected>>;
+export type T103 = ZuordTest.Assert<ZuordTest.IsEqual<T103_Sample, T103_Expected>>;
 
-type ImposeLoose_Test3Sample = Zuord.ImposeLoose<{ a: number; b: string }, { }, { a: 222; b: "test" }>;
+type T103_Sample = Zuord.ImposeLoose<{ a: number; b: string }, { }, { a: 222; b: "test" }>;
 
-type ImposeLoose_Test3Expected = { a: 222; b: "test" };
+type T103_Expected = { a: 222; b: "test" };
