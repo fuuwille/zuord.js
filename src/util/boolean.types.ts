2c42eb64ea683ff9626aaa3f89330c4bfa0809d0
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 15:17:07 +0300

    Remove IsBooleanTrue type definition and its export from boolean.types.ts

--- src/util/boolean.types.ts
+++ src/util/boolean.types.ts
@@ -1,7 +1,3 @@
 type IsBoolean<T> = [T] extends [boolean] ? true : false;
 
-type IsBooleanTrue<T> = [T] extends [true] ? true : false;
-
 export type { IsBoolean as ZuordIsBoolean };
-
-export type { IsBooleanTrue as ZuordIsBooleanTrue };
