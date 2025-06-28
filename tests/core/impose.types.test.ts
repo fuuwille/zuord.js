74d0db4bf27f569d5a887d45ef6cb2fd84c41200
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 16:15:20 +0300

    Refactor ImposeLooseT100 to consolidate test cases and improve clarity in type assertions

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -18,50 +18,20 @@ export type ImposeLooseComparison<TBase, TCurrent extends TBase, TComparisons ex
 //
 
 /**
- * base: `never` – patch `never` – current `never`
- * @returns `never`
+ * base: `never` – patch `all`  – current `never`
+ * @returns `patch self`
  */
-export type ImposeLoose100 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose100S, ImposeLoose100E>>;
-
-type ImposeLoose100S = Zuord.ImposeLoose<never, never, never>;
-
-type ImposeLoose100E = never;
-
-/**
- * base: `never` – patch `undefined` – current `never`
- * @returns `undefined`
- */
-export type ImposeLoose101 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose101S, ImposeLoose101E>>;
-
-type ImposeLoose101S = Zuord.ImposeLoose<never, undefined, never>;
-
-type ImposeLoose101E = undefined;
-
-/**
- * base: `never` – patch `null` – current `never`
- * @returns `null`
- */
-export type ImposeLoose102 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose102S, ImposeLoose102E>>;
-
-type ImposeLoose102S = Zuord.ImposeLoose<never, null, never>;
-
-type ImposeLoose102E = null;
-
-/**
- * base: `never` – patch `boolean` – current `never`
- * @returns `boolean`
- */
-export type ImposeLoose103 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose103S, ImposeLoose103E>>;
-
-type ImposeLoose103S = Zuord.ImposeLoose<never, boolean, never>;
-
-type ImposeLoose103E = boolean;
-
-/**
- * base: `never` – patch `void` – current `never`
- * @returns `void`
- */
-export type ImposeLoose104 = ZuordTest.Assert<ImposeLooseComparison<never, never, [
-    { type: void, expected: void },
+export type ImposeLooseT100 = ZuordTest.Assert<ImposeLooseComparison<never, never, [
+    { type: never, expected: never },
     { type: undefined, expected: undefined },
+    { type: null, expected: null },
+    { type: void, expected: void },
+    { type: boolean, expected: boolean },
+    { type: string, expected: string },
+    { type: number, expected: number },
+    { type: bigint, expected: bigint },
+    { type: symbol, expected: symbol },
+    { type: object, expected: object },
+    { type: [], expected: [] },
+    { type: {}, expected: {} },
 ]>>;
