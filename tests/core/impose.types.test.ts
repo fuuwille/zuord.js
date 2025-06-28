ddaaa489be44c290a97c90b8a17008e16dfd696e
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:08:41 +0300

    Add additional tests for Zuord impose functionality with undefined patch

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -9,3 +9,13 @@ export type Test1 = ZuordTest.Assert<ZuordTest.IsEqual<Test1Sample, Test1Expecte
 type Test1Sample = Zuord.ImposeRaw<{ a: number }, never, { a: 222 }>;
 
 type Test1Expected = { a: 222 };
+
+
+/**
+ * Patch is undefined, should return TCurrent
+ */
+export type Test2 = ZuordTest.Assert<ZuordTest.IsEqual<Test2Sample, Test2Expected>>;
+
+type Test2Sample = Zuord.ImposeRaw<{ a: number }, undefined, { a: 222 }>;
+
+type Test2Expected = { a: 222 };
