85e8a75029ad0ea2de65a19f6da81595a2dbc21b
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 14:38:34 +0300

    Refactor ImposeLoose101 type definition for clarity and consistency

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -10,3 +10,13 @@ export type ImposeLoose100 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose100S,
 type ImposeLoose100S = Zuord.ImposeLoose<never, never, never>;
 
 type ImposeLoose100E = never;
+
+/**
+ * base: `never` – patch `undefined` – current `never`
+ * @returns `undefined`
+ */
+export type ImposeLoose101 = ZuordTest.Assert<ZuordTest.IsEqual<ImposeLoose101S, ImposeLoose101E>>;
+
+type ImposeLoose101S = Zuord.ImposeLoose<never, undefined, never>;
+
+type ImposeLoose101E = undefined;
