064e7b764e66f0837cf8358c3db9eb98a9e1a598
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:56:21 +0300

    Refactor T102 type definition for loose mode testing with undefined patch

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -10,3 +10,13 @@ export type T101 = ZuordTest.Assert<ZuordTest.IsEqual<T101S, T101E>>;
 type T101S = Zuord.ImposeLoose<string, never, "test">;
 
 type T101E = "test";
+
+/**
+ * Loose mode with primitive base:
+ * Patch is `undefined`, should return current value
+ */
+export type T102 = ZuordTest.Assert<ZuordTest.IsEqual<T102S, T102E>>;
+
+type T102S = Zuord.ImposeLoose<string, undefined, "test">;
+
+type T102E = "test";
