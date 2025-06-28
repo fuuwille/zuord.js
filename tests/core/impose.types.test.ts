384e8c146e76015cdb049e95ab1ccc3c0b9e43ad
k4yr2 <kyrzkaya@gmail.com>
2025-06-28 08:10:07 +0300

    Add tests for Zuord impose functionality with a plain object patch

--- tests/core/impose.types.test.ts
+++ tests/core/impose.types.test.ts
@@ -19,3 +19,13 @@ export type Test2 = ZuordTest.Assert<ZuordTest.IsEqual<Test2Sample, Test2Expecte
 type Test2Sample = Zuord.ImposeRaw<{ a: number }, undefined, { a: 222 }>;
 
 type Test2Expected = { a: 222 };
+
+
+/**
+ * Patch is a plain object, should recursively impose
+ */
+export type Test3 = ZuordTest.Assert<ZuordTest.IsEqual<Test3Sample, Test3Expected>>;
+
+type Test3Sample = Zuord.ImposeRaw<{ a: number; b: string }, { a: 7 }, { a: 222; b: "test" }>;
+
+type Test3Expected = { a: 7; b: "test" };
