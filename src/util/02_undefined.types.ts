922f95c8e1fdd33b17257008568e1ecc27b06510
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 17:24:10 +0300

    Add EqAllUndefined type definition for comprehensive type checks

--- src/util/02_undefined.types.ts
+++ src/util/02_undefined.types.ts
@@ -4,6 +4,8 @@ type EqUndefined<T> = ZuordUtil.Eq<T, undefined>;
 
 type EqSomeUndefined<U extends readonly unknown[]> = ZuordUtil.EqSome<U, undefined>;
 
+type EqAllUndefined<U extends readonly unknown[]> = ZuordUtil.EqAll<U, undefined>;
+
 type IsUndefined<T> = ZuordUtil.Is<T, undefined>;
 
 type HasUndefined<T> = ZuordUtil.Has<T, undefined>;
@@ -13,6 +15,8 @@ type AsNonUndefined<T> = T extends any ? (EqUndefined<T> extends true ? never : 
 export type { EqUndefined as ZuordEqUndefined };
 
 export type { EqSomeUndefined as ZuordEqSomeUndefined };
+
+export type { EqAllUndefined as ZuordEqAllUndefined };
 
 export type { IsUndefined as ZuordIsUndefined };
 
