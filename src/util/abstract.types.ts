6ea45bb61b26ace5d3d7f39255c5e51d1a14fdc6
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 14:38:20 +0300

    Add Has type definition for type presence check in abstract.types.ts

--- src/util/abstract.types.ts
+++ src/util/abstract.types.ts
@@ -2,6 +2,10 @@ type Eq<A, B> = (<T>() => T extends A ? 0 : 1) extends (<T>() => T extends B ? 0
 
 type Is<A, B> = [A] extends [B] ? true : false;
 
+type Has<A, B> = true extends (A extends any ? Is<A, B> : never) ? true : false;
+
 export type { Eq as ZuordEq };
 
 export type { Is as ZuordIs };
+
+export type { Has as ZuordHas };
