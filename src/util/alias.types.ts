235ff3f7b32ad953b14d0ddac7333fbc54504254
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 17:29:54 +0300

    Add IsSomeUndefined type definition for enhanced type checks

--- src/util/alias.types.ts
+++ src/util/alias.types.ts
@@ -1,7 +1,7 @@
 import { Zuord } from "@/core/alias.types";
 import type { ZuordEq, ZuordEqSome, ZuordEqAll, ZuordIs, ZuordIsSome, ZuordIsAll, ZuordHas, ZuordHasSome, ZuordHasAll } from "./abstract.types";
 import type { ZuordIsNever, ZuordIsSomeNever, ZuordIsAllNever, ZuordHasNever, ZuordHasSomeNever, ZuordHasAllNever, ZuordAsNonNever } from "./01_never.types";
-import type { ZuordEqUndefined, ZuordEqSomeUndefined, ZuordEqAllUndefined, ZuordIsUndefined, ZuordHasUndefined, ZuordAsNonUndefined } from "./02_undefined.types";
+import type { ZuordEqUndefined, ZuordEqSomeUndefined, ZuordEqAllUndefined, ZuordIsUndefined, ZuordIsSomeUndefined, ZuordHasUndefined, ZuordAsNonUndefined } from "./02_undefined.types";
 import type { ZuordAsAny } from "./any.types";
 import type { ZuordIsTrue, ZuordIsSomeTrue, ZuordIsAllTrue, ZuordHasTrue, ZuordHasSomeTrue, ZuordHasAllTrue, ZuordExtractTrue, ZuordExcludeTrue } from "./true.types";
 import type { ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean, ZuordHasBoolean, ZuordHasSomeBoolean, ZuordHasAllBoolean, ZuordExtractBoolean, ZuordExcludeBoolean } from "./boolean.types";
@@ -73,6 +73,8 @@ export namespace ZuordUtil {
   export type EqAllUndefined<U extends readonly unknown[]> = ZuordEqAllUndefined<U>;
 
   export type IsUndefined<T> = ZuordIsUndefined<T>;
+
+  export type IsSomeUndefined<U extends readonly unknown[]> = ZuordIsSomeUndefined<U>;
   
   export type HasUndefined<T> = ZuordHasUndefined<T>;
   
