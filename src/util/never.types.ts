17526a4f29aadb9e72a527d8bdda5b0c754c413f
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 15:46:41 +0300

    Add IsAllNever type definition to ZuordUtil for comprehensive never type checks

--- src/util/never.types.ts
+++ src/util/never.types.ts
@@ -4,8 +4,11 @@ type IsNever<T> = ZuordUtil.Is<T, never>;
 
 type IsSomeNever<U extends readonly unknown[]> = ZuordUtil.IsSome<U, never>;
 
+type IsAllNever<U extends readonly unknown[]> = ZuordUtil.IsAll<U, never>;
+
 type AsNonNever<U extends unknown[]> = ZuordUtil.UnionOf<U>;
 
 export type { IsNever as ZuordIsNever };
 export type { IsSomeNever as ZuordIsSomeNever };
+export type { IsAllNever as ZuordIsAllNever };
 export type { AsNonNever as ZuordAsNonNever };
