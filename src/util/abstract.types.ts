8fdb34ce320941c86b3b34cff9562b7551933c50
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 14:42:07 +0300

    Add IsSome type definition for array element presence check in abstract.types.ts

--- src/util/abstract.types.ts
+++ src/util/abstract.types.ts
@@ -2,10 +2,16 @@ type Eq<A, B> = (<T>() => T extends A ? 0 : 1) extends (<T>() => T extends B ? 0
 
 type Is<A, B> = [A] extends [B] ? true : false;
 
+type IsSome<U extends readonly unknown[], B> = U extends [infer First, ...infer Rest] ? (
+    Is<First, B> extends true ? true : IsSome<Rest, B>
+) : false;
+
 type Has<A, B> = true extends (A extends any ? Is<A, B> : never) ? true : false;
 
 export type { Eq as ZuordEq };
 
 export type { Is as ZuordIs };
+
+export type { IsSome as ZuordIsSome };
 
 export type { Has as ZuordHas };
