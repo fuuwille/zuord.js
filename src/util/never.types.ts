c0a062d658fb84df2b4f81b300ebfd621f0fe105
k4yr2 <kyrzkaya@gmail.com>
2025-06-29 15:46:20 +0300

    Refactor IsNever and IsSomeNever types to utilize ZuordUtil for improved clarity and consistency

--- src/util/never.types.ts
+++ src/util/never.types.ts
@@ -1,10 +1,8 @@
 import { ZuordUtil } from "@/util/alias.types";
 
-type IsNever<T> = [T] extends [never] ? true : false;
+type IsNever<T> = ZuordUtil.Is<T, never>;
 
-type IsSomeNever<U extends readonly unknown[]> = U extends readonly [infer First, ...infer Rest]
-    ? (IsNever<First> extends true ? true : IsSomeNever<Rest>) 
-    : false;
+type IsSomeNever<U extends readonly unknown[]> = ZuordUtil.IsSome<U, never>;
 
 type AsNonNever<U extends unknown[]> = ZuordUtil.UnionOf<U>;
 
