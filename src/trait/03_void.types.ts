2b42785acebe0137f389e6a8aed5e516d317a7ab
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 18:02:45 +0300

    Refactor: Add EqVoid type definition to 03_void.types.ts

--- src/trait/03_void.types.ts
+++ src/trait/03_void.types.ts
@@ -1,10 +1,14 @@
 import { ZuordTrait } from "@/trait/_alias.types";
 
+type EqVoid<T> = ZuordTrait.Eq<T, void>;
+
 type IsVoid<T> = ZuordTrait.Is<T, void>;
 
 type IsSomeVoid<U extends readonly unknown[]> = ZuordTrait.IsSome<U, void>;
 
 type IsAllVoid<U extends readonly unknown[]> = ZuordTrait.IsAll<U, void>;
+
+export type { EqVoid as ZuordEqVoid };
 
 export type { IsVoid as ZuordIsVoid };
 
