16976d0e16d7d1cbcd0a0a09213ebc9d58821c10
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 21:22:14 +0300

    Refactor: Add EqSomeBoolean type for consistency with ZuordTrait

--- src/trait/_alias.types.ts
+++ src/trait/_alias.types.ts
@@ -4,7 +4,7 @@ import type { ZuordEqNever, ZuordEqSomeNever, ZuordEqAllNever, ZuordIsNever, Zuo
 import type { ZuordEqUndefined, ZuordEqSomeUndefined, ZuordEqAllUndefined, ZuordIsUndefined, ZuordIsSomeUndefined, ZuordIsAllUndefined, ZuordHasUndefined, ZuordHasSomeUndefined, ZuordHasAllUndefined, ZuordExcludeUndefined, ZuordExcludeExactUndefined , ZuordExtractUndefined, ZuordExtractExactUndefined } from "./02_undefined.types";
 import type { ZuordEqVoid, ZuordEqSomeVoid, ZuordEqAllVoid, ZuordIsVoid, ZuordIsSomeVoid, ZuordIsAllVoid, ZuordHasVoid, ZuordHasSomeVoid, ZuordHasAllVoid, ZuordExcludeVoid, ZuordExcludeExactVoid, ZuordExtractVoid, ZuordExtractExactVoid } from "./03_void.types";
 import type { ZuordEqNull, ZuordEqSomeNull, ZuordEqAllNull, ZuordIsNull, ZuordIsSomeNull, ZuordIsAllNull, ZuordHasNull, ZuordHasSomeNull, ZuordHasAllNull, ZuordExcludeNull, ZuordExcludeExactNull, ZuordExtractNull, ZuordExtractExactNull } from "./04_null.types";
-import type { ZuordEqBoolean, ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean, ZuordHasBoolean, ZuordHasSomeBoolean, ZuordHasAllBoolean, ZuordExcludeBoolean, ZuordExcludeExactBoolean, ZuordExtractBoolean, ZuordExtractExactBoolean } from "./05_boolean.types";
+import type { ZuordEqBoolean, ZuordEqSomeBoolean, ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean, ZuordHasBoolean, ZuordHasSomeBoolean, ZuordHasAllBoolean, ZuordExcludeBoolean, ZuordExcludeExactBoolean, ZuordExtractBoolean, ZuordExtractExactBoolean } from "./05_boolean.types";
 import type { ZuordAsAny } from "./any.types";
 import type { ZuordIsTrue, ZuordIsSomeTrue, ZuordIsAllTrue, ZuordHasTrue, ZuordHasSomeTrue, ZuordHasAllTrue, ZuordExtractTrue, ZuordExcludeTrue } from "./true.types";
 import type { ZuordHasOutcasts, ZuordExtractOutcasts, ZuordExcludeOutcasts } from "./outcasts.types";
@@ -157,6 +157,8 @@ export namespace ZuordTrait {
   // BOOLEAN
 
   export type EqBoolean<T> = ZuordEqBoolean<T>;
+
+  export type EqSomeBoolean<U extends readonly unknown[]> = ZuordEqSomeBoolean<U>;
 
   export type IsBoolean<T> = ZuordIsBoolean<T>;
 
