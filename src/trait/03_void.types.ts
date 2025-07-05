c99a3ca3bf9ca0b4f6a688fd5e9b3730d6dc234b
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 18:02:20 +0300

    Refactor: Add IsAllVoid type definition to 03_void.types.ts

--- src/trait/03_void.types.ts
+++ src/trait/03_void.types.ts
@@ -4,6 +4,10 @@ type IsVoid<T> = ZuordTrait.Is<T, void>;
 
 type IsSomeVoid<U extends readonly unknown[]> = ZuordTrait.IsSome<U, void>;
 
+type IsAllVoid<U extends readonly unknown[]> = ZuordTrait.IsAll<U, void>;
+
 export type { IsVoid as ZuordIsVoid };
 
 export type { IsSomeVoid as ZuordIsSomeVoid };
+
+export type { IsAllVoid as ZuordIsAllVoid };
