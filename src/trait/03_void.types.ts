9fd5d558645601d7a880867f466432d8d957a96c
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 18:02:07 +0300

    Refactor: Add IsSomeVoid type definition to 03_void.types.ts

--- src/trait/03_void.types.ts
+++ src/trait/03_void.types.ts
@@ -2,4 +2,8 @@ import { ZuordTrait } from "@/trait/_alias.types";
 
 type IsVoid<T> = ZuordTrait.Is<T, void>;
 
+type IsSomeVoid<U extends readonly unknown[]> = ZuordTrait.IsSome<U, void>;
+
 export type { IsVoid as ZuordIsVoid };
+
+export type { IsSomeVoid as ZuordIsSomeVoid };
