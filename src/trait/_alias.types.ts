7ae4df166bcbb8b15ac54cedb3d9360ed777bd9f
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 22:04:26 +0300

    Refactor: Add ExcludeExactBigInt type for exact bigint exclusion

--- src/trait/_alias.types.ts
+++ src/trait/_alias.types.ts
@@ -5,7 +5,7 @@ import type { ZuordEqUndefined, ZuordEqSomeUndefined, ZuordEqAllUndefined, Zuord
 import type { ZuordEqVoid, ZuordEqSomeVoid, ZuordEqAllVoid, ZuordIsVoid, ZuordIsSomeVoid, ZuordIsAllVoid, ZuordHasVoid, ZuordHasSomeVoid, ZuordHasAllVoid, ZuordExcludeVoid, ZuordExcludeExactVoid, ZuordExtractVoid, ZuordExtractExactVoid } from "./03_void.types";
 import type { ZuordEqNull, ZuordEqSomeNull, ZuordEqAllNull, ZuordIsNull, ZuordIsSomeNull, ZuordIsAllNull, ZuordHasNull, ZuordHasSomeNull, ZuordHasAllNull, ZuordExcludeNull, ZuordExcludeExactNull, ZuordExtractNull, ZuordExtractExactNull } from "./04_null.types";
 import type { ZuordEqBoolean, ZuordEqSomeBoolean, ZuordEqAllBoolean, ZuordIsBoolean, ZuordIsSomeBoolean, ZuordIsAllBoolean, ZuordHasBoolean, ZuordHasSomeBoolean, ZuordHasAllBoolean, ZuordExcludeBoolean, ZuordExcludeExactBoolean, ZuordExtractBoolean, ZuordExtractExactBoolean } from "./05_boolean.types";
-import type { ZuordEqBigInt, ZuordEqSomeBigInt, ZuordEqAllBigInt, ZuordIsBigInt, ZuordIsSomeBigInt , ZuordIsAllBigInt, ZuordHasBigInt, ZuordHasSomeBigInt, ZuordHasAllBigInt, ZuordExcludeBigInt } from "./06_bigint.types";
+import type { ZuordEqBigInt, ZuordEqSomeBigInt, ZuordEqAllBigInt, ZuordIsBigInt, ZuordIsSomeBigInt , ZuordIsAllBigInt, ZuordHasBigInt, ZuordHasSomeBigInt, ZuordHasAllBigInt, ZuordExcludeBigInt, ZuordExcludeExactBigInt } from "./06_bigint.types";
 import type { ZuordAsAny } from "./any.types";
 import type { ZuordIsTrue, ZuordIsSomeTrue, ZuordIsAllTrue, ZuordHasTrue, ZuordHasSomeTrue, ZuordHasAllTrue, ZuordExtractTrue, ZuordExcludeTrue } from "./true.types";
 import type { ZuordHasOutcasts, ZuordExtractOutcasts, ZuordExcludeOutcasts } from "./outcasts.types";
@@ -205,6 +205,8 @@ export namespace ZuordTrait {
   export type HasAllBigInt<U extends readonly unknown[]> = ZuordHasAllBigInt<U>;
 
   export type ExcludeBigInt<T> = ZuordExcludeBigInt<T>;
+
+  export type ExcludeExactBigInt<T> = ZuordExcludeExactBigInt<T>;
 
 
   // xB - TRUE
