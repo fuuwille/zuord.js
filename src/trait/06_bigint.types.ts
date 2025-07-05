f4f1771b355781327e5fc6224bb3798616695a74
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 21:36:20 +0300

    Refactor: Add IsSomeBigInt type for bigint array type checking

--- src/trait/06_bigint.types.ts
+++ src/trait/06_bigint.types.ts
@@ -2,4 +2,8 @@ import { ZuordTrait } from "@/trait/_alias.types";
 
 type IsBigInt<T> = ZuordTrait.Is<T, bigint>;
 
+type IsSomeBigInt<U extends readonly unknown[]> = ZuordTrait.IsSome<U, bigint>;
+
 export type { IsBigInt as ZuordIsBigInt };
+
+export type { IsSomeBigInt as ZuordIsSomeBigInt };
