0028a37b80022aad4ac7e1a82395e0f42b1bb206
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 20:31:23 +0300

    Refactor: Add HasNull type definition to 04_null.types.ts

--- src/trait/04_null.types.ts
+++ src/trait/04_null.types.ts
@@ -6,8 +6,12 @@ type IsSomeNull<U extends readonly unknown[]> = ZuordTrait.IsSome<U, null>;
 
 type IsAllNull<U extends readonly unknown[]> = ZuordTrait.IsAll<U, null>;
 
+type HasNull<T> = ZuordTrait.Has<T, null>;
+
 export type { IsNull as ZuordIsNull };
 
 export type { IsSomeNull as ZuordIsSomeNull };
 
 export type { IsAllNull as ZuordIsAllNull };
+
+export type { HasNull as ZuordHasNull };
