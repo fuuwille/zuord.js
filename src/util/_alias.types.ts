1319297face35916ddd33a765b470ed400c5e052
k4yr2 <kyrzkaya@gmail.com>
2025-07-05 10:59:31 +0300

    Refactor: Consolidate key-related type exports in ZuordUtil namespace

--- src/util/_alias.types.ts
+++ src/util/_alias.types.ts
@@ -1,3 +1,22 @@
+import { ZuordIsKey, ZuordIsRequiredKey, ZuordHasKey, ZuordAnyHasKey, ZuordAllHasKey, ZuordKeysOf, ZuordRequiredKeysOf, ZuordOptionalKeysOf } from "./key.types";
+
 export namespace ZuordUtil {
 
+    // KEY
+
+    export type IsKey<T, K> = ZuordIsKey<T, K>;
+
+    export type IsRequiredKey<T, K extends PropertyKey> = ZuordIsRequiredKey<T, K>;
+    
+    export type HasKey<T, K> = ZuordHasKey<T, K>;
+
+    export type AnyHasKey<U extends readonly unknown[], K> = ZuordAnyHasKey<U, K>;
+
+    export type AllHasKey<U extends readonly unknown[], K> = ZuordAllHasKey<U, K>;
+
+    export type KeysOf<U> = ZuordKeysOf<U>;
+
+    export type RequiredKeysOf<T> = ZuordRequiredKeysOf<T>;
+    
+    export type OptionalKeysOf<T> = ZuordOptionalKeysOf<T>;
 }
