7d407ebd81b0a873c59f720a31d88c8a39c9840c
k4yr2 <kyrzkaya@gmail.com>
2025-07-06 15:31:20 +0300

    Refactor: Add IsSomeFunction type for improved function type checks

--- src/trait/function.types.ts
+++ src/trait/function.types.ts
@@ -2,4 +2,8 @@ import { ZuordTrait } from "./_alias.types";
 
 type IsFunction<T> = ZuordTrait.Is<T, Function>;
 
+type IsSomeFunction<U extends readonly unknown[]> = ZuordTrait.IsSome<U, Function>;
+
 export type { IsFunction as ZuordIsFunction };
+
+export type { IsSomeFunction as ZuordIsSomeFunction };
