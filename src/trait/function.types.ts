f91d40108efa1a4d8ac8167b3444baeecc3a4a5f
k4yr2 <kyrzkaya@gmail.com>
2025-07-06 15:33:14 +0300

    Refactor: Add EqSomeFunction type for function equality checks

--- src/trait/function.types.ts
+++ src/trait/function.types.ts
@@ -2,6 +2,8 @@ import { ZuordTrait } from "@/trait/_alias.types";
 
 type EqFunction<T> = ZuordTrait.Eq<T, Function>;
 
+type EqSomeFunction<U extends readonly unknown[]> = ZuordTrait.EqSome<U, Function>;
+
 type IsFunction<T> = ZuordTrait.Is<T, Function>;
 
 type IsSomeFunction<U extends readonly unknown[]> = ZuordTrait.IsSome<U, Function>;
@@ -15,6 +17,8 @@ type HasSomeFunction<U extends readonly unknown[]> = ZuordTrait.HasSome<U, Funct
 type HasAllFunction<U extends readonly unknown[]> = ZuordTrait.HasAll<U, Function>;
 
 export type { EqFunction as ZuordEqFunction };
+
+export type { EqSomeFunction as ZuordEqSomeFunction };
 
 export type { IsFunction as ZuordIsFunction };
 
