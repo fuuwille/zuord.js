a84ad8f6e503669f8005c84c3ce9bb1a76f61933
k4yr2 <kyrzkaya@gmail.com>
2025-07-06 15:31:58 +0300

    Refactor: Add HasFunction type for enhanced function type checks

--- src/trait/function.types.ts
+++ src/trait/function.types.ts
@@ -6,8 +6,12 @@ type IsSomeFunction<U extends readonly unknown[]> = ZuordTrait.IsSome<U, Functio
 
 type IsAllFunction<U extends readonly unknown[]> = ZuordTrait.IsAll<U, Function>;
 
+type HasFunction<T> = ZuordTrait.Has<T, Function>;
+
 export type { IsFunction as ZuordIsFunction };
 
 export type { IsSomeFunction as ZuordIsSomeFunction };
 
 export type { IsAllFunction as ZuordIsAllFunction };
+
+export type { HasFunction as ZuordHasFunction };
