4e3955e50abf495a54b8f5d569613364b56e4daf
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 00:15:03 +0300

    Add Array type export to ZuordType namespace

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -1,9 +1,12 @@
 import type { ZuordAbsence } from "./absence.types";
 import type { ZuordFunction } from "./function.types";
+import type { ZuordArray } from "./array.types";
 
 export namespace ZuordType {
-    
-   export type Absence = ZuordAbsence;
 
-   export type Function = ZuordFunction;
+    export type Absence = ZuordAbsence;
+
+    export type Function = ZuordFunction;
+
+    export type Array = ZuordArray;
 }
