31072a21f28b0128ba527fdba55558074b330268
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 00:14:33 +0300

    Add Function type export to ZuordType namespace

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -1,5 +1,9 @@
 import type { ZuordAbsence } from "./absence.types";
+import type { ZuordFunction } from "./function.types";
 
 export namespace ZuordType {
+    
    export type Absence = ZuordAbsence;
+
+   export type Function = ZuordFunction;
 }
