556d5a023c069646ee3f5f35a7523e7813e9fb79
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 01:27:19 +0300

    Import ZuordPlainOf type for enhanced type handling in _alias.types.ts

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -2,7 +2,7 @@ import type { ZuordAbsence } from "./absence.types";
 import type { ZuordFunction } from "./function.types";
 import type { ZuordTuple } from "./tuple.types";
 import type { ZuordArray } from "./array.types";
-import type { ZuordPlain } from "./plain.types";
+import type { ZuordPlain, ZuordPlainOf } from "./plain.types";
 import type { ZuordObject } from "./object.types";
 
 export namespace ZuordType {
@@ -16,6 +16,8 @@ export namespace ZuordType {
     export type Array = ZuordArray;
 
     export type Plain = ZuordPlain;
+
+    export type PlainOf<T> = ZuordPlainOf<T>;
 
     export type Object = ZuordObject;
 }
