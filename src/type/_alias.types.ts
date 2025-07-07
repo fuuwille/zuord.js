c27fc218bf108ffaf5bcda01e7610938029095e2
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 00:27:29 +0300

    Add Tuple type alias to ZuordType for improved type handling

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -1,5 +1,6 @@
 import type { ZuordAbsence } from "./absence.types";
 import type { ZuordFunction } from "./function.types";
+import type { ZuordTuple } from "./tuple.types";
 import type { ZuordArray } from "./array.types";
 
 export namespace ZuordType {
@@ -7,6 +8,8 @@ export namespace ZuordType {
     export type Absence = ZuordAbsence;
 
     export type Function = ZuordFunction;
+
+    export type Tuple = ZuordTuple;
 
     export type Array = ZuordArray;
 }
