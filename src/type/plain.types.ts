933c3642b01912fcb4acbc1fbcd1fce27800bd0a
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 01:26:55 +0300

    Add PlainOf type for enhanced type handling in plain.types.ts

--- src/type/plain.types.ts
+++ src/type/plain.types.ts
@@ -2,4 +2,9 @@ type Plain = {
   [key: PropertyKey]: unknown;
 };
 
+type PlainOf<T> = [T] extends [never] ? unknown 
+    : ([T] extends [Plain] ? T : unknown);
+
 export type { Plain as ZuordPlain };
+
+export type { PlainOf as ZuordPlainOf };
