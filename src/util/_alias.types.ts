4b86746e290e2ae29b9c00cf6cb6e409842af4fc
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 00:37:37 +0300

    Add HasToEvery type definition to _alias.types.ts for array type handling

--- src/util/_alias.types.ts
+++ src/util/_alias.types.ts
@@ -1,7 +1,7 @@
 import { ZuordType } from "@/type/_alias.types";
 import type { ZuordEq, ZuordEqAny, ZuordEqEvery, ZuordEqToAny, ZuordEqAnyToAny, ZuordEqEveryToAny } from "./eq.types";
 import type { ZuordIs, ZuordIsAny, ZuordIsEvery, ZuordIsToAny, ZuordIsToEvery, ZuordIsAnyToAny, ZuordIsAnyToEvery, ZuordIsEveryToAny, ZuordIsEveryToEvery } from "./is.types";
-import type { ZuordHas, ZuordHasAny, ZuordHasEvery, ZuordHasToAny } from "./has.types";
+import type { ZuordHas, ZuordHasAny, ZuordHasEvery, ZuordHasToAny, ZuordHasToEvery } from "./has.types";
 import { ZuordValueAt } from "./value.types";
 import { ZuordUnionOf } from "./union.types";
 import { ZuordInstanceOf, ZuordInstanceTuple } from "./instance.types";
@@ -55,6 +55,8 @@ export namespace ZuordUtil {
 
     export type HasToAny<TSource, TBases extends ZuordType.Array> = ZuordHasToAny<TSource, TBases>;
 
+    export type HasToEvery<TSource, TBases extends ZuordType.Array> = ZuordHasToEvery<TSource, TBases>;
+    
     
     // VALUE
 
