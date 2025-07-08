3bda0710727911eaa4cb5ed112a9b7ef4e8d2c45
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 19:40:57 +0300

    Refactor EqAny type utility for improved clarity and structure in eq.types.ts

--- src/util/eq.types.ts
+++ src/util/eq.types.ts
@@ -1,3 +1,11 @@
+import { ZuordType } from "@/type/_alias.types";
+
 type Eq<T1, T2> = (<T>() => T extends T1 ? 0 : 1) extends (<T>() => T extends T2 ? 0 : 1) ? true : false;
 
+type EqAny<U1 extends ZuordType.Array, T2> = U1 extends [infer T1, ...infer R1] ? (
+    [Eq<T1, T2>] extends [true] ? true : EqAny<R1, T2>
+) : false;
+
 export type { Eq as ZuordEq };
+
+export type { EqAny as ZuordEqAny };
