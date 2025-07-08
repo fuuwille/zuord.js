3fa28d9bcee0d3001f6b8534012a16414f9f0770
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 17:33:14 +0300

    Fix variable naming consistency in IsAny and IsEvery type utilities

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -2,12 +2,12 @@ import { ZuordType } from "@/type/_alias.types";
 
 type Is<TSource, TBase> = [TSource] extends [TBase] ? true : false;
 
-type IsAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer Rest] ? (
-    Is<TCurrent, TBase> extends true ? true : IsAny<Rest, TBase>
+type IsAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer TRest] ? (
+    Is<TCurrent, TBase> extends true ? true : IsAny<TRest, TBase>
 ) : false;
 
-type IsEvery<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer Rest] ? (
-    [Is<TCurrent, TBase>] extends [true] ? (Rest extends readonly [infer __f, ...infer __r] ? IsEvery<Rest, TBase> : true) : false
+type IsEvery<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer TRest] ? (
+    [Is<TCurrent, TBase>] extends [true] ? (TRest extends readonly [infer __f, ...infer __r] ? IsEvery<TRest, TBase> : true) : false
 ) : false;
 
 export type { Is as ZuordIs };
