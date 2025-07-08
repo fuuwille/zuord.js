e3a1c41e28f3a767b31868a9209cb8d01e8844ca
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 17:26:45 +0300

    Add IsAny type utility for enhanced type checking in is.types.ts

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -1,3 +1,12 @@
+import { ZuordType } from "@/type/_alias.types";
+
 type Is<T1, T2> = [T1] extends [T2] ? true : false;
 
+type IsAny<U1 extends ZuordType.Array, T2> = U1 extends [infer First, ...infer Rest] ? (
+    Is<First, T2> extends true ? true : IsAny<Rest, T2>
+) : false;
+
+
 export type { Is as ZuordIs };
+
+export type { IsAny as ZuordIsAny };
