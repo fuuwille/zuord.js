35caa313a504e006df029b5d6b43c8e9f0bd37b1
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 22:49:16 +0300

    Add EqEvery type utility for checking equality of all elements in an array

--- src/util/eq.types.ts
+++ src/util/eq.types.ts
@@ -6,6 +6,10 @@ type EqAny<U1 extends ZuordType.Array, T2> = U1 extends [infer T1, ...infer R1] 
     [Eq<T1, T2>] extends [true] ? true : EqAny<R1, T2>
 ) : false;
 
+type EqEvery<U1 extends ZuordType.Array, T2> = U1 extends [infer T1, ...infer R1] ? (
+    [Eq<T1, T2>] extends [true] ? EqEvery<R1, T2> : false
+) : true;
+
 type EqToAny<T1, U2 extends ZuordType.Array> = U2 extends [infer T2, ...infer R2] ? (
     [Eq<T1, T2>] extends [true] ? true : EqToAny<T1, R2>
 ) : false;
@@ -13,5 +17,7 @@ type EqToAny<T1, U2 extends ZuordType.Array> = U2 extends [infer T2, ...infer R2
 export type { Eq as ZuordEq };
 
 export type { EqAny as ZuordEqAny };
+
+export type { EqEvery as ZuordEqEvery };
 
 export type { EqToAny as ZuordEqToAny };
