1e2b81f48cd9b22196e25d94ed00a3857ae59180
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 16:39:40 +0300

    Refactor dummy types to include DummyOf type for enhanced type handling

--- src/type/dummy.types.ts
+++ src/type/dummy.types.ts
@@ -1,3 +1,9 @@
+import { ZuordType } from "./_alias.types";
+
 type Dummy = { __brand: 'dummy' };
 
+type DummyOf<T extends ZuordType.Plain> = T & Dummy;
+
 export type { Dummy as ZuordDummy };
+
+export type { DummyOf as ZuordDummyOf };
