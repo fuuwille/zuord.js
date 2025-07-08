d899ff8369420e7dc07e70ec34f2e2adedbb495e
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 01:27:14 +0300

    Fix Exclude type definition to use ZuordUtil.Is for type exclusion

--- src/util/exclude.types.ts
+++ src/util/exclude.types.ts
@@ -3,6 +3,13 @@ import { ZuordUtil } from "./_alias.types";
 
 // EXCLUDE
 
-type Exclude<TSource, TBase> = TSource extends any ? ([ZuordUtil.Eq<TSource, TBase>] extends [false] ? TSource : never) : never;
+type Exclude<TSource, TBase> = TSource extends any ? ([ZuordUtil.Is<TSource, TBase>] extends [false] ? TSource : never) : never;
 
 export type { Exclude as ZuordExclude };
+
+
+// EXCLUDE EQ
+
+type ExcludeEq<TSource, TBase> = TSource extends any ? ([ZuordUtil.Eq<TSource, TBase>] extends [false] ? TSource : never) : never;
+
+export type { ExcludeEq as ZuordExcludeEq };
