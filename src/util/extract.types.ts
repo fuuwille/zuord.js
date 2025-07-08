27d89960695818bbb8fec09780a8220cf11f005e
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 01:30:53 +0300

    Add ExtractEq type for equality extraction in ZuordUtil

--- src/util/extract.types.ts
+++ src/util/extract.types.ts
@@ -6,3 +6,10 @@ import { ZuordUtil } from "./_alias.types";
 type Extract<TSource, TBase> = TSource extends any ? ([ZuordUtil.Is<TSource, TBase>] extends [true] ? TSource : never) : never;
 
 export type { Extract as ZuordExtract };
+
+
+// EXTRACT EQ
+
+type ExtractEq<TSource, TBase> = TSource extends any ? ([ZuordUtil.Eq<TSource, TBase>] extends [true] ? TSource : never) : never;
+
+export type { ExtractEq as ZuordExtractEq };
