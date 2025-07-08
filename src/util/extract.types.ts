de3cc756c409fd44d7c73a1b4e2ea86f3e96edfb
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 01:29:34 +0300

    Refactor Extract type to use ZuordUtil.Is for type checks instead of ZuordUtil.Eq

--- src/util/extract.types.ts
+++ src/util/extract.types.ts
@@ -3,6 +3,6 @@ import { ZuordUtil } from "./_alias.types";
 
 // EXTRACT
 
-type Extract<TSource, TBase> = TSource extends any ? ([ZuordUtil.Eq<TSource, TBase>] extends [true] ? TSource : never) : never;
+type Extract<TSource, TBase> = TSource extends any ? ([ZuordUtil.Is<TSource, TBase>] extends [true] ? TSource : never) : never;
 
 export type { Extract as ZuordExtract };
