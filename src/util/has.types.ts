0299d7f35fe8082ae53606a90ead3cdba603b54a
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 00:29:45 +0300

    Add HasAny type definition to has.types.ts for array type handling

--- src/util/has.types.ts
+++ src/util/has.types.ts
@@ -1,3 +1,4 @@
+import { ZuordType } from "@/type/_alias.types";
 import { ZuordUtil } from "./_alias.types";
 
 
@@ -6,3 +7,12 @@ import { ZuordUtil } from "./_alias.types";
 type Has<TSource, TBase> = true extends (TSource extends any ? ZuordUtil.Is<TSource, TBase> : never) ? true : false;
 
 export type { Has as ZuordHas };
+
+
+// HAS ANY
+
+type HasAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
+    [Has<TSource, TBase>] extends [true] ? true : HasAny<TRestSources, TBase>
+) : false;
+
+export type { HasAny as ZuordHasAny };
