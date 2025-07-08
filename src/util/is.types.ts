8059c5d8bb3b865dcf1bcc0a5dbcd972c26a98b2
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 23:38:26 +0300

    Format Is type definition for improved readability

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -3,7 +3,9 @@ import { ZuordType } from "@/type/_alias.types";
 
 // IS
 
-type Is<TSource, TBase> = [TSource] extends [TBase] ? ([TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true) : false;
+type Is<TSource, TBase> = [TSource] extends [TBase] ? (
+    [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
+) : false;
 
 export type { Is as ZuordIs };
 
