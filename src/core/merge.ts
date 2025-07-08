e02ecbaa18d7dbd660a4a9dd94c483eeeef96261
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 01:41:53 +0300

    Refactor merge functions to use zuordType for object validation

--- src/core/merge.ts
+++ src/core/merge.ts
@@ -1,11 +1,12 @@
 import { Zuord } from "@/core/alias.types"
-import { zuordUtil } from "@/trait/_alias";
+import { zuordType } from "@/type/_alias";
 
 function merge<U extends object[]>(...content: U) : Zuord.Merge<U> {
     return mergeBy({
         content
     }) as Zuord.Merge<U>;
 }
+
 function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[] = Zuord.DefaultOutcastConstructors, const M extends Partial<Zuord.MergeMode> = Zuord.MergeDefaultMode>(data : Zuord.DataOf<U, C, M> ): Zuord.Merge<U, Zuord.OptionsOf<typeof data>> {
     if (data.content.length === 0) {
         // If no content is provided, return an empty object
@@ -15,7 +16,7 @@ function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[] 
     const result: Record<string, unknown> = {};
     
     for (const obj of data.content) {
-        if (!zuordUtil.isObject(obj)) continue;
+        if (!zuordType.isObject(obj)) continue;
 
         // Iterate over each key-value pair in the object
         for (const [key, value] of Object.entries(obj)) {
@@ -25,7 +26,7 @@ function mergeBy<U extends object[], const C extends Zuord.OutcastConstructor[] 
             if (Array.isArray(value) && Array.isArray(existing)/* && mode?.includes("concat" as any)*/) {
                 // Combine arrays
                 result[key] = [...existing, ...value];
-            } else if (zuordUtil.isObject(value) && zuordUtil.isObject(existing)) {
+            } else if (zuordType.isObject(value) && zuordType.isObject(existing)) {
                 // Recursively merge objects
                 result[key] = mergeBy({
                     content: [existing as object, value as object],
