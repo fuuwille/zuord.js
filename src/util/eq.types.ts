59ca4d0d7d4354bf9396f39b7390040797fd0596
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 19:38:04 +0300

    Fix type parameter names in Eq type utility for clarity in eq.types.ts

--- src/util/eq.types.ts
+++ src/util/eq.types.ts
@@ -1,3 +1,3 @@
-type Eq<A, B> = (<T>() => T extends A ? 0 : 1) extends (<T>() => T extends B ? 0 : 1) ? true : false;
+type Eq<T1, T2> = (<T>() => T extends T1 ? 0 : 1) extends (<T>() => T extends T2 ? 0 : 1) ? true : false;
 
 export type { Eq as ZuordEq };
