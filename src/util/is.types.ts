25364142d4c2a82fdb8275663494b3c99deed8e6
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 23:34:43 +0300

    Refine Is type definition for improved type checking logic

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -3,7 +3,7 @@ import { ZuordType } from "@/type/_alias.types";
 
 // IS
 
-type Is<TSource, TBase> = [TSource] extends [TBase] ? true : false;
+type Is<TSource, TBase> = [TSource] extends [TBase] ? ([TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true) : false;
 
 export type { Is as ZuordIs };
 
