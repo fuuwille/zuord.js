a166b68f5f4ba69dc4ba03f0f3980a09301703d3
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 23:08:12 +0300

    Refactor is.types.ts to streamline type exports and enhance readability

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -1,56 +1,78 @@
 import { ZuordType } from "@/type/_alias.types";
 
+
+// IS
+
 type Is<TSource, TBase> = [TSource] extends [TBase] ? true : false;
+
+export type { Is as ZuordIs };
+
+// IS ANY
 
 type IsAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
     [Is<TSource, TBase>] extends [true] ? true : IsAny<TRestSources, TBase>
 ) : false;
 
+export type { IsAny as ZuordIsAny };
+
+// IS EVERY
+
 type IsEvery<TSources extends ZuordType.Array, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
     [Is<TSource, TBase>] extends [true] ? (TRestSources extends readonly [infer __f, ...infer __r] ? IsEvery<TRestSources, TBase> : true) : false
 ) : false;
+
+export type { IsEvery as ZuordIsEvery };
+
+// IS TO ANY
 
 type IsToAny<TSource, TBases extends ZuordType.Array> = TBases extends [infer TBase, ...infer TRestBases] ? (
     [Is<TSource, TBase>] extends [true] ? true : IsToAny<TSource, TRestBases>
 ) : false;
 
+export type { IsToAny as ZuordIsToAny };
+
+// IS TO EVERY
+
 type IsToEvery<TSource, TBases extends ZuordType.Array> = TBases extends [infer TBase, ...infer TRestBases] ? (
     [Is<TSource, TBase>] extends [true] ? (TRestBases extends readonly [infer __f, ...infer __r] ? IsToEvery<TSource, TRestBases> : true) : false
 ) : false;
+
+export type { IsToEvery as ZuordIsToEvery };
+
+// IS ANY TO ANY
 
 type IsAnyToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRestSources] ? (
     IsToAny<TSource, TBases> extends true ? true : IsAnyToAny<TRestSources, TBases>
 ) : false;
 
+export type { IsAnyToAny as ZuordIsAnyToAny };
+
+// IS ANY TO EVERY
+
 type IsAnyToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRestSources] ? (
     IsToEvery<TSource, TBases> extends true ? true : IsAnyToEvery<TRestSources, TBases>
 ) : false;
+
+export type { IsAnyToEvery as ZuordIsAnyToEvery };
+
+// IS EVERY TO ANY
 
 type IsEveryToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRestSources] ? (
     IsToAny<TSource, TBases> extends true ? (TRestSources extends readonly [infer _f, ...infer _r] ? IsEveryToAny<TRestSources, TBases> : true) : false
 ) : false;
 
+export type { IsEveryToAny as ZuordIsEveryToAny };
+
+// IS EVERY TO EVERY
+
 type IsEveryToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRestSources] ? (
     IsToEvery<TSource, TBases> extends true ? (TRestSources extends readonly [infer _f, ...infer _r] ? IsEveryToEvery<TRestSources, TBases> : true) : false
 ) : false;
 
-export type { Is as ZuordIs };
-
-export type { IsAny as ZuordIsAny };
-
-export type { IsEvery as ZuordIsEvery };
-
-export type { IsToAny as ZuordIsToAny };
-
-export type { IsToEvery as ZuordIsToEvery };
-
-export type { IsAnyToAny as ZuordIsAnyToAny };
-
-export type { IsAnyToEvery as ZuordIsAnyToEvery };
-
-export type { IsEveryToAny as ZuordIsEveryToAny };
-
 export type { IsEveryToEvery as ZuordIsEveryToEvery };
+
+
+//
 
 
 type Test1 = IsAny<[1, 2, 3], 3>;   // true
