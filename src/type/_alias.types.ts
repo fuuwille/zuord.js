75473ecac8fa5ad66eb0a0f9bd37c5fe8047abf2
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 22:48:22 +0300

    Add ZuordEmptyArray type alias to ZuordType namespace in _alias.types.ts

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -2,7 +2,7 @@ import type { ZuordDummy, ZuordDummyOf, ZuordExDummy } from "./dummy.types";
 import type { ZuordAbsence } from "./absence.types";
 import type { ZuordFunction } from "./function.types";
 import type { ZuordTuple } from "./tuple.types";
-import type { ZuordArray } from "./array.types";
+import type { ZuordArray, ZuordEmptyArray } from "./array.types";
 import type { ZuordPlain, ZuordPlainOf } from "./plain.types";
 import type { ZuordObject } from "./object.types";
 
@@ -21,6 +21,8 @@ export namespace ZuordType {
     export type Tuple = ZuordTuple;
 
     export type Array = ZuordArray;
+
+    export type EmptyArray = ZuordEmptyArray;
 
     export type Plain = ZuordPlain;
 
