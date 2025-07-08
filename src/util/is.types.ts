9433da8eab71d2bd6fb142e78be633db4bcd2c9b
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 19:30:47 +0300

    Refactor type utility definitions for consistency in is.types.ts

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -2,36 +2,36 @@ import { ZuordType } from "@/type/_alias.types";
 
 type Is<TSource, TBase> = [TSource] extends [TBase] ? true : false;
 
-type IsAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    [Is<TCurrent, TBase>] extends [true] ? true : IsAny<TRest, TBase>
+type IsAny<TSources extends ZuordType.Array, TBase> = TSources extends [infer TSource, ...infer TRest] ? (
+    [Is<TSource, TBase>] extends [true] ? true : IsAny<TRest, TBase>
 ) : false;
 
-type IsEvery<TSources extends ZuordType.Array, TBase> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    [Is<TCurrent, TBase>] extends [true] ? (TRest extends readonly [infer __f, ...infer __r] ? IsEvery<TRest, TBase> : true) : false
+type IsEvery<TSources extends ZuordType.Array, TBase> = TSources extends [infer TSource, ...infer TRest] ? (
+    [Is<TSource, TBase>] extends [true] ? (TRest extends readonly [infer __f, ...infer __r] ? IsEvery<TRest, TBase> : true) : false
 ) : false;
 
-type IsToAny<TSource, TBases extends ZuordType.Array> = TBases extends [infer TCurrent, ...infer TRest] ? (
-    [Is<TSource, TCurrent>] extends [true] ? true : IsToAny<TSource, TRest>
+type IsToAny<TSource, TBases extends ZuordType.Array> = TBases extends [infer TBase, ...infer TRest] ? (
+    [Is<TSource, TBase>] extends [true] ? true : IsToAny<TSource, TRest>
 ) : false;
 
-type IsToEvery<TSource, TBases extends ZuordType.Array> = TBases extends [infer TCurrent, ...infer TRest] ? (
-    [Is<TSource, TCurrent>] extends [true] ? (TRest extends readonly [infer __f, ...infer __r] ? IsToEvery<TSource, TRest> : true) : false
+type IsToEvery<TSource, TBases extends ZuordType.Array> = TBases extends [infer TBase, ...infer TRest] ? (
+    [Is<TSource, TBase>] extends [true] ? (TRest extends readonly [infer __f, ...infer __r] ? IsToEvery<TSource, TRest> : true) : false
 ) : false;
 
-type IsAnyToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    IsToAny<TCurrent, TBases> extends true ? true : IsAnyToAny<TRest, TBases>
+type IsAnyToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRest] ? (
+    IsToAny<TSource, TBases> extends true ? true : IsAnyToAny<TRest, TBases>
 ) : false;
 
-type IsAnyToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    IsToEvery<TCurrent, TBases> extends true ? true : IsAnyToEvery<TRest, TBases>
+type IsAnyToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRest] ? (
+    IsToEvery<TSource, TBases> extends true ? true : IsAnyToEvery<TRest, TBases>
 ) : false;
 
-type IsEveryToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    IsToAny<TCurrent, TBases> extends true ? (TRest extends readonly [infer _f, ...infer _r] ? IsEveryToAny<TRest, TBases> : true) : false
+type IsEveryToAny<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRest] ? (
+    IsToAny<TSource, TBases> extends true ? (TRest extends readonly [infer _f, ...infer _r] ? IsEveryToAny<TRest, TBases> : true) : false
 ) : false;
 
-type IsEveryToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TCurrent, ...infer TRest] ? (
-    IsToEvery<TCurrent, TBases> extends true ? (TRest extends readonly [infer _f, ...infer _r] ? IsEveryToEvery<TRest, TBases> : true) : false
+type IsEveryToEvery<TSources extends ZuordType.Array, TBases extends ZuordType.Array> = TSources extends [infer TSource, ...infer TRest] ? (
+    IsToEvery<TSource, TBases> extends true ? (TRest extends readonly [infer _f, ...infer _r] ? IsEveryToEvery<TRest, TBases> : true) : false
 ) : false;
 
 export type { Is as ZuordIs };
