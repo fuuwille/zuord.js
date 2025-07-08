61025e0870b47a1e8c2bf18368460df274917e07
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 17:51:38 +0300

    Add test cases for type utilities in is.types.ts

--- src/util/is.types.ts
+++ src/util/is.types.ts
@@ -51,3 +51,31 @@ export type { IsAnyToEvery as ZuordIsAnyToEvery };
 export type { IsEveryToAny as ZuordIsEveryToAny };
 
 export type { IsEveryToEvery as ZuordIsEveryToEvery };
+
+
+type Test1 = IsAny<[1, 2, 3], 3>;   // true
+type Test2 = IsAny<[4, 5, 6], 1>;   // false
+type Test3 = IsAny<[], 4>;          // false (boş tuple)
+
+type Test4 = IsEvery<[1, 2, 3], 3>; // false
+type Test5 = IsEvery<[3, 3, 3], 3>; // true
+type Test6 = IsEvery<[], 3>;        // false (boş tuple)
+
+type Test7 = IsToAny<1, [1, 2, 3]>; // true
+type Test8 = IsToAny<4, [1, 2, 3]>; // false
+type Test9 = IsToAny<1, []>;        // false (boş tuple)
+
+type Test10 = IsToEvery<{ hello: string, world: string }, [{ hello: string }, { world: string} ]>; // true
+type Test11 = IsToEvery<{ hello: string, world: string }, [{ hello: string }, { world: number} ]>; // false
+type Test12 = IsToEvery<{ hello: string, world: string }, []>; // false (boş tuple)
+
+type Test13 = IsAnyToAny<[1, 2, 3], [1, 2, 3]>; // true
+type Test14 = IsAnyToAny<[1, 2, 3], [2, 3, 4]>; // true
+type Test15 = IsAnyToAny<[1, 2, 3], [4, 5, 6]>; // false
+type Test16 = IsAnyToAny<[], [1, 2, 3]>;        // false (boş tuple)
+type Test17 = IsAnyToAny<[1, 2, 3], []>;        // false (boş tuple)
+type Test18 = IsAnyToAny<[], []>;               // false (boş tuple)
+
+type Test19 = IsAnyToEvery<[{ hello: string }, { world: string}, { hello: string, world: string}], [{ hello: string }, { world: string} ]>;
+type Test20 = IsAnyToEvery<[{ hello: string }, { world: number}, { hello: string, world: string}], [{ hello: string }, { world: number} ]>;
+type Test21 = IsAnyToEvery<[], [{ hello: string }, { world: string} ]>; // false (boş tuple)
