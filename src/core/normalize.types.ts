3317c7f2c6d5b7974ab6a2c16e51643e19db2219
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 02:37:58 +0300

    Refactor Normalize type to replace 'unknown' with 'never' for improved type safety

--- src/core/normalize.types.ts
+++ src/core/normalize.types.ts
@@ -4,17 +4,17 @@ import { ZuordUtil } from "@/util/_alias.types";
 
 type Normalize<T, Options extends NormalizeOptions = NormalizeDefaultOptions> = [ZuordUtil.Eq<T, any>] extends [false] ? (
     [ZuordUtil.Has<T, ZuordType.Tuple>] extends [true] ? (
-        (ZuordUtil.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, Options> : unknown) |
+        (ZuordUtil.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, Options> : never) |
         (ZuordUtil.Extract<T, ZuordType.Tuple> extends infer TExtracted ? ({  
             [K in keyof TExtracted]: Normalize<TExtracted[K], Options> 
         }) : never )
     ) :
     [ZuordUtil.Has<T, ZuordType.Array>] extends [true] ? (
-        (ZuordUtil.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, Options> : unknown) |
+        (ZuordUtil.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, Options> : never) |
         (ZuordUtil.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, readonly unknown[]>[number], Options>[] : never)
     ) :     
     [ZuordUtil.Has<T, ZuordType.Plain>] extends [true] ? (
-        (ZuordUtil.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, Options> : unknown) | 
+        (ZuordUtil.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, Options> : never) | 
         (ZuordUtil.Extract<T, ZuordType.Plain> extends infer TExtracted ? ({
             [K in keyof TExtracted]: Normalize<TExtracted[K], Options>;
         }) : never )
