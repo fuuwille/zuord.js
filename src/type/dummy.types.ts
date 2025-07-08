88a15709a63b0677153c583d9d9f5f08f8b86431
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 16:42:28 +0300

    Add ExDummy type definition to enhance type handling in dummy.types.ts

--- src/type/dummy.types.ts
+++ src/type/dummy.types.ts
@@ -4,6 +4,10 @@ type Dummy = { __brand: 'dummy' };
 
 type DummyOf<T extends ZuordType.Plain> = T & Dummy;
 
+type ExDummy = DummyOf<{ ex: true }>;
+
 export type { Dummy as ZuordDummy };
 
 export type { DummyOf as ZuordDummyOf };
+
+export type { ExDummy as ZuordExDummy };
