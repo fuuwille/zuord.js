c2ef7691de0b87385cc61206b236b3fa1a48fa11
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 22:19:48 +0300

    Add EqToAny type utility for type equality checking in eq.types.ts

--- src/util/eq.types.ts
+++ src/util/eq.types.ts
@@ -6,6 +6,12 @@ type EqAny<U1 extends ZuordType.Array, T2> = U1 extends [infer T1, ...infer R1] 
     [Eq<T1, T2>] extends [true] ? true : EqAny<R1, T2>
 ) : false;
 
+type EqToAny<T1, U2 extends ZuordType.Array> = U2 extends [infer T2, ...infer R2] ? (
+    [Eq<T1, T2>] extends [true] ? true : EqToAny<T1, R2>
+) : false;
+
 export type { Eq as ZuordEq };
 
 export type { EqAny as ZuordEqAny };
+
+export type { EqToAny as ZuordEqToAny };
