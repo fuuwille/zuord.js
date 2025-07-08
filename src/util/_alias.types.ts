d2e500f309e14c1212d307aed5af471c30f5f8c0
k4yr2 <kyrzkaya@gmail.com>
2025-07-08 17:54:41 +0300

    Remove unused key type utilities from _alias.types.ts and delete key.types.ts

--- src/util/_alias.types.ts
+++ src/util/_alias.types.ts
@@ -1,29 +1,9 @@
-import { ZuordIsKey, ZuordIsRequiredKey, ZuordHasKey, ZuordAnyHasKey, ZuordAllHasKey, ZuordKeysOf, ZuordRequiredKeysOf, ZuordOptionalKeysOf } from "./key.types";
 import { ZuordValueAt } from "./value.types";
 import { ZuordUnionOf } from "./union.types";
 import { ZuordInstanceOf, ZuordInstanceTuple } from "./instance.types";
 import { ZuordArrayDepth, ZuordArrayIn } from "./array.types";
 
 export namespace ZuordUtil {
-
-    // KEY
-
-    export type IsKey<T, K> = ZuordIsKey<T, K>;
-
-    export type IsRequiredKey<T, K extends PropertyKey> = ZuordIsRequiredKey<T, K>;
-    
-    export type HasKey<T, K> = ZuordHasKey<T, K>;
-
-    export type AnyHasKey<U extends readonly unknown[], K> = ZuordAnyHasKey<U, K>;
-
-    export type AllHasKey<U extends readonly unknown[], K> = ZuordAllHasKey<U, K>;
-
-    export type KeysOf<U> = ZuordKeysOf<U>;
-
-    export type RequiredKeysOf<T> = ZuordRequiredKeysOf<T>;
-
-    export type OptionalKeysOf<T> = ZuordOptionalKeysOf<T>;
-
     
     // VALUE
 
@@ -40,7 +20,7 @@ export namespace ZuordUtil {
     
     export type InstanceTuple<T> = ZuordInstanceTuple<T>;
 
-    
+
     // ARRAY
 
     export type ArrayIn<T> = ZuordArrayIn<T>;
