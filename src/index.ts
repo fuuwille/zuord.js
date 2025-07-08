b5cb079981d9668cb779b9c185a054ffcb015070
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 01:26:09 +0300

    Refactor type exports in index.ts to correctly export ZuordUtil

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { zuord } from "./core/alias";
 import { zuordUtil } from "./trait/_alias";
 import { Zuord } from "./core/alias.types";
-import { ZuordTrait } from "./trait/_alias.types";
+import { ZuordUtil } from "./util/_alias.types";
 
 export { zuord };
 export { zuordUtil };
 export type { Zuord };
-export type { ZuordTrait as ZuordUtil };
+export type { ZuordUtil };
