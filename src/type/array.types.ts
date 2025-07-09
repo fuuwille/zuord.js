0cfe35ca778f167c3d3a72d7a6d4fb448c8d9c50
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:12:55 +0300

    Reorganize type definitions in array.types.ts for improved readability

--- src/type/array.types.ts
+++ src/type/array.types.ts
@@ -1,12 +1,20 @@
+
+// ARRAY
+
 type Array = readonly unknown[];
-
-type ArrayInfer<T> = T extends readonly (infer TInfer)[] ? TInfer : never;
-
-type EmptyArray = readonly [];
-
 
 export type { Array as ZuordArray };
 
+
+// ARRAY INFER
+
+type ArrayInfer<T> = T extends readonly (infer TInfer)[] ? TInfer : never;
+
 export type { ArrayInfer as ZuordArrayInfer };
+
+
+// EMPTY ARRAY
+
+type EmptyArray = readonly [];
 
 export type { EmptyArray as ZuordEmptyArray };
