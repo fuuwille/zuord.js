f0f60b415210bc536c5a494c359228e7684141ac
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:12:14 +0300

    Add ArrayInfer type to array.types.ts for inferring element types from readonly arrays

--- src/type/array.types.ts
+++ src/type/array.types.ts
@@ -1,7 +1,12 @@
 type Array = readonly unknown[];
 
+type ArrayInfer<T> = T extends readonly (infer TInfer)[] ? TInfer : never;
+
 type EmptyArray = readonly [];
 
+
 export type { Array as ZuordArray };
+
+export type { ArrayInfer as ZuordArrayInfer };
 
 export type { EmptyArray as ZuordEmptyArray };
