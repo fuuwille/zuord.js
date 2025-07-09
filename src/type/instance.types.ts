6cadc5d67df392df4c4e95c58f16dcb6f511d0c3
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:47:43 +0300

    Add InstanceOf and InstanceTuple type definitions for improved type safety

---
+++ src/type/instance.types.ts
@@ -0,0 +1,9 @@
+type InstanceOf<T> = T extends new (...args: any[]) => infer R ? R : never;
+
+type InstanceTuple<T> = {
+    [K in keyof T]: InstanceOf<T[K]>
+};
+
+export type { InstanceOf as ZuordInstanceOf };
+
+export type { InstanceTuple as ZuordInstanceTuple };
