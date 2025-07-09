d379dd78d3e48f0627f2fe249cf3f8e435ae1faf
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:15:14 +0300

    Add ArrayDepth type to array.types.ts for calculating nested array depth

--- src/type/array.types.ts
+++ src/type/array.types.ts
@@ -13,6 +13,15 @@ type ArrayInfer<T> = T extends readonly (infer TInfer)[] ? TInfer : never;
 export type { ArrayInfer as ZuordArrayInfer };
 
 
+// ARRAY DEPTH
+
+type ArrayDepth<T, D extends unknown[] = []> = T extends readonly (infer U)[] ? (
+    ArrayDepth<U, [unknown, ...D]> 
+) : D['length'];
+
+export type { ArrayDepth as ZuordArrayDepth };
+
+
 // EMPTY ARRAY
 
 type EmptyArray = readonly [];
