c1eeedd926354801e12ab29d1d07538ee0af5590
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:20:35 +0300

    Add ZuordArrayDepth type alias to ZuordType namespace in _alias.types.ts

--- src/type/_alias.types.ts
+++ src/type/_alias.types.ts
@@ -1,7 +1,7 @@
 import type { ZuordAbsence } from "./absence.types";
 import type { ZuordFunction } from "./function.types";
 import type { ZuordTuple } from "./tuple.types";
-import type { ZuordArray, ZuordArrayInfer, ZuordEmptyArray } from "./array.types";
+import type { ZuordArray, ZuordArrayInfer, ZuordArrayDepth, ZuordEmptyArray } from "./array.types";
 import type { ZuordPlain, ZuordPlainOf } from "./plain.types";
 import type { ZuordObject } from "./object.types";
 
@@ -16,6 +16,8 @@ export namespace ZuordType {
     export type Array = ZuordArray;
 
     export type ArrayInfer<T> = ZuordArrayInfer<T>;
+
+    export type ArrayDepth<T> = ZuordArrayDepth<T>;
 
     export type EmptyArray = ZuordEmptyArray;
 
