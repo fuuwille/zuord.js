4041badf03f3f8de54d20142ef241ffe68f88975
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:42:29 +0300

    Format Exclude type definition for improved readability

--- src/util/exclude.types.ts
+++ src/util/exclude.types.ts
@@ -4,7 +4,9 @@ import { ZuordUtil } from "./_alias.types";
 
 // EXCLUDE
 
-type Exclude<TSource, TBase> = TSource extends any ? ([ZuordUtil.Is<TSource, TBase>] extends [false] ? TSource : never) : never;
+type Exclude<TSource, TBase> = TSource extends any ? (
+    [ZuordUtil.Is<TSource, TBase>] extends [false] ? TSource : never
+) : never;
 
 export type { Exclude as ZuordExclude };
 
