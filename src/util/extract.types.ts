4fe8f6160279ae4ee6fcfdacee13957ee13e3b81
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 20:42:51 +0300

    Refactor Extract type definition for improved readability

--- src/util/extract.types.ts
+++ src/util/extract.types.ts
@@ -4,7 +4,9 @@ import { ZuordUtil } from "./_alias.types";
 
 // EXTRACT
 
-type Extract<TSource, TBase> = TSource extends any ? ([ZuordUtil.Is<TSource, TBase>] extends [true] ? TSource : never) : never;
+type Extract<TSource, TBase> = TSource extends any ? (
+    [ZuordUtil.Is<TSource, TBase>] extends [true] ? TSource : never
+) : never;
 
 export type { Extract as ZuordExtract };
 
