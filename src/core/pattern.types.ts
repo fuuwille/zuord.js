437ba07525b51c2b04e5e8e92056948f57face78
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 11:58:39 +0300

    Refactor PatternRaw type to use ZuordType.Plain for object checks

--- src/core/pattern.types.ts
+++ src/core/pattern.types.ts
@@ -1,11 +1,12 @@
 import { Zuord } from "@/core/alias.types"
+import { ZuordType } from "@/type/_alias.types";
 
 type Pattern<T> = Zuord.Normalize<PatternRaw<T>>
 
 type PatternRaw<T> = true | {
     [K in keyof T]?: T[K] extends Array<unknown>
         ? true
-        : T[K] extends object
+        : T[K] extends ZuordType.Plain
             ? PatternRaw<T[K]>
             : T[K] extends undefined
                 ? never
