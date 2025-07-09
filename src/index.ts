47ef9a7069274c4d7b778e6bb5def96e3060eeb4
k4yr2 <kyrzkaya@gmail.com>
2025-07-09 13:58:16 +0300

    Add exports for zuordType and ZuordType in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { zuord } from "./core/alias";
 import { Zuord } from "./core/alias.types";
+import { zuordType } from "./type/_alias";
+import { ZuordType } from "./type/_alias.types";
 import { ZuordUtil } from "./util/_alias.types";
 
 export { zuord };
 export type { Zuord };
+export { zuordType };
+export type { ZuordType };
 export type { ZuordUtil };
