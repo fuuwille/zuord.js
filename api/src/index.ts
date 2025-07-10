996572580f3da519dd5a8870f65391ce81e247f8
k4yr2 <kyrzkaya@gmail.com>
2025-07-10 05:50:03 +0300

    fix: remove unused exports for zuordType and ZuordType in index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,8 @@
 import { zuord } from "./alias";
 export { zuord };
 
-import { zuordType } from "./type/_alias";
-export { zuordType };
-
 import { Zuord } from "./alias.types";
 export type { Zuord };
-
-import { ZuordType } from "./type/_alias.types";
-export type { ZuordType };
 
 import { ZuordUtil } from "./util/_alias.types";
 export type { ZuordUtil };
