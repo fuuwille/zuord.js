5160bb7fc4953e0a76fab4ba7ae20db24f541df3
k4yr2 <kyrzkaya@gmail.com>
2025-07-10 15:47:26 +0300

    fix: update tsconfig path in rollup configuration to tsconfig.dist.json

--- util/rollup.config.mjs
+++ util/rollup.config.mjs
@@ -2,5 +2,5 @@ import { createConfig } from '../rollup.config.mjs';
 
 export default createConfig({
     input: 'src/index.ts',
-    tsconfig: './tsconfig.json'
+    tsconfig: './tsconfig.dist.json'
 });
