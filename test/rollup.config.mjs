18315f1534f6112f5628e0063f5f0cc99f48d420
k4yr2 <kyrzkaya@gmail.com>
2025-07-10 18:22:28 +0300

    feat: update package.json, add npmignore, and create rollup config for testing

---
+++ test/rollup.config.mjs
@@ -0,0 +1,7 @@
+import { createConfig } from '../rollup.config.mjs';
+
+export default createConfig({
+    input: 'src/index.ts',
+    tsconfig: './tsconfig.dist.json',
+    external: ['@zuord/type']
+});
