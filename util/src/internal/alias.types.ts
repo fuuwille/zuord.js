50581af95e31b17af740ec009c02eef20ca1689d
k4yr2 <kyrzkaya@gmail.com>
2025-07-11 09:05:34 +0300

    feat: add type aliases for equality checks in InternalZuordUtil

--- util/src/internal/alias.types.ts
+++ util/src/internal/alias.types.ts
@@ -1,3 +1,10 @@
+import { ZuordEq, ZuordEqAny, ZuordEqEvery } from "./eq.types";
+
 export namespace InternalZuordUtil {
     
+    export type Eq<T1, T2> = ZuordEq<T1, T2>;
+
+    export type EqAny<U1, T2> = ZuordEqAny<U1, T2>;
+
+    export type EqEvery<U1, T2> = ZuordEqEvery<U1, T2>;
 }
