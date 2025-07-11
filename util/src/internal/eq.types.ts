68e40e2962df9a984d5444b5fc587097a2bef10a
k4yr2 <kyrzkaya@gmail.com>
2025-07-11 09:00:20 +0300

    feat: define Eq type for strict equality checks

--- util/src/internal/eq.types.ts
+++ util/src/internal/eq.types.ts
@@ -1,4 +1,11 @@
 
+// EQ
+
+type Eq<T1, T2> = (<T>() => T extends T1 ? 0 : 1) extends (<T>() => T extends T2 ? 0 : 1) ? true : false;
+
+export type { Eq as ZuordEq };
+
+
 // EQ ANY
 
 type EqAny<U1, T2> = U1 extends [infer T1, ...infer R1] ? (
