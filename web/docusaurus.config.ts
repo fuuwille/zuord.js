4e334b1cc83acb9de8d9d22f0ba67e06b9af08f8
k4yr2 <kyrzkaya@gmail.com>
2025-07-12 11:47:00 +0300

    fix: update favicon in Docusaurus config from ICO to SVG format

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Zuord',
   tagline: 'Typescript is cool, but type utilities are cooler',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
