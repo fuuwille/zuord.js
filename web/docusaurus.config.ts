d1608ad10e7910c44ec3aeed360818f49dd26b53
k4yr2 <kyrzkaya@gmail.com>
2025-07-13 23:44:26 +0300

    fix: update copyright information in docusaurus configuration

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -109,7 +109,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} k4yr2`,
     },
     prism: {
       theme: prismThemes.vsLight,
