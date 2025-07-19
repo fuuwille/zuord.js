179c8b2a379215c121807f8fa9f1dbce9c39f114
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 15:15:13 +0300

    refactor: update ImposeLoose type to use InternalZuord.Impose for consistency

--- core/src/impose.ts
+++ core/src/impose.ts
@@ -2,4 +2,4 @@ import { InternalZuord } from "./internal";
 
 export type Impose<TBase, TPatch extends InternalZuord.Optional<TBase>, TCurrent extends TBase = TBase> = InternalZuord.Impose<TBase, TPatch, TCurrent>;
 
-export type ImposeLoose<TBase, TPatch, TCurrent extends TBase = TBase> = InternalZuord.ImposeLoose<TBase, TPatch, TCurrent>;
+export type ImposeLoose<TBase, TPatch, TCurrent extends TBase = TBase> = InternalZuord.Impose<TBase, TPatch, TCurrent>;
