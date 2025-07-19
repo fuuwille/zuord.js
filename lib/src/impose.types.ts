5bdd8a660094743fc084f614b0c90e76f9bab1f0
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 19:45:03 +0300

    feat: add ImposeStrict type for enhanced type safety

--- lib/src/impose.types.ts
+++ lib/src/impose.types.ts
@@ -3,4 +3,6 @@ import { ZuordUtil } from "@zuord/util";
 
 export type Impose<TBase, TPatch extends ZuordUtil.Partialize<TBase>, TCurrent extends TBase = TBase> = InternalZuord.Impose<TBase, TPatch, TCurrent>;
 
+export type ImposeStrict<TBase, TPatch extends TBase, TCurrent extends TBase = TBase> = InternalZuord.Impose<TBase, TPatch, TCurrent>;
+
 export type ImposeLoose<TBase, TPatch, TCurrent extends TBase = TBase> = InternalZuord.Impose<TBase, TPatch, TCurrent>;
