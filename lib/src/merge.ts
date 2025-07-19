07b1cca26886d0e3836749403e75adfc8c6db7bb
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 22:22:46 +0300

    fix: update merge function to use Zuord.MergeResolvedMode for improved type handling

--- lib/src/merge.ts
+++ lib/src/merge.ts
@@ -1,10 +1,5 @@
 import { Zuord } from "zuord";
-import { InternalZuord, internalZuord } from "./internal";
-
-type MergeModeWithDefault<T extends Partial<InternalZuord.MergeBaseMode>> =
-  Required<Omit<InternalZuord.MergeDefaultMode, keyof T> & T>;
+import { internalZuord } from "./internal";
 
 export const merge = <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode> = Zuord.MergeDefaultMode>
-    (content: [...TContent], mode?: TMode) : InternalZuord.Normalize<MergeModeWithDefault<TMode>> => internalZuord.merge(content, mode);
-
-    
+    (content: [...TContent], mode?: TMode) : Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>> => internalZuord.merge(content, mode);
