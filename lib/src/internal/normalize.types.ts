4ef50976460d4e4036d1b338a1416be5fe885edd
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 19:31:21 +0300

    feat: refactor Normalize type to use TMode for improved consistency

--- lib/src/internal/normalize.types.ts
+++ lib/src/internal/normalize.types.ts
@@ -1,28 +1,27 @@
 import { InternalZuord } from "./index";
 import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
+import { ZuordCore } from "@zuord/core";
 
-export type Normalize<T, Options extends NormalizeOptions = NormalizeDefaultOptions> = [ZuordTrait.Eq<T, any>] extends [false] ? (
+export type Normalize<T, TMode extends NormalizeBaseMode = NormalizeDefaultMode> = [ZuordTrait.Eq<T, any>] extends [false] ? (
     [ZuordTrait.Has<T, ZuordType.Tuple>] extends [true] ? (
-        (ZuordTrait.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, Options> : never) |
+        (ZuordTrait.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
         (ZuordTrait.Extract<T, ZuordType.Tuple> extends infer TExtracted ? ({  
-            [K in keyof TExtracted]: Normalize<TExtracted[K], Options> 
+            [K in keyof TExtracted]: Normalize<TExtracted[K], TMode> 
         }) : never )
     ) :
     [ZuordTrait.Has<T, ZuordType.Array>] extends [true] ? (
-        (ZuordTrait.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, Options> : never) |
-        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, readonly unknown[]>[number], Options>[] : never)
+        (ZuordTrait.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
+        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, readonly unknown[]>[number], TMode>[] : never)
     ) :     
     [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
-        (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, Options> : never) | 
+        (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
         (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? ({
-            [K in keyof TExtracted]: Normalize<TExtracted[K], Options>;
+            [K in keyof TExtracted]: Normalize<TExtracted[K], TMode>;
         }) : never )
     ) : T
 ) : any;
 
-export type NormalizeOptions = InternalZuord.Options
+export type NormalizeBaseMode = ZuordCore.ModeOf<[ZuordCore.BaseMode]>;
 
-export type NormalizeDefaultOptions = InternalZuord.DefaultOptions;
-
-export type NormalizeResolveOptions<T extends Partial<NormalizeOptions>> = Omit<NormalizeDefaultOptions, keyof T>;
+export type NormalizeDefaultMode = ZuordCore.DefaultMode;
