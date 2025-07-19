86f65b5678cdcf9c644008c633e22e3dfe90c36a
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 20:53:41 +0300

    fix: make mode parameter optional in merge function and add optional chaining for concat mode

--- lib/src/internal/merge.ts
+++ lib/src/internal/merge.ts
@@ -1,7 +1,7 @@
 import { InternalZuord } from "./index"
 import { zuordType } from "@zuord/type";
 
-export function merge<TContent extends object[], const TMode extends InternalZuord.MergeBaseMode = InternalZuord.MergeDefaultMode>(content: TContent, mode: TMode ) : InternalZuord.Merge<TContent, TMode> {
+export function merge<TContent extends object[], const TMode extends InternalZuord.MergeBaseMode = InternalZuord.MergeDefaultMode>(content: TContent, mode?: TMode ) : InternalZuord.Merge<TContent, TMode> {
     if (content.length === 0) {
         // If no content is provided, return an empty object
         return {} as InternalZuord.Merge<TContent, TMode>;
@@ -17,7 +17,7 @@ export function merge<TContent extends object[], const TMode extends InternalZuo
             const existing = result[key];
 
             // If the key already exists, we need to merge
-            if (Array.isArray(value) && Array.isArray(existing) && mode["concat"]) {
+            if (Array.isArray(value) && Array.isArray(existing) && mode?.["concat"]) {
                 // Combine arrays
                 result[key] = [...existing, ...value];
             } else if (zuordType.isObject(value) && zuordType.isObject(existing)) {
