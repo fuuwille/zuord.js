f2a75cf5df929ab68f8aab18894735d6b8b999fc
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 17:47:09 +0300

    feat: enhance Options type definition for improved flexibility with mode arrays

--- core/src/options.ts
+++ core/src/options.ts
@@ -1,7 +1,14 @@
-import { ShallowMode } from "./mode.types";
+import { InternalZuordCore } from "./internal";
+import { Mode, ShallowMode } from "./mode.types";
+
+export type Options<TModes extends Mode[]> = InternalZuordCore.Options<TModes> extends infer TOptions ? {
+    [K in keyof TOptions]: [TOptions[K]] extends [never] ? never : TOptions[K];
+} : never;
 
 export type LibOptions = ShallowMode;
 
 export type DefaultMode = {
     shallow: false;
 };
+
+type Test = Options<[ShallowMode, { concat: true }]>;
