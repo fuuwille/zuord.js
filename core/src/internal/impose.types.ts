b37e8a2346247c2497eda1671207b6b62dbf8e05
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 15:15:16 +0300

    refactor: replace ImposeLoose with recursive Impose type for improved consistency

--- core/src/internal/impose.types.ts
+++ core/src/internal/impose.types.ts
@@ -1,11 +1,9 @@
 import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
 
-export type Impose<TBase, TPatch, TCurrent extends TBase> = ImposeLoose<TBase, TPatch, TCurrent>;
-
-export type ImposeLoose<TBase, TPatch, TCurrent extends TBase> = [ZuordUtil.Is<TPatch, never>] extends [false] ? (
+export type Impose<TBase, TPatch, TCurrent extends TBase> = [ZuordUtil.Is<TPatch, never>] extends [false] ? (
     [ZuordUtil.Is<TBase, ZuordType.Plain>] extends [true] ? ({
-        [K in keyof TBase]: ImposeLoose<TBase[K], NonNullable<TPatch> extends infer TInfer ?
+        [K in keyof TBase]: Impose<TBase[K], NonNullable<TPatch> extends infer TInfer ?
             (K extends keyof TInfer ? TInfer[K] : TCurrent[K]) : never, TCurrent[K]>
     }) : (
         [ZuordUtil.EqSome<TPatch, [undefined, never]>] extends [true] ? (
