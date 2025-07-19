2a899fcb1dcbee977e13957a51efa8b467d05834
k4yr2 <kyrzkaya@gmail.com>
2025-07-19 19:37:21 +0300

    feat: refactor Integrate type to use TMode for improved consistency

--- lib/src/internal/integrate.types.ts
+++ lib/src/internal/integrate.types.ts
@@ -2,19 +2,19 @@ import { ZuordCore } from "@zuord/core";
 import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
 
-export type Integrate<A, B, Options extends IntegrateBaseMode = IntegrateDefaultMode> = [ZuordTrait.IsAny<[A, B], never>] extends [false] ? (
+export type Integrate<A, B, TMode extends IntegrateBaseMode = IntegrateDefaultMode> = [ZuordTrait.IsAny<[A, B], never>] extends [false] ? (
     [ZuordTrait.IsEvery<[A, B], ZuordType.Array>] extends [true] ? (
-        [Options["concat"]] extends [true] 
+        [TMode["concat"]] extends [true] 
             ? Array<ZuordType.ArrayInfer<B> | ZuordType.ArrayInfer<A>>
             : Array<ZuordType.ArrayInfer<B>>
     ) : 
     [ZuordTrait.IsEvery<[A, B], ZuordType.Plain>] extends [true] ? ({
         [K in (keyof A | keyof B)]: (
-            [Options["shallow"]] extends [false] ? (
+            [TMode["shallow"]] extends [false] ? (
                 Integrate<
                     K extends keyof A ? A[K] : never,
                     K extends keyof B ? B[K] : never,
-                    Options
+                    TMode
                 > 
             ) : K extends keyof A ? A[K] : K extends keyof B ? B[K] : never
         )
