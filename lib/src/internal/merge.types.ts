50a8648a82da83d0506cdfdf55f28b84f9771e7d
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 19:46:55 +0300

    refactor: rename InternalZuord import for consistency

--- lib/src/internal/merge.types.ts
+++ lib/src/internal/merge.types.ts
@@ -1,10 +1,10 @@
 import { ZuordCore } from "@zuord/core";
-import { InternalZuord } from "./index"
+import { InternalZuord as Internal } from "./index"
 import { ZuordType } from "@zuord/type";
 
 export type Merge<TContent, TMode extends MergeBaseMode = MergeDefaultMode> = (ZuordType.ArrayDepth<TContent> extends 1 ? (
     TContent extends [...infer Rest, infer Head] ? (
-        InternalZuord.Integrate<Merge<Rest, TMode>, Head, TMode>
+        Internal.Integrate<Merge<Rest, TMode>, Head, TMode>
     ) : {}
 ) :
     ZuordType.ArrayInfer<TContent> extends infer TInfer ? (
@@ -14,9 +14,9 @@ export type Merge<TContent, TMode extends MergeBaseMode = MergeDefaultMode> = (Z
     ) : {}
 );
 
-export type MergeBaseMode = InternalZuord.IntegrateBaseMode;
+export type MergeBaseMode = Internal.IntegrateBaseMode;
 
-export type MergeDefaultMode = InternalZuord.IntegrateBaseMode;
+export type MergeDefaultMode = Internal.IntegrateBaseMode;
 
 export type MergeResolvedMode<TMode extends Partial<MergeBaseMode>> 
 = ZuordCore.ModeResolve<[MergeDefaultMode, TMode]>;
