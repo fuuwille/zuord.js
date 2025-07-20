f50bc8e5193b8fafb02aa56dd07682da25324d1e
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 20:41:57 +0300

    refactor: rename integrateDefaultMode to integrateBaseMode for consistency

--- lib/src/internal/integrate.ts
+++ lib/src/internal/integrate.ts
@@ -1,4 +1,4 @@
 import { zuordCore } from "@zuord/core";
 import { InternalZuord as Internal } from ".";
 
-export const integrateDefaultMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies Internal.IntegrateBaseMode;
+export const integrateBaseMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies Internal.IntegrateBaseMode;
--- lib/src/internal/integrate.types.ts
+++ lib/src/internal/integrate.types.ts
@@ -3,7 +3,7 @@ import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
 import { internalZuord as internal } from ".";
 
-export type Integrate<A, B, TMode extends IntegrateBaseMode = typeof internal.integrateDefaultMode> = [ZuordTrait.IsAny<[A, B], never>] extends [false] ? (
+export type Integrate<A, B, TMode extends IntegrateBaseMode = typeof internal.integrateBaseMode> = [ZuordTrait.IsAny<[A, B], never>] extends [false] ? (
     [ZuordTrait.IsEvery<[A, B], ZuordType.Array>] extends [true] ? (
         [TMode["concat"]] extends [true] 
             ? Array<ZuordType.ArrayInfer<B> | ZuordType.ArrayInfer<A>>
