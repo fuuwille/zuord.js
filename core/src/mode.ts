b092f39d94ac01707a70ef63fef18f2afecc4304
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 10:56:36 +0300

    refactor: change mode and modeOf functions to use function declaration for consistency

--- core/src/mode.ts
+++ core/src/mode.ts
@@ -1,9 +1,9 @@
 import { Mode, ModeOf } from "./mode.types";
 
-export const mode = <K extends string>(key: K, value: boolean = false) : Mode<K> => {
+export function mode <K extends string>(key: K, value: boolean = false) : Mode<K> {
     return { [key]: value } as Mode<K>;
 }
 
-export const modeOf = <const TOf extends Mode[]>(modes: TOf): ModeOf<TOf> => {
+export function modeOf <const TOf extends Mode[]>(modes: TOf): ModeOf<TOf> {
     return Object.assign({}, ...modes);
 };
