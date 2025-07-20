d941b8250d8f9f972f36112724c31ffd2fd457b5
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 10:59:40 +0300

    refactor: update mode function signatures for consistency and clarity

--- core/src/mode.ts
+++ core/src/mode.ts
@@ -1,7 +1,18 @@
 import { Mode, ModeOf } from "./mode.types";
 
-export function mode <K extends string>(key: K, value: boolean = false) : Mode<K> {
-    return { [key]: value } as Mode<K>;
+export function mode <K extends string>(key: K, value: boolean) : Mode<K>
+
+export function mode <K extends string>(key: K[], value: boolean) : Mode<K>
+
+export function mode <K extends string>(key: K | K[], value: boolean = false) : Mode<K> {
+    if (Array.isArray(key)) {
+        const obj = {} as Mode<K>;   
+        for (const k of key) 
+            obj[k] = value;
+        return obj;
+    } else {
+        return { [key]: value } as Mode<K>;
+    }
 }
 
 export function modeOf <const TOf extends Mode[]>(modes: TOf): ModeOf<TOf> {
