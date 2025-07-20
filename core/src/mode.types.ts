7bd78b638d1c9ea9e48fa8457d9a1b55699bc7aa
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 13:20:35 +0300

    refactor: add documentation comment for ModeFrom type for clarity

--- core/src/mode.types.ts
+++ core/src/mode.types.ts
@@ -2,6 +2,9 @@ import { InternalZuordCore as Internal } from "./internal";
 
 export type Mode<K extends string = string> = Internal.Mode<K>;
 
+/**
+ * @puretype
+ */
 export type ModeFrom<TMode extends TFrom, TFrom extends Internal.Mode> = Internal.ModeFrom<TMode, TFrom>;
 
 export type ModeResolve<TModes extends Mode[]> = Internal.ModeResolve<TModes>;
