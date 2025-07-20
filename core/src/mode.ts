8c4663eabadbdc76161ee9664697c6837f20c0ad
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 10:32:53 +0300

    feat: add modeOf function for merging multiple modes into a single object

--- core/src/mode.ts
+++ core/src/mode.ts
@@ -3,3 +3,7 @@ import { Mode } from "./mode.types";
 export const mode = <K extends string>(key: K, value: boolean = false) : Mode<K> => {
     return { [key]: value } as Mode<K>;
 }
+
+export const modeOf = (modes: Mode[]): Mode => {
+    return Object.assign({}, ...modes);
+};
