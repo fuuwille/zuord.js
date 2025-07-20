bab5b335bd48808c9d11d04652c3b3fc35afa477
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 11:26:42 +0300

    refactor: format ModeOf and ModeFrom types for improved readability

--- core/src/internal/mode.types.ts
+++ core/src/internal/mode.types.ts
@@ -1,7 +1,7 @@
 export type Mode<K extends string = string> = Record<K, boolean>;
 
 export type ModeOf<TModes> = TModes extends [infer TMode, ...infer TRest]
-  ? TMode & ModeOf<TRest>
-  : {};
+  ? TMode & ModeOf<TRest> : {};
 
-export type ModeFrom<TFrom, TMode extends Partial<TFrom>, TCurrent> = Required<Omit<TCurrent, keyof TMode> & TMode>;
+export type ModeFrom<TFrom, TMode extends Partial<TFrom>, TCurrent> 
+  = Required<Omit<TCurrent, keyof TMode> & TMode>;
