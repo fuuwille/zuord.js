88cdf4b24ff061b145715aed77a38d1c36961f47
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 20:51:24 +0300

    refactor: resolve mode in merge function for improved functionality

--- lib/src/merge.ts
+++ lib/src/merge.ts
@@ -1,6 +1,8 @@
 import { Zuord } from "zuord";
 import { internalZuord as internal } from "./internal";
+import { zuordCore } from "@zuord/core";
 
 export function merge <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode>> (content: [...TContent], mode?: TMode) {
-    return internal.merge(content, mode) as Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>>;
+    const resolvedMode = mode ? zuordCore.modeResolve([internal.mergeBaseMode, mode]) : internal.mergeBaseMode;
+    return internal.merge(content, resolvedMode) as Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>>;
 }
