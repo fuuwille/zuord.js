fe5e86181f8fcd5a241cd91b0bcb0434c18b2668
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 13:00:16 +0300

    refactor: update ModeOf type to improve handling of tuple types

--- core/src/internal/mode.types.ts
+++ core/src/internal/mode.types.ts
@@ -1,7 +1,8 @@
 export type Mode<K extends string = string> = Record<K, boolean>;
 
-export type ModeOf<TModes> = TModes extends [infer TMode, ...infer TRest]
-  ? TMode & ModeOf<TRest> : {};
+export type ModeOf<TModes> = TModes extends [...infer TRest, infer TLast]
+  ? Omit<ModeOf<TRest>, keyof TLast> & TLast
+  : {};
 
 export type ModeFrom<TFrom, TMode extends Partial<TFrom>, TCurrent> 
   = Required<Omit<TCurrent, keyof TMode> & TMode>;
