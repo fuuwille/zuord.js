87e4ad5732ec3f76a96a978622cae4b5d8bdd0e2
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 13:20:21 +0300

    refactor: add documentation comment for ModeFrom type for clarity

--- core/src/internal/mode.types.ts
+++ core/src/internal/mode.types.ts
@@ -1,5 +1,8 @@
 export type Mode<K extends string = string> = Record<K, boolean>;
 
+/**
+ * @puretype
+ */
 export type ModeFrom<TMode extends TFrom, TFrom extends Mode> = TMode;
 
 export type ModeResolve<TModes> = TModes extends [...infer TRest, infer TLast]
