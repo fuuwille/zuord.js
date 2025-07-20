8684c72e14d23166a917688eef7b15087c0fffa5
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 20:48:31 +0300

    refactor: remove explicit return type in merge function for simplicity

--- lib/src/merge.ts
+++ lib/src/merge.ts
@@ -1,7 +1,6 @@
 import { Zuord } from "zuord";
 import { internalZuord as internal } from "./internal";
 
-export function merge <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode>>
-(content: [...TContent], mode?: TMode) : Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>> {
+export function merge <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode>> (content: [...TContent], mode?: TMode) {
     return internal.merge(content, mode) as Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>>;
 }
