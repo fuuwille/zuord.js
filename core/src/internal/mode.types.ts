bb2279f116ace985a8a61b4d9e5e097f89ae709d
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 18:26:29 +0300

    refactor: remove ModeFrom type and simplify DefaultMode definition

--- core/src/internal/mode.types.ts
+++ core/src/internal/mode.types.ts
@@ -1,10 +1,5 @@
 export type Mode<K extends string = string> = Record<K, boolean>;
 
-/**
- * @puretype
- */
-export type ModeFrom<TMode extends TFrom, TFrom extends Mode> = TMode;
-
 export type ModeResolve<TModes> = TModes extends [...infer TRest, infer TLast]
   ? Required<Omit<ModeResolve<TRest>, keyof TLast> & TLast>
   : {};
--- core/src/mode.types.ts
+++ core/src/mode.types.ts
@@ -2,20 +2,15 @@ import { InternalZuordCore as Internal } from "./internal";
 
 export type Mode<K extends string = string> = Internal.Mode<K>;
 
-/**
- * @puretype
- */
-export type ModeFrom<TMode extends TFrom, TFrom extends Internal.Mode> = Internal.ModeFrom<TMode, TFrom>;
-
 export type ModeResolve<TModes extends Mode[]> = Internal.ModeResolve<TModes>;
 
 // Define specific modes
 
 export type BaseMode = ModeResolve<[ShallowMode]>;
 
-export type DefaultMode = ModeFrom<{
+export type DefaultMode = {
     shallow: false;
-}, BaseMode>;
+};
 
 export type ResolvedMode<TMode extends Partial<BaseMode>> = ModeResolve<[DefaultMode, TMode]>;
 
