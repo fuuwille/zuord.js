fd118de01c9f65f3cec30271e6a282e6a9a5468f
k4yr2 <kyrzkaya@gmail.com>
2025-07-20 19:18:56 +0300

    feat: add shallowMode, concatMode, and defaultMode exports for improved module accessibility

--- core/src/mode.ts
+++ core/src/mode.ts
@@ -14,3 +14,9 @@ export function modeField <K extends string>(key: K | K[], value: boolean = fals
 export function modeResolve <const TModes extends ZuordCore.ModeField[]>(modes: TModes): ZuordCore.ModeResolve<TModes> {
     return internal.modeResolve(modes);
 };
+
+export const shallowMode: ZuordCore.ShallowMode = internal.shallowMode;
+
+export const concatMode: ZuordCore.ConcatMode = internal.concatMode;
+
+export const defaultMode: ZuordCore.BaseMode = internal.defaultMode;
