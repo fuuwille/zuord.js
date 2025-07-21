3c4d901e9e5fa2ea20ff57de5c512550b19c334f
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 14:51:41 +0300

    refactor: remove redundant MergeResolvedMode type definition

--- lib/src/merge.types.ts
+++ lib/src/merge.types.ts
@@ -3,6 +3,3 @@ import { internalZuord as internal, InternalZuord as Internal } from "./internal
 export type Merge<TContent, TMode extends MergeBaseMode = typeof internal.mergeBaseMode> = Internal.Merge<TContent, TMode>;
 
 export type MergeBaseMode = Internal.MergeBaseMode;
-
-export type MergeResolvedMode<TMode extends Partial<MergeBaseMode>>
-= Internal.MergeResolvedMode<TMode>;
