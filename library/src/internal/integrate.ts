bf5e07503e368757a5933a3ca423d074e0745ad6
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 22:44:07 +0300

    feat: enhance integrate function to support merging plain objects with shallow and concat options

--- library/src/internal/integrate.ts
+++ library/src/internal/integrate.ts
@@ -1,4 +1,42 @@
 import { zuordCore } from "@zuord/core";
 import { InternalZuord as Internal } from ".";
+import { ZuordType, zuordType } from "@zuord/type";
+
+export const integrate = <A, B, TMode extends Internal.IntegrateMode>(a: A, b: B, mode: TMode) => {
+    const concat = mode.concat ?? false;
+    const shallow = mode.shallow ?? false;
+
+    if (Array.isArray(a) && Array.isArray(b)) {
+        if (concat) {
+            return [...a, ...b];
+        } else {
+            return b;
+        }
+    }
+
+    if(zuordType.plain(a) && zuordType.plain(b)) {
+        const integrated : ZuordType.Plain = {};
+
+        const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+
+        for (const key of keys) {
+            if (!shallow) {
+                integrated[key] = integrate(
+                    key in a ? a[key] : undefined,
+                    key in b ? b[key] : undefined,
+                    mode
+                );
+            } else {
+                if (key in a) {
+                    integrated[key] = a[key];
+                } else if (key in b) {
+                    integrated[key] = b[key];
+                }
+            }
+        }
+
+        return integrated;
+    }
+}
 
 export const integrateMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies Internal.IntegrateMode;
