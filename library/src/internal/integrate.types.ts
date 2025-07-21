fca6beebdde0b791ec05c1f007581ec429e129ea
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 23:55:09 +0300

    refactor: improve type inference in Integrate and related types for better clarity and structure

--- library/src/internal/integrate.types.ts
+++ library/src/internal/integrate.types.ts
@@ -4,32 +4,36 @@ import { ZuordTrait } from "@zuord/trait";
 import { internalZuord as internal } from ".";
 
 export type Integrate<A, B, TMode extends IntegrateMode = typeof internal.integrateMode> = [ZuordTrait.IsAny<[A, B], never>] extends [false] ? (
-    A extends infer ArrayA extends ZuordType.Array ? (
-        B extends infer ArrayB extends ZuordType.Array ? (
-            IntegrateArray<ArrayA, ArrayB, TMode>
-        ) : never
-    ) :
-    A extends infer PlainA extends ZuordType.Plain ? (
-        B extends infer PlainB extends ZuordType.Plain ? (
-            IntegratePlain<PlainA, PlainB, TMode>
-        ) : never
+    [ZuordTrait.IsEvery<[A, B], ZuordType.Array>] extends [true] ? (
+        IntegrateArray<A, B, TMode>
+    ) : 
+    [ZuordTrait.IsEvery<[A, B], ZuordType.Plain>] extends [true] ? (
+        IntegratePlain<A, B, TMode>
     ) : B
 ) : ZuordType.UnionOf<[A, B]>;
 
-export type IntegrateArray<A extends ZuordType.Array, B extends ZuordType.Array, TMode extends IntegrateMode = typeof internal.integrateMode> = (
-    [TMode["concat"]] extends [true] ? [...A, ...B] : B
+export type IntegrateArray<A, B, TMode extends IntegrateMode = typeof internal.integrateMode> = (
+    A extends infer ArrayA extends ZuordType.Array ? (
+        B extends infer ArrayB extends ZuordType.Array ? (
+            [TMode["concat"]] extends [true] ? [...ArrayA, ...ArrayB] : ArrayB
+        ) : never
+    ) : never
 )
 
-export type IntegratePlain<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends IntegrateMode = typeof internal.integrateMode> = ({
-    [K in (keyof A | keyof B)]: (
-        [TMode["shallow"]] extends [false] ? (
-            Integrate<
-                K extends keyof A ? A[K] : never,
-                K extends keyof B ? B[K] : never,
-                TMode
-            >
-        ) : K extends keyof A ? A[K] : K extends keyof B ? B[K] : never
-    )
-})
+export type IntegratePlain<A, B, TMode extends IntegrateMode = typeof internal.integrateMode> = (
+    A extends infer PlainA extends ZuordType.Plain ? (
+        B extends infer PlainB extends ZuordType.Plain ? ({
+            [K in (keyof PlainA | keyof PlainB)]: (
+                [TMode["shallow"]] extends [false] ? (
+                    Integrate<
+                        K extends keyof PlainA ? PlainA[K] : never,
+                        K extends keyof PlainB ? PlainB[K] : never,
+                        TMode
+                    >
+                ) : K extends keyof PlainA ? PlainA[K] : K extends keyof PlainB ? PlainB[K] : never
+            )
+        }) : never
+    ) : never
+)
 
 export type IntegrateMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode]>;
