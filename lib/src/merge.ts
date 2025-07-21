1d3c8870a0e6f0aaea07293c7761195fc226c956
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 14:32:12 +0300

    refactor: improve type resolution in merge function

--- lib/src/merge.ts
+++ lib/src/merge.ts
@@ -1,8 +1,10 @@
 import { Zuord } from "zuord";
 import { internalZuord as internal } from "./internal";
-import { zuordCore } from "@zuord/core";
+import { ZuordCore, zuordCore } from "@zuord/core";
 
 export function merge <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode>> (content: [...TContent], mode?: TMode) {
     const resolvedMode = mode ? zuordCore.modeResolve([internal.mergeBaseMode, mode]) : internal.mergeBaseMode;
-    return internal.merge(content, resolvedMode) as Zuord.Merge<TContent, Zuord.MergeResolvedMode<TMode>>;
+    type ResolvedMode = typeof resolvedMode;
+
+    return internal.merge(content, resolvedMode) as Zuord.Merge<TContent, ResolvedMode>;
 }
