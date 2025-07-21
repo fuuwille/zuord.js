ab6ec2ecdd4c932cd582fafcc53f739dda36e003
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 22:55:34 +0300

    refactor: streamline integrate function by delegating to integratePlain

--- library/src/internal/integrate.ts
+++ library/src/internal/integrate.ts
@@ -15,28 +15,32 @@ export const integrate = <A, B, TMode extends Internal.IntegrateMode>(a: A, b: B
     }
 
     if(zuordType.plain(a) && zuordType.plain(b)) {
-        const integrated : ZuordType.Plain = {};
+        integratePlain(a, b, mode);
+    }
+}
 
-        const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+export const integratePlain = <A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends Internal.IntegrateMode>(a: A, b: B, mode: TMode) => {
+    const integrated : ZuordType.Plain = {};
 
-        for (const key of keys) {
-            if (!shallow) {
-                integrated[key] = integrate(
-                    key in a ? a[key] : undefined,
-                    key in b ? b[key] : undefined,
-                    mode
-                );
-            } else {
-                if (key in a) {
-                    integrated[key] = a[key];
-                } else if (key in b) {
-                    integrated[key] = b[key];
-                }
+    const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+
+    for (const key of keys) {
+        if (!mode.shallow) { // ? CHECK LATER
+            integrated[key] = integrate(
+                key in a ? a[key] : undefined,
+                key in b ? b[key] : undefined,
+                mode
+            );
+        } else {
+            if (key in a) {
+                integrated[key] = a[key];
+            } else if (key in b) {
+                integrated[key] = b[key];
             }
         }
-
-        return integrated;
     }
+
+    return integrated;
 }
 
 export const integrateMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies Internal.IntegrateMode;
