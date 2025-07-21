196c8252f0413a3e02aef3ddb18f5c358769a702
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 22:46:44 +0300

    feat: add integrate function to handle merging with resolved modes

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -1,3 +1,13 @@
+import { zuord, Zuord } from "src";
 import { internalZuord as internal, InternalZuord as Internal } from "./internal";
+import { ZuordCore, zuordCore } from "@zuord/core";
+
+export function integrate <A, B, TMode extends Partial<Zuord.IntegrateMode>> (a: A, b: B, mode?: TMode) {
+    const resolvedMode = zuordCore.modeResolve([zuord.integrateMode, mode ?? {}]);
+    type ResolvedMode = ZuordCore.ModeResolve<[typeof zuord.integrateMode, TMode]>;
+
+    return internal.integrate(a, b, resolvedMode) as Zuord.Integrate<A, B, ResolvedMode>;
+}
+
 
 export const integrateMode = internal.integrateMode satisfies Internal.IntegrateMode;
