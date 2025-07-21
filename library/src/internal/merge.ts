cffe99479d8ae8d0f0e4bdb009ceeb651aed83e3
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 23:23:43 +0300

    refactor: improve readability of merge function by adding braces and simplifying return statements

--- library/src/internal/merge.ts
+++ library/src/internal/merge.ts
@@ -2,19 +2,25 @@ import { InternalZuord as Internal, internalZuord as internal } from "./index"
 import { ZuordType, zuordType } from "@zuord/type";
 
 export const merge = <TContent extends ZuordType.Plain[], TMode extends Partial<Internal.MergeMode>> (content: TContent, mode?: TMode) : ZuordType.Plain => {
-    if (content.length === 0) return {};
+    if (content.length === 0) {
+        return {};
+    }
 
     const [head, ...rest] = content;
 
+    let merged;
+
     if(rest.length === 0) {
-        return head;
+        merged = head;
     }
     else if(rest.length === 0) {
-        return internal.integratePlain(head, rest[0], mode);
+        merged = internal.integratePlain(head, rest[0], mode);
     }
     else {
-        return internal.integratePlain(head, merge(rest, mode), mode);
+        merged = internal.integratePlain(head, merge(rest, mode), mode);
     }
+
+    return merged;
 }
 
 export const mergeMode = internal.integrateMode satisfies Internal.MergeMode;
