951722c5ebc31fc4e9f2d4b8df6fb243cf1f7404
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 15:08:18 +0300

    refactor: simplify Merge type by removing array depth condition

--- lib/src/internal/merge.types.ts
+++ lib/src/internal/merge.types.ts
@@ -1,10 +1,8 @@
 import { ZuordType } from "@zuord/type";
 import { InternalZuord as Internal, internalZuord as internal } from "./index"
 
-export type Merge<TContent, TMode extends MergeMode = typeof internal.mergeMode> = ZuordType.ArrayDepth<TContent> extends 1 ? (
-    TContent extends [...infer Rest, infer Head] ? (
-        Internal.Integrate<Head, Merge<Rest, TMode>, TMode>
-    ) : {}
-) : never;
+export type Merge<TContent, TMode extends MergeMode = typeof internal.mergeMode> = TContent extends [...infer Rest, infer Head] ? (
+    Internal.Integrate<Head, Merge<Rest, TMode>, TMode>
+) : never
 
 export type MergeMode = Internal.IntegrateMode;
