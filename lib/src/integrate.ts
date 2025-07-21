9ad6b9865a0d21c817f4fe44de5d2d7ccb1dc32e
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 14:29:11 +0300

    feat: add integrateBaseMode function to integrate module

---
+++ lib/src/integrate.ts
@@ -0,0 +1,3 @@
+import { internalZuord as internal, InternalZuord as Internal } from "./internal";
+
+export const integrateBaseMode = internal.integrateBaseMode satisfies Internal.IntegrateBaseMode;
