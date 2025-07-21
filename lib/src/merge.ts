449072861180c575e1e3cd90635489a7ea99837f
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 14:55:35 +0300

    refactor: update merge function to use zuord.mergeBaseMode and add newline at end of file

--- lib/src/merge.ts
+++ lib/src/merge.ts
@@ -1,10 +1,12 @@
-import { Zuord } from "zuord";
+import { Zuord, zuord } from "zuord";
 import { internalZuord as internal } from "./internal";
 import { ZuordCore, zuordCore } from "@zuord/core";
 
 export function merge <TContent extends object[], TMode extends Partial<Zuord.MergeBaseMode>> (content: [...TContent], mode?: TMode) {
-    const resolvedMode = mode ? zuordCore.modeResolve([internal.mergeBaseMode, mode]) : internal.mergeBaseMode;
-    type ResolvedMode = ZuordCore.ModeResolve<[typeof internal.mergeBaseMode, TMode]>;
+    const resolvedMode = mode ? zuordCore.modeResolve([zuord.mergeBaseMode, mode]) : zuord.mergeBaseMode;
+    type ResolvedMode = ZuordCore.ModeResolve<[typeof zuord.mergeBaseMode, TMode]>;
 
     return internal.merge(content, resolvedMode) as Zuord.Merge<TContent, ResolvedMode>;
 }
+
+export const mergeBaseMode = internal.mergeBaseMode satisfies Zuord.MergeBaseMode;
