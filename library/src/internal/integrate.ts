92aef3f4a640aa8f0cb043ef00cbaa2f2bdc0147
k4yr2 <kyrzkaya@gmail.com>
2025-07-21 19:09:07 +0300

    refactor: restructure core package and update imports

--- library/src/internal/integrate.ts
+++ library/src/internal/integrate.ts
@@ -1,4 +1,4 @@
-import { zuordCore } from "@zuord/core";
+import { zuordCore } from "../../../packages/core/src";
 import { InternalZuord as Internal } from ".";
 
 export const integrateMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies Internal.IntegrateMode;
