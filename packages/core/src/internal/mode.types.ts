5407409a4508dee3ea4c5497885ebbbcaf0822ed
k4yr2 <kyrzkaya@gmail.com>
2025-07-22 21:55:11 +0300

    fix: refactor ModeResolve type for improved type inference and structure

--- packages/core/src/internal/mode.types.ts
+++ packages/core/src/internal/mode.types.ts
@@ -2,9 +2,13 @@ import { ZuordType } from "@zuord/type";
 
 export type ModeField<K extends string = string, V extends boolean = boolean> = Record<K, V>;
 
-export type ModeResolve<TModes> = TModes extends [...infer TRest, infer TLast]
-  ? ModeResolve<TRest> & ZuordType.RequiredOnly<TLast>
-  : {};
+export type ModeResolve<TModes> = TModes extends [...infer TRest, infer TLast] ? (
+  ModeResolve<TRest> extends infer TResolvedRest ? (
+    ZuordType.RequiredOnly<{
+      [K in keyof TResolvedRest | keyof TLast]: K extends keyof TLast ? TLast[K] : K extends keyof TResolvedRest ? TResolvedRest[K] : never;
+    }>
+  ) : never
+) : {};
 
 export type ShallowMode = ModeField<"shallow">;
 
