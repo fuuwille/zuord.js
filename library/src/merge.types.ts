2e8b534cb0bc11f656f37f6898176964cccf8e67
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 02:34:36 +0300

    refactor: update merge and merge types to use ZuordType.Plain[] for improved type consistency

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -2,7 +2,7 @@ import { InternalZuord as Internal } from "./internal";
 import { ZuordType } from "@zuord/type";
 import { mergeMode } from "./merge";
 
-export type Merge<TContent extends ZuordType.TupleLike<ZuordType.Plain>, TMode extends MergeMode = typeof mergeMode> = ZuordType.ArrayDepth<TContent> extends 1 ? (
+export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = ZuordType.ArrayDepth<TContent> extends 1 ? (
     Internal.Merge<TContent, TMode>
 ) : never;
 
