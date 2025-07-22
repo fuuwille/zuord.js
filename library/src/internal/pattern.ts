b66c0347fa44aa9bffc4ac2b97193cf2e7edaa78
k4yr2 <kyrzkaya@gmail.com>
2025-07-22 19:06:41 +0300

    fix: update pattern function to use direct import for Pattern type

--- library/src/internal/pattern.ts
+++ library/src/internal/pattern.ts
@@ -1,5 +1,5 @@
-import { InternalZuord } from ".";
+import { Pattern } from "./pattern.types";
 
-export function pattern<T extends object, P extends InternalZuord.Pattern<T> = InternalZuord.Pattern<T>>(pattern: P) {
+export function pattern<T extends object, P extends Pattern<T> = Pattern<T>>(pattern: P) {
     return pattern as P;
 }
