bdb55ae322074a62c11e16a1e59455ec70f72f30
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 02:36:40 +0300

    refactor: enhance Merge type to include ZuordTrait for improved type handling

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -1,9 +1,12 @@
 import { InternalZuord as Internal } from "./internal";
 import { ZuordType } from "@zuord/type";
 import { mergeMode } from "./merge";
+import { ZuordTrait } from "@zuord/trait";
 
-export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = ZuordType.ArrayDepth<TContent> extends 1 ? (
-    Internal.Merge<TContent, TMode>
-) : never;
+export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = ZuordTrait.Is<TContent, ZuordType.Tuple> extends true ? (
+    ZuordType.ArrayDepth<TContent> extends 1 ? (
+        Internal.Merge<TContent, TMode>
+    ) : never
+) : Internal.Normalize<TContent>;
 
 export type MergeMode = Internal.MergeMode;
