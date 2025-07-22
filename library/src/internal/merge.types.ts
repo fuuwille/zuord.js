2b9612c4ab2ed45428368c6ff90f2eddb515fe9c
k4yr2 <kyrzkaya@gmail.com>
2025-07-22 13:40:22 +0300

    refactor: enhance conditional logic in Merge type for improved clarity and structure

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -1,7 +1,14 @@
 import { InternalZuord as Internal, internalZuord as internal } from "./index"
 
 export type Merge<TContent, TMode extends MergeMode = typeof internal.mergeMode> = TContent extends [...infer Rest, infer Head] ? (
-    Internal.Integrate<Head, Merge<Rest, TMode>, TMode>
+    Rest["length"] extends 0 ? (
+        Head
+    ) : 
+    Rest["length"] extends 1 ? (
+        Internal.Integrate<Head, Rest[0], TMode>
+    ) : (
+        Internal.Integrate<Head, Merge<Rest, TMode>, TMode>
+    )
 ) : {};
 
 export type MergeMode = Internal.IntegrateMode;
