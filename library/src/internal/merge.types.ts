ca827f9922e6e7799b4d570bc7303fd50ad5e4dc
k4yr2 <kyrzkaya@gmail.com>
2025-07-22 19:05:02 +0300

    fix: refactor merge types to use direct imports for Integrate and IntegrateMode

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -1,15 +1,15 @@
 import { ZuordUtil } from "@zuord/util";
-import { InternalZuord as Internal } from "."
+import { Integrate, IntegrateMode } from "./integrate.types";
 
 export type Merge<TContent, TMode extends ZuordUtil.Partialize<MergeMode>> = TContent extends [...infer Rest, infer Head] ? (
     Rest["length"] extends 0 ? (
         Head
     ) : 
     Rest["length"] extends 1 ? (
-        Internal.Integrate<Head, Rest[0], TMode>
+        Integrate<Head, Rest[0], TMode>
     ) : (
-        Internal.Integrate<Head, Merge<Rest, TMode>, TMode>
+        Integrate<Head, Merge<Rest, TMode>, TMode>
     )
 ) : {};
 
-export type MergeMode = Internal.IntegrateMode;
+export type MergeMode = IntegrateMode;
