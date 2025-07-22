f125a9f1c17df6e6a12bd0ff3986b09231f314fb
k4yr2 <kyrzkaya@gmail.com>
2025-07-22 21:50:09 +0300

    fix: redefine RequiredOnly type for improved type safety

--- packages/type/src/required.types.ts
+++ packages/type/src/required.types.ts
@@ -1,5 +1,3 @@
-export type RequiredKeys<T> = {
-  [K in keyof T]-?: {} extends Pick<T, K> ? never : K
-}[keyof T];
-
-export type RequiredOnly<T> = Pick<T, RequiredKeys<T>>;
+export type RequiredOnly<T> = {
+  [K in keyof T]-?: T[K] extends undefined ? false : T[K] extends boolean ? T[K] : false;
+};
