e04e3e832cd945b3fc7d729f44e6eb19cefce155
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 01:26:31 +0300

    refactor: simplify Pick type implementation by removing Normalize and renaming PickRaw to Pick

--- library/src/internal/pick.types.ts
+++ library/src/internal/pick.types.ts
@@ -1,15 +1,12 @@
-import { Normalize } from "./normalize.types";
 import { IsPattern, Pattern } from "./pattern.types";
 
-export type Pick<T, U> = Normalize<PickRaw<T, U>>;
-
-export type PickRaw<T, U> = {
+export type Pick<T, U> =  {
     [K in keyof T & keyof U as IsPattern<U[K]> extends true ? K : never]:
         U[K] extends true
             ? T[K]
             : U[K] extends object
                 ? T[K] extends object
-                    ? PickRaw<T[K], U[K]>
+                    ? Pick<T[K], U[K]>
                     : never
                 : never;
 };
