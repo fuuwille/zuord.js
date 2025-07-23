046b338c6a681549fe7cabaa060f556805ac67ae
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 19:13:07 +0300

    refactor: update Merge type to use ZuordTrait for improved type validation

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -1,15 +1,21 @@
 import { ZuordUtil } from "@zuord/util";
-import { Integrate, IntegrateMode, IntegratePlain } from "./integrate.types";
+import { IntegrateMode, IntegratePlain } from "./integrate.types";
+import { ZuordTrait } from "@zuord/trait";
+import { ZuordType } from "@zuord/type";
 
-export type Merge<TContent, TMode extends ZuordUtil.Partialize<MergeMode>> = TContent extends [...infer TRest, infer TLast] ? (
-    TRest["length"] extends 0 ? (
-        TLast
-    ) : 
-    TRest["length"] extends 1 ? (
-        IntegratePlain<TRest[0], TLast, TMode>
-    ) : (
-        IntegratePlain<Merge<TRest, TMode>, TLast, TMode>
-    )
-) : {};
+export type Merge<TContent, TMode extends Partial<MergeMode>> = ZuordTrait.Is<TContent, ZuordType.Tuple> extends true ? (
+    ZuordType.ArrayDepth<TContent> extends 1 ? (
+        TContent extends [...infer TRest, infer TLast] ? (
+            TRest["length"] extends 0 ? (
+                TLast
+            ) : 
+            TRest["length"] extends 1 ? (
+                IntegratePlain<TRest[0], TLast, TMode>
+            ) : (
+                IntegratePlain<Merge<TRest, TMode>, TLast, TMode>
+            )
+        ) : {}
+    ) : never
+) : ZuordUtil.Normalize<TContent, TMode>;
 
 export type MergeMode = IntegrateMode;
