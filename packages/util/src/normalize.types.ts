e74d2ae259c3f8b76c18bb21f3313fc429256290
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 18:04:17 +0300

    refactor: streamline Normalize type by removing separate Tuple, Array, and Plain normalization types for improved clarity

--- packages/util/src/normalize.types.ts
+++ packages/util/src/normalize.types.ts
@@ -5,29 +5,23 @@ import { ZuordCore } from "@zuord/core";
 export type Normalize<T, TMode extends NormalizeBaseMode> = [ZuordTrait.Eq<T, any>] extends [false] ? (
     [ZuordTrait.Has<T, ZuordType.Tuple>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
-        (ZuordTrait.Extract<T, ZuordType.Tuple> extends infer TExtracted ? NormalizeTuple<TExtracted, TMode> : never )
+        (ZuordTrait.Extract<T, ZuordType.Tuple> extends infer TExtracted ? {
+            [K in keyof TExtracted]: Normalize<TExtracted[K], TMode> 
+        } : never )
     ) :
     [ZuordTrait.Has<T, ZuordType.Array>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
-        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? NormalizeArray<TExtracted, TMode> : never )
+        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted extends ZuordType.Array ? (
+            Normalize<TExtracted[number], TMode>[]
+        ) : never)
     ) :     
     [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
-        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? NormalizePlain<TExtracted, TMode> : never )
+        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? {
+            [K in TExtracted extends any ? keyof TExtracted : never]: K extends keyof TExtracted ? Normalize<TExtracted[K], TMode> : never;
+        } : never )
     ) : T
 ) : any;
-
-export type NormalizeTuple<T, TMode extends NormalizeBaseMode> = T extends ZuordType.Tuple ? {
-    [K in keyof T]: Normalize<T[K], TMode> 
-} : never;
-
-export type NormalizeArray<T, TMode extends NormalizeBaseMode> = T extends ZuordType.Array ? (
-    Normalize<T[number], TMode>[]
-) : never;
-
-export type NormalizePlain<T, TMode extends NormalizeBaseMode> = T extends ZuordType.Plain ? {
-    [K in T extends any ? keyof T : never]: K extends keyof T ? Normalize<T[K], TMode> : never;
-} : never;
 
 export type NormalizeBaseMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode]>;
 
