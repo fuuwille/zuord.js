911482abac46a5689d7a355f8f880adce5743c96
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:38:40 +0300

    refactor: rename MergeSpecific to MergeFromTuple for consistency in type definitions

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -4,22 +4,22 @@ import { ZuordType } from "@zuord/type";
 
 export type Merge<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = (
     TContent extends ZuordType.PlainTuple ? (
-        MergeSpecific<TContent, TMode>
+        MergeFromTuple<TContent, TMode>
     ) : MergeCommon<TContent, TMode>
 );
 
-export type MergeSpecific<TContent extends ZuordType.PlainTuple, TMode extends Partial<MergeMode>> = (
+export type MergeFromTuple<TContent extends ZuordType.PlainTuple, TMode extends Partial<MergeMode>> = (
     TContent extends [...infer TRest extends ZuordType.PlainTuple, infer TLast extends ZuordType.Plain] ? (
         TRest["length"] extends 0 ? TLast : 
         TRest["length"] extends 1 ? IntegratePlain<TRest[0], TLast, TMode> 
-        : IntegratePlain<MergeSpecific<TRest, TMode>, TLast, TMode>
+        : IntegratePlain<MergeFromTuple<TRest, TMode>, TLast, TMode>
     ) : {}
 ) extends infer TMerged extends ZuordType.Plain ? TMerged : never;
 
 export type MergeCommon<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = TContent extends readonly (infer TInfer)[] ? (
     ZuordType.PlainOnlyRequired<TInfer> extends infer TPlain extends ZuordType.Plain ? (
         ZuordType.UnionToTuple<TPlain> extends infer TNormalized extends ZuordType.PlainTuple ? (
-            MergeSpecific<TNormalized, TMode> 
+            MergeFromTuple<TNormalized, TMode> 
         ) : never
     ) : never
 ) : never;
