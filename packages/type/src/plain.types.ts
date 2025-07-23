363f2a6ace32044f2c07b52d6d85e668f765d3ec
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:34:48 +0300

    refactor: add PlainOnlyRequired type definition for improved type handling

--- packages/type/src/plain.types.ts
+++ packages/type/src/plain.types.ts
@@ -3,3 +3,7 @@ export type Plain = {
 };
 
 export type PlainTuple = readonly [Plain, ...Plain[]];
+
+export type PlainOnlyRequired<T> = T extends Plain ? {
+  [K in keyof T as {} extends Pick<T, K> ? never : K]: PlainOnlyRequired<Exclude<T[K], undefined>>;
+} : T;
