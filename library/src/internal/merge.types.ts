fd47d9f68cf5058c8ba8f88dc1d7113a2e470a9b
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 19:05:49 +0300

    refactor: update Merge type to use IntegratePlain for improved type handling

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -1,14 +1,14 @@
 import { ZuordUtil } from "@zuord/util";
-import { Integrate, IntegrateMode } from "./integrate.types";
+import { Integrate, IntegrateMode, IntegratePlain } from "./integrate.types";
 
 export type Merge<TContent, TMode extends ZuordUtil.Partialize<MergeMode>> = TContent extends [...infer TRest, infer TLast] ? (
     TRest["length"] extends 0 ? (
         TLast
     ) : 
     TRest["length"] extends 1 ? (
-        Integrate<TRest[0], TLast, TMode>
+        IntegratePlain<TRest[0], TLast, TMode>
     ) : (
-        Integrate<Merge<TRest, TMode>, TLast, TMode>
+        IntegratePlain<Merge<TRest, TMode>, TLast, TMode>
     )
 ) : {};
 
