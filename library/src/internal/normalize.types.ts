e7bf180b70b26f8b8bc4ed7798aea16067250f4d
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 13:34:59 +0300

    refactor: correct type extraction for ZuordType.Array and ZuordType.Plain in Normalize type

--- library/src/internal/normalize.types.ts
+++ library/src/internal/normalize.types.ts
@@ -11,11 +11,11 @@ export type Normalize<T, TMode extends NormalizeBaseMode = NormalizeDefaultMode>
     ) :
     [ZuordTrait.Has<T, ZuordType.Array>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
-        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, readonly unknown[]>[number], TMode>[] : never)
+        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, ZuordType.Array>[number], TMode>[] : never)
     ) :     
     [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
-        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted extends any ? ({
+        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? ({
             [K in TExtracted extends any ? keyof TExtracted : never]: K extends keyof TExtracted ? Normalize<TExtracted[K], TMode> : never;
         }) : never )
     ) : T
