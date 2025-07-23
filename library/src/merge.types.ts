ce0e6ae370e56605469bcc38e4d9938cfb40b1ec
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 19:15:57 +0300

    refactor: update Merge type to use Partial<MergeMode> for improved type flexibility

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -1,9 +1,6 @@
 import { InternalZuord as Internal } from "./internal";
-import { mergeMode } from "./merge";
-import { ZuordUtil } from "@zuord/util";
 import { ZuordType } from "@zuord/type";
-import { ZuordTrait } from "@zuord/trait";
 
-export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = Internal.Merge<TContent, TMode>;
+export type Merge<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = Internal.Merge<TContent, TMode>;
 
 export type MergeMode = Internal.MergeMode;
