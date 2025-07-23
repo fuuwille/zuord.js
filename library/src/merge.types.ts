c15fdec08bb4525d18058d4a727a1bed4ce53141
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 14:22:28 +0300

    refactor: update Merge type to use ZuordUtil.Normalize for improved consistency

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -1,12 +1,13 @@
 import { InternalZuord as Internal } from "./internal";
-import { ZuordType } from "@zuord/type";
 import { mergeMode } from "./merge";
+import { ZuordUtil } from "@zuord/util";
+import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
 
 export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = ZuordTrait.Is<TContent, ZuordType.Tuple> extends true ? (
     ZuordType.ArrayDepth<TContent> extends 1 ? (
         Internal.Merge<TContent, TMode>
     ) : never
-) : Internal.Normalize<TContent>;
+) : ZuordUtil.Normalize<TContent>;
 
 export type MergeMode = Internal.MergeMode;
