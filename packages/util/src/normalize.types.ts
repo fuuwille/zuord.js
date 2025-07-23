c11dc673eabcf7eb0b82321a51e4baadc4a80bd3
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 14:21:10 +0300

    feat: add Normalize type for enhanced type handling and flexibility

---
+++ packages/util/src/normalize.types.ts
@@ -0,0 +1,26 @@
+import { ZuordType } from "@zuord/type";
+import { ZuordTrait } from "@zuord/trait";
+import { ZuordCore } from "@zuord/core";
+
+export type Normalize<T, TMode extends NormalizeBaseMode = NormalizeDefaultMode> = [ZuordTrait.Eq<T, any>] extends [false] ? (
+    [ZuordTrait.Has<T, ZuordType.Tuple>] extends [true] ? (
+        (ZuordTrait.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
+        (ZuordTrait.Extract<T, ZuordType.Tuple> extends infer TExtracted ? ({  
+            [K in keyof TExtracted]: Normalize<TExtracted[K], TMode> 
+        }) : never )
+    ) :
+    [ZuordTrait.Has<T, ZuordType.Array>] extends [true] ? (
+        (ZuordTrait.Exclude<T, ZuordType.Array> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
+        (ZuordTrait.Extract<T, ZuordType.Array> extends infer TExtracted ? Normalize<Extract<TExtracted, ZuordType.Array>[number], TMode>[] : never)
+    ) :     
+    [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
+        (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
+        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? ({
+            [K in TExtracted extends any ? keyof TExtracted : never]: K extends keyof TExtracted ? Normalize<TExtracted[K], TMode> : never;
+        }) : never )
+    ) : T
+) : any;
+
+export type NormalizeBaseMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode]>;
+
+export type NormalizeDefaultMode = ZuordCore.BaseMode;
