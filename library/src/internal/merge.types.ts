27a62d8ade29dab774ce21d189ede02ff70d02b6
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:04:44 +0300

    refactor: restructure Merge type for improved clarity and consistency

--- library/src/internal/merge.types.ts
+++ library/src/internal/merge.types.ts
@@ -1,21 +1,23 @@
 import { ZuordUtil } from "@zuord/util";
-import { IntegrateMode, Integrate } from "./integrate.types";
-import { ZuordTrait } from "@zuord/trait";
+import { IntegrateMode, IntegratePlain } from "./integrate.types";
 import { ZuordType } from "@zuord/type";
 
-export type Merge<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = ZuordTrait.Is<TContent, ZuordType.Tuple> extends true ? (
-    ZuordType.ArrayDepth<TContent> extends 1 ? (
-        TContent extends [...infer TRest extends ZuordType.Plain[], infer TLast extends ZuordType.Plain] ? (
-            TRest["length"] extends 0 ? (
-                TLast
-            ) : 
-            TRest["length"] extends 1 ? (
-                Integrate<TRest[0], TLast, TMode>
-            ) : (
-                Integrate<Merge<TRest, TMode>, TLast, TMode>
-            )
-        ) : {}
-    ) : never
-) : ZuordUtil.Normalize<TContent, TMode>;
+export type Merge<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = (
+    TContent extends ZuordType.PlainTuple ? (
+        MergeSpecific<TContent, TMode>
+    ) : MergeCommon<TContent, TMode>
+);
+
+export type MergeSpecific<TContent extends ZuordType.PlainTuple, TMode extends Partial<MergeMode>> = (
+    TContent extends [...infer TRest extends ZuordType.PlainTuple, infer TLast extends ZuordType.Plain] ? (
+        TRest["length"] extends 0 ? TLast : 
+        TRest["length"] extends 1 ? IntegratePlain<TRest[0], TLast, TMode> 
+        : IntegratePlain<MergeSpecific<TRest, TMode>, TLast, TMode>
+    ) : {}
+) extends infer TMerged extends ZuordType.Plain ? TMerged : never;
+
+export type MergeCommon<TContent extends ZuordType.Plain[], TMode extends Partial<MergeMode>> = (
+    ZuordUtil.Normalize<TContent, TMode>
+);
 
 export type MergeMode = IntegrateMode;
