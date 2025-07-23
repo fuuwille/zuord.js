0d82220f5d3fb67080eabe4918c44470eb2c92da
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 19:13:23 +0300

    refactor: simplify Merge type by removing conditional logic for improved clarity

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -4,10 +4,6 @@ import { ZuordUtil } from "@zuord/util";
 import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
 
-export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = ZuordTrait.Is<TContent, ZuordType.Tuple> extends true ? (
-    ZuordType.ArrayDepth<TContent> extends 1 ? (
-        Internal.Merge<TContent, TMode>
-    ) : never
-) : ZuordUtil.Normalize<TContent, TMode>;
+export type Merge<TContent extends ZuordType.Plain[], TMode extends MergeMode = typeof mergeMode> = Internal.Merge<TContent, TMode>;
 
 export type MergeMode = Internal.MergeMode;
