efce4acdddd8e8e3b25b7cdf4ccf6b324950ea10
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:31:57 +0300

    refactor: ensure consistent formatting and structure in union types definition

--- packages/type/src/union.types.ts
+++ packages/type/src/union.types.ts
@@ -1,3 +1,5 @@
 import { ZuordType } from ".";
 
 export type UnionOf<U extends ZuordType.Tuple> = U[number]
+
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I: never;
