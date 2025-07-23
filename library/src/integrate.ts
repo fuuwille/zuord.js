42da07f4e0baa921e16f60db1a1298f5fa51d4b7
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 19:18:45 +0300

    refactor: simplify integrate function by removing unnecessary variable assignment for improved clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -1,11 +1,9 @@
 import { internalZuord as internal } from "./internal";
-import { ZuordCore, zuordCore } from "@zuord/core";
+import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
 
 export function integrate <A extends unknown, B extends unknown, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
-    const resolvedMode = zuordCore.modeResolve([integrateMode, mode ?? {}]);
-
-    return internal.integrate(a, b, resolvedMode) as Integrate<A, B, TMode>;
+    return internal.integrate(a, b, zuordCore.modeResolve([integrateMode, mode ?? {}])) as Integrate<A, B, TMode>;
 }
 
 export const integrateMode = internal.integrateMode satisfies IntegrateMode;
