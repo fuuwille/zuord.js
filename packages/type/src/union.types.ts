7e23f7a261d2ce114ee3fc6817e63104a62efab4
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:33:36 +0300

    refactor: add UnionToTuple type definition for improved type handling

--- packages/type/src/union.types.ts
+++ packages/type/src/union.types.ts
@@ -5,3 +5,5 @@ export type UnionOf<U extends ZuordType.Tuple> = U[number]
 export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
 export type UnionToLast<U> = UnionToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
+
+export type UnionToTuple<U, R extends any[] = []> = [U] extends [never]? R : UnionToTuple<Exclude<U, UnionToLast<U>>, [UnionToLast<U>, ...R]>;
