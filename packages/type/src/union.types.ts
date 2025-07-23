0d3a8ba781be64fd9e453bc7c563047aee4ae595
k4yr2 <kyrzkaya@gmail.com>
2025-07-23 20:33:14 +0300

    refactor: improve formatting and add UnionToLast type definition for clarity

--- packages/type/src/union.types.ts
+++ packages/type/src/union.types.ts
@@ -2,4 +2,6 @@ import { ZuordType } from ".";
 
 export type UnionOf<U extends ZuordType.Tuple> = U[number]
 
-export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I: never;
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+
+export type UnionToLast<U> = UnionToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
