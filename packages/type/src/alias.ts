e7b11112817900712cd9c22cd0459b308c0a1541
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:09:12 +0300

    refactor: remove unused array type alias and related exports for consistency

--- packages/type/src/alias.ts
+++ packages/type/src/alias.ts
@@ -1,3 +1,2 @@
 export * from "./object";
-export * from "./array";
 export * from "./plain";
--- packages/type/src/array.ts
+++
@@ -1,5 +0,0 @@
-import { ZuordType } from ".";
-
-export function array(obj: unknown) : obj is ZuordType.Array {
-    return Array.isArray(obj);
-}
