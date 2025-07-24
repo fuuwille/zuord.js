440aab124d256dd1ac93669e0c27529507a170c5
k4yr2 <kyrzkaya@gmail.com>
2025-07-24 21:56:35 +0300

    refactor: correct import statement for ZuordType and replace zuordType.plain with isPlain function for consistency

--- library/src/internal/integrate.ts
+++ library/src/internal/integrate.ts
@@ -1,5 +1,5 @@
 import { zuordCore } from "@zuord/core";
-import { zuordType } from "@zuord/type";
+import { ZuordType } from "@zuord/type";
 import { IntegrateMode, IntegrateShape } from "./integrate.types";
 
 export function integrate<TShape extends IntegrateShape, A extends TShape, B extends TShape, TMode extends Partial<IntegrateMode>>(a: A, b: B, mode?: TMode) {
@@ -23,7 +23,7 @@ export function integrate<TShape extends IntegrateShape, A extends TShape, B ext
 
             if (concat && Array.isArray(valA) && Array.isArray(valB)) {
                 target[key] = [...valA, ...valB];
-            } else if (valB !== undefined && zuordType.plain(valA) && zuordType.plain(valB)) {
+            } else if (valB !== undefined && isPlain(valA) && isPlain(valB)) {
                 target[key] = {};
                 stack.push({ target: target[key], sourceA: valA, sourceB: valB });
             } else if (valB !== undefined) {
@@ -38,3 +38,10 @@ export function integrate<TShape extends IntegrateShape, A extends TShape, B ext
 }
 
 export const integrateMode = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode]) satisfies IntegrateMode;
+
+
+export function isPlain(obj: unknown) : obj is ZuordType.Plain {
+    if (typeof obj !== "object" || obj === null) return false;
+
+    return Object.getPrototypeOf(obj) === Object.prototype;
+}
