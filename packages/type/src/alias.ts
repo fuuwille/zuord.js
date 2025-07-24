92a0a3d842ab810dc9c9ce24eb2dd5f38cd88675
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:24:26 +0300

    refactor: restore Tuple and TupleLike types in tuple.ts for consistency

--- packages/type/src/alias.ts
+++ packages/type/src/alias.ts
@@ -1,6 +1,6 @@
 export type * from "./absence";
 export type * from "./function";
-export type * from "./tuple.types";
+export type * from "./tuple";
 export type * from "./array";
 export type * from "./plain";
 export type * from "./union.types";
