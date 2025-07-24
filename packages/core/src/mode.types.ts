ad67fd169d1339d460b6396f226bc48f1db901e4
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:52:58 +0300

    refactor: add ModeOn type for improved type resolution

--- packages/core/src/mode.types.ts
+++ packages/core/src/mode.types.ts
@@ -4,6 +4,8 @@ export type ModeField<K extends string = string> = Internal.ModeField<K>;
 
 export type ModeResolve<TModes extends ModeField[]> = Internal.ModeResolve<TModes>;
 
+export type ModeOn<TMode, TKey> = Internal.ModeOn<TMode, TKey>;
+
 export type ShallowMode = Internal.ShallowMode;
 
 export type ConcatMode = Internal.ConcatMode;
