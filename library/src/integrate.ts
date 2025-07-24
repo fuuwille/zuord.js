5a88118f92cda9d468a33833b8d12241e54e2907
k4yr2 <kyrzkaya@gmail.com>
2025-07-24 21:58:19 +0300

    refactor: simplify generic parameters in integrate function for clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -3,7 +3,7 @@ import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
 import { IntegrateShape } from "./internal/integrate.types";
 
-export function integrate <TShape extends IntegrateShape, A extends TShape, B extends TShape, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
+export function integrate <A extends IntegrateShape, B extends IntegrateShape, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
     return internal.integrate(a, b, zuordCore.modeResolve([integrateMode, mode ?? {}])) as Integrate<A, B, TMode>;
 }
 
