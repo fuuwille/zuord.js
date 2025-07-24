7e6b100fc0d780d133dba28363d263f3a5e38c5e
k4yr2 <kyrzkaya@gmail.com>
2025-07-24 18:19:19 +0300

    refactor: update type parameters in integrate function for improved clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -2,7 +2,7 @@ import { internalZuord as internal } from "./internal";
 import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
 
-export function integrate <A extends unknown, B extends unknown, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
+export function integrate <A extends object, B extends object, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
     return internal.integrate(a, b, zuordCore.modeResolve([integrateMode, mode ?? {}])) as Integrate<A, B, TMode>;
 }
 
