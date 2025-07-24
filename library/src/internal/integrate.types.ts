7783abaabe22fa942e49d83bac1a7990f091a980
k4yr2 <kyrzkaya@gmail.com>
2025-07-24 22:00:36 +0300

    refactor: remove ZuordUtil import and standardize TMode type across Integrate functions

--- library/src/internal/integrate.types.ts
+++ library/src/internal/integrate.types.ts
@@ -1,7 +1,6 @@
 import { ZuordCore } from "@zuord/core";
 import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
-import { ZuordUtil } from "@zuord/util";
 
 export type Integrate<A, B, TMode extends Partial<IntegrateMode>> = (
     [ZuordTrait.IsEvery<[A, B], ZuordType.Array>] extends [true] ? (
@@ -16,23 +15,23 @@ export type Integrate<A, B, TMode extends Partial<IntegrateMode>> = (
     ) : B
 );
 
-export type IntegrateArray<A extends ZuordType.Array, B extends ZuordType.Array, TMode extends ZuordUtil.Partialize<IntegrateMode>> = (
+export type IntegrateArray<A extends ZuordType.Array, B extends ZuordType.Array, TMode extends Partial<IntegrateMode>> = (
     [TMode["concat"]] extends [true] ? [...A, ...B] : B
 )
 
-export type IntegratePlain<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends ZuordUtil.Partialize<IntegrateMode>> = (
+export type IntegratePlain<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends Partial<IntegrateMode>> = (
     (IntegratePlainOverlap<A, B, TMode> & IntegratePlainExtras<A, B, TMode>) extends infer TIntegrated ? ({
         [K in keyof TIntegrated]: TIntegrated[K];
     }) : never
 )
 
-export type IntegratePlainOverlap<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends ZuordUtil.Partialize<IntegrateMode>> = ({
+export type IntegratePlainOverlap<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode extends Partial<IntegrateMode>> = ({
     [K in keyof A]: K extends keyof B ? (
         [TMode["shallow"]] extends [true] ? B[K] : Integrate<A[K], B[K], TMode>
     ) : A[K];
 });
 
-export type IntegratePlainExtras<A extends ZuordType.Plain, B extends ZuordType.Plain, _TMode extends ZuordUtil.Partialize<IntegrateMode>> = ({
+export type IntegratePlainExtras<A extends ZuordType.Plain, B extends ZuordType.Plain, _TMode extends Partial<IntegrateMode>> = ({
     [K in keyof B as K extends keyof A ? never : K]: B[K];
 });
 
