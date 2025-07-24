29f88c469105427547719492d43f74f9942abe94
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:12:09 +0300

    refactor: remove duplicate export of merge types for consistency

--- library/src/internal/alias.types.ts
+++ library/src/internal/alias.types.ts
@@ -1,6 +1,3 @@
 export type * from "./outcast.types";
 export type * from "./merge.types";
-export type * from "./merge.types";
-export type * from "./merge.types";
-export type * from "./integrate.types";
 export type * from "./integrate.types";
