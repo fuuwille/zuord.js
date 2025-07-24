c5795d00c6543d066ce22d3bb2ec64eef074cc7a
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:32:37 +0300

    refactor: update PlainArray and PlainTuple types for consistency

--- packages/type/src/plain.ts
+++ packages/type/src/plain.ts
@@ -1,12 +1,13 @@
+import { ArrayOf } from "./array";
 import { TupleOf } from "./tuple";
 
 export type Plain = {
   [key: PropertyKey]: unknown;
 };
 
-export type PlainTuple = TupleOf<Plain>;
+export type PlainArray = ArrayOf<Plain>;
 
-export type PlainArray = readonly Plain[];
+export type PlainTuple = TupleOf<Plain>;
 
 export type PlainAsRequired<T> = T extends Plain ? {
   [K in keyof T as {} extends Pick<T, K> ? never : K]: PlainAsRequired<Exclude<T[K], undefined>>;
