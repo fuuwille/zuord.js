bcc468931ff8b96c5bba57427920301997ffdb68
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 01:11:00 +0300

    refactor: improve input validation in integrate function to ensure both arguments are either plain objects or arrays

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -4,6 +4,14 @@ import { Integrate, IntegrateMode } from "./integrate.types";
 import { IntegrateShape } from "./internal/integrate.types";
 
 export function integrate <A extends IntegrateShape, B extends IntegrateShape, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
+    if(!(internal.integrateShape(a))) {
+        throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
+    }
+    
+    if(!(internal.integrateShape(b))) {
+        throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
+    }
+
     return internal.integrate(a, b, zuordCore.modeResolve([integrateMode, mode ?? {}])) as Integrate<A, B, TMode>;
 }
 
