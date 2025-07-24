2e376d5f4ab5d2b1519772b96647d7f2b65a2e04
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 01:13:07 +0300

    refactor: add type signature for integrate function to enhance clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -3,11 +3,14 @@ import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
 import { IntegrateShape } from "./internal/integrate.types";
 
+export function integrate <A extends IntegrateShape, B extends IntegrateShape> (a: A, b: B)
+    : Integrate<A, B>;
+
 export function integrate <A extends IntegrateShape, B extends IntegrateShape, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
     if(!(internal.integrateShape(a))) {
         throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
     }
-    
+
     if(!(internal.integrateShape(b))) {
         throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
     }
