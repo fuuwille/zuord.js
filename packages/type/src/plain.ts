03f12a3e4bf686850a582818da514a61b52d1d3a
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 00:09:28 +0300

    refactor: remove unused plain function and related imports for consistency

--- packages/type/src/plain.ts
+++
@@ -1,7 +0,0 @@
-import { ZuordType } from ".";
-
-export function plain(obj: unknown) : obj is ZuordType.Plain {
-    if (typeof obj !== "object" || obj === null) return false;
-
-    return Object.getPrototypeOf(obj) === Object.prototype;
-}
