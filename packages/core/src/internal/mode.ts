eff42b08e0eb6b1b6fce79d897f4068d277c2444
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 10:06:53 +0300

    refactor: replace Internal types with direct imports for ModeField and ModeResolve

--- packages/core/src/internal/mode.ts
+++ packages/core/src/internal/mode.ts
@@ -1,7 +1,7 @@
-import { InternalZuordCore as Internal } from "./index";
+import { BaseMode, ConcatMode, ModeField, ModeResolve, ShallowMode } from "./mode.types";
 
 export const modeField = <const K extends string, const V extends boolean>(key: K | K[], value: V) => {
-    let field = {} as Internal.ModeField<K, V>;
+    let field = {} as ModeField<K, V>;
 
     const keys = Array.isArray(key) ? key : [key];
     for (const k of keys) field[k] = value;
@@ -9,12 +9,12 @@ export const modeField = <const K extends string, const V extends boolean>(key: 
     return field;
 }
 
-export const modeResolve = <const TModes extends Internal.ModeField[]>(modes: TModes) => {
-    return Object.assign({}, ...modes) as Internal.ModeResolve<TModes>;
+export const modeResolve = <const TModes extends ModeField[]>(modes: TModes) => {
+    return Object.assign({}, ...modes) as ModeResolve<TModes>;
 };
 
-export const shallowMode = modeField("shallow", false) satisfies Internal.ShallowMode;
+export const shallowMode = modeField("shallow", false) satisfies ShallowMode;
 
-export const concatMode = modeField("concat", false) satisfies Internal.ConcatMode;
+export const concatMode = modeField("concat", false) satisfies ConcatMode;
 
-export const baseMode = modeResolve([shallowMode]) satisfies Internal.BaseMode;
+export const baseMode = modeResolve([shallowMode]) satisfies BaseMode;
