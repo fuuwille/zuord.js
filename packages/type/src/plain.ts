59b5c28bfc96fcdb9a0ab1700c3abf8e1a8bea11
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 17:08:22 +0300

    feat: add plainArray function for type checking of PlainArray objects

--- packages/type/src/plain.ts
+++ packages/type/src/plain.ts
@@ -1,8 +1,14 @@
-import { Plain } from "./plain.types";
+import { Plain, PlainArray } from "./plain.types";
 
 export function plain(obj: unknown) : obj is Plain {
     if (typeof obj !== 'object' || obj === null) return false;
 
     let proto = Object.getPrototypeOf(obj);
     return proto === null || proto === Object.prototype;
+}
+
+export function plainArray(obj: unknown) : obj is PlainArray {
+    if (!Array.isArray(obj)) return false;
+
+    return obj.every(item => plain(item));
 }
