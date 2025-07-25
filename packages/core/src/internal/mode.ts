6d64cb5ad0598b106ce94848fe626b0e4ca953ee
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 10:28:02 +0300

    refactor: remove unused BaseMode, ShallowMode, and ConcatMode type definitions for simplification

--- packages/core/src/internal/mode.ts
+++ packages/core/src/internal/mode.ts
@@ -1,4 +1,4 @@
-import { BaseMode, ConcatMode, ModeField, ModeResolve, ShallowMode } from "./mode.types";
+import { ModeField, ModeResolve } from "./mode.types";
 
 export const modeField = <const K, const V>(key: K | K[], value: V) => {
     let field : Record<string, boolean> = {};
@@ -14,9 +14,3 @@ export const modeField = <const K, const V>(key: K | K[], value: V) => {
 export const modeResolve = <const TModes extends ModeField[]>(modes: TModes) => {
     return Object.assign({}, ...modes) as ModeResolve<TModes>;
 };
-
-export const shallowMode = modeField("shallow", false) satisfies ShallowMode;
-
-export const concatMode = modeField("concat", false) satisfies ConcatMode;
-
-export const baseMode = modeResolve([shallowMode]) satisfies BaseMode;
