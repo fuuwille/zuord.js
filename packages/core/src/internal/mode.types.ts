646552eaec0ab599ed3e98d72fb8641cb5529183
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 10:26:33 +0300

    refactor: update ModeResolve type to correctly infer boolean values from TModes

--- packages/core/src/internal/mode.types.ts
+++ packages/core/src/internal/mode.types.ts
@@ -4,7 +4,10 @@ export type ModeField<K = string, V = boolean> = K extends string ? {
 
 export type ModeResolve<TModes> = TModes extends [...infer TRest, infer TLast] ? (
   ModeResolve<TRest> extends infer TResolvedRest ? {
-    [K in keyof TResolvedRest | keyof TLast]: boolean
+    [K in keyof TResolvedRest | keyof TLast]: (
+      K extends keyof TLast ? TLast[K] :
+      K extends keyof TResolvedRest ? TResolvedRest[K] : never
+    ) extends infer TResolved extends boolean ? TResolved : false
   } : never
 ) : {};
 
