f78bbb0868f285a61d277fcff4f171b6cdb52bd6
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 16:21:53 +0300

    fix: correct import alias for InternalZuord in pick.types.ts

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -1,5 +1,5 @@
 import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
-import { InternalZuord } from "./internal";
+import { InternalZuord as Internal } from "./internal";
 
-export type Pick<TShape extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern<TShape>> = InternalZuord.Pick<TShape, TPattern>;
+export type Pick<TShape extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern<TShape>> = Internal.Pick<TShape, TPattern>;
