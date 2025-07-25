6f8b54f98798c6e66e62314a10a5daa7609800c0
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 10:16:40 +0300

    refactor: enhance modeField type definition to support generic boolean values

--- packages/core/src/mode.ts
+++ packages/core/src/mode.ts
@@ -1,14 +1,14 @@
 import { internalZuordCore as internal } from "./internal";
 import { ModeField, ModeResolve, ShallowMode, ConcatMode, BaseMode } from "./mode.types";
 
-export function modeField <K extends string>(key: K, value?: boolean) : ModeField<K>
+export function modeField <K extends string, const V extends boolean>(key: K, value?: V) : ModeField<K>
 
-export function modeField <K extends string>(key: K[], value?: boolean) : ModeField<K>
+export function modeField <K extends string, const V extends boolean>(key: K[], value?: V) : ModeField<K>
 
-export function modeField <K extends string>(key: K | K[], value?: boolean) : ModeField<K>;
+export function modeField <K extends string, const V extends boolean>(key: K | K[], value?: V) : ModeField<K>;
 
-export function modeField <K extends string>(key: K | K[], value: boolean = false) : ModeField<K> {
-    return internal.modeField(key, value);
+export function modeField <K extends string, const V extends boolean>(key: K | K[], value?: V) {
+    return internal.modeField(key, value ?? false) as ModeField<K, V>;
 }
 
 export function modeResolve <const TModes extends ModeField[]>(modes: TModes): ModeResolve<TModes> {
