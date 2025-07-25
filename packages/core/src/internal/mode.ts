79719b3491c7d4d632a3bae729f039f04cbd0c7e
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 10:36:56 +0300

    feat: add modeOn function for mode management

--- packages/core/src/internal/mode.ts
+++ packages/core/src/internal/mode.ts
@@ -1,4 +1,4 @@
-import { ModeField, ModeResolve } from "./mode.types";
+import { ModeField, ModeResolve, ModeOn } from "./mode.types";
 
 export const modeField = <const K, const V>(key: K | K[], value: V) => {
     let field : Record<string, boolean> = {};
@@ -14,3 +14,21 @@ export const modeField = <const K, const V>(key: K | K[], value: V) => {
 export const modeResolve = <const TModes extends ModeField[]>(modes: TModes) => {
     return Object.assign({}, ...modes) as ModeResolve<TModes>;
 };
+
+export const modeOn = <TMode, TKey>(mode: TMode, key: TKey | TKey[]) => {
+    if(!isModeField(mode)) {
+        throw new TypeError("Mode must be an object.");
+    }
+
+    if(typeof key == "string") {
+        return mode[key] ?? false as ModeOn<TMode, TKey>;
+    }
+}
+
+const isModeField = (value: unknown): value is ModeField => {
+    if (typeof value !== "object" || value === null || Array.isArray(value)) {
+        return false;
+    }
+
+    return Object.values(value).every(v => typeof v === "boolean");
+}
