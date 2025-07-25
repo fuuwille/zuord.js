365f752bb80799db79222bade9e339b9719ba091
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 01:22:40 +0300

    fix: correct key mapping in Pick type definition for accurate property selection

--- library/src/internal/pick.types.ts
+++ library/src/internal/pick.types.ts
@@ -2,7 +2,7 @@ import { ZuordType } from "@zuord/type";
 
 export type Pick<T, P> = (
     T extends ZuordType.Plain ? P extends ZuordType.Plain ? ({
-        [K in keyof P]: (
+        [K in keyof T & keyof P]: (
             [P[K]] extends [true] ? T[K] : Pick<T[K], P[K]>
         )
     }) : never : never
