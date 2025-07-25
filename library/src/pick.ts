ac29ce5c2cd94bf07fb10046f8b6970943115552
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 00:49:52 +0300

    feat: add pick function for selecting object properties based on a pattern

---
+++ library/src/pick.ts
@@ -0,0 +1,8 @@
+import { ZuordType } from "@zuord/type";
+import { ZuordUtil } from "@zuord/util";
+import { internalZuord } from "./internal";
+import { Pick } from "./pick.types";
+
+export function pick<TPlain extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern<TPlain>>(plain: TPlain, pattern: TPattern) {
+    return internalZuord.pick(plain, pattern) as Pick<TPlain, TPattern>;
+}
