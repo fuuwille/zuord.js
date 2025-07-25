4999bb266d9f0e52faf52be9450bb299a5df582d
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 00:40:37 +0300

    refactor: update Merge type to use ShapeZuord.MergeContent for improved type accuracy

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -1,10 +1,9 @@
 import { InternalZuord as Internal } from "./internal";
 import { ZuordCore } from "@zuord/core";
 import { mergeMode } from "./merge";
+import { ShapeZuord } from "./shape";
 
-export type Merge<TContent extends MergeShape, TMode extends Partial<MergeMode> = {}> 
+export type Merge<TContent extends ShapeZuord.MergeContent, TMode extends Partial<MergeMode> = {}> 
     = Internal.Merge<TContent, ZuordCore.ModeResolve<[typeof mergeMode, TMode]>>;
 
 export type MergeMode = Internal.MergeMode;
-
-export type MergeShape = Internal.MergeShape;
