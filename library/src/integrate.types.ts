65df70499e398b2214327923eb8e67ce1019e7d5
k4yr2 <kyrzkaya@gmail.com>
2025-07-25 17:22:29 +0300

    refactor: update Integrate type to use ZuordShape for improved type consistency

--- library/src/integrate.types.ts
+++ library/src/integrate.types.ts
@@ -1,12 +1,8 @@
 import { InternalZuord as Internal } from "./internal";
 import { integrateMode } from "./integrate";
 import { ZuordCore } from "@zuord/core";
+import { ZuordShape } from "./shape";
 
-export type Integrate<A extends IntegrateShape, B extends IntegrateShape, TMode extends Partial<IntegrateMode> = {}> = Internal.Integrate<A, B, ZuordCore.ModeResolve<[typeof integrateMode, TMode]>>;
+export type Integrate<A extends ZuordShape.Integrate, B extends ZuordShape.Integrate, TMode extends Partial<IntegrateMode> = {}> = Internal.Integrate<A, B, ZuordCore.ModeResolve<[typeof integrateMode, TMode]>>;
 
 export type IntegrateMode = Internal.IntegrateMode;
-
-/**
- * @puretype
- */
-export type IntegrateShape = Internal.IntegrateShape;
