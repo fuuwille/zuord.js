ab6bec25d119307edcf22e8a3794fc4dc4cb8dd4
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 00:41:04 +0300

    refactor: update merge function types to use ShapeZuord for improved type consistency

--- library/src/merge.ts
+++ library/src/merge.ts
@@ -1,14 +1,15 @@
 import { internalZuord as internal } from "./internal";
 import { zuordCore } from "@zuord/core";
-import { MergeMode, Merge, MergeShape } from "./merge.types";
+import { MergeMode, Merge } from "./merge.types";
+import { ShapeZuord } from "./shape";
 
-export function merge <TContent extends MergeShape> (content: [...TContent]) 
+export function merge <TContent extends ShapeZuord.MergeContent> (content: [...TContent]) 
     : Merge<TContent>;
 
-export function merge <TContent extends MergeShape, TMode extends Partial<MergeMode>> (content: [...TContent], mode: TMode)
+export function merge <TContent extends ShapeZuord.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode: TMode)
     : Merge<TContent, TMode>;
 
-export function merge <TContent extends MergeShape, TMode extends Partial<MergeMode>> (content: [...TContent], mode?: TMode) {
+export function merge <TContent extends ShapeZuord.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode?: TMode) {
     return internal.merge(content, zuordCore.modeResolve([mergeMode, mode ?? {}])) as Merge<TContent, TMode>;
 }
 
