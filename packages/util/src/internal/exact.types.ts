578a78a20b287c4b50df92d06196c15ff1435607
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 08:50:12 +0300

    feat: simplify Exact type definition for improved readability and structure

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -2,13 +2,11 @@ import { ZuordType } from "@zuord/type";
 
 export type Exact<Base, Input> =
   [Base] extends [ZuordType.Plain]
-    ? [Input] extends [ZuordType.Plain]
-      ? [keyof Input] extends [never]
-        ? {}
-        : {
+    ? [Input] extends [ZuordType.Plain] ?
+        {
             [K in Exclude<keyof Input, Exclude<keyof Input, keyof Base>>]: Exact<Base[K], Input[K]>
-          } & {
+        } & {
             [K in Exclude<keyof Input, keyof Base>]: never;
-          }
+        }
       : never
     : Input extends true ? Input : Base;
