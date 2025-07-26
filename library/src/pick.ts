8b48f711992fb74b862dbfb5fd16da68397db9b7
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:36:28 +0300

    refactor: update pick function to use Shape.PickPlain for improved type consistency

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -3,6 +3,6 @@ import { internalZuord as internal } from "./internal";
 import { Pick } from "./pick.types";
 import { ShapeZuord as Shape } from "./shape";
 
-export function pick<TPlain extends ZuordType.Plain, const TPattern extends Shape.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+export function pick<TPlain extends Shape.PickPlain, const TPattern extends Shape.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     return internal.pick(plain, pattern) as Pick<TPlain, TPattern>;
 }
