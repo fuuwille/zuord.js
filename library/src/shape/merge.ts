fe8d7288782e0b0b74b3eb003e36bc2f19b3d9cc
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:56:32 +0300

    refactor: consolidate MergeMode import and add integrateMode function for improved clarity

--- library/src/shape/merge.ts
+++ library/src/shape/merge.ts
@@ -1,7 +1,11 @@
 import { zuordType } from "@zuord/type";
 import { integrateElement } from "./integrate";
-import { MergeContent } from "./merge.types";
+import { MergeContent, MergeMode } from "./merge.types";
 
 export function mergeContent(obj: unknown): obj is MergeContent {
     return zuordType.arrayOf(obj, integrateElement);
 }
+
+export const integrateMode = (obj: unknown): obj is MergeMode => {
+    return zuordType.plain(obj);
+};
