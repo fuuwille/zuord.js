a28ca4dcb177c325b73ee46f4973e5d5ecfae377
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 08:45:41 +0300

    feat: improve key mapping in Pick type for enhanced type safety

--- library/src/internal/pick.types.ts
+++ library/src/internal/pick.types.ts
@@ -2,7 +2,7 @@ import { ZuordType } from "@zuord/type";
 
 export type Pick<T, P> = (
     T extends ZuordType.Plain ? P extends ZuordType.Plain ? ({
-        [K in keyof T & keyof P]: (
+        [K in keyof T as K extends keyof P ? K : never]: (
             [P[K]] extends [true] ? T[K] : Pick<T[K], P[K]>
         )
     }) : never : never
