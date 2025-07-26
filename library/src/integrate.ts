b61fc475226590cc186cf1ba6077d047dbb84192
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:45:10 +0300

    refactor: correct import alias for shapeZuord for improved clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -1,7 +1,7 @@
 import { internalZuord as internal } from "./internal";
 import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
-import { shapeZuord, ShapeZuord as Shape } from "./shape";
+import { shapeZuord as shape, ShapeZuord as Shape } from "./shape";
 
 export function integrate <A extends Shape.IntegrateElement, B extends Shape.IntegrateElement> (a: A, b: B)
     : Integrate<A, B>;
@@ -10,11 +10,11 @@ export function integrate <A extends Shape.IntegrateElement, B extends Shape.Int
     : Integrate<A, B, TMode>;
 
 export function integrate <A extends Shape.IntegrateElement, B extends Shape.IntegrateElement, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
-    if(!(shapeZuord.integrateElement(a))) {
+    if(!(shape.integrateElement(a))) {
         throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
     }
 
-    if(!(shapeZuord.integrateElement(b))) {
+    if(!(shape.integrateElement(b))) {
         throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
     }
 
