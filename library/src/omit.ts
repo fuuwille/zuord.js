c7fd8ba1b9d54352a6998ca218dcebe94c688493
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 15:41:16 +0300

    feat: implement omit and omitLoose functions for enhanced object manipulation

---
+++ library/src/omit.ts
@@ -0,0 +1,19 @@
+import { internalZuord as internal } from "./internal";
+import { ShapeZuord as Shape, shapeZuord as shape } from "./shape";
+import { Omit, OmitLoose } from "./omit.types";
+
+export function omit<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+    if(!shape.omitPlain(plain)) {
+        throw new TypeError("Pick function expects the target object to be a plain.");
+    }
+
+    return internal.omit(plain, pattern) as Omit<TPlain, TPattern>;
+}
+
+export function omitLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPatternLoose<TPlain>>(plain: TPlain, pattern: TPattern) {
+    if(!shape.omitPlain(plain)) {
+        throw new TypeError("Pick function expects the target object to be a plain.");
+    }
+
+    return internal.omit(plain, pattern) as OmitLoose<TPlain, TPattern>;
+}
