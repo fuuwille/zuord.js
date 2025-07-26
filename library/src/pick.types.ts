88910c01b3895be2149a54784f275814bcad63a8
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:27:55 +0300

    refactor: update Pick type definition to use ExactKeys for improved type safety

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -2,4 +2,4 @@ import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
 import { InternalZuord as Internal } from "./internal";
 
-export type Pick<TShape extends ZuordType.Plain, TPattern extends ZuordUtil.Exact<ZuordUtil.Pattern<TShape>, TPattern>> = Internal.Pick<TShape, TPattern>;
+export type Pick<TShape extends ZuordType.Plain, TPattern extends ZuordUtil.ExactKeys<ZuordUtil.Pattern<TShape>, TPattern>> = Internal.Pick<TShape, TPattern>;
