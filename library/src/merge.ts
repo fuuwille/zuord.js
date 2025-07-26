ef1acd848ac406c7bfda5e06fd4dbb61ea6d47b2
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:46:23 +0300

    refactor: correct import aliases for improved clarity

--- library/src/merge.ts
+++ library/src/merge.ts
@@ -1,20 +1,20 @@
 import { internalZuord as internal } from "./internal";
-import { zuordCore } from "@zuord/core";
+import { zuordCore as zuord } from "@zuord/core";
 import { MergeMode, Merge } from "./merge.types";
-import { shapeZuord, ShapeZuord } from "./shape";
+import { shapeZuord as shape, ShapeZuord as Shape } from "./shape";
 
-export function merge <TContent extends ShapeZuord.MergeContent> (content: [...TContent]) 
+export function merge <TContent extends Shape.MergeContent> (content: [...TContent]) 
     : Merge<TContent>;
 
-export function merge <TContent extends ShapeZuord.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode: TMode)
+export function merge <TContent extends Shape.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode: TMode)
     : Merge<TContent, TMode>;
 
-export function merge <TContent extends ShapeZuord.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode?: TMode) {
-    if(!(shapeZuord.mergeContent(content))) {
+export function merge <TContent extends Shape.MergeContent, TMode extends Partial<MergeMode>> (content: [...TContent], mode?: TMode) {
+    if(!(shape.mergeContent(content))) {
         throw new TypeError("Merge function expects the content to be an array of integrate elements.");
     }
 
-    return internal.merge(content, zuordCore.modeResolve([mergeMode, mode ?? {}])) as Merge<TContent, TMode>;
+    return internal.merge(content, zuord.modeResolve([mergeMode, mode ?? {}])) as Merge<TContent, TMode>;
 }
 
 export const mergeMode = internal.mergeMode satisfies MergeMode;
