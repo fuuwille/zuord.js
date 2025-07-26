99200c1191445ab4b6e94a06035aa1df376d60a8
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 09:22:00 +0300

    refactor: simplify Exact type definition for improved clarity

--- packages/util/src/exact.types.ts
+++ packages/util/src/exact.types.ts
@@ -1,3 +1,3 @@
 import { InternalZuordUtil as Internal } from "./internal";
 
-export type Exact<TBase, TContent> = Internal.Exact<TBase, TContent> extends infer TExact extends TBase ? TExact : never;
+export type Exact<TBase, TContent> = Internal.Exact<TBase, TContent>;
