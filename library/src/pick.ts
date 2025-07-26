4dc32224a0a983aa2a82054f3f872cc647cbd181
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:35:50 +0300

    refactor: standardize imports and improve function definition in pick.ts

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -1,8 +1,8 @@
 import { ZuordType } from "@zuord/type";
-import { internalZuord } from "./internal";
+import { internalZuord as internal } from "./internal";
 import { Pick } from "./pick.types";
-import { ShapeZuord } from "./shape";
+import { ShapeZuord as Shape } from "./shape";
 
-export function pick<TPlain extends ZuordType.Plain, const TPattern extends ShapeZuord.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
-    return internalZuord.pick(plain, pattern) as Pick<TPlain, TPattern>;
+export function pick<TPlain extends ZuordType.Plain, const TPattern extends Shape.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+    return internal.pick(plain, pattern) as Pick<TPlain, TPattern>;
 }
