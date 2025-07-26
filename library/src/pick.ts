f52f25aa1335206942671dfad4d4982450583011
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 12:05:00 +0300

    refactor: update pick function to use PickExactPattern for improved type accuracy

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -1,11 +1,19 @@
 import { internalZuord as internal } from "./internal";
 import { ShapeZuord as Shape, shapeZuord as shape } from "./shape";
-import { Pick } from "./pick.types";
+import { Pick, PickLoose } from "./pick.types";
 
-export function pick<TPlain extends Shape.PickPlain, const TPattern extends Shape.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+export function pick<TPlain extends Shape.PickPlain, const TPattern extends Shape.PickExactPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     if(!shape.pickPlain(plain)) {
         throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
     return internal.pick(plain, pattern) as Pick<TPlain, TPattern>;
+}
+
+export function pickLoose<TPlain extends Shape.PickPlain, const TPattern extends Shape.PickPattern<TPlain>>(plain: TPlain, pattern: TPattern) {
+    if(!shape.pickPlain(plain)) {
+        throw new TypeError("Pick function expects the target object to be a plain.");
+    }
+
+    return internal.pick(plain, pattern) as PickLoose<TPlain, TPattern>;
 }
