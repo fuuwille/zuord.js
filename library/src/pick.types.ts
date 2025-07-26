32ef679f7a938336e9046988e86aa163094b223c
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 12:02:12 +0300

    refactor: update Pick type to use PickExactPattern for improved type accuracy

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -1,4 +1,6 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
 
-export type Pick<TPlain extends Shape.PickPlain, TPattern extends Shape.PickPattern<TPlain>> = Internal.Pick<TPlain, TPattern>;
+export type Pick<TPlain extends Shape.PickPlain, TPattern extends Shape.PickExactPattern<TPlain, Shape.PickPattern<TPlain>>> = Internal.Pick<TPlain, TPattern>;
+
+export type PickLose<TPlain extends Shape.PickPlain, TPattern extends Shape.PickPattern<TPlain>> = Internal.Pick<TPlain, TPattern>;
