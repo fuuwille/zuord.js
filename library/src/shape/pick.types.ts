89a1eceeba6763bfef6e39ae1bf354c81b5c27b5
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:33:34 +0300

    refactor: add PickPattern type definition for enhanced type validation

--- library/src/shape/pick.types.ts
+++ library/src/shape/pick.types.ts
@@ -1,3 +1,6 @@
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordUtil as Util } from "@zuord/util";
 
 export type PickPlain = Type.Plain;
+
+export type PickPattern<TPlain extends PickPlain, TPattern extends Type.Plain> = Util.ExactKeys<Util.Pattern<TPlain>, TPattern>
