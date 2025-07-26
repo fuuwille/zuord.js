730070b1775dcfe4393a26064f8cfa6afc952f82
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 08:24:38 +0300

    feat: refactor Exact type definition for improved type inference and structure

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -1,7 +1,12 @@
-export type Exact<Base, Input> ={
-    [K in keyof Input]: K extends keyof Base
-    ? Exact<Base[K], Input[K]>
-    : never;
-} & {
-    [K in Exclude<keyof Base, keyof Input>]?: never;
-}
+export type Exact<Base, Input> = Base extends object
+    ? Input extends object
+      ? {
+          [K in Exclude<keyof Input, Exclude<keyof Input, keyof Base>>]:
+            K extends keyof Input & keyof Base
+              ? Exact<Base[K], Input[K]>
+              : Input[K];
+        } & {
+          [K in Exclude<keyof Input, keyof Base>]: never;
+        }
+      : never
+    : Input extends true ? Input : never;
