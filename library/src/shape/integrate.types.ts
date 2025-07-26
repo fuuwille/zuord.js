cf145569e27a039ab0cb7448feb65216101425ee
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 01:24:34 +0300

    refactor: simplify IntegrateBase and IntegrateInput types for consistency

--- library/src/shape/integrate.types.ts
+++ library/src/shape/integrate.types.ts
@@ -1,15 +1,9 @@
 import { ZuordType as Type } from "@zuord/type";
-import { ZuordUtil as Util } from "@zuord/util";
 import { InternalZuord as Internal } from "zuord/internal";
 
-export type IntegrateBase = IntegrateBaseLoose;
+export type IntegrateBase = Type.Array | Type.Plain;
 
-export type IntegrateBaseLoose = Type.Array | Type.Plain;
-
-export type IntegrateInput<TBase extends IntegrateBase, TInput extends IntegrateInputLoose> 
-    = TBase extends Type.Plain ? TInput extends Type.Plain ? Util.ExactKeys<TBase, TInput> : TInput : TInput;
-
-export type IntegrateInputLoose = Type.Array | Type.Plain;
+export type IntegrateInput = Type.Array | Type.Plain;
 
 /**
  * Allowed element types for the `integrate` operations.
