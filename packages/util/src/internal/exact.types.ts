ae56ff4dcfdea3c43b0d97fe6cc6324573219b75
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:11:53 +0300

    refactor: restructure Exact type definition for improved clarity and maintainability

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -1,11 +1,17 @@
 import { ZuordType } from "@zuord/type";
 
-export type Exact<Base, Input> = (ExactContent<Base, Input> & {
-        [K in Exclude<keyof Input, keyof Base>]: never;
-}) extends infer TExact ? { [K in keyof TExact]: TExact[K] } : never;
+export type Exact<Base, Input> = Base extends ZuordType.Plain ? (
+    (ExactContent<Base, Input> & ExactExtra<Base, Input>) extends infer TExact ? {
+        [K in keyof TExact]: TExact[K] 
+    } : never
+) : never;
 
 export type ExactContent<TBase, TInput> = TBase extends ZuordType.Plain ? {
     [K in Exclude<keyof TInput, Exclude<keyof TInput, keyof TBase>>]: (
         [TInput[K]] extends [true] ? true : Exact<TBase[K], TInput[K]>
     )
+} : never;
+
+export type ExactExtra<TBase, TInput> = TBase extends ZuordType.Plain ? {
+    [K in Exclude<keyof TInput, keyof TBase>]: never;
 } : never;
