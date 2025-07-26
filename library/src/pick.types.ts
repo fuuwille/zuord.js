57b08c1f6ed8cc883f222cf032852d7dab4fec43
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 12:04:49 +0300

    refactor: fix type constraint in Pick type for improved accuracy

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -1,6 +1,6 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
 
-export type Pick<TPlain extends Shape.PickPlain, TPattern extends Shape.PickExactPattern<TPlain, Shape.PickPattern<TPlain>>> = Internal.Pick<TPlain, TPattern>;
+export type Pick<TPlain extends Shape.PickPlain, TPattern extends Shape.PickExactPattern<TPlain, TPattern>> = Internal.Pick<TPlain, TPattern>;
 
 export type PickLoose<TPlain extends Shape.PickPlain, TPattern extends Shape.PickPattern<TPlain>> = Internal.Pick<TPlain, TPattern>;
