b87f6cb9f29b911de6418dd74cfb6d4f3f8fc038
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 17:08:31 +0300

    docs: update comments for IntegrateElement and IntegrateMode types to reflect pluralization for consistency

--- library/src/shape/integrate.types.ts
+++ library/src/shape/integrate.types.ts
@@ -2,13 +2,13 @@ import { ZuordType as Type } from "@zuord/type";
 import { InternalZuord as Internal } from "zuord/internal";
 
 /**
- * Allowed element types for the `integrate` operation.
+ * Allowed element types for the `integrate` operations.
  * 
  * Restricts inputs to plain objects or arrays that can be deeply integrated
 */
 export type IntegrateElement = Type.Array | Type.Plain;
 
 /**
- * Partial mode settings to customize the behavior of the `integrate` operation.
+ * Partial mode settings to customize the behavior of the `integrate` operations.
 */
 export type IntegrateMode = Partial<Internal.IntegrateMode>;
