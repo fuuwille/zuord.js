e6d854426fa5fe087c3361a95e57d8227099229d
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:14:41 +0300

    refactor: improve readability of ExactContent type definition by restructuring conditional logic

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -8,7 +8,9 @@ export type Exact<Base, Input> = Base extends ZuordType.Plain ? (
 
 export type ExactContent<TBase, TInput> = {
     [K in Exclude<keyof TInput, Exclude<keyof TInput, keyof TBase>>]: (
-        [TInput[K]] extends [true] ? true : K extends keyof TBase ? Exact<TBase[K], TInput[K]> : never
+        [TInput[K]] extends [true] ? true : (
+            K extends keyof TBase ? Exact<TBase[K], TInput[K]> : never
+        )
     )
 };
 
