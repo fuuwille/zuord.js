fa8dd9a0bfa6ab0961f49e794f6c57d2abdf24c1
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:44:53 +0300

    refactor: update type references in integrate function for improved clarity

--- library/src/integrate.ts
+++ library/src/integrate.ts
@@ -1,15 +1,15 @@
 import { internalZuord as internal } from "./internal";
 import { zuordCore } from "@zuord/core";
 import { Integrate, IntegrateMode } from "./integrate.types";
-import { shapeZuord, ShapeZuord } from "./shape";
+import { shapeZuord, ShapeZuord as Shape } from "./shape";
 
-export function integrate <A extends ShapeZuord.IntegrateElement, B extends ShapeZuord.IntegrateElement> (a: A, b: B)
+export function integrate <A extends Shape.IntegrateElement, B extends Shape.IntegrateElement> (a: A, b: B)
     : Integrate<A, B>;
 
-export function integrate <A extends ShapeZuord.IntegrateElement, B extends ShapeZuord.IntegrateElement, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode: TMode)
+export function integrate <A extends Shape.IntegrateElement, B extends Shape.IntegrateElement, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode: TMode)
     : Integrate<A, B, TMode>;
 
-export function integrate <A extends ShapeZuord.IntegrateElement, B extends ShapeZuord.IntegrateElement, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
+export function integrate <A extends Shape.IntegrateElement, B extends Shape.IntegrateElement, TMode extends Partial<IntegrateMode>> (a: A, b: B, mode?: TMode) {
     if(!(shapeZuord.integrateElement(a))) {
         throw new TypeError("Integrate function expects both arguments to be either plain objects or arrays.");
     }
