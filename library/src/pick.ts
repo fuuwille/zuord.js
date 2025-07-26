b6fa6488b5e8ed102d2f57827e056d20037a6d40
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 04:52:58 +0300

    fix: add const modifier to TPattern in pick function for improved type inference

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -3,6 +3,6 @@ import { ZuordUtil } from "@zuord/util";
 import { internalZuord } from "./internal";
 import { Pick } from "./pick.types";
 
-export function pick<TPlain extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern<TPlain>>(plain: TPlain, pattern: TPattern) {
+export function pick<TPlain extends ZuordType.Plain, const TPattern extends ZuordUtil.Pattern<TPlain>>(plain: TPlain, pattern: TPattern) {
     return internalZuord.pick(plain, pattern) as Pick<TPlain, TPattern>;
 }
