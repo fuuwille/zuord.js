abb1bf6fb94f7fdcff73b017aceee3a3ce1f8871
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 04:40:46 +0300

    refactor: remove unused ArrayInfer and ArrayDepth type definitions for cleaner code

--- packages/type/src/array.types.ts
+++ packages/type/src/array.types.ts
@@ -3,11 +3,3 @@ export type Array = ArrayOf<unknown>;
 export type ArrayOf<T> = readonly T[];
 
 export type ArrayEmpty = readonly [];
-
-export type ArrayInfer<T> = T extends readonly (infer TInfer)[] ? TInfer : never;
-
-export type ArrayDepth<T> = ArrayDepthImpl<T, []>;
-
-type ArrayDepthImpl<T, D extends Array> = T extends readonly (infer U)[] ? (
-    ArrayDepthImpl<U, [unknown, ...D]>
-) : D['length'];
