1355baac4f2d0ef5c61358af988e38276ad8fb1e
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 17:04:44 +0300

    docs: update comments for IntegrateElement type to clarify allowed input types

--- library/src/shape/integrate.types.ts
+++ library/src/shape/integrate.types.ts
@@ -1,6 +1,11 @@
 import { ZuordType as Type } from "@zuord/type";
 import { InternalZuord as Internal } from "zuord/internal";
 
+/**
+ * Allowed element types for the `integrate` operation.
+ * 
+ * Restricts inputs to plain objects or arrays that can be deeply integrated
+*/
 export type IntegrateElement = Type.Array | Type.Plain;
 
 export type IntegrateMode = Partial<Internal.IntegrateMode>;
