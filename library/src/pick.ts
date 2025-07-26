e16f968627861fe6c69a8768b87df1fba6169d2a
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:37:04 +0300

    refactor: remove unused import from pick.ts for cleaner code

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -1,7 +1,6 @@
-import { ZuordType } from "@zuord/type";
 import { internalZuord as internal } from "./internal";
-import { Pick } from "./pick.types";
 import { ShapeZuord as Shape } from "./shape";
+import { Pick } from "./pick.types";
 
 export function pick<TPlain extends Shape.PickPlain, const TPattern extends Shape.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     return internal.pick(plain, pattern) as Pick<TPlain, TPattern>;
