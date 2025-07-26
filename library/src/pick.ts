19e0b96cfb08621c17da2b0545773213e004b6bb
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 11:35:32 +0300

    refactor: update pick function to use ShapeZuord.PickPattern for pattern validation

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -1,8 +1,8 @@
 import { ZuordType } from "@zuord/type";
-import { ZuordUtil } from "@zuord/util";
 import { internalZuord } from "./internal";
 import { Pick } from "./pick.types";
+import { ShapeZuord } from "./shape";
 
-export function pick<TPlain extends ZuordType.Plain, const TPattern extends ZuordUtil.Exact<ZuordUtil.Pattern<TPlain>, TPattern>>(plain: TPlain, pattern: TPattern) {
+export function pick<TPlain extends ZuordType.Plain, const TPattern extends ShapeZuord.PickPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     return internalZuord.pick(plain, pattern) as Pick<TPlain, TPattern>;
 }
