092f931a5bf85f2654610251189ef630220fbfe6
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 08:45:44 +0300

    feat: enhance Exact type definition for improved handling of plain types

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -1,12 +1,14 @@
-export type Exact<Base, Input> = Base extends object
-    ? Input extends object
-      ? {
-          [K in Exclude<keyof Input, Exclude<keyof Input, keyof Base>>]:
-            K extends keyof Input & keyof Base
-              ? Exact<Base[K], Input[K]>
-              : Input[K];
-        } & {
-          [K in Exclude<keyof Input, keyof Base>]: never;
-        }
+import { ZuordType } from "@zuord/type";
+
+export type Exact<Base, Input> =
+  [Base] extends [ZuordType.Plain]
+    ? [Input] extends [ZuordType.Plain]
+      ? [keyof Input] extends [never]
+        ? {}
+        : {
+            [K in Exclude<keyof Input, Exclude<keyof Input, keyof Base>>]: Exact<Base[K], Input[K]>
+          } & {
+            [K in Exclude<keyof Input, keyof Base>]: never;
+          }
       : never
-    : Input extends true ? Input : never;
+    : Input extends true ? Input : Base;
