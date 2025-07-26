94aceb3ab9ebe314f90ab744538488dcd5706abb
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:47:31 +0300

    refactor: consolidate imports and add integrateMode function for improved clarity

--- library/src/shape/integrate.ts
+++ library/src/shape/integrate.ts
@@ -1,6 +1,10 @@
 import { zuordType } from "@zuord/type";
-import { IntegrateElement } from "./integrate.types";
+import { IntegrateElement, IntegrateMode } from "./integrate.types";
 
 export const integrateElement = (obj: unknown) : obj is IntegrateElement => {
     return zuordType.plain(obj) || zuordType.array(obj);
 }
+
+export const integrateMode = (obj: unknown): obj is IntegrateMode => {
+    return zuordType.plain(obj);
+};
