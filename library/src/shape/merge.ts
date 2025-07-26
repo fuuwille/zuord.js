18a85540ceffc2c47e2978ccb3d72806f9122c05
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:59:28 +0300

    refactor: change integrateMode to a function declaration for consistency

--- library/src/shape/merge.ts
+++ library/src/shape/merge.ts
@@ -6,6 +6,6 @@ export function mergeContent(obj: unknown): obj is MergeContent {
     return zuordType.arrayOf(obj, integrateElement);
 }
 
-export const integrateMode = (obj: unknown): obj is MergeMode => {
+export function mergeMode(obj: unknown): obj is MergeMode {
     return zuordType.plain(obj);
-};
+}
