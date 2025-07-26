2968cdba26f60b27a4a14f5082644f82a5733da3
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 15:04:18 +0300

    fix: update Pick type definition to include 'any' in IsAny check for correctness

--- library/src/internal/pick.types.ts
+++ library/src/internal/pick.types.ts
@@ -2,5 +2,5 @@ import { ZuordTrait } from "@zuord/trait";
 import { ZuordType } from "@zuord/type";
 
 export type Pick<T, P> = P extends ZuordType.Plain ? {
-    -readonly [K in keyof T as [ZuordTrait.IsAny<[unknown, never], P[K]>] extends [true] ? never : K]: Pick<T[K], P[K]>;
+    -readonly [K in keyof T as [ZuordTrait.IsAny<[unknown, never, any], P[K]>] extends [true] ? never : K]: Pick<T[K], P[K]>;
 } : T
