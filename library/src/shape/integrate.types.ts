1f73a406ac1ded2f8d86c84ca8678b9d517b60d4
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 10:55:00 +0300

    refactor: update IntegrateMode type to use Partial<Internal.IntegrateMode> for improved clarity

--- library/src/shape/integrate.types.ts
+++ library/src/shape/integrate.types.ts
@@ -1,5 +1,6 @@
 import { ZuordType } from "@zuord/type";
+import { InternalZuord as Internal } from "zuord/internal";
 
 export type IntegrateElement = ZuordType.Array | ZuordType.Plain;
 
-export type IntegrateMode = ZuordType.Plain;
+export type IntegrateMode = Partial<Internal.IntegrateMode>;
