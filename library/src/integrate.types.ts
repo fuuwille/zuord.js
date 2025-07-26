00c52b51a3e0da887bddd743a18c25b40a81d214
k4yr2 <kyrzkaya@gmail.com>
2025-07-26 17:15:27 +0300

    docs: clarify TMode template parameter description in Integrate type

--- library/src/integrate.types.ts
+++ library/src/integrate.types.ts
@@ -8,8 +8,9 @@ import { ShapeZuord as Shape } from "./shape";
  *
  * @template TBase - The base element to integrate into.
  * @template TInput - The element providing new or overriding types.
- * @template TMode - Optional integration mode settings.
+ * @template TMode - Optional integration mode for customizing behavior.
  */
 export type Integrate<TBase extends Shape.IntegrateElement, TInput extends Shape.IntegrateElement, TMode extends Shape.IntegrateMode = {}> = Internal.Integrate<TBase, TInput, ZuordCore.ModeResolve<[typeof integrateMode, TMode]>>;
+
 
 export type IntegrateMode = Internal.IntegrateMode;
