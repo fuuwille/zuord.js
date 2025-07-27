803d2a9292f1ceb4cbbef37a4fb1a9e6d8d6b246
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 21:25:59 +0300

    refactor: format type definitions for improved readability

--- library/src/integrate.types.ts
+++ library/src/integrate.types.ts
@@ -4,11 +4,14 @@ import { ZuordCore } from "@zuord/core";
 import { ShapeZuord as Shape } from "./shape";
 
 export namespace Integrate {
-    export type Object<TBase extends Shape.Integrate.Object, TInput extends Shape.Integrate.Object, TMode extends Shape.Integrate.Mode = {}> = Internal.Integrate<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
+    export type Object<TBase extends Shape.Integrate.Object, TInput extends Shape.Integrate.Object, TMode extends Shape.Integrate.Mode = {}>
+        = Internal.Integrate<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
 
-    export type Plain<TBase extends Shape.Integrate.Plain, TInput extends Shape.Integrate.Plain, TMode extends Shape.Integrate.Mode = {}> = Internal.IntegratePlain<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
+    export type Plain<TBase extends Shape.Integrate.Plain, TInput extends Shape.Integrate.Plain, TMode extends Shape.Integrate.Mode = {}> 
+        = Internal.IntegratePlain<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
 
-    export type Array<TBase extends Shape.Integrate.Array, TInput extends Shape.Integrate.Array, TMode extends Shape.Integrate.Mode = {}> = Internal.IntegrateArray<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
+    export type Array<TBase extends Shape.Integrate.Array, TInput extends Shape.Integrate.Array, TMode extends Shape.Integrate.Mode = {}> 
+        = Internal.IntegrateArray<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
 
     export type Mode = Internal.IntegrateMode;
 }
