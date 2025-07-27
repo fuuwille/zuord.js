3b89cffb0e9d380709272ad87d5e93d4e90ac079
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 20:49:24 +0300

    refactor: add Plain type definition for improved type integration

--- library/src/integrate.types.ts
+++ library/src/integrate.types.ts
@@ -5,4 +5,6 @@ import { ShapeZuord as Shape } from "./shape";
 
 export type Object<TBase extends Shape.Integrate.Object, TInput extends Shape.Integrate.Object, TMode extends Shape.Integrate.Mode = {}> = Internal.Integrate<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
 
+export type Plain<TBase extends Shape.Integrate.Plain, TInput extends Shape.Integrate.Plain, TMode extends Shape.Integrate.Mode = {}> = Internal.IntegratePlain<TBase, TInput, ZuordCore.ModeResolve<[typeof defaultMode, TMode]>>;
+
 export type Mode = Internal.IntegrateMode;
