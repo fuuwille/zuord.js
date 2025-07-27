0035de3541a050e9dcb1609f1b058d7d177a61e7
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 16:12:18 +0300

    refactor: add array merge function and improve organization of merge functions

--- library/src/merge.ts
+++ library/src/merge.ts
@@ -4,6 +4,8 @@ import { MergeMode, Merge } from "./merge.types";
 import { shapeZuord as shape, ShapeZuord as Shape } from "./shape";
 import { ZuordType } from "@zuord/type";
 
+// PLAIN
+
 export function plain <TContent extends ZuordType.Plain[]> (content: [...TContent]) 
     : Merge<TContent>;
 
@@ -21,5 +23,27 @@ export function plain <TContent extends Shape.MergeContent, TMode extends Shape.
 
     return internal.merge(content, zuord.modeResolve([mode, mode ?? {}])) as Merge<TContent, TMode>;
 }
+
+// ARRAY
+
+export function array <TContent extends ZuordType.Array[]> (content: [...TContent]) 
+    : Merge<TContent>;
+
+export function array <TContent extends ZuordType.Array[], TMode extends Shape.MergeMode> (content: [...TContent], mode: TMode)
+    : Merge<TContent, TMode>;
+
+export function array <TContent extends Shape.MergeContent, TMode extends Shape.MergeMode> (content: [...TContent], mode?: TMode) {
+    if(!shape.mergeContent(content)) {
+        throw new TypeError("Merge function expects the content to be an array of integrate elements.");
+    }
+
+    if(!shape.mergeMode(mode)) {
+        throw new TypeError("Merge function expects the mode to be a valid MergeMode.");
+    }
+
+    return internal.merge(content, zuord.modeResolve([mode, mode ?? {}])) as Merge<TContent, TMode>;
+}
+
+// MODE
 
 export const mode = internal.mergeMode satisfies MergeMode;
