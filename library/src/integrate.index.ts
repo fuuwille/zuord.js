2e69eaa6bb1372f9b06830d3fcf47046b936db70
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 21:16:04 +0300

    refactor: define IntegrateAPI type for better structure and clarity

--- library/src/integrate.index.ts
+++ library/src/integrate.index.ts
@@ -1,2 +1,14 @@
-export * as integrate from "./integrate";
+import * as m_integrate from "./integrate";
+
+type IntegrateAPI = {
+    plain: typeof m_integrate.plain;
+    plainLoose: typeof m_integrate.plainLoose;
+    plainStrict: typeof m_integrate.plainStrict;
+    array: typeof m_integrate.array;
+    arrayStrict: typeof m_integrate.arrayStrict;
+    defaultMode: typeof m_integrate.defaultMode;
+}
+
+export const integrate: IntegrateAPI = m_integrate;
+
 export { Integrate } from "./integrate.types";
