0e368f30600e10662502baa630bf9dbe24a54fa2
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 16:01:39 +0300

    refactor: redefine IntegrateAPI type structure for improved clarity

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,13 +1,16 @@
 import * as _integrate from "./integrate";
 
-export const integrate: {
+type IntegrateAPI = {
     plain: typeof _integrate.plain;
     plainLoose: typeof _integrate.plainLoose;
     plainStrict: typeof _integrate.plainStrict;
     array: typeof _integrate.array;
     arrayStrict: typeof _integrate.arrayStrict;
     mode: typeof _integrate.mode;
-} = _integrate;
+}
+
+export const integrate: IntegrateAPI = _integrate;
+
 
 export * from "./merge";
 export * from "./pick";
