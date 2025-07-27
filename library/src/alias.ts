aa6cfc36282a9bf8fc967588ff3e55a59c40abc3
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 15:54:42 +0300

    refactor: update import structure and define integrate constant for improved clarity

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,4 +1,7 @@
-export * as integrate from "./integrate";
+import * as integrateModule from "./integrate";
+import type { IntegrateAPI } from "./integrate.types";
 export * from "./merge";
 export * from "./pick";
 export * from "./omit";
+
+export const integrate: IntegrateAPI = integrateModule;
