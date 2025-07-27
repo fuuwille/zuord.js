1f0a2b8dd1fcdee01df8a370c04d4ea9272ebd51
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 15:59:45 +0300

    refactor: update import alias for integrate module for consistency

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,13 +1,13 @@
-import * as integrateModule from "./integrate";
+import * as _integrate from "./integrate";
 
 export const integrate: {
-    plain: typeof integrateModule.plain;
-    plainLoose: typeof integrateModule.plainLoose;
-    plainStrict: typeof integrateModule.plainStrict;
-    array: typeof integrateModule.array;
-    arrayStrict: typeof integrateModule.arrayStrict;
-    mode: typeof integrateModule.mode;
-} = integrateModule;
+    plain: typeof _integrate.plain;
+    plainLoose: typeof _integrate.plainLoose;
+    plainStrict: typeof _integrate.plainStrict;
+    array: typeof _integrate.array;
+    arrayStrict: typeof _integrate.arrayStrict;
+    mode: typeof _integrate.mode;
+} = _integrate;
 
 export * from "./merge";
 export * from "./pick";
