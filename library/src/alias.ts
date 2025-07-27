054908d9ca92f67bd2b1ba49cc8992d1fea9952e
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 15:55:42 +0300

    refactor: fix export statement for integrate and ensure consistent formatting

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,7 +1,8 @@
 import * as integrateModule from "./integrate";
 import type { IntegrateAPI } from "./integrate.types";
+
+export const integrate: IntegrateAPI = integrateModule;
+
 export * from "./merge";
 export * from "./pick";
 export * from "./omit";
-
-export const integrate: IntegrateAPI = integrateModule;
