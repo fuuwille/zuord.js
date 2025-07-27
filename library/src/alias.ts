59b9e01a4651616cecba336a56ea5062f204fabe
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 20:48:16 +0300

    refactor: standardize import aliasing for integrate and merge modules

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,30 +1,30 @@
 // INTEGRATE
 
-import * as _integrate from "./integrate";
+import * as m_integrate from "./integrate";
 
 type Integrate = {
-    plain: typeof _integrate.plain;
-    plainLoose: typeof _integrate.plainLoose;
-    plainStrict: typeof _integrate.plainStrict;
-    array: typeof _integrate.array;
-    arrayStrict: typeof _integrate.arrayStrict;
-    defaultMode: typeof _integrate.defaultMode;
+    plain: typeof m_integrate.plain;
+    plainLoose: typeof m_integrate.plainLoose;
+    plainStrict: typeof m_integrate.plainStrict;
+    array: typeof m_integrate.array;
+    arrayStrict: typeof m_integrate.arrayStrict;
+    defaultMode: typeof m_integrate.defaultMode;
 }
 
-export const integrate: Integrate = _integrate;
+export const integrate: Integrate = m_integrate;
 
 
 // MERGE
 
-import * as _merge from "./merge";
+import * as m_merge from "./merge";
 
 type Merge = {
-    plain: typeof _merge.plain;
-    array: typeof _merge.array;
-    mode: typeof _merge.mode;
+    plain: typeof m_merge.plain;
+    array: typeof m_merge.array;
+    mode: typeof m_merge.mode;
 }
 
-export const merge: Merge = _merge;
+export const merge: Merge = m_merge;
 
 
 //
