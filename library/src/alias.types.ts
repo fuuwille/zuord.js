81292a38be02cd8b9cf1baa7e4e0ac0abcc46318
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 21:20:24 +0300

    refactor: remove direct exports from alias files for better organization

--- library/src/alias.types.ts
+++ library/src/alias.types.ts
@@ -1,4 +1,2 @@
-export * as Integrate from "./integrate.types";
-export * from "./merge.types";
 export * from "./pick.types";
 export * from "./omit.types";
--- library/src/index.ts
+++ library/src/index.ts
@@ -1,2 +1,5 @@
 export * as zuord from "./alias";
 export * as Zuord from "./alias.types";
+
+export * from "./integrate.alias";
+export * from "./merge.alias";
