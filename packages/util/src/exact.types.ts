42084ee1d3dc903f18a7a6543a29deea0d5e3643
k4yr2 <kyrzkaya@gmail.com>
2025-07-28 00:42:29 +0300

    refactor: update PlainStrict type to use Internal.ExactShape for improved type validation

--- packages/util/src/exact.types.ts
+++ packages/util/src/exact.types.ts
@@ -5,6 +5,6 @@ export namespace Exact {
     export type Plain<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
         = Internal.ExactKeys<TBase, TContent>;
 
-    export type PlainStrict<TBase extends ZuordType.Plain, TContent extends TBase> 
-        = Internal.ExactKeys<TBase, TContent>;
+    export type PlainStrict<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
+        = Internal.ExactShape<TBase, TContent> extends infer TExact extends ZuordType.Plain ? TExact : never;
 }
