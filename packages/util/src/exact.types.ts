5e5a1d987df334ebf3aae5d42cfe2f77e47304fa
k4yr2 <kyrzkaya@gmail.com>
2025-07-28 00:13:03 +0300

    refactor: format type definitions for improved readability

--- packages/util/src/exact.types.ts
+++ packages/util/src/exact.types.ts
@@ -2,7 +2,9 @@ import { ZuordType } from "@zuord/type";
 import { InternalZuordUtil as Internal } from "./internal";
 
 export namespace Exact {
-    export type Plain<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> = Internal.ExactKeys<TBase, TContent>;
+    export type Plain<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
+        = Internal.ExactKeys<TBase, TContent>;
 
-    export type PlainStrict<TBase extends ZuordType.Plain, TContent extends TBase> = Internal.ExactKeys<TBase, TContent>;
+    export type PlainStrict<TBase extends ZuordType.Plain, TContent extends TBase> 
+        = Internal.ExactKeys<TBase, TContent>;
 }
