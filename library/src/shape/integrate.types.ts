31c9d04290a6129f86d2d6afc9896fdea42f6dd5
k4yr2 <kyrzkaya@gmail.com>
2025-07-27 20:40:28 +0300

    refactor: reorder type definitions for Item, Plain, and Array for clarity

--- library/src/shape/integrate.types.ts
+++ library/src/shape/integrate.types.ts
@@ -1,6 +1,10 @@
 import { ZuordType as Type } from "@zuord/type";
 import { InternalZuord as Internal } from "zuord/internal";
 
-export type Item = Type.Array | Type.Plain;
+export type Item = Plain | Array;
+
+export type Plain = Type.Plain;
+
+export type Array = Type.Array;
 
 export type Mode = Partial<Internal.IntegrateMode>;
