d8f5ffe1b80434cf38c64dfe4baa835e23094970
k4yr2 <kyrzkaya@gmail.com>
2025-07-28 21:37:22 +0300

    refactor: streamline ExactKeys type definition for improved clarity and organization

--- packages/util/src/internal/exact.types.ts
+++ packages/util/src/internal/exact.types.ts
@@ -1,20 +1,17 @@
 import { ZuordType } from "@zuord/type";
 
-export type ExactKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
-    ExactSharedKeys<TBase, TInput> & ExactNoExtraKeys<TBase, TInput>
-) : never;
 
-export type ExactSharedKeys<TBase, TInput> = {
-    [K in Exclude<keyof TInput, Exclude<keyof TInput, keyof TBase>>]: (
-        [TInput[K]] extends [true] ? true : (
-            K extends keyof TBase ? ExactKeys<TBase[K], TInput[K]> : never
-        )
-    )
-};
+export type ExactKeys<TBase, TInput> = 
+    TBase extends ZuordType.Plain
+        ? {
+            [K in keyof TInput]: K extends keyof TBase
+                ? ExactKeys<TBase[K], TInput[K]>
+                : never
+        } & {
+            [K in Exclude<keyof TBase, keyof TInput>]?: never
+        }
+        : TInput extends TBase ? TBase : never;
 
-export type ExactNoExtraKeys<TBase, TInput> = {
-    [K in Exclude<keyof TInput, keyof TBase>]: never;
-}
 
 export type ExactShape<TBase, TInput> = 
   TBase extends ZuordType.Plain ? (
