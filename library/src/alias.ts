4d210c1410fce7a07e5dca5743ce2a52b327401e
k4yr2 <kyrzkaya@gmail.com>
2025-07-28 22:55:28 +0300

    refactor: restructure alias exports and define zuord type for improved organization

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -1,4 +1,18 @@
-export { integrate } from "./integrate";
-export { merge } from "./merge.index";
+import { integrate } from "./integrate";
+import { merge } from "./merge.index";
 export * from "./pick";
 export * from "./omit";
+
+type zuord = {
+    integrate: typeof integrate;
+    merge: typeof merge;
+}
+
+export const zuord : zuord = {
+    get integrate() {
+        return integrate;
+    },
+    get merge() {
+        return merge;
+    }
+}
