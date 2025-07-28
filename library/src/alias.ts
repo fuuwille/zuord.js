3759a09634f9679befa7e6fc465490ec7461a2a0
k4yr2 <kyrzkaya@gmail.com>
2025-07-28 23:13:48 +0300

    refactor: add JSDoc comments for integrate and merge properties in zuord type for improved documentation

--- library/src/alias.ts
+++ library/src/alias.ts
@@ -2,7 +2,13 @@ import { integrate } from "./integrate";
 import { merge } from "./merge.index";
 
 type zuord = {
+    /**
+     * Integration operations for various data structures.
+     */
     readonly integrate: typeof integrate;
+    /**
+     * Merge operations for combining objects and arrays.
+     */
     readonly merge: typeof merge;
 }
 
