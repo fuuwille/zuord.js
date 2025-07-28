e08b646e63e8824073515690e585cb80b5700dd4
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 01:51:02 +0300

    feat: add mode integration and type definition for improved functionality

---
+++ library/src/main/integrate/mode.ts
@@ -0,0 +1,5 @@
+import { internalZuord as internal, InternalZuord as Internal  } from "../../internal";
+
+export const mode = internal.integrateMode satisfies Mode;
+
+export type Mode = Internal.IntegrateMode;
