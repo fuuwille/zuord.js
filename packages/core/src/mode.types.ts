f9a932dd42e3e4ada67c31770f8f1fc8d2ae2ee4
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 20:45:41 +0300

    fix: update Mode imports to use internal mode.types for consistency

--- packages/core/src/mode.types.ts
+++ packages/core/src/mode.types.ts
@@ -1,15 +1,7 @@
-import { InternalZuordCore as Internal } from "./internal";
+import { Mode as _Mode } from "./internal/mode.types";
 
 export namespace Mode {
-    export type Field<K extends string = string, V extends boolean = boolean> = Internal.Mode.Flag<K, V>;
+    export type Field<K extends string = string, V extends boolean = boolean> = _Mode.Flag<K, V>;
 
-    export type Resolve<TModes extends Mode.Field[]> = Internal.Mode.Resolve<TModes>;
+    export type Resolve<TModes extends Mode.Field[]> = _Mode.Resolve<TModes>;
 }
-
-export type ShallowMode = Mode.Field<"shallow">;
-
-export type ConcatMode = Mode.Field<"concat">;
-
-export type UniqueMode = Mode.Field<"unique">;
-
-export type BaseMode = Mode.Resolve<[ShallowMode]>;
