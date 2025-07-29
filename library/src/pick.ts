7cca99b935c283b8271788b6701c278fe170d35b
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:10:25 +0300

    refactor: consolidate pick functions and update type exports for clarity

--- library/src/pick.ts
+++ library/src/pick.ts
@@ -1,19 +1,32 @@
 import { internalZuord as internal } from "./internal";
 import { ShapeZuord as Shape, shapeZuord as shape } from "./shape";
-import { Pick, PickLoose, PickPattern } from "./pick.types";
+import { Pick } from "./pick.types";
 
-export function pick<TSouce extends Shape.PickSource, const TPattern extends PickPattern<TSouce, TPattern>>(source: TSouce, pattern: TPattern) {
-    if(!shape.pickPlain(source)) {
-        throw new TypeError("Pick function expects the source to be a plain.");
+function plain<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+    if(!shape.omitPlain(plain)) {
+        throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
-    return internal.pick.plain(source, pattern) as Pick<TSouce, TPattern>;
+    return internal.pick.plain(plain, pattern) as Pick.Plain<TPlain, TPattern>;
 }
 
-export function pickLoose<TSouce extends Shape.PickSource, const TPattern extends Shape.PickPattern<TSouce>>(source: TSouce, pattern: TPattern) {
-    if(!shape.pickPlain(source)) {
-        throw new TypeError("Pick function expects the source to be a plain.");
+function plainLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPatternLoose<TPlain>>(plain: TPlain, pattern: TPattern) {
+    if(!shape.omitPlain(plain)) {
+        throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
-    return internal.pick.plain(source, pattern) as PickLoose<TSouce, TPattern>;
+    return internal.pick.plain(plain, pattern) as Pick.PlainLoose<TPlain, TPattern>;
 }
+
+
+// EXPORT
+
+type pick = {
+    readonly plain: typeof plain;
+    readonly plainLoose: typeof plainLoose;
+}
+
+export const pick: pick = {
+    plain: plain,
+    plainLoose: plainLoose
+};
--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -1,10 +1,8 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
-import { ZuordType as Type } from "@zuord/type";
-import { ZuordUtil as Util } from "@zuord/util";
 
-export type Pick<TSouce extends Shape.PickSource, TPattern extends PickPattern<TSouce, TPattern>> = Internal.Pick.Plain<TSouce, TPattern>;
+export namespace Pick {
+    export type Plain<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPattern<TPlain, TPattern>> = Internal.Pick.Plain<TPlain, TPattern>;
 
-export type PickLoose<TSouce extends Shape.PickSource, TPattern extends Shape.PickPattern<TSouce>> = Internal.Pick.Plain<TSouce, TPattern>;
-
-export type PickPattern<TSouce extends Shape.PickSource, TPattern extends Type.Plain> = Util.Exact.Plain<Shape.PickPattern<TSouce>, TPattern>
+    export type PlainLoose<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPatternLoose<TPlain>> = Internal.Pick.Plain<TPlain, TPattern>;
+}
