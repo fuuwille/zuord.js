2e5edc4d1f8b9403a644e23cfc5d5e20e9344c57
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:11:39 +0300

    refactor: reorganize Mode namespace and ensure consistent type exports

--- library/src/mode.types.ts
+++ library/src/mode.types.ts
@@ -1,3 +1,11 @@
+import { ZuordCore } from "@zuord/core";
+
 export namespace Mode {
-    
+    export type Integrate = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
+
+    export type Merge = Integrate;
+
+    export type Pick = ZuordCore.BaseMode;
+
+    export type Omit = ZuordCore.BaseMode;
 }
