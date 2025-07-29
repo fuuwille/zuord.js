2ecf3365934c138cb64d4a36615fb86a05cf8472
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 12:39:20 +0300

    refactor: include mode.types in alias exports and ensure omit.types is correctly exported

--- library/src/internal/alias.types.ts
+++ library/src/internal/alias.types.ts
@@ -2,3 +2,4 @@ export type * from "./integrate.types";
 export type * from "./merge.types";
 export type * from "./pick.types";
 export type * from "./omit.types";
+export type * from "./mode.types";
