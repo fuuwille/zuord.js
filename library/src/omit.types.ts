db21756b731f177d21ce7c8eaf88e86260b88603
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 18:40:19 +0300

    fix: change Omit namespace declaration to 'declare' for consistency

--- library/src/omit.types.ts
+++ library/src/omit.types.ts
@@ -1,7 +1,7 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
 
-export namespace Omit {
+export declare namespace Omit {
     export type Plain<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPattern<TPlain, TPattern>> = Internal.Omit.Plain<TPlain, TPattern>;
 
     export type PlainLoose<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPatternLoose<TPlain>> = Internal.Omit.Plain<TPlain, TPattern>;
