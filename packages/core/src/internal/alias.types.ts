bc7d0316565eba642b45e26834a58983c4fec35f
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 19:27:40 +0300

    fix: import Flags module for internal type definitions

--- packages/core/src/internal/alias.types.ts
+++ packages/core/src/internal/alias.types.ts
@@ -1,5 +1,7 @@
 import * as ModeModule from "./mode.types";
+import { Flags as FlagsModule } from "./flags.types";
 
 export declare namespace InternalZuordCore {
     export import Mode = ModeModule.Mode;
+    export import Flags = FlagsModule;
 }
