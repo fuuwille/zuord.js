0278a19334cb535c96728780cbb93786a71de1b2
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 18:38:55 +0300

    fix: change Omit namespace declaration to 'declare' for consistency

--- library/src/internal/omit.types.ts
+++ library/src/internal/omit.types.ts
@@ -1,7 +1,7 @@
 import { ZuordTrait } from "@zuord/trait";
 import { ZuordType } from "@zuord/type";
 
-export namespace Omit {
+export declare namespace Omit {
     export type Plain<T, P> = P extends ZuordType.Plain ? {
         -readonly [K in keyof T as [ZuordTrait.EqAny<[unknown, never, any], P[K]>] extends [true] ? K : never]: Omit.Plain<T[K], P[K]>;
     } : T
