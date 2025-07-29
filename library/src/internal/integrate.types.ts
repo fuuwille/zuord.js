976476e3439983598d89efd03e229d5e8058f6b4
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 12:50:45 +0300

    refactor: update type definitions in Integrate namespace for improved clarity and consistency

--- library/src/internal/integrate.types.ts
+++ library/src/internal/integrate.types.ts
@@ -5,21 +5,21 @@ import { ZuordUtil } from "@zuord/util";
 
 export namespace Integrate {
     export type Object<A, B, TMode> = (
-        [ZuordTrait.IsEvery<[A, B], ZuordType.Array>] extends [true] ? (
+        [A, B] extends [infer A extends ZuordType.Array, infer B extends ZuordType.Array] ? (
             Array<A, B, TMode>
         ) : 
-        [ZuordTrait.IsEvery<[A, B], ZuordType.Plain>] extends [true] ? (
+        [A, B] extends [infer A extends ZuordType.Plain, infer B extends ZuordType.Plain] ? (
             Plain<A, B, TMode>
         ) : B
     );
 
-    export type Array<A, B, TMode> = (
+    export type Array<A extends ZuordType.Array, B extends ZuordType.Array, TMode> = (
         Collection<A, B, TMode> extends infer TIntegrated extends ZuordType.Array ? (
             ZuordUtil.Mutable<TIntegrated[number][]>
         ) : never
     );
 
-    export type Collection<A, B, TMode> = (
+    export type Collection<A extends ZuordType.Array, B extends ZuordType.Array, TMode> = (
         A extends ZuordType.Array ? B extends ZuordType.Array ? (
             TMode extends { concat: true } ? (
                 [...A, ...B]
@@ -27,19 +27,19 @@ export namespace Integrate {
         ) : never : never
     );
 
-    export type Plain<A, B, TMode> = (
+    export type Plain<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode> = (
         (Overlap<A, B, TMode> & Extras<A, B>) extends infer TIntegrated ? ({
             -readonly [K in keyof TIntegrated]: TIntegrated[K];
         }) : never
     )
 
-    export type Overlap<A, B, TMode> = ({
+    export type Overlap<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode> = ({
         [K in keyof A]: K extends keyof B ? (
             [ZuordCore.ModeOn<TMode, "shallow">] extends [true] ? B[K] : Object<A[K], B[K], TMode>
         ) : A[K];
     });
 
-    export type Extras<A, B> = ({
+    export type Extras<A extends ZuordType.Plain, B extends ZuordType.Plain> = ({
         [K in keyof B as K extends keyof A ? never : K]: B[K];
     });
 }
