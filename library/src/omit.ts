99b3a11de3cb6e850521b2115aeab367b2f5b70f
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:07:11 +0300

    refactor: rename export functions to improve clarity and structure

--- library/src/omit.ts
+++ library/src/omit.ts
@@ -2,7 +2,7 @@ import { internalZuord as internal } from "./internal";
 import { ShapeZuord as Shape, shapeZuord as shape } from "./shape";
 import { Omit, OmitLoose } from "./omit.types";
 
-export function omit<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
+function plain<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     if(!shape.omitPlain(plain)) {
         throw new TypeError("Pick function expects the target object to be a plain.");
     }
@@ -10,10 +10,23 @@ export function omit<TPlain extends Shape.OmitPlain, const TPattern extends Shap
     return internal.omit.plain(plain, pattern) as Omit<TPlain, TPattern>;
 }
 
-export function omitLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPatternLoose<TPlain>>(plain: TPlain, pattern: TPattern) {
+function plainLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPatternLoose<TPlain>>(plain: TPlain, pattern: TPattern) {
     if(!shape.omitPlain(plain)) {
         throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
     return internal.omit.plain(plain, pattern) as OmitLoose<TPlain, TPattern>;
 }
+
+
+// EXPORT
+
+type omit = {
+    readonly plain: typeof plain;
+    readonly plainLoose: typeof plainLoose;
+}
+
+export const omit = {
+    plain: plain,
+    plainLoose: plainLoose
+};
