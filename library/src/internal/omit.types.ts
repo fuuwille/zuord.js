a4e5524d55ba2879f77443d0bcb48f15b277c484
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 16:49:18 +0300

    refactor: update Omit type exports to use namespace for consistency and clarity

--- library/src/internal/omit.types.ts
+++ library/src/internal/omit.types.ts
@@ -1,6 +1,8 @@
 import { ZuordTrait } from "@zuord/trait";
 import { ZuordType } from "@zuord/type";
 
-export type Omit<T, P> = P extends ZuordType.Plain ? {
-    -readonly [K in keyof T as [ZuordTrait.EqAny<[unknown, never, any], P[K]>] extends [true] ? K : never]: Omit<T[K], P[K]>;
-} : T
+export namespace Omit {
+    export type Plain<T, P> = P extends ZuordType.Plain ? {
+        -readonly [K in keyof T as [ZuordTrait.EqAny<[unknown, never, any], P[K]>] extends [true] ? K : never]: Omit.Plain<T[K], P[K]>;
+    } : T
+}
