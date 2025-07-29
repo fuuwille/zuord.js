a59240b6fa545507ae45beed0130b5edfeeb0ab8
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 18:38:58 +0300

    fix: change Pick namespace declaration to 'declare' for consistency

--- library/src/internal/pick.types.ts
+++ library/src/internal/pick.types.ts
@@ -1,7 +1,7 @@
 import { ZuordTrait } from "@zuord/trait";
 import { ZuordType } from "@zuord/type";
 
-export namespace Pick {
+export declare namespace Pick {
     export type Plain<T, P> = P extends ZuordType.Plain ? {
         -readonly [K in keyof T as [ZuordTrait.EqAny<[unknown, never, any], P[K]>] extends [true] ? never : K]: Pick.Plain<T[K], P[K]>;
     } : T
