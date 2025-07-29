033f535f3f1f0e63982aa4ef36bb22632a544dd0
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:19:43 +0300

    fix: add missing imports for Pick, Omit, and Mode types in Zuord namespace

--- library/src/alias.types.ts
+++ library/src/alias.types.ts
@@ -1,7 +1,13 @@
 import * as IntegrateModule from "./integrate.types";
 import * as MergeModule from "./merge.types";
+import * as PickModule from "./pick.types";
+import * as OmitModule from "./omit.types";
+import * as ModeModule from "./mode.types";
 
 export namespace Zuord {
     export import Integrate = IntegrateModule.Integrate;
     export import Merge = MergeModule.Merge;
+    export import Pick = PickModule.Pick;
+    export import Omit = OmitModule.Omit;
+    export import Mode = ModeModule.Mode;
 }
