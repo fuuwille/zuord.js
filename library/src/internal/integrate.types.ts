86e48690dd9d947c49ac168995fb554cd3a2edd0
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 14:28:05 +0300

    refactor: rename types for consistency and clarity in integrate namespace

--- library/src/internal/integrate.types.ts
+++ library/src/internal/integrate.types.ts
@@ -13,12 +13,12 @@ export namespace Integrate {
     );
 
     export type Array<A extends ZuordType.Array, B extends ZuordType.Array, TMode> = (
-        Collection<A, B, TMode> extends infer TIntegrated extends ZuordType.Array ? (
+        ArrayCollection<A, B, TMode> extends infer TIntegrated extends ZuordType.Array ? (
             ZuordUtil.Mutable<TIntegrated[number][]>
         ) : never
     );
 
-    export type Collection<A extends ZuordType.Array, B extends ZuordType.Array, TMode> = (
+    export type ArrayCollection<A extends ZuordType.Array, B extends ZuordType.Array, TMode> = (
         A extends ZuordType.Array ? B extends ZuordType.Array ? (
             TMode extends { concat: true } ? (
                 [...A, ...B]
@@ -27,18 +27,18 @@ export namespace Integrate {
     );
 
     export type Plain<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode> = (
-        (Overlap<A, B, TMode> & Extras<A, B>) extends infer TIntegrated ? ({
+        (PlainOverlap<A, B, TMode> & PlainExtras<A, B>) extends infer TIntegrated ? ({
             -readonly [K in keyof TIntegrated]: TIntegrated[K];
         }) : never
     )
 
-    export type Overlap<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode> = ({
+    export type PlainOverlap<A extends ZuordType.Plain, B extends ZuordType.Plain, TMode> = ({
         [K in keyof A]: K extends keyof B ? (
             [ZuordCore.ModeOn<TMode, "shallow">] extends [true] ? B[K] : Object<A[K], B[K], TMode>
         ) : A[K];
     });
 
-    export type Extras<A extends ZuordType.Plain, B extends ZuordType.Plain> = ({
+    export type PlainExtras<A extends ZuordType.Plain, B extends ZuordType.Plain> = ({
         [K in keyof B as K extends keyof A ? never : K]: B[K];
     });
 }
