d56af72bd3bb83657f0fd48dbd4066a449b0ff30
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 20:30:21 +0300

    fix: add flags type to internalZuordCore for improved type consistency

--- packages/core/src/internal/alias.ts
+++ packages/core/src/internal/alias.ts
@@ -1,11 +1,14 @@
 import { mode } from "./mode";
+import { flags } from "./flags";
 
 type internalZuordCore = {
-    readonly mode: typeof mode
+    readonly mode: typeof mode,
+    readonly flags: typeof flags;
 }
 
 const internalZuordCore: internalZuordCore = {
-    mode: mode
+    mode: mode,
+    flags: flags
 }
 
 export { internalZuordCore };
