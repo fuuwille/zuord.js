e1d042d3b15a0bc931a530d6ad0050846705d0e9
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:09:36 +0300

    fix: update OmitPattern type to use Exact.Plain for improved type accuracy

--- library/src/shape/omit.types.ts
+++ library/src/shape/omit.types.ts
@@ -3,6 +3,6 @@ import { ZuordUtil as Util } from "@zuord/util";
 
 export type OmitPlain = Type.Plain;
 
-export type OmitPattern<TPlain extends OmitPlain, TPattern extends Type.Plain> = Util.ExactKeys<OmitPatternLoose<TPlain>, TPattern>
+export type OmitPattern<TPlain extends OmitPlain, TPattern extends Type.Plain> = Util.Exact.Plain<OmitPatternLoose<TPlain>, TPattern>
 
 export type OmitPatternLoose<TPlain extends OmitPlain> = Util.Pattern<TPlain>; 
