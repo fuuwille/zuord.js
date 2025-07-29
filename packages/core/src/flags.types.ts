12cad34566b8b1c558fed2bdd50d51c32a3d30fd
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 20:31:42 +0300

    feat: add flags type definitions for Shallow, Concat, Unique, and Base

---
+++ packages/core/src/flags.types.ts
@@ -0,0 +1,11 @@
+import { InternalZuordCore as Internal } from "./internal";
+
+export namespace Flags {
+    export type Shallow = Internal.Flags.Shallow;
+
+    export type Concat = Internal.Flags.Concat;
+
+    export type Unique = Internal.Flags.Unique;
+
+    export type Base = Internal.Flags.Base;
+}
