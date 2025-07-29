41d3c35dbcca51a6848d09e73c4fdf8241e36f67
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 18:40:22 +0300

    fix: change Pick namespace declaration to 'declare' for consistency

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -1,7 +1,7 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
 
-export namespace Pick {
+export declare namespace Pick {
     export type Plain<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPattern<TPlain, TPattern>> = Internal.Pick.Plain<TPlain, TPattern>;
 
     export type PlainLoose<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPatternLoose<TPlain>> = Internal.Pick.Plain<TPlain, TPattern>;
