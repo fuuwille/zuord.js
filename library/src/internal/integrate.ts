b335160d95fed262975c919c2288a900fce1355f
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 12:53:14 +0300

    refactor: rename integrate function to object and streamline mode handling

--- library/src/internal/integrate.ts
+++ library/src/internal/integrate.ts
@@ -1,52 +1,60 @@
 import { zuordType, ZuordType } from "@zuord/type";
 import { Mode } from "./mode.types";
 
-export const integrate = <TBase, TInput, TMode>(base: TBase, input: TInput, mode: TMode, _strict: boolean) => {
-    const { shallow, concat, unique } = mode as Mode.Integrate;
+const object = <TBase, TInput, TMode>(base: TBase, input: TInput, mode: TMode, _strict: boolean) => {
 
     if(zuordType.array(base) && zuordType.array(input)) {
-        return concat ? ( unique 
-            ? Array.from(new Set([...base, ...input])) 
-            : [...base, ...input]
-        ) : input;
+        return array(base, input, mode);
     }
 
     if(zuordType.plain(base) && zuordType.plain(input)) {
-
-        const result: any = {};
-        const stack: Array<{ target: ZuordType.Plain; sourceA: ZuordType.Plain; sourceB: ZuordType.Plain }> = [{ target: result, sourceA: base, sourceB: input }];
-
-        while (stack.length) {
-            const { target, sourceA, sourceB } = stack.pop()!;
-
-            if (shallow) {
-                Object.assign(target, sourceA, sourceB);
-                continue;
-            }
-
-            const keys = new Set([...Object.keys(sourceA || {}), ...Object.keys(sourceB || {})]);
-
-            keys.forEach(key => {
-                const valA = sourceA?.[key];
-                const valB = sourceB?.[key];
-
-                if (Array.isArray(valA) && Array.isArray(valB)) {
-                    target[key] = concat ? (
-                        unique ? Array.from(new Set([...valA, ...valB])) 
-                        : [...valA, ...valB]
-                    ) : valB;
-                } else if (valB !== undefined && zuordType.plain(valA) && zuordType.plain(valB)) {
-                    stack.push({ target: target[key] = {}, sourceA: valA, sourceB: valB });
-                } else if (valB !== undefined) {
-                    target[key] = valB;
-                } else {
-                    target[key] = valA;
-                }
-            });
-        }
-
-        return result;
+        return plain(base, input, mode);
     }
 
     throw new TypeError("Both arguments must be either plain objects or arrays.");
+}
+
+const plain = <TBase extends ZuordType.Plain, TInput extends ZuordType.Plain, TMode>(base: TBase, input: TInput, mode: TMode ) => {
+    const { shallow, concat, unique } = mode as Mode.Integrate;
+
+    const result: any = {};
+    const stack: Array<{ target: ZuordType.Plain; sourceA: ZuordType.Plain; sourceB: ZuordType.Plain }> = [{ target: result, sourceA: base, sourceB: input }];
+
+    while (stack.length) {
+        const { target, sourceA, sourceB } = stack.pop()!;
+
+        if (shallow) {
+            Object.assign(target, sourceA, sourceB);
+            continue;
+        }
+
+        const keys = new Set([...Object.keys(sourceA || {}), ...Object.keys(sourceB || {})]);
+
+        keys.forEach(key => {
+            const valA = sourceA?.[key];
+            const valB = sourceB?.[key];
+
+            if (Array.isArray(valA) && Array.isArray(valB)) {
+                target[key] = array(valA, valB, mode);
+            } else if (valB !== undefined && zuordType.plain(valA) && zuordType.plain(valB)) {
+                stack.push({ target: target[key] = {}, sourceA: valA, sourceB: valB });
+            } else if (valB !== undefined) {
+                target[key] = valB;
+            } else {
+                target[key] = valA;
+            }
+        });
+    }
+
+    return result;
+}
+
+const array = <TBase extends ZuordType.Array, TInput extends ZuordType.Array, TMode>(base: TBase, input: TInput, mode: TMode) => {
+    const { concat, unique } = mode as Mode.Integrate;
+
+    if (concat) {
+        return unique ? Array.from(new Set([...base, ...input])) : [...base, ...input];
+    }
+
+    return input;
 }
