729ec00e9c0a18bd932c06e96a8c62385cd75128
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 20:41:53 +0300

    fix: standardize ZuordCore imports and update Mode types for consistency

--- library/src/mode.types.ts
+++ library/src/mode.types.ts
@@ -1,11 +1,11 @@
-import { ZuordCore } from "@zuord/core";
+import { ZuordCore as Core } from "@zuord/core";
 
 export declare namespace Mode {
-    export type Integrate = ZuordCore.Mode.Resolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
+    export type Integrate = Core.Mode.Resolve<[Core.Flags.Base, Core.Flags.Concat, Core.Flags.Unique]>;
 
     export type Merge = Integrate;
 
-    export type Pick = ZuordCore.BaseMode;
+    export type Pick = Core.Flags.Base;
 
-    export type Omit = ZuordCore.BaseMode;
+    export type Omit = Core.Flags.Base;
 }
