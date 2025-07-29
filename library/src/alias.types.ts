505c5d3420d3c7544c57d2f7797af5e073b4546f
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 02:40:00 +0300

    fix: simplify imports in alias.types.ts for clarity and consistency

--- library/src/alias.types.ts
+++ library/src/alias.types.ts
@@ -1,13 +1,13 @@
-import * as IntegrateModule from "./integrate.types";
-import * as MergeModule from "./merge.types";
-import * as PickModule from "./pick.types";
-import * as OmitModule from "./omit.types";
-import * as ModeModule from "./mode.types";
+import { Integrate as IntegrateModule } from "./integrate.types";
+import { Merge as MergeModule } from "./merge.types";
+import { Pick as PickModule } from "./pick.types";
+import { Omit as OmitModule } from "./omit.types";
+import { Mode as ModeModule } from "./mode.types";
 
 export declare namespace Zuord {
-    export import Integrate = IntegrateModule.Integrate;
-    export import Merge = MergeModule.Merge;
-    export import Pick = PickModule.Pick;
-    export import Omit = OmitModule.Omit;
-    export import Mode = ModeModule.Mode;
+    export import Integrate = IntegrateModule;
+    export import Merge = MergeModule;
+    export import Pick = PickModule;
+    export import Omit = OmitModule;
+    export import Mode = ModeModule;
 }
