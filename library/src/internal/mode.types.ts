c6480fd4346174f397a70422888f633ea1db4b18
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 12:33:49 +0300

    refactor: rename IntegrateMode to Integrate and MergeMode to Merge

--- library/src/internal/mode.types.ts
+++ library/src/internal/mode.types.ts
@@ -1,7 +1,7 @@
 import { ZuordCore } from "@zuord/core";
 
 export namespace Mode {
-    export type IntegrateMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
+    export type Integrate = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
 
-    export type MergeMode = IntegrateMode;
+    export type Merge = Integrate;
 }
