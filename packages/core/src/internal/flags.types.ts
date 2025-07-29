25857a772eae46cb5b91b2bbdf5991ed278b0796
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 19:25:46 +0300

    feat: add flags type definitions for mode handling

---
+++ packages/core/src/internal/flags.types.ts
@@ -0,0 +1,11 @@
+import { InternalZuordCore as Internal } from ".";
+
+export namespace Flags {
+    export type Shallow = Internal.Mode.Flag<"shallow">;
+    
+    export type Concat = Internal.Mode.Flag<"concat">;
+    
+    export type Unique = Internal.Mode.Flag<"unique">;
+    
+    export type Base = Internal.Mode.Flag<[Flags.Shallow]>;
+}
