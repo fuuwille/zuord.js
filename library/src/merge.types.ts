f82d9947c81a6219436cb36e6d4eb96cd757cf96
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:15:20 +0300

    fix: update Merge.Object type to use correct mode reference for improved type resolution

--- library/src/merge.types.ts
+++ library/src/merge.types.ts
@@ -1,9 +1,9 @@
 import { InternalZuord as Internal } from "./internal";
 import { ZuordCore } from "@zuord/core";
 import { ShapeZuord as Shape } from "./shape";
-import { Mode } from "./mode.types";
+import { mode } from "./mode";
 
 export namespace Merge {
     export type Object<TContent extends Shape.MergeContent, TMode extends Shape.MergeMode = {}> 
-        = Internal.Merge.Object<TContent, ZuordCore.ModeResolve<[Mode.Merge, TMode]>>;
+        = Internal.Merge.Object<TContent, ZuordCore.ModeResolve<[typeof mode.merge, TMode]>>;
 }
