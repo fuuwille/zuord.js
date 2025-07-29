4379e2b660fe998be7d34d18f837a039c50dfd9e
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 16:51:46 +0300

    refactor: update Pick and PickLoose types to use Internal.Pick.Plain for consistency and clarity

--- library/src/pick.types.ts
+++ library/src/pick.types.ts
@@ -3,8 +3,8 @@ import { ShapeZuord as Shape } from "./shape";
 import { ZuordType as Type } from "@zuord/type";
 import { ZuordUtil as Util } from "@zuord/util";
 
-export type Pick<TSouce extends Shape.PickSource, TPattern extends PickPattern<TSouce, TPattern>> = Internal.Pick<TSouce, TPattern>;
+export type Pick<TSouce extends Shape.PickSource, TPattern extends PickPattern<TSouce, TPattern>> = Internal.Pick.Plain<TSouce, TPattern>;
 
-export type PickLoose<TSouce extends Shape.PickSource, TPattern extends Shape.PickPattern<TSouce>> = Internal.Pick<TSouce, TPattern>;
+export type PickLoose<TSouce extends Shape.PickSource, TPattern extends Shape.PickPattern<TSouce>> = Internal.Pick.Plain<TSouce, TPattern>;
 
 export type PickPattern<TSouce extends Shape.PickSource, TPattern extends Type.Plain> = Util.Exact.Plain<Shape.PickPattern<TSouce>, TPattern>
