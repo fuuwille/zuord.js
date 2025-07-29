6c1f79e3e3f9a089aba651841edbbf7ce704e48f
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 11:59:36 +0300

    refactor: remove deprecated integration functions and types

--- library/src/main/integrate/index.ts
+++
@@ -1,9 +0,0 @@
-import { plain } from "./plain"
-import { plainLoose } from "./plainLoose";
-import { mode } from "./mode";
-
-export const integrate = {
-    plain,
-    plainLoose,
-    mode
-}
--- library/src/main/integrate/mode.ts
+++
@@ -1,5 +0,0 @@
-import { internalZuord as internal, InternalZuord as Internal  } from "../../internal";
-
-export const mode = internal.integrateMode satisfies Mode;
-
-export type Mode = Internal.IntegrateMode;
