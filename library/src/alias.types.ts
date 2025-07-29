a5d7fee75a030a86e886961f8ed4c72340947c85
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 16:55:45 +0300

    fix: correct import path for MergeModule in alias.types.ts

--- library/src/alias.types.ts
+++ library/src/alias.types.ts
@@ -1,5 +1,5 @@
 import * as IntegrateModule from "./integrate.types";
-import * as MergeModule from "./merge.index";
+import * as MergeModule from "./merge.types";
 
 export namespace Zuord {
     export import Integrate = IntegrateModule.Integrate;
