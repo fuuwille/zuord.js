4eafc94426db844a6c27516d3d3c354326939e11
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:11:52 +0300

    feat: implement mode integration with core modes and define type exports

---
+++ library/src/mode.ts
@@ -0,0 +1,24 @@
+import { zuordCore } from "@zuord/core";
+import { Mode } from "./mode.types";
+
+const integrate = zuordCore.modeResolve([zuordCore.baseMode, zuordCore.concatMode, zuordCore.uniqueMode]) satisfies Mode.Integrate;
+
+const merge = integrate satisfies Mode.Merge;
+
+const pick = zuordCore.baseMode satisfies Mode.Pick;
+
+const omit = zuordCore.baseMode satisfies Mode.Omit;
+
+type mode = {
+    readonly integrate: typeof integrate;
+    readonly merge: typeof merge;
+    readonly pick: typeof pick;
+    readonly omit: typeof omit;
+}
+
+export const mode : mode = {
+    integrate: integrate,
+    merge: merge,
+    pick: pick,
+    omit: omit
+}
