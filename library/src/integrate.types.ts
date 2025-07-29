a43681f446e9c4c5e30611950f53a4fec7653bac
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:12:32 +0300

    fix: update mode resolution in Integrate types for consistency with Mode integration

--- library/src/integrate.types.ts
+++ library/src/integrate.types.ts
@@ -2,17 +2,18 @@ import { InternalZuord as Internal } from "./internal";
 import { ZuordCore } from "@zuord/core";
 import { ZuordUtil as Util } from "@zuord/util";
 import { ShapeZuord as Shape } from "./shape";
+import { Mode } from "./mode.types";
 
 export namespace Integrate {
     export type Plain<TBase extends Shape.Integrate.Plain, TInput extends Util.Exact.Plain<TBase, TInput>, TMode extends Shape.Integrate.Mode = {}> 
-        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[TMode]>>;
+        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[Mode.Integrate, TMode]>>;
 
     export type PlainLoose<TBase extends Shape.Integrate.Plain, TInput extends Shape.Integrate.Plain, TMode extends Shape.Integrate.Mode = {}> 
-        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[TMode]>>;
+        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[Mode.Integrate, TMode]>>;
 
     export type PlainStrict<TBase extends Shape.Integrate.Plain, TInput extends Util.Exact.PlainStrict<TBase, TInput>, TMode extends Shape.Integrate.Mode = {}> 
-        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[TMode]>>;
+        = Internal.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[Mode.Integrate, TMode]>>;
 
     export type Array<TBase extends Shape.Integrate.Array, TInput extends Shape.Integrate.Array, TMode extends Shape.Integrate.Mode = {}> 
-        = Internal.Integrate.Array<TBase, TInput, ZuordCore.ModeResolve<[TMode]>>;
+        = Internal.Integrate.Array<TBase, TInput, ZuordCore.ModeResolve<[Mode.Integrate, TMode]>>;
 }
