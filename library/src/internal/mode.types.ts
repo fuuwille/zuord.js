a8be57ce299602bd764cbdd28df8fb7b6583ccfe
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 12:32:45 +0300

    feat: define MergeMode type as an alias for IntegrateMode

--- library/src/internal/mode.types.ts
+++ library/src/internal/mode.types.ts
@@ -2,4 +2,6 @@ import { ZuordCore } from "@zuord/core";
 
 export namespace Mode {
     export type IntegrateMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
+
+    export type MergeMode = IntegrateMode;
 }
