bfe57e43f4cc12f8f131c33ad48da59759788c9b
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 20:39:31 +0300

    fix: add flags type to zuordCore for consistency and clarity

--- packages/core/src/alias.ts
+++ packages/core/src/alias.ts
@@ -1,11 +1,14 @@
 import { mode } from "./mode";
+import { flags } from "./flags";
 
 type zuordCore = {
-    readonly mode: typeof mode
+    readonly mode: typeof mode;
+    readonly flags: typeof flags;
 }
 
 const zuordCore: zuordCore = {
-    mode: mode
+    mode: mode,
+    flags: flags
 }
 
 export { zuordCore };
