697b4bc045759982a6bacceb817a0abc98777c21
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 17:08:21 +0300

    refactor: update omit types to use namespace for better organization

--- library/src/omit.ts
+++ library/src/omit.ts
@@ -1,13 +1,13 @@
 import { internalZuord as internal } from "./internal";
 import { ShapeZuord as Shape, shapeZuord as shape } from "./shape";
-import { Omit, OmitLoose } from "./omit.types";
+import { Omit } from "./omit.types";
 
 function plain<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPattern<TPlain, TPattern>>(plain: TPlain, pattern: TPattern) {
     if(!shape.omitPlain(plain)) {
         throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
-    return internal.omit.plain(plain, pattern) as Omit<TPlain, TPattern>;
+    return internal.omit.plain(plain, pattern) as Omit.Plain<TPlain, TPattern>;
 }
 
 function plainLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape.OmitPatternLoose<TPlain>>(plain: TPlain, pattern: TPattern) {
@@ -15,7 +15,7 @@ function plainLoose<TPlain extends Shape.OmitPlain, const TPattern extends Shape
         throw new TypeError("Pick function expects the target object to be a plain.");
     }
 
-    return internal.omit.plain(plain, pattern) as OmitLoose<TPlain, TPattern>;
+    return internal.omit.plain(plain, pattern) as Omit.PlainLoose<TPlain, TPattern>;
 }
 
 
--- library/src/omit.types.ts
+++ library/src/omit.types.ts
@@ -1,6 +1,8 @@
 import { InternalZuord as Internal } from "./internal";
 import { ShapeZuord as Shape } from "./shape";
 
-export type Omit<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPattern<TPlain, TPattern>> = Internal.Omit.Plain<TPlain, TPattern>;
+export namespace Omit {
+    export type Plain<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPattern<TPlain, TPattern>> = Internal.Omit.Plain<TPlain, TPattern>;
 
-export type OmitLoose<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPatternLoose<TPlain>> = Internal.Omit.Plain<TPlain, TPattern>;
+    export type PlainLoose<TPlain extends Shape.OmitPlain, TPattern extends Shape.OmitPatternLoose<TPlain>> = Internal.Omit.Plain<TPlain, TPattern>;
+}
