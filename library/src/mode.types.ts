4804da4e73849295108040fa77cd147367230f1d
k4yr2 <kyrzkaya@gmail.com>
2025-07-29 18:40:15 +0300

    fix: change Mode namespace declaration to 'declare' for consistency

--- library/src/mode.types.ts
+++ library/src/mode.types.ts
@@ -1,6 +1,6 @@
 import { ZuordCore } from "@zuord/core";
 
-export namespace Mode {
+export declare namespace Mode {
     export type Integrate = ZuordCore.ModeResolve<[ZuordCore.BaseMode, ZuordCore.ConcatMode, ZuordCore.UniqueMode]>;
 
     export type Merge = Integrate;
