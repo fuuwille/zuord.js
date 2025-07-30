3e375f17ae61596ea7c12ad68817f177d7d2496c
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 00:22:18 +0300

    fix: correct type definition for Integrate.Plain and add missing type for Integrate.Array

--- library/src/extended/mode/integrate.types.ts
+++ library/src/extended/mode/integrate.types.ts
@@ -1,5 +1,7 @@
 import { ZuordCore as Core } from "@zuord/core";
 
 export declare namespace Integrate {
-    export type Plain = Core.Mode.Resolve<[Core.Flags.Base, Core.Flags.Concat, Core.Flags.Unique]>;
+    export type Plain = Core.Mode.Resolve<[Core.Flags.Base, Core.Flags.Concat]>;
+
+    export type Array = Core.Mode.Resolve<[Core.Flags.Base, Core.Flags.Concat, Core.Flags.Unique]>;
 }
