1cc6d2599e410416bb2c242b839dd0f7cc5b4dbb
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 15:27:03 +0300

    style: format Keys and ListKeys types for improved readability in Restrict namespace

--- packages/util/src/restrict.types.ts
+++ packages/util/src/restrict.types.ts
@@ -2,7 +2,9 @@ import { Restrict as $Restrict } from "./internal/restrict.types";
 import { ZuordType as Type } from "@zuord/type";
 
 export declare namespace Restrict {
-    export type Keys<TBase extends Type.Plain, TInput extends Type.Plain> =  $Restrict.Keys<TBase, TInput>
+    export type Keys<TBase extends Type.Plain, TInput extends Type.Plain> 
+        = $Restrict.Keys<TBase, TInput>
 
-    export type ListKeys<TBase extends Type.Plain, TInputs extends Type.PlainArray> = $Restrict.ListKeys<TBase, TInputs>
+    export type ListKeys<TBase extends Type.Plain, TInputs extends Type.PlainArray> 
+        = $Restrict.ListKeys<TBase, TInputs>  extends infer T extends Type.PlainArray ? T : never
 }
