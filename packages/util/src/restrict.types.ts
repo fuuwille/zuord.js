d8f6c04c3d4ba1f136d9479768d4686882562aba
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 15:19:35 +0300

    feat: import types for Restrict namespace and enhance Keys type definition

--- packages/util/src/restrict.types.ts
+++ packages/util/src/restrict.types.ts
@@ -1,3 +1,6 @@
-export declare namespace Restrict {
+import { Restrict as $Restrict } from "./internal/restrict.types";
+import { ZuordType as Type } from "@zuord/type";
 
+export declare namespace Restrict {
+    export type Keys<TBase extends Type.Plain, TInput extends Type.Plain> =  $Restrict.Keys<TBase, TInput>
 }
