358679dd2d7e604637cf3dc08bf89e8d8f10f708
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 06:35:53 +0300

    fix: update PlainArray type for improved handling of exact keys with stricter type parameters

--- packages/util/src/exact.types.ts
+++ packages/util/src/exact.types.ts
@@ -5,8 +5,8 @@ export namespace Exact {
     export type Plain<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
         = Internal.ExactKeys<TBase, TContent>;
 
-    export type PlainArray<TContent extends ZuordType.Plain[]> 
-        = Internal.ExactKeysFromInputs<TContent>;
+    export type PlainArray<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain[]> 
+        = Internal.ExactKeysArray<TBase, TContent> extends infer TSantized extends any[] ? TSantized : never;
 
     export type PlainStrict<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
         = Internal.ExactShape<TBase, TContent>;
