fea65d0fdb54e055bbca515e905e2361b527772f
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 00:23:22 +0300

    feat: update integrate export to include array type

--- library/src/extended/mode/integrate.ts
+++ library/src/extended/mode/integrate.ts
@@ -1,14 +1,18 @@
 import { zuordCore as core } from "@zuord/core";
 import { Integrate } from "./integrate.types";
 
-const plain = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies Integrate.Plain;
+const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies Integrate.Plain;
+
+const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies Integrate.Array;
 
 // EXPORT
 
 type integrate = {
     readonly plain: typeof plain;
+    readonly array: typeof array;
 }
 
 export const integrate: integrate = {
-    plain: plain
+    plain: plain,
+    array: array
 };
