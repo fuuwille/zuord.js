c561f942555f95d5d6734cdc080e9fff9233e78b
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 23:44:02 +0300

    refactor: swap definitions of Loose and Restrict types for improved clarity

--- library/src/extended/integrate/plain.types.ts
+++ library/src/extended/integrate/plain.types.ts
@@ -5,10 +5,10 @@ import { ZuordUtil as Util } from "@zuord/util";
 import { mode, Mode } from "../mode";
 
 export declare namespace Integrate {
-    export type Restrict<TBase extends Type.Plain, TInput extends Util.Restrict.Keys<TBase, TInput>, TMode extends Partial<Mode.Integrate.Plain> = {}> 
+    export type Loose<TBase extends Type.Plain, TInput extends Type.Plain, TMode extends Partial<Mode.Integrate.Plain> = {}> 
         = Internal.Integrate.Plain<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.plain, TMode]>>;
 
-    export type Loose<TBase extends Type.Plain, TInput extends Type.Plain, TMode extends Partial<Mode.Integrate.Plain> = {}> 
+    export type Restrict<TBase extends Type.Plain, TInput extends Util.Restrict.Keys<TBase, TInput>, TMode extends Partial<Mode.Integrate.Plain> = {}> 
         = Internal.Integrate.Plain<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.plain, TMode]>>;
 
     export type Strict<TBase extends Type.Plain, TInput extends Util.Strict.Keys<TBase, TInput>, TMode extends Partial<Mode.Integrate.Plain> = {}> 
