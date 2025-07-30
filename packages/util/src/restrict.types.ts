34735ada9e6ce0de97c9c3907711e9028adc45be
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 15:20:26 +0300

    feat: add ListKeys type for enhanced key handling in Restrict namespace

--- packages/util/src/restrict.types.ts
+++ packages/util/src/restrict.types.ts
@@ -3,4 +3,6 @@ import { ZuordType as Type } from "@zuord/type";
 
 export declare namespace Restrict {
     export type Keys<TBase extends Type.Plain, TInput extends Type.Plain> =  $Restrict.Keys<TBase, TInput>
+
+    export type ListKeys<TBase extends Type.Plain, TInputs extends Type.PlainArray> = $Restrict.ListKeys<TBase, TInputs>
 }
