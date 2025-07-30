45112d9b9999ace1f5892a2da71ea40268eeecf9
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 23:28:34 +0300

    feat: add Mode and Integrate namespaces with type definitions for modular structure

--- library/src/extended/mode/alias.types.ts
+++ library/src/extended/mode/alias.types.ts
@@ -1,4 +1,4 @@
-import { Integrate as IntegrateModule } from "./integrate";
+import { Integrate as IntegrateModule } from "./integrate.types";
 
 export declare namespace Mode {
     export import Integrate = IntegrateModule;
