fac507e0b8ec69c7f2f93cbe0841b1e74eff8f45
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 15:13:03 +0300

    fix: format restrict types for improved readability and consistency

--- packages/util/src/internal/restrict.types.ts
+++ packages/util/src/internal/restrict.types.ts
@@ -1,17 +1,17 @@
 import { ZuordType as Type } from "@zuord/type";
 
 export namespace Restrict {
-  export type Keys<TBase, TInput> = [TBase, TInput] extends [infer TBase extends Type.Plain, infer TInput extends Type.Plain] ? (
-    KeysIncluded<TBase, TInput> & KeysExcluded<TBase, TInput>
-  ) : TInput;
+    export type Keys<TBase, TInput> = [TBase, TInput] extends [infer TBase extends Type.Plain, infer TInput extends Type.Plain] ? (
+        KeysIncluded<TBase, TInput> & KeysExcluded<TBase, TInput>
+    ) : TInput;
 
-  export type KeysIncluded<TBase extends Type.Plain, TInput extends Type.Plain> = {
-    [K in keyof TInput]: K extends keyof TBase
-        ? Keys<TBase[K], TInput[K]>
-        : never
-  } 
+    export type KeysIncluded<TBase extends Type.Plain, TInput extends Type.Plain> = {
+        [K in keyof TInput]: K extends keyof TBase
+            ? Keys<TBase[K], TInput[K]>
+            : never
+    } 
 
-  export type KeysExcluded<TBase extends Type.Plain, TInput extends Type.Plain> = {
-    [K in Exclude<keyof TBase, keyof TInput>]?: never
-  };
+    export type KeysExcluded<TBase extends Type.Plain, TInput extends Type.Plain> = {
+        [K in Exclude<keyof TBase, keyof TInput>]?: never
+    };
 }
