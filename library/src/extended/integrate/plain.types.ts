e4ddf104d40d7a9b8463860be8ad60fd68b6bb46
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 23:35:26 +0300

    feat: add Integrate namespace with type definitions for modular structure

---
+++ library/src/extended/integrate/plain.types.ts
@@ -0,0 +1,16 @@
+import { InternalZuord as Internal } from "../../internal";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
+import { ZuordUtil as Util } from "@zuord/util";
+import { mode, Mode } from "../mode";
+
+export declare namespace Integrate {
+    export type Restrict<TBase extends Type.Plain, TInput extends Util.Restrict.Keys<TBase, TInput>, TMode extends Partial<Mode.Integrate.Plain> = {}> 
+        = Internal.Integrate.Plain<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.plain, TMode]>>;
+
+    export type Loose<TBase extends Type.Plain, TInput extends Type.Plain, TMode extends Partial<Mode.Integrate.Plain> = {}> 
+        = Internal.Integrate.Plain<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.plain, TMode]>>;
+
+    export type Strict<TBase extends Type.Plain, TInput extends Util.Exact.Keys<TBase, TInput>, TMode extends Partial<Mode.Integrate.Plain> = {}> 
+        = Internal.Integrate.Plain<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.plain, TMode]>>;
+}
