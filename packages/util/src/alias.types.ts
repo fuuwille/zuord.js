c40576f4cf05ca61c069695a241a1e9cc1174594
k4yr2 <kyrzkaya@gmail.com>
2025-07-30 15:20:44 +0300

    feat: export Restrict from restrict.types for enhanced type accessibility

--- packages/util/src/alias.types.ts
+++ packages/util/src/alias.types.ts
@@ -3,3 +3,4 @@ export * from "./normalize.types";
 export * from "./mutable.types";
 export * from "./pattern.types";
 export { Exact } from "./exact.index";
+export { Restrict } from "./restrict.types";
