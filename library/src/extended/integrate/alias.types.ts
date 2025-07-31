ac990bd1d8a07b6d20d68a2963f0caf9664592b9
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 07:36:38 +0300

    feat: add Array import to Integrate namespace for improved type accessibility

--- library/src/extended/integrate/alias.types.ts
+++ library/src/extended/integrate/alias.types.ts
@@ -1,5 +1,8 @@
 import { Plain as PlainModule } from "./plain.types";
+import { Array as ArrayModule } from "./array.types";
+
 
 export declare namespace Integrate {
     export import Plain = PlainModule;
+    export import Array = ArrayModule;
 }
