10ac1bcaf766cf001cf28d81ebeefc770daa26de
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 07:44:45 +0300

    feat: implement Array integration with type definitions for loose mode

---
+++ library/src/extended/integrate/array.ts
@@ -0,0 +1,18 @@
+import { internalZuord as internal } from "../../internal";
+import { zuordCore as core } from "@zuord/core";
+import { ZuordType as Type } from "@zuord/type";
+import { mode } from "../mode";
+import type { ZuordX } from "..";
+
+const $array = ($base: Type.Array, $input: Type.Array, $mode: Partial<ZuordX.Mode.Integrate.Array>) => {
+    return internal.integrate.array($base, $input, core.mode.resolve([mode.integrate.array, $mode]));
+}
+
+function loose <TBase extends Type.Array, TInput extends Type.Array> (base: TBase, input: TInput)
+    : ZuordX.Integrate.Array.Loose<TBase, TInput>;
+
+function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordX.Mode.Integrate.Array>> (base: TBase, input: TInput, mode : TMode)
+    : ZuordX.Integrate.Array.Loose<TBase, TInput, TMode>;
+
+function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordX.Mode.Integrate.Array>> (base: TBase, input: TInput, mode : TMode = {} as TMode)
+    : ZuordX.Integrate.Array.Loose<TBase, TInput, TMode> { return $array(base, input, mode) as ZuordX.Integrate.Array.Loose<TBase, TInput, TMode>; }
