c1b88cf6b093683e73aec2cc7eeabe0468d79276
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 22:59:39 +0300

    feat: add namespace ZuordX with type imports for Integrate and Mode

--- library/src/extended/index.d.ts
+++ library/src/extended/index.d.ts
@@ -1,6 +1,12 @@
 import { integrate, mode } from "./alias";
+import type { Integrate as $Integrate, Mode as $Mode } from "./alias.types";
 
 export declare const zuordX: {
     readonly integrate: typeof integrate;
     readonly mode: typeof mode;
 };
+
+export declare namespace ZuordX {
+    export import Integrate = $Integrate;
+    export import Mode = $Mode;
+}
