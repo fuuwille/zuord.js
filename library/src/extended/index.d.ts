27c4397fef4678d4053845eba84c1de555c6529b
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 23:00:01 +0300

    fix: update imports in index.d.ts to use alias for integrate and mode

--- library/src/extended/index.d.ts
+++ library/src/extended/index.d.ts
@@ -1,9 +1,9 @@
-import { integrate, mode } from "./alias";
+import { integrate as $integrate, mode as $mode } from "./alias";
 import type { Integrate as $Integrate, Mode as $Mode } from "./alias.types";
 
 export declare const zuordX: {
-    readonly integrate: typeof integrate;
-    readonly mode: typeof mode;
+    readonly integrate: typeof $integrate;
+    readonly mode: typeof $mode;
 };
 
 export declare namespace ZuordX {
