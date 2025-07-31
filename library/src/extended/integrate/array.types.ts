095c36bb682d26bafc7f0f432a995a458abdd283
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 07:32:09 +0300

    feat: add Array namespace with type definition for Integrate.Array

---
+++ library/src/extended/integrate/array.types.ts
@@ -0,0 +1,9 @@
+import { InternalZuord as Internal } from "../../internal";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
+import { mode, Mode } from "../mode";
+
+export namespace Array {
+    export type Array<TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<Mode.Integrate.Array> = {}> 
+        = Internal.Integrate.Array<TBase, TInput, Core.Mode.Resolve<[typeof mode.integrate.array, TMode]>>;
+}
