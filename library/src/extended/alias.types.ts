3fdd74d8a9782dcf2ea09cdd24e4ea158c42b241
k4yr2 <kyrzkaya@gmail.com>
2025-07-31 22:40:27 +0300

    refactor: remove alias files for integrate and mode to simplify codebase

--- library/src/extended/alias.types.ts
+++
@@ -1,7 +0,0 @@
-import { Integrate as IntegrateModule } from "./integrate";
-import { Mode as ModeModule } from "./mode";
-
-export declare namespace X {
-    export import Integrate = IntegrateModule;
-    export import Mode = ModeModule;
-}
