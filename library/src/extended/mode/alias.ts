451fbc615c3487c31d7dccc8fde6e674d3b1cc4b
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:23:50 +0300

    refactor: remove unused alias and type definitions for integrate module

--- library/src/extended/mode/alias.ts
+++
@@ -1,9 +0,0 @@
-import { integrate } from "./integrate.runtime";
-
-type mode = {
-    readonly integrate: typeof integrate;
-}
-
-export const mode: mode = {
-    integrate: integrate
-};
--- library/src/extended/mode/alias.types.ts
+++
@@ -1,5 +0,0 @@
-import { Integrate as IntegrateModule } from "./integrate.types";
-
-export declare namespace Mode {
-    export import Integrate = IntegrateModule;
-}
