523acd7a9a0e9c1f7dd954cec858ef670ec98a51
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 00:07:57 +0300

    fix: add array export to alias.d.ts for loose function

--- library/src/extended/integrate/alias.d.ts
+++ library/src/extended/integrate/alias.d.ts
@@ -1,7 +1,12 @@
 import * as $plain from "./plain";
+import * as $array from "./array";
 
 export declare const plain: {
     loose: typeof $plain.loose;
     restrict: typeof $plain.restrict;
     strict: typeof $plain.strict;
 }
+
+export declare const array: {
+    loose: typeof $array.loose;
+};
