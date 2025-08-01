fe16c0368a9256ed338bcdd9e0874346cd63f293
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:34:04 +0300

    refactor: define zuordXAPI type for better structure and clarity

--- library/src/extended/index.d.ts
+++ library/src/extended/index.d.ts
@@ -1,10 +1,12 @@
 import { integrate as $integrate, mode as $mode } from "./alias";
 import type { Integrate as $Integrate, Mode as $Mode } from "./alias.types";
 
-export declare const zuordX: {
+type zuordXAPI = {
     readonly integrate: typeof $integrate;
     readonly mode: typeof $mode;
-};
+}
+
+export declare const zuordX: zuordXAPI;
 
 export declare namespace ZuordX {
     export import Integrate = $Integrate;
