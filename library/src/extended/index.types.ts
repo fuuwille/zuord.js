36ecb091eae9f20cb11b21b0fdb6370dea143eb1
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:46:50 +0300

    feat: add initial declaration of ZuordX namespace with Integrate and Mode imports

---
+++ library/src/extended/index.types.ts
@@ -0,0 +1,7 @@
+import { Integrate as $Integrate } from "./integrate";
+import { Mode as $Mode } from "./mode";
+
+export declare namespace ZuordX {
+    export import Integrate = $Integrate;
+    export import Mode = $Mode;
+}
