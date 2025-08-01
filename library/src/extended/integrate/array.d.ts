a17f68e483e8fad596b331741afefa0ed7138e16
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 00:53:43 +0300

    fix: ensure consistent export of Array type in array integration files

--- library/src/extended/integrate/array.d.ts
+++ library/src/extended/integrate/array.d.ts
@@ -5,3 +5,5 @@ type arrayAPI = {
 }
 
 export declare const array: arrayAPI;
+
+export { Array } from "./array.types";
--- library/src/extended/integrate/array.js
+++ library/src/extended/integrate/array.js
@@ -1,2 +1,1 @@
 export * as array from "./array.runtime";
-export { Array } from "./array.types";
