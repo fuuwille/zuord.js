1cdf13343e27907965dc3b7472c2510e4b304607
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:24:50 +0300

    feat: add integrate API type definitions and exports

---
+++ library/src/extended/mode/integrate.d.ts
@@ -0,0 +1,9 @@
+export { plain as $plain } from "./integrate.runtime";
+export { array as $array } from "./integrate.runtime";
+
+type integrateAPI = {
+    readonly plain: typeof $plain;
+    readonly array: typeof $array;
+}
+
+export declare const integrate: integrateAPI;
