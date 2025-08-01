250da1f6792ccb5705c8804883b3ccdf440c8b46
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:37:35 +0300

    refactor: update imports to use ZuordX type for mode integration

--- library/src/extended/mode/integrate.runtime.ts
+++ library/src/extended/mode/integrate.runtime.ts
@@ -1,6 +1,10 @@
 import { zuordCore as core } from "@zuord/core";
-import { Integrate } from "./integrate.types";
 
-export const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies Integrate.Plain;
+import type { ZuordX } from "..";
 
-export const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies Integrate.Array;
+
+//
+
+export const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies ZuordX.Mode.Integrate.Plain;
+
+export const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies ZuordX.Mode.Integrate.Array;
