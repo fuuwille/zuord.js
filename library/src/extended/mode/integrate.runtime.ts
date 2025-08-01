48f9843367df3607f2de74cc4174ecbf01869f32
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:23:54 +0300

    feat: export plain and array constants from integrate.runtime module

--- library/src/extended/mode/integrate.runtime.ts
+++ library/src/extended/mode/integrate.runtime.ts
@@ -1,18 +1,6 @@
 import { zuordCore as core } from "@zuord/core";
 import { Integrate } from "./integrate.types";
 
-const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies Integrate.Plain;
+export const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies Integrate.Plain;
 
-const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies Integrate.Array;
-
-// EXPORT
-
-type integrate = {
-    readonly plain: typeof plain;
-    readonly array: typeof array;
-}
-
-export const integrate: integrate = {
-    plain: plain,
-    array: array
-};
+export const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies Integrate.Array;
