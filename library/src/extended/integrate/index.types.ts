7e808c6e901e4e06a18889b599d3df5c61670308
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 01:46:47 +0300

    refactor: change namespace declaration to 'declare' for Integrate

--- library/src/extended/integrate/index.types.ts
+++ library/src/extended/integrate/index.types.ts
@@ -1,7 +1,7 @@
 import { Plain as $Plain } from "./plain";
 import { Array as $Array } from "./array";
 
-export namespace Integrate {
+export declare namespace Integrate {
     export import Plain = $Plain;
     export import Array = $Array;
 }
