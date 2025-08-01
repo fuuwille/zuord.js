21a587159b4064a493e27bf39eb29f5d5c06a547
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 00:23:32 +0300

    fix: reorder imports for consistency in array.ts

--- library/src/extended/integrate/array.ts
+++ library/src/extended/integrate/array.ts
@@ -1,12 +1,19 @@
-import { internalZuord as internal } from "../../internal";
-import { zuordCore as core } from "@zuord/core";
-import { ZuordType as Type } from "@zuord/type";
 import { mode } from "../mode";
+import { zuordCore as core } from "@zuord/core";
+import { internalZuord as internal } from "../../internal";
+
 import type { ZuordX } from "..";
+import type { ZuordType as Type } from "@zuord/type";
+
+
+//
 
 const $ = ($base: Type.Array, $input: Type.Array, $mode: Partial<ZuordX.Mode.Integrate.Array>) => {
     return internal.integrate.array($base, $input, core.mode.resolve([mode.integrate.array, $mode]));
 }
+
+
+// LOOSE
 
 export function loose <TBase extends Type.Array, TInput extends Type.Array> (base: TBase, input: TInput)
     : ZuordX.Integrate.Array.Loose<TBase, TInput>;
