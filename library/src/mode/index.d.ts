9f951e7e2a34ee4a73f08ff622c9f1f8f76971bf
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 00:40:43 +0300

    feat: add essential type to zuordModeAPI in index.d.ts

--- library/src/mode/index.d.ts
+++ library/src/mode/index.d.ts
@@ -1,7 +1,9 @@
 import { extended as $extended } from "./index.runtime";
+import { essential as $essential } from "./index.runtime";
 
 type zuordModeAPI = {
     extended: typeof $extended;
+    essential: typeof $essential;
 }
 
 export declare const zuordMode: zuordModeAPI;
