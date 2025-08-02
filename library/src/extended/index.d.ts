71643da6c5687f2235f5aed3d310493e998ee57a
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:14:00 +0300

    refactor: remove mode type from zuordXAPI and ZuordX namespace for cleanup

--- library/src/extended/index.d.ts
+++ library/src/extended/index.d.ts
@@ -3,12 +3,10 @@ import type { Integrate as $Integrate, Mode as $Mode } from "./alias.types";
 
 type zuordXAPI = {
     integrate: typeof $integrate;
-    mode: typeof $mode;
 }
 
 export declare const zuordX: zuordXAPI;
 
 export declare namespace ZuordX {
     export import Integrate = $Integrate;
-    export import Mode = $Mode;
 }
