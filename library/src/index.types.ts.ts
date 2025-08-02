798f4f6a2914996d542cb7d63ce8e3f948d54fa5
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 09:52:04 +0300

    feat: add IntegrateArray import to Zuord namespace

--- library/src/index.types.ts.ts
+++ library/src/index.types.ts.ts
@@ -4,5 +4,7 @@ import { Mode as $Mode } from "./mode";
 export declare namespace Zuord {
     export import Integrate = $Integrate.Plain.Restrict;
 
+    export import IntegrateArray = $Integrate.Array.Loose;
+
     export import Mode = $Mode;
 }
