8b7d05a7b34c8debb11901874acd9030b5fb4ac8
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:16:56 +0300

    refactor: remove mode type from zuordAPI for cleanup

--- library/src/index.d.ts
+++ library/src/index.d.ts
@@ -1,11 +1,9 @@
 import { integrate as $integrate } from "./index.runtime";
 import { integrateArray as $integrateArray } from "./index.runtime";
-import { mode as $mode } from "./index.runtime";
 
 type zuordAPI = {
     integrate: typeof $integrate;
     integrateArray: typeof $integrateArray;
-    mode: typeof $mode;
 }
 
 export declare const zuord: zuordAPI;
