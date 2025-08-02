8809a6b2f54705fead6d94884a5a11a0eed8b04a
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 09:25:39 +0300

    feat: add integrateArray type to zuordAPI definition

--- library/src/index.d.ts
+++ library/src/index.d.ts
@@ -1,8 +1,10 @@
 import { integrate as $integrate } from "./index.runtime";
+import { integrateArray as $integrateArray } from "./index.runtime";
 import { mode as $mode } from "./index.runtime";
 
 type zuordAPI = {
     integrate: typeof $integrate;
+    integrateArray: typeof $integrateArray;
     mode: typeof $mode;
 }
 
