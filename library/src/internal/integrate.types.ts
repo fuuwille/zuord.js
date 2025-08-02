a1dd16628ca1f4abc947b92d56ced92d677ee6c1
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 10:34:26 +0300

    feat: reorder and restructure Array and ArrayOverlap types for improved clarity

--- library/src/internal/integrate.types.ts
+++ library/src/internal/integrate.types.ts
@@ -3,26 +3,12 @@ import { ZuordUtil as Util } from "@zuord/util";
 
 export declare namespace Integrate {
     export type Unknown<A, B, TMode> = (
-        [A, B] extends [infer A extends Type.Array, infer B extends Type.Array] ? (
-            Array<A, B, TMode>
-        ) : 
         [A, B] extends [infer A extends Type.Plain, infer B extends Type.Plain] ? (
             Plain<A, B, TMode>
+        ) :
+        [A, B] extends [infer A extends Type.Array, infer B extends Type.Array] ? (
+            Array<A, B, TMode>
         ) : B
-    );
-
-    export type Array<A extends Type.Array, B extends Type.Array, TMode> = (
-        ArrayOverlap<A, B, TMode> extends infer TIntegrated extends Type.Array ? (
-            Util.Mutable<TIntegrated[number][]>
-        ) : never
-    );
-
-    export type ArrayOverlap<A extends Type.Array, B extends Type.Array, TMode> = (
-        A extends Type.Array ? B extends Type.Array ? (
-            TMode extends { concat: true } ? (
-                [...A, ...B]
-            ) : B
-        ) : never : never
     );
 
     export type Plain<A extends Type.Plain, B extends Type.Plain, TMode> = (
@@ -40,4 +26,18 @@ export declare namespace Integrate {
     export type PlainExtras<A extends Type.Plain, B extends Type.Plain> = ({
         [K in keyof B as K extends keyof A ? never : K]: B[K];
     });
+
+    export type Array<A extends Type.Array, B extends Type.Array, TMode> = (
+        ArrayOverlap<A, B, TMode> extends infer TIntegrated extends Type.Array ? (
+            Util.Mutable<TIntegrated[number][]>
+        ) : never
+    );
+
+    export type ArrayOverlap<A extends Type.Array, B extends Type.Array, TMode> = (
+        A extends Type.Array ? B extends Type.Array ? (
+            TMode extends { concat: true } ? (
+                [...A, ...B]
+            ) : B
+        ) : never : never
+    );
 }
