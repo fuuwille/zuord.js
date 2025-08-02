a2acef23d2a2bee103ac5e9b181d902249ad4058
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:38:30 +0300

    feat: update array.types.ts to use zuordModeX and adjust type definitions

--- library/src/extended/zuord/integrate/array.types.ts
+++ library/src/extended/zuord/integrate/array.types.ts
@@ -1,6 +1,6 @@
-import { zuordMode } from "../../../mode";
+import { zuordModeX } from "../../mode";
 
-import type { ZuordMode } from "../../../mode";
+import type { ZuordModeX } from "../../mode";
 import type { ZuordType as Type } from "@zuord/type";
 import type { ZuordCore as Core } from "@zuord/core";
 import type { $Zuord } from "../../../internal";
@@ -9,6 +9,6 @@ import type { $Zuord } from "../../../internal";
 //
 
 export namespace Array {
-    export type Loose<TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordMode.Extended.Integrate.Array> = {}> 
-        = $Zuord.Integrate.Array<TBase, TInput, Core.Mode.Resolve<[typeof zuordMode.extended.integrate.array, TMode]>>;
+    export type Loose<TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array> = {}> 
+        = $Zuord.Integrate.Array<TBase, TInput, Core.Mode.Resolve<[typeof zuordModeX.integrate.array, TMode]>>;
 }
