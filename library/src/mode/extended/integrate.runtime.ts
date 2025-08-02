97afa9a529c41b93d11863b800963860f42ca274
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 00:23:46 +0300

    fix: update type imports in integrate.runtime.ts to use ZuordMode

--- library/src/mode/extended/integrate.runtime.ts
+++ library/src/mode/extended/integrate.runtime.ts
@@ -1,10 +1,10 @@
 import { zuordCore as core } from "@zuord/core";
 
-import type { ZuordX } from "../../extended";
+import type { ZuordMode } from "..";
 
 
 //
 
-export const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies ZuordX.Mode.Integrate.Plain;
+export const plain = core.mode.resolve([core.flags.base, core.flags.concat]) satisfies ZuordMode.Extended.Integrate.Plain;
 
-export const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies ZuordX.Mode.Integrate.Array;
+export const array = core.mode.resolve([core.flags.base, core.flags.concat, core.flags.unique]) satisfies ZuordMode.Extended.Integrate.Array;
