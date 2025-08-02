173c16760d6d4a97e2b483af54b38cdb56f1206e
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 10:44:50 +0300

    fix: rename and export unknown function in merge.ts for clarity

--- library/src/internal/merge.ts
+++ library/src/internal/merge.ts
@@ -1,18 +1,10 @@
 import { integrate } from "./integrate";
 
-const object = <TContent, TMode>(content: TContent, mode: TMode)=> {
+export const unknown = <TContent, TMode>(content: TContent, mode: TMode)=> {
     if (!Array.isArray(content) || content.length == 0) return {};
 
     return content.reduce((acc, current) => {
         if (acc === undefined) return current;
         return integrate.unknown(acc, current, mode, false);
     });
-}
-
-type merge = {
-    object: typeof object;
-}
-
-export const merge: merge = {
-    object
 }
