954580436d5d121f7d58c0e09096e3c7722f786a
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 00:40:04 +0300

    feat: import and export Essential from essential module in index.types.ts

--- library/src/mode/index.types.ts
+++ library/src/mode/index.types.ts
@@ -1,5 +1,7 @@
 import { Extended as $Extended } from "./extended"
+import { Essential as $Essential } from "./essential";
 
 export namespace ZuordMode {
     export import Extended = $Extended;
+    export import Essential = $Essential;
 }
