7020220b5708c7e74cd35b982d93ef74c2e9aebf
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:56:56 +0300

    feat: add exports for ZuordX and ZuordModeX in index.d.ts

--- library/src/extended/index.d.ts
+++ library/src/extended/index.d.ts
@@ -4,3 +4,6 @@ import { zuordModeX as $zuordModeX } from "./mode";
 export declare const zuordX: typeof $zuordX;
 
 export declare const zuordModeX: typeof $zuordModeX;
+
+export { ZuordX } from "./zuord";
+export { ZuordModeX } from "./mode";
