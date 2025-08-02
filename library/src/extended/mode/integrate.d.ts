c47dfbdfff96bd5e5f5e928dab12c69c7b6e81b5
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:23:49 +0300

    feat: add integrate API type definition for runtime integration

---
+++ library/src/extended/mode/integrate.d.ts
@@ -0,0 +1,9 @@
+import { plain as $plain } from "./integrate.runtime";
+import { array as $array } from "./integrate.runtime";
+
+type integrateAPI = {
+    plain: typeof $plain;
+    array: typeof $array;
+}
+
+export declare const integrate: integrateAPI;
