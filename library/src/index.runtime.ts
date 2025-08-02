599de254c23dfe819151735863de2aea1fd7d587
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 02:04:39 +0300

    refactor: remove unused mode import from index.runtime.ts and index.types.ts

--- library/src/index.runtime.ts
+++ library/src/index.runtime.ts
@@ -1,8 +1,5 @@
 import { integrate as $integrate } from "./extended/integrate";
-import { mode as $mode } from './mode';
 
 export const integrate = $integrate.plain.restrict;
 
 export const integrateArray = $integrate.array.loose;
-
-export const mode = $mode;
--- library/src/index.types.ts
+++ library/src/index.types.ts
@@ -1,10 +1,7 @@
 import { Integrate as $Integrate } from "./extended/integrate";
-import { Mode as $Mode } from "./mode";
 
 export declare namespace Zuord {
     export import Integrate = $Integrate.Plain.Restrict;
 
     export import IntegrateArray = $Integrate.Array.Loose;
-
-    export import Mode = $Mode;
 }
