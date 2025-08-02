11808297ff1e9cdc7b4aca9fbd121349798e5a02
k4yr2 <kyrzkaya@gmail.com>
2025-08-02 08:38:28 +0300

    feat: export mode from zuord.runtime module

--- library/src/zuord.runtime.ts
+++ library/src/zuord.runtime.ts
@@ -1,3 +1,6 @@
 import { integrate as $integrate } from "./extended/integrate";
+import { mode as $mode } from './mode';
 
 export const integrate = $integrate.plain.restrict;
+
+export const mode = $mode;
