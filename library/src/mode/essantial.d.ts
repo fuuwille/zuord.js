22cf9aad36ef4b680445a36740b6458102baac41
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 00:30:12 +0300

    feat: create essantial.d.ts to define essantialAPI and export its methods

---
+++ library/src/mode/essantial.d.ts
@@ -0,0 +1,9 @@
+import { integrate as $integrate } from "./index.runtime";
+import { integrateArray as $integrateArray } from "./index.runtime";
+
+type essantialAPI = {
+    readonly integrate: typeof $integrate;
+    readonly integrateArray: typeof $integrateArray;
+}
+
+export declare const essantial: essantialAPI;
