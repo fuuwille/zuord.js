5b940ae813cbbab9cc74ea33c867c9b85b24e0d7
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:44:57 +0300

    feat: update Loose type to use ZuordModeX.Merge.Array for improved merging

--- library/src/extended/zuord/merge/array.types.ts
+++ library/src/extended/zuord/merge/array.types.ts
@@ -3,6 +3,6 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordModeX } from "zuord/extended";
 
 export namespace Array {
-    export type Loose<TContent extends Type.ArrayOf<Type.Array>, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
+    export type Loose<TContent extends Type.ArrayOf<Type.Array>, TMode extends Partial<ZuordModeX.Merge.Array> = {}> 
         = $Zuord.Merge.Object<TContent, TMode>;
 }
