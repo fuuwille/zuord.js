04187e8a6949663bcc609a5f69e2604c569a6e09
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:35:44 +0300

    feat: add merge functionality to zuordXAPI type definition

--- library/src/extended/zuord/zuord.d.ts
+++ library/src/extended/zuord/zuord.d.ts
@@ -1,7 +1,9 @@
 import { integrate as $integrate } from "./zuord.runtime";
+import { merge as $merge } from "./zuord.runtime";
 
 type zuordXAPI = {
     integrate: typeof $integrate;
+    merge: typeof $merge;
 }
 
 export declare const zuordX: zuordXAPI;
