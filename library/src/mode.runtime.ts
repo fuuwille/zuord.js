5aafed270bc0a98e718295e338126e9eeab8b1ee
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:48:55 +0300

    feat: add merge and integrate functions for array support

--- library/src/mode.runtime.ts
+++ library/src/mode.runtime.ts
@@ -3,3 +3,7 @@ import { zuordModeX } from "./extended";
 export const integrate = zuordModeX.integrate.plain;
 
 export const integrateArray = zuordModeX.integrate.array;
+
+export const merge = zuordModeX.merge.plain;
+
+export const mergeArray = zuordModeX.merge.array;
