73df1149aacf4e28ace1fa2b6ac94a08ae05ece5
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:34:58 +0300

    feat: add Merge import to ZuordX namespace

--- library/src/extended/zuord/zuord.types.ts
+++ library/src/extended/zuord/zuord.types.ts
@@ -1,5 +1,7 @@
 import { Integrate as $Integrate } from "./integrate";
+import { Merge as $Merge } from "./merge";
 
 export namespace ZuordX {
     export import Integrate = $Integrate;
+    export import Merge = $Merge;
 }
