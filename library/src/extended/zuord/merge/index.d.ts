a4b44b46aafaa30c221d20dbd8d1b5ee637aec31
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:47:21 +0300

    feat: add array type to mergeAPI for enhanced merging capabilities

--- library/src/extended/zuord/merge/index.d.ts
+++ library/src/extended/zuord/merge/index.d.ts
@@ -1,7 +1,9 @@
 import { plain as $plain } from "./index.runtime";
+import { array as $array } from "./index.runtime";
 
 type mergeAPI = {
     plain: typeof $plain;
+    array: typeof $array;
 }
 
 export declare const merge: mergeAPI;
