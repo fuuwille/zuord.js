a8befcb2b3e52a46378b36a1833d3bd6c5fb3125
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 02:21:31 +0300

    refactor: add TupleWith and EndingTupleWith types for improved tuple handling

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -2,6 +2,10 @@ export type Tuple = TupleOf<unknown>;
 
 export type TupleOf<T> = readonly [T, ...T[]];
 
+export type TupleWith<TFirst, TRest> = readonly [TFirst, ...TRest[]];
+
 export type EndingTuple = EndingTupleOf<unknown>;
 
 export type EndingTupleOf<T> = readonly [...T[], T];
+
+export type EndingTupleWith<TRest, TLast> = readonly [...TRest[], TLast];
