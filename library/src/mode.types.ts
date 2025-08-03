5bf4b3a6ebdee58c8a0dfa5c7924e272adeedfb4
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:49:13 +0300

    feat: add Merge and MergeArray imports to ZuordMode namespace for enhanced functionality

--- library/src/mode.types.ts
+++ library/src/mode.types.ts
@@ -4,4 +4,8 @@ export namespace ZuordMode {
     export import Integrate = ZuordModeX.Integrate.Plain;
 
     export import IntegrateArray = ZuordModeX.Integrate.Array;
+
+    export import Merge = ZuordModeX.Merge.Plain;
+
+    export import MergeArray = ZuordModeX.Merge.Array;
 }
