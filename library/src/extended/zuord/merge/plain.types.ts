5000674878b3a2c4fddbfea845c308b5465b23a0
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:25:02 +0300

    feat: add Plain namespace with Loose type definition for merge functionality

---
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -0,0 +1,8 @@
+import { $Zuord } from "zuord/internal";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordModeX } from "zuord/extended";
+
+export namespace Plain {
+    export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain>> 
+        = $Zuord.Merge.Object<TContent, TMode>;
+}
