8e36a374f09e980a420ea186f9ad8bfa20f245f3
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:21:14 +0300

    feat: add Merge import to ZuordModeX namespace

--- library/src/extended/mode/mode.types.ts
+++ library/src/extended/mode/mode.types.ts
@@ -1,5 +1,7 @@
 import { Integrate as $Integrate } from "./integrate.types";
+import { Merge as $Merge } from "./merge.types";
 
 export declare namespace ZuordModeX {
     export import Integrate = $Integrate;
+    export import Merge = $Merge;
 }
