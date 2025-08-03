c3c50a7c64a219945ca70a0577349ee87b073c7d
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:43:20 +0300

    feat: add array types for loose merging in Zuord extended module

---
+++ library/src/extended/zuord/merge/array.types.ts
@@ -0,0 +1,8 @@
+import { $Zuord } from "zuord/internal";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordModeX } from "zuord/extended";
+
+export namespace Array {
+    export type Loose<TContent extends Type.ArrayOf<Type.Array>, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
+        = $Zuord.Merge.Object<TContent, TMode>;
+}
