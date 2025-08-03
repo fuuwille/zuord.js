1a17fbc2a5b86cb9f855c47fbc5a975d2fa112d8
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 02:18:36 +0300

    refactor: reorganize tuple types for improved clarity and consistency

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -1,3 +1,7 @@
 export type Tuple = TupleOf<unknown>;
 
 export type TupleOf<T> = readonly [T, ...T[]];
+
+export type EndingTuple = EndingTupleOf<unknown>;
+
+export type EndingTupleOf<T> = readonly [...T[], T];
