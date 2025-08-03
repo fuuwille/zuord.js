f4f4656b4913aa038fd13260628744341a0d5107
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 02:23:48 +0300

    refactor: add type constraints to Tuple, TupleWith, EndingTuple, and EndingTupleWith for improved type safety

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -1,11 +1,11 @@
 export type Tuple = TupleOf<unknown>;
 
-export type TupleOf<T> = readonly [T, ...T[]];
+export type TupleOf<T extends unknown> = readonly [T, ...T[]];
 
-export type TupleWith<TFirst, TRest> = readonly [TFirst, ...TRest[]];
+export type TupleWith<TFirst extends unknown, TRest extends unknown[]> = readonly [TFirst, ...TRest];
 
 export type EndingTuple = EndingTupleOf<unknown>;
 
-export type EndingTupleOf<T> = readonly [...T[], T];
+export type EndingTupleOf<T extends unknown> = readonly [...T[], T];
 
-export type EndingTupleWith<TRest, TLast> = readonly [...TRest[], TLast];
+export type EndingTupleWith<TRest extends unknown[], TLast extends unknown> = readonly [...TRest, TLast];
