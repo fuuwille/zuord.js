6f3f363e85ccd059f62fd2b5ab47750703828556
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 03:02:51 +0300

    feat: add mode.d.ts to define zuordModeAPI with integrate and integrateArray types

---
+++ library/src/mode.d.ts
@@ -0,0 +1,9 @@
+import { integrate as $integrate } from "./mode.runtime";
+import { integrateArray as $integrateArray } from "./mode.runtime";
+
+type zuordModeAPI = {
+    integrate: typeof $integrate;
+    integrateArray: typeof $integrateArray;
+}
+
+export declare const zuordMode: zuordModeAPI;
