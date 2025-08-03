bc62b39d83194c68ca051a6b5e8c0ccf2724075a
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:50:57 +0300

    feat: add Merge and MergeArray imports to Zuord namespace for enhanced functionality

--- library/src/zuord.types.ts
+++ library/src/zuord.types.ts
@@ -4,4 +4,8 @@ export declare namespace Zuord {
     export import Integrate = ZuordX.Integrate.Plain.Restrict;
 
     export import IntegrateArray = ZuordX.Integrate.Array.Loose;
+
+    export import Merge = ZuordX.Merge.Plain.Loose;
+
+    export import MergeArray = ZuordX.Merge.Array.Loose;
 }
