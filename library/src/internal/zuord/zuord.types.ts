3f503d51016683af40065565d3e866cb6bed9a80
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:23:19 +0300

    feat: add Merge import to $Zuord namespace

--- library/src/internal/zuord/zuord.types.ts
+++ library/src/internal/zuord/zuord.types.ts
@@ -1,5 +1,7 @@
 import { Integrate as $Integrate } from "./integrate.types";
+import { Merge as $Merge } from "./merge.types";
 
 export namespace $Zuord {
     export import Integrate = $Integrate;
+    export import Merge = $Merge;
 }
