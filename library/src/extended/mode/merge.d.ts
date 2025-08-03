3ff736a13d5526cc5a7db5568654925940a978a3
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:48:45 +0300

    feat: add array type to mergeAPI for improved type support

--- library/src/extended/mode/merge.d.ts
+++ library/src/extended/mode/merge.d.ts
@@ -1,7 +1,9 @@
 import { plain as $plain } from "./merge.runtime";
+import { array as $array } from "./merge.runtime";
 
 type mergeAPI = {
     plain: typeof $plain;
+    array: typeof $array;
 }
 
 export declare const merge: mergeAPI;
