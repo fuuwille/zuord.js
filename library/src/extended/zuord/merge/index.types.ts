a98ca596a8e4505f4060a146372bf5a58a820ad7
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:46:49 +0300

    feat: export Array type in Merge namespace for extended functionality

--- library/src/extended/zuord/merge/index.types.ts
+++ library/src/extended/zuord/merge/index.types.ts
@@ -1,5 +1,7 @@
 import { Plain as $Plain } from "./plain";
+import { Array as $Array } from "./array";
 
 export namespace Merge {
     export import Plain = $Plain;
+    export import Array = $Array;
 }
