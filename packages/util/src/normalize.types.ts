f82de1a67eb372078e8b10611252e707a2b1eceb
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 01:29:59 +0300

    refactor: simplify Normalize type definition by removing unnecessary constraints

--- packages/util/src/normalize.types.ts
+++ packages/util/src/normalize.types.ts
@@ -1,8 +1,7 @@
 import { ZuordType } from "@zuord/type";
 import { ZuordTrait } from "@zuord/trait";
-import { ZuordCore } from "@zuord/core";
 
-export type Normalize<T, TMode extends Partial<NormalizeBaseMode>> = [ZuordTrait.Eq<T, any>] extends [false] ? (
+export type Normalize<T, TMode> = [ZuordTrait.Eq<T, any>] extends [false] ? (
     [ZuordTrait.Has<T, ZuordType.Tuple>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Tuple> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) |
         (ZuordTrait.Extract<T, ZuordType.Tuple> extends infer TExtracted extends ZuordType.Tuple ? {
@@ -18,11 +17,9 @@ export type Normalize<T, TMode extends Partial<NormalizeBaseMode>> = [ZuordTrait
     [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
         (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted extends ZuordType.Plain ? {
-            [K in TExtracted extends any ? keyof TExtracted : never]: K extends keyof TExtracted ? Normalize<TExtracted[K], TMode> : never;
-        } : never )
+            [K in TExtracted extends any ? keyof TExtracted : never]: TExtracted extends any ? K extends keyof TExtracted ? TExtracted[K] : never : never;
+        } extends infer TTO ? {
+            [K in keyof TTO]: Normalize<TTO[K], TMode>;
+        } : never: never )
     ) : T
 ) : any;
-
-export type NormalizeBaseMode = ZuordCore.ModeResolve<[ZuordCore.BaseMode]>;
-
-export type NormalizeDefaultMode = ZuordCore.BaseMode;
