80aa270df276d6621b5c9e65f102d58516d24a38
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:28:11 +0300

    fix: add default value for TMode in Loose type definition

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -3,6 +3,6 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordModeX } from "zuord/extended";
 
 export namespace Plain {
-    export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain>> 
+    export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
         = $Zuord.Merge.Object<TContent, TMode>;
 }
