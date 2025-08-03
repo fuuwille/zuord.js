86f86c1b644152073ce9c3738cc937404318ea19
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:21:37 +0300

    feat: add merge type to zuordModeXAPI

--- library/src/extended/mode/mode.d.ts
+++ library/src/extended/mode/mode.d.ts
@@ -1,7 +1,9 @@
 import { integrate as $integrate } from "./mode.runtime";
+import { merge as $merge } from "./mode.runtime";
 
 type zuordModeXAPI = {
     integrate: typeof $integrate;
+    merge: typeof $merge;
 }
 
 export declare const zuordModeX: zuordModeXAPI;
