62654320cb17ffb0ac127cff3623278fa48e8b47
k4yr2 <kyrzkaya@gmail.com>
2025-08-03 04:49:41 +0300

    feat: add merge and mergeArray types to zuordModeAPI for enhanced functionality

--- library/src/mode.d.ts
+++ library/src/mode.d.ts
@@ -1,9 +1,13 @@
 import { integrate as $integrate } from "./mode.runtime";
 import { integrateArray as $integrateArray } from "./mode.runtime";
+import { merge as $merge } from "./mode.runtime";
+import { mergeArray as $mergeArray } from "./mode.runtime";
 
 type zuordModeAPI = {
     integrate: typeof $integrate;
     integrateArray: typeof $integrateArray;
+    merge: typeof $merge;
+    mergeArray: typeof $mergeArray;
 }
 
 export declare const zuordMode: zuordModeAPI;
