0786dd6a2dc727ff191f176c9eeeb9ad0305f3fb
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 20:20:41 +0300

    refactor: rename Plain type to PlainResolve for consistency in type definitions

--- packages/util/src/normalize.types.ts
+++ packages/util/src/normalize.types.ts
@@ -1,5 +1,5 @@
 import { Normalize as $Normalize } from "./internal/zuord";
 
 export namespace Normalize {
-    export type Plain<T, TMode> = $Normalize.Plain<T, TMode>;
+    export type Plain<T, TMode> = $Normalize.PlainResolve<T, TMode>;
 }
