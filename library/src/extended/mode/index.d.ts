dd13dc10ce5d79bb729749e72c7db55bdd3b4ff1
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:40:06 +0300

    feat: add evolve type to zuordModeXAPI in index.d.ts

--- library/src/extended/mode/index.d.ts
+++ library/src/extended/mode/index.d.ts
@@ -1,9 +1,11 @@
 import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
+import { evolve as $evolve } from "./index.runtime";
 
 type zuordModeXAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
+    evolve: typeof $evolve;
 }
 
 export declare const zuordModeX: zuordModeXAPI;
