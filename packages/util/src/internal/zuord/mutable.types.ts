0b99f93bcf20e02bdb8cd3a5487ea82b6da09ce1
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 20:49:28 +0300

    feat: refactor Mutable.Resolve type to use ArrayResolve for improved clarity and structure

--- packages/util/src/internal/zuord/mutable.types.ts
+++ packages/util/src/internal/zuord/mutable.types.ts
@@ -5,12 +5,16 @@ export namespace Mutable {
         T extends ZuordType.Plain | ZuordType.Tuple ? (
             StructResolve<T>
         ) :
-        T extends ZuordType.ArrayOf<infer U> ? ( 
-            Mutable.Resolve<U>[]
+        T extends ZuordType.Array ? ( 
+            ArrayResolve<T>
         ) : T
     )
 
     export type StructResolve<T> = ({ 
         -readonly [K in keyof T]: Mutable.Resolve<T[K]> 
     })
+
+    export type ArrayResolve<T extends ZuordType.Array> = (
+        T extends ZuordType.ArrayOf<infer U> ?  Mutable.Resolve<U>[] : never
+    );
 }
