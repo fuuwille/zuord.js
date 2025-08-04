94f0e031370beb8fad6bd65c9ed6fb7b8ef404ff
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 23:12:29 +0300

    feat: update Plain type to use PlainResolve for consistency in Normalize namespace feat: update Pattern type to use PlainResolve for consistency in pattern types

--- packages/util/src/pattern.types.ts
+++ packages/util/src/pattern.types.ts
@@ -1,4 +1,4 @@
 import { $ZuordUtil } from "./internal";
 import { ZuordType } from "@zuord/type";
 
-export type Pattern<T extends ZuordType.Plain> = $ZuordUtil.Pattern.ResolvePlainOnly<T>;
+export type Pattern<T extends ZuordType.Plain> = $ZuordUtil.Pattern.PlainResolve<T>;
