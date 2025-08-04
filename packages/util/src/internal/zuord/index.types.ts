f62c2270968c629d5d58c98288ce09ad8c1c4017
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 20:38:21 +0300

    feat: create index.types.ts to export utility types under $ZuordUtil namespace

---
+++ packages/util/src/internal/zuord/index.types.ts
@@ -0,0 +1,13 @@
+import { Normalize as $Normalize } from "./normalize.types";
+import { Restrict as $Restrict } from "./restrict.types";
+import { Strict as $Strict } from "./strict.types";
+import { Mutable as $Mutable } from "./mutable.types";
+import { Pattern as $Pattern } from "./pattern.types";
+
+export namespace $ZuordUtil {
+    export import Normalize = $Normalize;
+    export import Restrict = $Restrict;
+    export import Strict = $Strict;
+    export import Mutable = $Mutable;
+    export import Pattern = $Pattern;
+}
