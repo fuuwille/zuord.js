f1e15b73c907e7191f8491712db7d62467d7b8bb
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 23:12:46 +0300

    feat: rename ResolvePlainOnly type to PlainResolve for consistency in Pattern namespace

--- packages/util/src/internal/zuord/pattern.types.ts
+++ packages/util/src/internal/zuord/pattern.types.ts
@@ -1,7 +1,7 @@
 import { ZuordType } from "@zuord/type";
 
 export namespace Pattern {
-    export type ResolvePlainOnly<T> = T extends ZuordType.Plain ? {
-        [K in keyof T]?: true | ResolvePlainOnly<T[K]>
+    export type PlainResolve<T> = T extends ZuordType.Plain ? {
+        [K in keyof T]?: true | PlainResolve<T[K]>
     } : never;
 }
