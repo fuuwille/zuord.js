672442c87be258ec90670a60ddfd63fa259376e9
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 23:16:38 +0300

    feat: add Normalize namespace with Plain type definition using PlainResolve

---
+++ packages/util/src/zuord/normalize.types.ts
@@ -0,0 +1,6 @@
+import { $ZuordUtil } from "../internal";
+import { ZuordType as Type } from "@zuord/type";
+
+export namespace Normalize {
+    export type Plain<T extends Type.Plain, TMode> = $ZuordUtil.Normalize.PlainResolve<T, TMode>;
+}
