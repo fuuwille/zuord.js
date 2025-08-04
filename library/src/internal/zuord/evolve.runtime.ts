b3b4b06ce553e38441c0beac6fd1da8fc9bfa2f3
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:21:34 +0300

    feat: implement evolve runtime with integrate and merge functions

---
+++ library/src/internal/zuord/evolve.runtime.ts
@@ -0,0 +1,18 @@
+import { integrate } from "./integrate";
+import { merge } from "./merge";
+
+const $ = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode, integrate: (base: any, input: any, mode: any) => any, merge: (content: any, mode: any) => any) => {
+    return integrate(base, merge(content, mode), mode);
+}
+
+export const unknown = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode) => {
+    return $(base, content, mode, integrate.unknown, merge.unknown);
+}
+
+export const plain = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode) => {
+    return $(base, content, mode, integrate.plain, merge.plain);
+}
+
+export const array = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode) => {
+    return $(base, content, mode, integrate.array, merge.array);
+}
