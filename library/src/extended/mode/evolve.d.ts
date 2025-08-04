6b79d5deb7eedde67e32308804d3445159589bfc
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:38:40 +0300

    feat: create evolve.d.ts to define evolveAPI type and export evolve

---
+++ library/src/extended/mode/evolve.d.ts
@@ -0,0 +1,9 @@
+import { plain as $plain } from "./evolve.runtime";
+import { array as $array } from "./evolve.runtime";
+
+type evolveAPI = {
+    plain: typeof $plain;
+    array: typeof $array;
+};
+
+export declare const evolve: evolveAPI;
