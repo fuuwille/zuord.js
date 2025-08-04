6e779eb01f6dbbfd65eab05009e4f619f8b0bbfa
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:30:13 +0300

    feat: add evolve type to $zuordAPI in index.d.ts

--- library/src/internal/zuord/index.d.ts
+++ library/src/internal/zuord/index.d.ts
@@ -1,9 +1,11 @@
 import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
+import { evolve as $evolve } from "./evolve.runtime";
 
 type $zuordAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
+    evolve: typeof $evolve;
 }
 
 export declare const $zuord: $zuordAPI;
