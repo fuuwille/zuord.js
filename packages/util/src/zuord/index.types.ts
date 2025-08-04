a584159d2ce37270758c56480faf6c8318626adc
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 01:53:21 +0300

    feat: add Mutable import to ZuordUtil namespace in index.types

--- packages/util/src/zuord/index.types.ts
+++ packages/util/src/zuord/index.types.ts
@@ -1,9 +1,11 @@
 import { Normalize as $Normalize } from "./normalize";
+import { Mutable as $Mutable } from "./mutable";
 import { Restrict as $Restrict } from "./restrict";
 import { Strict as $Strict } from "./strict";
 
 export declare namespace ZuordUtil {
     export import Normalize = $Normalize;
+    export import Mutable = $Mutable;
     export import Restrict = $Restrict;
     export import Strict = $Strict;
 }
