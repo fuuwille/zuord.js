bd07dc2a45ee853758ff792320f3ef4955255435
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 23:30:07 +0300

    feat: add Restrict import to ZuordUtil namespace in index.types

--- packages/util/src/zuord/index.types.ts
+++ packages/util/src/zuord/index.types.ts
@@ -1,7 +1,9 @@
 import { Normalize as $Normalize } from "./normalize";
+import { Restrict as $Restrict } from "./restrict";
 import { Strict as $Strict } from "./strict";
 
 export declare namespace ZuordUtil {
     export import Normalize = $Normalize;
+    export import Restrict = $Restrict;
     export import Strict = $Strict;
 }
