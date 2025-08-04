1c2b2f5d279c1010b75feff96da819427a2b954e
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 20:01:43 +0300

    refactor: remove IsUnion type and simplify PlainExtract implementation for clarity

--- packages/util/src/internal/zuord/normalize.types.ts
+++ packages/util/src/internal/zuord/normalize.types.ts
@@ -1,19 +1,15 @@
 import { ZuordTrait } from "@zuord/trait";
 import { ZuordType } from "@zuord/type";
 
-type IsUnion<T, U = T> = T extends any
-    ? [U] extends [T] ? false : true
-    : false;
-
 export declare namespace Normalize {
     export type Plain<T, TMode> = [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
         | (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize.Plain<TExcluded, TMode> : never)
         | (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? Normalize.PlainExtract<TExtracted, TMode> : never)
     ) : T;
 
-    export type PlainExtract<T, TMode> = [IsUnion<T>] extends [true] ? ({
-            [K in T extends any ? keyof T : never]: T extends any ? K extends keyof T ? T[K] : never : never;
-        } extends infer V extends ZuordType.Plain ? { [K in keyof V]: Normalize.Plain<V[K], TMode> }: never
-    ) : T;
+    export type PlainExtract<T, TMode> = (
+        ZuordType.UnionToIntersection<T> extends infer V ? ({ 
+            [K in keyof V]: Normalize.Plain<V[K], TMode>      
+        }) : never
+    );
 }
-
