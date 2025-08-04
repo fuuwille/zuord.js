5801134b98836543c5a51a47b560336af3d02469
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 19:34:34 +0300

    refactor: implement Normalize namespace with Plain and PlainExtract types for enhanced type handling

---
+++ packages/util/src/internal/zuord/normalize.types.ts
@@ -0,0 +1,19 @@
+import { ZuordTrait } from "@zuord/trait";
+import { ZuordType } from "@zuord/type";
+
+type IsUnion<T, U = T> = T extends any
+    ? [U] extends [T] ? false : true
+    : false;
+
+export namespace Normalize {
+    export type Plain<T, TMode> = [ZuordTrait.Is<T, ZuordType.Plain>] extends [true] ? (
+        | ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize.Plain<TExcluded, TMode> : never
+        | ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? PlainExtract<TExtracted, TMode> : never
+    ) : T;
+
+    export type PlainExtract<T, TMode> = [IsUnion<T>] extends [true] ? ({
+            [K in T extends any ? keyof T : never]: T extends any ? K extends keyof T ? T[K] : never : never;
+        } extends infer V extends ZuordType.Plain ? { [K in keyof V]: Normalize.Plain<V[K], TMode> }: never
+    ) : T;
+}
+
