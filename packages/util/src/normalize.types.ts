ad6b79ce226424f4fe483a08f1661552337ae17f
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 12:55:47 +0300

    refactor: simplify Normalize type handling for Plain types

--- packages/util/src/normalize.types.ts
+++ packages/util/src/normalize.types.ts
@@ -18,11 +18,7 @@ export type Normalize<T, TMode> = (
     ) :     
     [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? (
         (ZuordTrait.Exclude<T, ZuordType.Plain> extends infer TExcluded ? Normalize<TExcluded, TMode> : never) | 
-        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted extends ZuordType.Plain ? {
-            [K in TExtracted extends any ? keyof TExtracted : never]: TExtracted extends any ? K extends keyof TExtracted ? TExtracted[K] : never : never;
-        } extends infer TTO ? {
-            [K in keyof TTO]: Normalize<TTO[K], TMode>;
-        } : never: never )
+        (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted extends ZuordType.Plain ? NormalizePlain<TExtracted, TMode> : never)
     ) : T
 )
 
