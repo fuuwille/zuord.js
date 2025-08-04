6a13084ae8abd3b43954a8a6806cd7fbbf200366
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 01:54:39 +0300

    feat: add Pattern import to ZuordUtil namespace in index.types

--- packages/util/src/zuord/index.types.ts
+++ packages/util/src/zuord/index.types.ts
@@ -2,10 +2,15 @@ import { Normalize as $Normalize } from "./normalize";
 import { Mutable as $Mutable } from "./mutable";
 import { Restrict as $Restrict } from "./restrict";
 import { Strict as $Strict } from "./strict";
+import { Pattern as $Pattern } from "./pattern";
+
+
+//
 
 export declare namespace ZuordUtil {
     export import Normalize = $Normalize;
     export import Mutable = $Mutable;
     export import Restrict = $Restrict;
     export import Strict = $Strict;
+    export import Pattern = $Pattern;
 }
