5008fe75110fc0358f48874a7f87182139f2f837
k4yr2 <kyrzkaya@gmail.com>
2025-08-04 23:28:04 +0300

    feat: add Strict import to ZuordUtil namespace in index.types

--- packages/util/src/zuord/index.types.ts
+++ packages/util/src/zuord/index.types.ts
@@ -1,5 +1,7 @@
 import { Normalize as $Normalize } from "./normalize";
+import { Strict as $Strict } from "./strict";
 
 export declare namespace ZuordUtil {
     export import Normalize = $Normalize;
+    export import Strict = $Strict;
 }
