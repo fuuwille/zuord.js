9a73caa08f9b99fde0d5eaeb9f9567a56e3d61ea
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:39:29 +0300

    feat: import Evolve from evolve module in index.types.ts

--- library/src/extended/mode/index.types.ts
+++ library/src/extended/mode/index.types.ts
@@ -1,7 +1,9 @@
-import { Integrate as $Integrate } from "./integrate.types";
-import { Merge as $Merge } from "./merge.types";
+import { Integrate as $Integrate } from "./integrate";
+import { Merge as $Merge } from "./merge";
+import { Evolve as $Evolve } from "./evolve";
 
 export declare namespace ZuordModeX {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
+    export import Evolve = $Evolve;
 }
