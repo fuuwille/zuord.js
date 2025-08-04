559691060f30179f285e50160ef8b2e5c45d978b
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 02:32:56 +0300

    feat: add Evolve import to $Zuord namespace in index.types.ts

--- library/src/internal/zuord/index.types.ts
+++ library/src/internal/zuord/index.types.ts
@@ -1,7 +1,9 @@
 import { Integrate as $Integrate } from "./integrate.types";
 import { Merge as $Merge } from "./merge.types";
+import { Evolve as $Evolve } from "./evolve.types";
 
 export namespace $Zuord {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
+    export import Evolve = $Evolve;
 }
