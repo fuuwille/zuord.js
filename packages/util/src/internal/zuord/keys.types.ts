ebd62a1fa49c8363b30c058b58e76c3f644b7d6b
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 23:34:37 +0300

    feat: add Keys namespace with All, Required, and Optional types for enhanced type utility

---
+++ packages/util/src/internal/zuord/keys.types.ts
@@ -0,0 +1,9 @@
+export namespace Keys {
+    export type All<T> = T extends any ? keyof T : never;
+
+    export type Required<T> = Exclude<Keys.All<T>, Keys.Optional<T>>;
+
+    export type Optional<T> = {
+        [K in Keys.All<T>]: T extends { [P in K]-?: T[K] } ? never : K
+    }[Keys.All<T>];
+}
