73a7eaedb34c0b54852f05cd2154f0188e651f45
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 21:04:23 +0300

    fix: refine RequiredKeys and RequiredKeysRaw type definitions for improved type utility

--- packages/util/src/internal/zuord/object.types.ts
+++ packages/util/src/internal/zuord/object.types.ts
@@ -1,11 +1,21 @@
 export namespace Object {
     export type Keys<T> = T extends unknown ? keyof T : never;
 
-    export type RequiredKeys<T> = {
+    export type RequiredKeys<T> = Keys<T> extends infer K ? (
+        K extends keyof any ? (
+            T extends unknown ?( 
+                K extends RequiredKeysRaw<T> ? K : never
+            ) : never
+        ) : never
+    ) : never;
+
+    export type RequiredKeysRaw<T> = {
         [K in keyof T]-?: {} extends Pick<T, K> ? never : K
     }[keyof T];
 
     export type OptionalKeys<T> = {
         [K in keyof T]-?: {} extends Pick<T, K> ? K : never
     }[keyof T];
+
+
 }
