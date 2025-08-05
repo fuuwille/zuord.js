7bd45b769ed0428e43a9e0a95cd6b7d146274a3d
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 00:26:05 +0300

    fix: enhance ResolvePlain and ResolvePlainComposite types for improved type resolution

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -10,16 +10,23 @@ export namespace One {
     );
 
     export type ResolvePlain<T, TMode> = (
+        | (Trait.Exclude<T, Type.Plain> extends infer TExcluded ? One.ResolveHybrid<TExcluded, TMode> : never)
+        | (Trait.Extract<T, Type.Plain> extends infer TExtracted ? One.ResolvePlainComposite<TExtracted, TMode> : never)
+    ) extends infer T ? T : never;
+
+    export type ResolvePlainComposite<T, TMode> = (
         (One.ResolvePlainRequired<T> & One.ResolvePlainOptional<T>) extends infer TOne ? {
             [K in keyof TOne]: TMode extends { shallow: true } ? TOne[K] : One.ResolveHybrid<TOne[K], TMode>;
         } : never
-    )
+    );
 
     export type ResolvePlainRequired<T> = {
         [K in $ZuordUtil.Keys.Required<T>]: T[K]
     };
 
     export type ResolvePlainOptional<T> = {
-        [K in $ZuordUtil.Keys.Optional<T>]?: T[K]
+        [K in $ZuordUtil.Keys.Optional<T>]?: T extends any ? (
+            K extends keyof T ? T[K] : never
+        ) : never;
     };
 }
