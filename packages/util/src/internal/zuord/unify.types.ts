c6437c95d818e1baa6077e79f9b7b1544be4b90e
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 23:50:02 +0300

    fix: correct type resolution logic in Unify namespace for improved type handling

--- packages/util/src/internal/zuord/unify.types.ts
+++ packages/util/src/internal/zuord/unify.types.ts
@@ -5,7 +5,7 @@ export declare namespace Unify {
     export type Resolve<T, TMode> = (
         [ZuordTrait.Eq<T, any>] extends [true] ? any :
         [ZuordTrait.Is<T, ZuordType.Primitive>] extends [true] ? T :
-        [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? Unify.ResolvePlain<T, TMode> : never
+        [ZuordTrait.Has<T, ZuordType.Plain>] extends [true] ? Unify.ResolvePlain<T, TMode> : T
     );
 
     export type ResolvePlain<T, TMode> = (
@@ -13,13 +13,13 @@ export declare namespace Unify {
         | (ZuordTrait.Extract<T, ZuordType.Plain> extends infer TExtracted ? Unify.ResolvePlainExtract<TExtracted, TMode> : never)
     ) extends infer T ? T : never;
 
-    export type ResolvePlainExtract<T, TMode> = [ZuordTrait.Is<T, ZuordType.Plain>] extends [true] ? (
+    export type ResolvePlainExtract<T, TMode> = (
         ResolvePlainComposite<T> extends infer TFilled ? ({
             [K in keyof TFilled]: (
-                TMode extends { shallow: true } ? TFilled[K] : Unify.ResolvePlain<TFilled[K], TMode>
+                TMode extends { shallow: true } ? TFilled[K] : Unify.Resolve<TFilled[K], TMode>
             )
         }) : never
-    ) : any;
+    );
 
     export type ResolvePlainComposite<T> = {
         [K in T extends any ? keyof T : never]: T extends any ? K extends keyof T ? T[K] : never : never;
