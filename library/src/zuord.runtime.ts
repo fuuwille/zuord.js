f521a40cfc2f3e66a3f9311e75dd6b1fab252a05
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:19:29 +0300

    feat: add evolve export to zuord.runtime.ts

--- library/src/zuord.runtime.ts
+++ library/src/zuord.runtime.ts
@@ -7,3 +7,5 @@ export const integrateArray = zuordX.integrate.array.loose;
 export const merge = zuordX.merge.plain.loose;
 
 export const mergeArray = zuordX.merge.array.loose;
+
+export const evolve = zuordX.evolve.plain.restrict;
