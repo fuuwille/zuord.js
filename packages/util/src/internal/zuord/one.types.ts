4ae5915ed053f8496b70f474c7f05fb5f36ffdc8
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 00:11:33 +0300

    feat: add Hybrid type to One namespace for enhanced type resolution

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -1,6 +1,14 @@
 import { $ZuordUtil } from ".";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordTrait as Trait } from "@zuord/trait";
 
 export namespace One {
+    export type Hybrid<T, TMode> = (
+        [Trait.Eq<T, any>] extends [true] ? any :
+        [Trait.Is<T, Type.Primitive>] extends [true] ? T :
+        [Trait.Has<T, Type.Plain>] extends [true] ? One.Plain<T> : T
+    );
+
     export type Plain<T> = (
         (One.PlainRequired<T> & One.PlainOptional<T>) extends infer TOne ? {
             [K in keyof TOne]: TOne[K];
