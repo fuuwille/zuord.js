77dee744a5e661cb4db435f61fceb6b6cf8070ac
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:20:39 +0300

    feat: add evolve import to zuordAPI in zuord.d.ts

--- library/src/zuord.d.ts
+++ library/src/zuord.d.ts
@@ -2,12 +2,14 @@ import { integrate as $integrate } from "./zuord.runtime";
 import { integrateArray as $integrateArray } from "./zuord.runtime";
 import { merge as $merge } from "./zuord.runtime";
 import { mergeArray as $mergeArray } from "./zuord.runtime";
+import { evolve as $evolve } from "./zuord.runtime";
 
 type zuordAPI = {
     integrate: typeof $integrate;
     integrateArray: typeof $integrateArray;
     merge: typeof $merge;
     mergeArray: typeof $mergeArray;
+    evolve: typeof $evolve;
 }
 
 export declare const zuord: zuordAPI;
