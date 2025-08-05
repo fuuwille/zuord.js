4cb1bf9804db50cbcf324b98326adf7a24c3a80a
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 21:00:24 +0300

    feat: add RequiredKeys type to Object namespace for enhanced type utility

--- packages/util/src/internal/zuord/object.types.ts
+++ packages/util/src/internal/zuord/object.types.ts
@@ -1,3 +1,7 @@
 export namespace Object {
     export type Keys<T> = T extends unknown ? keyof T : never;
+
+    export type RequiredKeys<T> = {
+        [K in keyof T]-?: {} extends Pick<T, K> ? never : K
+    }[keyof T];
 }
