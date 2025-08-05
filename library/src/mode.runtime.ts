a43aece055fa86bfa030be500f79e64e1d8a7731
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:17:51 +0300

    feat: add evolve export to mode.runtime.ts

--- library/src/mode.runtime.ts
+++ library/src/mode.runtime.ts
@@ -7,3 +7,5 @@ export const integrateArray = zuordModeX.integrate.array;
 export const merge = zuordModeX.merge.plain;
 
 export const mergeArray = zuordModeX.merge.array;
+
+export const evolve = zuordModeX.evolve.plain;
