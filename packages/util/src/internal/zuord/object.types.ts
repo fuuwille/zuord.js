56987280794d5179c147512d6d37f83cfeb08f63
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 21:00:43 +0300

    feat: add OptionalKeys type to Object namespace for enhanced type utility

--- packages/util/src/internal/zuord/object.types.ts
+++ packages/util/src/internal/zuord/object.types.ts
@@ -4,4 +4,8 @@ export namespace Object {
     export type RequiredKeys<T> = {
         [K in keyof T]-?: {} extends Pick<T, K> ? never : K
     }[keyof T];
+
+    export type OptionalKeys<T> = {
+        [K in keyof T]-?: {} extends Pick<T, K> ? K : never
+    }[keyof T];
 }
