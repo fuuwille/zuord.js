617fb4df040ebc352ccb356d36ace6b42c7339ee
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 22:50:20 +0300

    feat: add RequiredKeys and OptionalKeys types to Object namespace for enhanced type utility

--- packages/util/src/internal/zuord/object.types.ts
+++ packages/util/src/internal/zuord/object.types.ts
@@ -2,4 +2,10 @@ export namespace Object {
     export type Keys<T> = keyof T;
       
     export type AllKeys<T> = T extends any ? keyof T : never;
+
+    export type RequiredKeys<T> = Exclude<AllKeys<T>, OptionalKeys<T>>;
+
+    export type OptionalKeys<T> = {
+        [K in Object.AllKeys<T>]: T extends { [P in K]-?: T[K] } ? never : K
+    }[Object.AllKeys<T>];
 }
