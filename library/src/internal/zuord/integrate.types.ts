d89bb383cf34567d5c986e37c2256ed25b0a4b2e
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:57:50 +0300

    feat: update Array type to use ResolveArray instead of Array in integrate.types.ts

--- library/src/internal/zuord/integrate.types.ts
+++ library/src/internal/zuord/integrate.types.ts
@@ -4,7 +4,7 @@ import { ZuordUtil as Util } from "@zuord/util";
 export declare namespace Integrate {
     export type Unknown<A, B, TMode> = (
         [A, B] extends [infer A extends Type.Plain, infer B extends Type.Plain] ? ResolvePlain<A, B, TMode> :
-        [A, B] extends [infer A extends Type.Array, infer B extends Type.Array] ? Array<A, B, TMode> : B
+        [A, B] extends [infer A extends Type.Array, infer B extends Type.Array] ? ResolveArray<A, B, TMode> : B
     );
 
     export type ResolvePlain<A extends Type.Plain, B extends Type.Plain, TMode> = (
@@ -23,13 +23,13 @@ export declare namespace Integrate {
         [K in keyof B as K extends keyof A ? never : K]: B[K];
     });
 
-    export type Array<A extends Type.Array, B extends Type.Array, TMode> = (
-        ArrayOverlap<A, B, TMode> extends infer TIntegrated extends Type.Array ? (
+    export type ResolveArray<A extends Type.Array, B extends Type.Array, TMode> = (
+        ResolveArrayOverlap<A, B, TMode> extends infer TIntegrated extends Type.Array ? (
             Util.Mutable.Hybrid<TIntegrated[number][]>
         ) : never
     );
 
-    export type ArrayOverlap<A extends Type.Array, B extends Type.Array, TMode> = (
+    export type ResolveArrayOverlap<A extends Type.Array, B extends Type.Array, TMode> = (
         A extends Type.Array ? B extends Type.Array ? (
             TMode extends { concat: true } ? (
                 [...A, ...B]
