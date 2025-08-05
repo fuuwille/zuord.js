7dd8176e6921eba5d5a7ac6aa7fc722594ed4a6e
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 00:14:56 +0300

    fix: rename types in One namespace for consistency and improved clarity

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -3,23 +3,23 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordTrait as Trait } from "@zuord/trait";
 
 export namespace One {
-    export type Hybrid<T, TMode> = (
+    export type ResolveHybrid<T, TMode> = (
         [Trait.Eq<T, any>] extends [true] ? any :
         [Trait.Is<T, Type.Primitive>] extends [true] ? T :
-        [Trait.Has<T, Type.Plain>] extends [true] ? One.Plain<T, TMode> : T
+        [Trait.Has<T, Type.Plain>] extends [true] ? One.ResolvePlain<T, TMode> : T
     );
 
-    export type Plain<T, TMode> = (
-        (One.PlainRequired<T> & One.PlainOptional<T>) extends infer TOne ? {
-            [K in keyof TOne]: TMode extends { shallow: true } ? TOne[K] : One.Hybrid<TOne[K], TMode>;
+    export type ResolvePlain<T, TMode> = (
+        (One.ResolvePlainRequired<T> & One.ResolvePlainOptional<T>) extends infer TOne ? {
+            [K in keyof TOne]: TMode extends { shallow: true } ? TOne[K] : One.ResolveHybrid<TOne[K], TMode>;
         } : never
     )
 
-    export type PlainRequired<T> = {
+    export type ResolvePlainRequired<T> = {
         [K in $ZuordUtil.Keys.Required<T>]: T[K]
     };
 
-    export type PlainOptional<T> = {
+    export type ResolvePlainOptional<T> = {
         [K in $ZuordUtil.Keys.Optional<T>]?: T[K]
     };
 }
