2218226fc91daa36c7f064a6e1f687414970d747
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 01:26:38 +0300

    fix: refine ResolveHybrid and ResolveArray types for improved type resolution

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -6,21 +6,20 @@ export namespace One {
     export type ResolveHybrid<T, TMode> = (
         [Trait.Eq<T, any>] extends [true] ? any :
         [Trait.Has<T, Type.Primitive>] extends [true] ? One.ResolvePrimitive<T, TMode> :
-        [Trait.Has<T, Type.Plain>] extends [true] ? One.ResolvePlain<T, TMode> :
-        [Trait.Has<T, Type.Array>] extends [true] ? One.ResolveArray<T, TMode> : T
+        [T] extends [Type.Plain] ? One.ResolvePlain<T, TMode> :
+        [T] extends [Type.Array]? One.ResolveArray<T, TMode> : T
     );
 
     export type ResolvePrimitive<T, TMode> = (
-        | (Trait.Exclude<T, Type.Primitive> extends infer TExcluded ? One.ResolveHybrid<TExcluded, TMode> : never)
+        | (Trait.Exclude<T, Type.Primitive> extends infer TExcluded ? (
+            One.ResolveHybrid<TExcluded, TMode> extends infer THybrid ? (
+                [{}] extends [THybrid] ? never : THybrid
+            ) : never
+        ) : never)
         | (Trait.Extract<T, Type.Primitive> extends infer TExtracted ? TExtracted : never)
-    );
-
-    export type ResolvePlain<T, TMode> = (
-        | (Trait.Exclude<T, Type.Plain> extends infer TExcluded ? One.ResolveHybrid<TExcluded, TMode> : never)
-        | (Trait.Extract<T, Type.Plain> extends infer TExtracted extends Type.Plain ? One.ResolvePlainComposite<TExtracted, TMode> : never)
-    );
-
-    export type ResolvePlainComposite<T extends Type.Plain, TMode> = (
+    ) extends infer T ? T : never;
+    
+    export type ResolvePlain<T extends Type.Plain, TMode> = (
         (One.ResolvePlainRequired<T> & One.ResolvePlainOptional<T>) extends infer TOne ? {
             [K in keyof TOne]: TMode extends { shallow: true } ? TOne[K] : One.ResolveHybrid<TOne[K], TMode>;
         } : never
@@ -36,12 +35,7 @@ export namespace One {
         ) : never;
     };
 
-    export type ResolveArray<T, TMode> = (
-        | (Trait.Exclude<T, Type.Array> extends infer TExcluded ? One.ResolveHybrid<TExcluded, TMode> : never)
-        | (Trait.Extract<T, Type.Array> extends infer TExtracted extends Type.Array ? One.ResolveArrayComposite<TExtracted, TMode> : never)
-    )
-
-    export type ResolveArrayComposite<T extends Type.Array, TMode> = (
+    export type ResolveArray<T extends Type.Array, TMode> = (
         ResolveHybrid<T[number], TMode>[]
     ) extends infer T ? T : never;
 }
