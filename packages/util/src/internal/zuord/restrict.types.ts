3876ae2b05971bb9d810ffacdb1b4ad890dc6547
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:16:05 +0300

    refactor: simplify ListKeys type definition in restrict.types.ts

--- packages/util/src/internal/zuord/restrict.types.ts
+++ packages/util/src/internal/zuord/restrict.types.ts
@@ -17,17 +17,7 @@ export namespace Restrict {
         [K in Exclude<keyof TBase, keyof TInput>]?: never
     };
 
-    export type ListKeys<TBase, TInputs> = TInputs extends Type.TupleOf<infer T> ? (
-        ListKeysFromTuple<TBase, T>
-    ) : TInputs extends Type.ArrayOf<infer U> ? (
-        ListKeysFromArray<TBase, Type.UnionToTuple<U>>[number][]
-    ) : never;
-
-    export type ListKeysFromTuple<TBase, TInputs> = TInputs extends [infer TFirst, ...infer TRest]
-        ? [Keys<TBase, TFirst>, ...ListKeysFromTuple<TBase, TRest>]
-        : TInputs;
-
-    export type ListKeysFromArray<TBase, TInputs extends readonly unknown[]> = {
-        [K in keyof TInputs]: Keys<TBase, TInputs[K]>
-    } extends infer T extends Type.Array ? T : never;
+    export type ListKeys<TBase, TInputs extends readonly unknown[]> = {
+        [I in keyof TInputs]: Keys<TBase, TInputs[I]>;
+    };
 }
