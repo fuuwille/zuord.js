f61b34c22400ec8ec7ec4a6dfcd1e0a6d5b60504
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 11:05:48 +0300

    feat: include evolve type in zuordXAPI declaration

--- library/src/extended/zuord/index.d.ts
+++ library/src/extended/zuord/index.d.ts
@@ -1,9 +1,11 @@
 import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
+import { evolve as $evolve } from "./index.runtime";
 
 type zuordXAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
+    evolve: typeof $evolve;
 }
 
 export declare const zuordX: zuordXAPI;
