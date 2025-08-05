1726de242423f342f8abeb6949805ae71f22690d
k4yr2 <kyrzkaya@gmail.com>
2025-08-05 18:42:06 +0300

    feat: add Unify import to $ZuordUtil namespace in index.types.ts

--- packages/util/src/internal/zuord/index.types.ts
+++ packages/util/src/internal/zuord/index.types.ts
@@ -1,4 +1,5 @@
 import { Normalize as $Normalize } from "./normalize.types";
+import { Unify as $Unify } from "./unify.types";
 import { Restrict as $Restrict } from "./restrict.types";
 import { Strict as $Strict } from "./strict.types";
 import { Mutable as $Mutable } from "./mutable.types";
@@ -6,6 +7,7 @@ import { Pattern as $Pattern } from "./pattern.types";
 
 export namespace $ZuordUtil {
     export import Normalize = $Normalize;
+    export import Unify = $Unify;
     export import Restrict = $Restrict;
     export import Strict = $Strict;
     export import Mutable = $Mutable;
