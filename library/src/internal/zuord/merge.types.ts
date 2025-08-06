cc0521b61bea75d16049eee2ecc2023da26f78a8
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 12:27:02 +0300

    fix: update ResolvePlain types to enforce TMode extends ZuordCore.Mode.Field for improved type safety

--- library/src/internal/zuord/merge.types.ts
+++ library/src/internal/zuord/merge.types.ts
@@ -4,7 +4,7 @@ import { ZuordCore } from "@zuord/core";
 import { ZuordUtil } from "@zuord/util";
 
 export declare namespace Merge {
-    export type ResolvePlain<TContent, TMode> = (
+    export type ResolvePlain<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.PureTuple ? (
             Merge.ResolvePlainFromTuple<TContent, TMode>
         ) : 
@@ -13,7 +13,7 @@ export declare namespace Merge {
         ) : never
     );
 
-    export type ResolvePlainFromTuple<TContent, TMode> = (
+    export type ResolvePlainFromTuple<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.EndingTupleWith<infer TRest, infer TLast> ? (
             TRest["length"] extends 0 ? TLast : 
             TRest["length"] extends 1 ? Integrate.Unknown<TRest[0], TLast, TMode> 
@@ -21,9 +21,9 @@ export declare namespace Merge {
         ) : never
     );
 
-    export type ResolvePlainFromArray<TContent, TMode> = TContent extends readonly (infer TInfer)[] ? (
+    export type ResolvePlainFromArray<TContent, TMode extends ZuordCore.Mode.Field> = TContent extends readonly (infer TInfer)[] ? (
         ZuordType.PlainAsRequired<TInfer> extends infer TRequired extends ZuordType.Plain ? (
-            ZuordUtil.Unify.Plain<TRequired, TMode>
+            ZuordUtil.One.Plain<TRequired, TMode>
         ) : never
     ) : never;
 }
