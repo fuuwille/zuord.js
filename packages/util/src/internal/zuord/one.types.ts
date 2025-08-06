d5d51f9845ff0b002f6575044c00a0797ec2006d
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 13:55:08 +0300

    fix: rename All type to ResolveAll in One namespace for consistency and clarity

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -4,23 +4,23 @@ import { ZuordCore as Core } from "@zuord/core";
 import { ZuordTrait as Trait } from "@zuord/trait";
 
 export namespace One {
-    export type All<T, TMode extends Core.Mode.Field> = Trait.Is<T, Type.Plain> extends true ? (
+    export type ResolveAll<T, TMode extends Core.Mode.Field> = Trait.Is<T, Type.Plain> extends true ? (
         (ResolveRequired<T, TMode> & ResolveOptional<T, TMode>) extends infer TInfer ? ({
-            [K in keyof TInfer]: One.All<TInfer[K], TMode>;
+            [K in keyof TInfer]: One.ResolveAll<TInfer[K], TMode>;
         }) : never
     ) : T;
 
     export type ResolveRequired<T, TMode extends Core.Mode.Field> = [T] extends [Type.Plain] ? {
         [K in $Util.Keys.Required<T>]: (
             [TMode ]extends [{ "$one.all": true }] ? (
-                One.All<T[K], TMode>
+                One.ResolveAll<T[K], TMode>
             ) : One.ResolveRequired<T[K], TMode>
         )
     } : T;
 
     export type ResolveOptional<T, TMode extends Core.Mode.Field> = {
         [K in $Util.Keys.Optional<T>]?: T extends any ? (
-            K extends keyof T ? One.All<T[K], TMode> : never
+            K extends keyof T ? One.ResolveAll<T[K], TMode> : never
         ) : never
     };
 }
