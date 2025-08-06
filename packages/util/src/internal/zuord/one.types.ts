287754293a17da2a1bc40711f042a3f0c0658277
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 13:52:04 +0300

    fix: update references to All type in One namespace for consistency and clarity

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -5,17 +5,17 @@ import { ZuordTrait as Trait } from "@zuord/trait";
 export namespace One {
     export type All<T> = Trait.Is<T, Type.Plain> extends true ? (
         (ResolveRequired<T> & ResolveOptional<T>) extends infer TInfer ? ({
-            [K in keyof TInfer]: All<TInfer[K]>;
+            [K in keyof TInfer]: One.All<TInfer[K]>;
         }) : never
     ) : T;
 
     export type ResolveRequired<T> = [T] extends [Type.Plain] ? {
-        [K in $Util.Keys.Required<T>]: All<T[K]>
+        [K in $Util.Keys.Required<T>]: One.All<T[K]>
     } : T;
 
     export type ResolveOptional<T> = {
         [K in $Util.Keys.Optional<T>]?: T extends any ? (
-            K extends keyof T ? All<T[K]> : never
+            K extends keyof T ? One.All<T[K]> : never
         ) : never
     };
 }
