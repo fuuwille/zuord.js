f9b8287b4a68f5b741fd44fe62cc1b8e217cd001
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 14:21:44 +0300

    feat: enhance Required type to include mode parameter in Only namespace

--- packages/util/src/zuord/only.types.ts
+++ packages/util/src/zuord/only.types.ts
@@ -1,6 +1,7 @@
 import { $ZuordUtil as $Util } from "../internal";
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
 
 export namespace Only {
-    export type Required<T extends Type.Plain> = $Util.Only.ResolveRequired<T>;
+    export type Required<T extends Type.Plain, _TMode extends Core.Mode.Field> = $Util.Only.ResolveRequired<T>;
 }
