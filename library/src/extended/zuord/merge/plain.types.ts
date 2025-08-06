0f05acbc34a53e53749df83c53ca8d0d1864d777
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 00:16:28 +0300

    fix: enhance Loose type definition for improved type resolution and handling

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,8 +1,15 @@
 import { $Zuord } from "zuord/internal";
 import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
+import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
-    export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = $Zuord.Merge.ResolvePlain<TContent, TMode>;
+    export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> = (
+        $Zuord.Merge.ResolvePlain<TContent, TMode> extends infer TResolved extends Type.Plain ? (
+            Util.Unify.Hybrid<TResolved, Core.Mode.Resolve<[TMode, { 
+                unifyPlain: TContent extends Type.PureTuple ? false : true
+            }]>>
+        ) : never
+    )
 }
