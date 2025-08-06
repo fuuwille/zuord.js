f8a136bf3daef3f8b28bc7204f3f2610cd921ead
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 18:40:58 +0300

    fix: rename ResolvePlainFromTuple and ResolvePlainFromArray to ResolvePlainTuple and ResolvePlainArray for consistency

--- library/src/internal/zuord/merge.types.ts
+++ library/src/internal/zuord/merge.types.ts
@@ -6,22 +6,22 @@ import { ZuordUtil } from "@zuord/util";
 export declare namespace Merge {
     export type ResolvePlain<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.PureTuple ? (
-            Merge.ResolvePlainFromTuple<TContent, TMode>
+            Merge.ResolvePlainTuple<TContent, TMode>
         ) : 
         TContent extends ZuordType.Array ? (
-            Merge.ResolvePlainFromArray<TContent, TMode>
+            Merge.ResolvePlainArray<TContent, TMode>
         ) : never
     );
 
-    export type ResolvePlainFromTuple<TContent, TMode extends ZuordCore.Mode.Field> = (
+    export type ResolvePlainTuple<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.EndingTupleWith<infer TRest, infer TLast> ? (
             TRest["length"] extends 0 ? TLast : 
             TRest["length"] extends 1 ? Integrate.Unknown<TRest[0], TLast, TMode> 
-            : Integrate.Unknown<ResolvePlainFromTuple<TRest, TMode>, TLast, TMode>
+            : Integrate.Unknown<ResolvePlainTuple<TRest, TMode>, TLast, TMode>
         ) : never
     );
 
-    export type ResolvePlainFromArray<TContent, TMode extends ZuordCore.Mode.Field> = TContent extends readonly (infer TInfer extends ZuordType.Plain)[] ? (
+    export type ResolvePlainArray<TContent, TMode extends ZuordCore.Mode.Field> = TContent extends readonly (infer TInfer extends ZuordType.Plain)[] ? (
         ZuordUtil.Unify.Hybrid<ZuordUtil.Only.Required<TInfer, TMode>, TMode>
     ) : never;
 }
