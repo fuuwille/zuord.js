e0a243ac5817196afad10328d2650dc59fb50412
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 14:06:42 +0300

    feat: add Required type to One namespace for enhanced type resolution

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -4,4 +4,6 @@ import { ZuordCore as Core } from "@zuord/core";
 
 export namespace One {
     export type All<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveAll<T, TMode>;
+
+    export type Required<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveRequired<T, TMode>;
 }
