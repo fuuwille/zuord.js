df034f2e37e9004059a30fdc6508738dcaef667d
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 18:52:48 +0300

    fix: simplify Loose type by removing unnecessary Core.Mode.Resolve dependency

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,10 +1,9 @@
 import { $Zuord } from "zuord/internal";
 import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
-import { ZuordCore as Core } from "@zuord/core";
 import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
     export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = Util.Unify.Hybrid<$Zuord.Merge.ResolvePlainArray<TContent, TMode>, Core.Mode.Resolve<[{ onePlain: false }, TMode]>>;
+        = Util.Unify.Hybrid<$Zuord.Merge.ResolvePlainArray<TContent, TMode>, TMode>;
 }
