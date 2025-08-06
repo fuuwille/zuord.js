5a15588a60f4b7ad93c466a9d48319e5bf727e92
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 18:59:17 +0300

    fix: simplify Loose type by removing unnecessary Util.Unify.Hybrid wrapper

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,9 +1,8 @@
 import { $Zuord } from "zuord/internal";
 import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
-import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
     export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = Util.Unify.Hybrid<$Zuord.Merge.ResolvePlainArray<TContent, TMode>, TMode>;
+        = $Zuord.Merge.ResolvePlainArray<TContent, TMode>;
 }
