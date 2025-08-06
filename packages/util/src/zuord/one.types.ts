925e2319edece2499886db19dd75f3e96008ea33
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 10:48:17 +0300

    fix: add missing newline at the end of one.types.ts file

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -3,4 +3,6 @@ import { ZuordType as Type } from "@zuord/type";
 
 export namespace One {
     export type Hybrid<T extends Type.Plain | Type.Array, TMode> = $ZuordUtil.One.ResolveHybrid<T, TMode>;
+
+    export type Plain<T extends Type.Plain, TMode> = $ZuordUtil.One.ResolvePlain<T, TMode>;
 }
