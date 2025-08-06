d718edc7e82c449cf71e1be8693249b839ba58f3
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 11:55:33 +0300

    fix: add TMode type constraint to Hybrid, Plain, and Array types for improved type safety

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -1,10 +1,11 @@
 import { $ZuordUtil } from "../internal";
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
 
 export namespace One {
-    export type Hybrid<T extends Type.Plain | Type.Array, TMode> = $ZuordUtil.One.ResolveHybrid<T, TMode>;
+    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveHybrid<T, TMode>;
 
-    export type Plain<T extends Type.Plain, TMode> = $ZuordUtil.One.ResolvePlain<T, TMode>;
+    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolvePlain<T, TMode>;
 
-    export type Array<T extends Type.Array, TMode> = $ZuordUtil.One.ResolveArray<T, TMode>;
+    export type Array<T extends Type.Array, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveArray<T, TMode>;
 }
