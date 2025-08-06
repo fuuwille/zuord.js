88be691ba1e51d3bce3f8cb494d1eb8c6fb5e854
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 12:08:47 +0300

    fix: update Hybrid type to use Core.Mode.Resolve for improved type handling

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -3,7 +3,8 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
 export namespace One {
-    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveHybrid<T, TMode>;
+    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field> 
+        = $ZuordUtil.One.ResolveHybrid<T, Core.Mode.Resolve<[TMode, { onePlain: true, oneArray: true }]>>;
 
     export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolvePlain<T, TMode>;
 
