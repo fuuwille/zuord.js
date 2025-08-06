f8ecf397cb2a4071f61ddafff66c4045111636eb
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 14:06:23 +0300

    feat: create one.types.ts to define One namespace and All type using $ZuordUtil

---
+++ packages/util/src/zuord/one.types.ts
@@ -0,0 +1,7 @@
+import { $ZuordUtil } from "../internal";
+import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
+
+export namespace One {
+    export type All<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveAll<T, TMode>;
+}
