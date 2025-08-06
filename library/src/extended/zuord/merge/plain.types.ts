d2060c71f9a56a8609770ab71c2361477d429dc7
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 11:03:13 +0300

    feat: enhance Loose type by utilizing Util.One.Hybrid for improved type resolution

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,8 +1,9 @@
 import { $Zuord } from "zuord/internal";
 import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
     export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = $Zuord.Merge.ResolvePlain<TContent, TMode>;
+        = Util.One.Hybrid<$Zuord.Merge.ResolvePlain<TContent, TMode>, { hybrid: true }>;
 }
