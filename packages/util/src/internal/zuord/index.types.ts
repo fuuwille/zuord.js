39dd935eae4423b5e5374861d275f380cb6ca55d
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 13:29:13 +0300

    fix: remove unused Object namespace and related imports for improved clarity and maintainability

--- packages/util/src/internal/zuord/index.types.ts
+++ packages/util/src/internal/zuord/index.types.ts
@@ -1,6 +1,5 @@
 import { Keys as $Keys } from "./keys";
 import { Unify as $Unify } from "./unify";
-import { Object as $Object } from "./object";
 import { Normalize as $Normalize } from "./normalize";
 import { Restrict as $Restrict } from "./restrict";
 import { Strict as $Strict } from "./strict";
@@ -10,7 +9,6 @@ import { Pattern as $Pattern } from "./pattern";
 export namespace $ZuordUtil {
     export import Keys = $Keys;
     export import Unify = $Unify;
-    export import Object = $Object;
     export import Normalize = $Normalize;
     export import Restrict = $Restrict;
     export import Strict = $Strict;
