6c43f8953a2f9e0583b69aeb0804fabdeb765514
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 12:14:52 +0300

    fix: update Loose type in Plain namespace to use Core.Mode.Resolve for improved type handling

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,9 +1,10 @@
 import { $Zuord } from "zuord/internal";
 import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
+import { ZuordCore as Core } from "@zuord/core";
 import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
     export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = Util.One.Hybrid<$Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
+        = Util.One.Hybrid<$Zuord.Merge.ResolvePlain<TContent, TMode>, Core.Mode.Resolve<[{ onePlain: false }, TMode]>>;
 }
