91f8608f091156ba50875e211b138e8916c349e3
k4yr2 <kyrzkaya@gmail.com>
2025-08-06 11:14:57 +0300

    feat: enhance Loose type to include skipPlain option for improved plain type resolution

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -5,5 +5,5 @@ import { ZuordUtil as Util } from "@zuord/util";
 
 export namespace Plain {
     export type Loose<TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
-        = Util.One.Hybrid<$Zuord.Merge.ResolvePlain<TContent, TMode>, { hybrid: true }>;
+        = Util.One.Hybrid<$Zuord.Merge.ResolvePlain<TContent, TMode>, { hybrid: true, skipPlain: true }>;
 }
