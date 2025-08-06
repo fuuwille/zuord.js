be3809db6705663e942c3068b0f83713674de867
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 00:12:26 +0300

    fix: rename ResolvePlainTuple to ResolvePlainFromTuple for consistency and clarity

--- library/src/internal/zuord/merge.types.ts
+++ library/src/internal/zuord/merge.types.ts
@@ -6,20 +6,26 @@ import { ZuordUtil } from "@zuord/util";
 export declare namespace Merge {
     export type ResolvePlain<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.PureTuple ? (
-            Merge.ResolvePlainTuple<TContent, TMode> extends infer TPlain extends ZuordType.Plain ? (
+            Merge.ResolvePlainFromTuple<TContent, TMode> extends infer TPlain extends ZuordType.Plain ? (
                 ZuordUtil.Unify.Hybrid<TPlain, ZuordCore.Mode.Resolve<[TMode, { unifyPlain: false }]>>
             ) : never
         ) : 
-        TContent extends ZuordType.ArrayOf<infer TInfer extends ZuordType.Plain> ? (
-            ZuordUtil.Unify.Hybrid<ZuordUtil.Only.Required<TInfer, TMode>, TMode>
+        TContent extends ZuordType.Array ? (
+            ResolvePlainFromArray<TContent, TMode>
         ) : never
     ) extends infer TPlain extends ZuordType.Plain ? TPlain : never;
 
-    export type ResolvePlainTuple<TContent, TMode extends ZuordCore.Mode.Field> = (
+    export type ResolvePlainFromTuple<TContent, TMode extends ZuordCore.Mode.Field> = (
         TContent extends ZuordType.EndingTupleWith<infer TRest, infer TLast> ? (
             TRest["length"] extends 0 ? TLast : 
             TRest["length"] extends 1 ? Integrate.Unknown<TRest[0], TLast, TMode> 
-            : Integrate.Unknown<ResolvePlainTuple<TRest, TMode>, TLast, TMode>
+            : Integrate.Unknown<ResolvePlainFromTuple<TRest, TMode>, TLast, TMode>
         ) : never
     );
+
+    export type ResolvePlainFromArray<TContent, TMode extends ZuordCore.Mode.Field> = (
+        TContent extends ZuordType.ArrayOf<infer TInfer extends ZuordType.Plain> ? (
+            ZuordUtil.Only.Required<TInfer, TMode>
+        ) : never
+    )
 }
