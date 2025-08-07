247afd059b42cfe57dddea533aef9025a9f47da3
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 14:58:51 +0300

    feat: add Unify type to Tuple namespace for enhanced type unification

--- packages/util/src/zuord/tuple.types.ts
+++ packages/util/src/zuord/tuple.types.ts
@@ -3,4 +3,6 @@ import { ZuordType as Type } from "@zuord/type";
 
 export namespace Tuple {
     export type Keys<T extends Type.Tuple> = $Util.Tuple.Keys<T>;
+
+    export type Unify<T extends Type.Tuple> = $Util.Tuple.Unify<T>;
 }
