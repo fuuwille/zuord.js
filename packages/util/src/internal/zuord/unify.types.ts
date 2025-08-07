e347c414764c018d670614b180f887addf60743a
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 09:33:19 +0300

    fix: update hybrid mode identifiers for consistency in type resolution

--- packages/util/src/internal/zuord/unify.types.ts
+++ packages/util/src/internal/zuord/unify.types.ts
@@ -28,7 +28,7 @@ export namespace Unify {
 
     export type ResolveHybridPlain<T extends Type.Plain, TMode extends Core.Mode.Field> = (
         TMode extends { unifyPlain: true } ? (
-            ResolveExtractedPlain<T, Core.Mode.Resolve<[TMode, { $hybrid: true }]>>
+            ResolveExtractedPlain<T, Core.Mode.Resolve<[TMode, { unifyHybrid: true }]>>
         ) : ResolveSkippedPlain<T, TMode>
     );
 
@@ -41,7 +41,7 @@ export namespace Unify {
             [K in keyof TOne]: TMode extends { shallow: true } ? (
                 TOne[K]
             ) : (
-                TMode extends { $hybrid: true } 
+                TMode extends { unifyHybrid: true } 
                     ? Unify.ResolveHybrid<TOne[K], TMode>
                     : Unify.ResolvePlain<TOne[K], TMode>
             )
@@ -59,7 +59,7 @@ export namespace Unify {
 
     export type ResolveHybridArray<T extends Type.Array, TMode extends Core.Mode.Field> = (
         TMode extends { unifyArray: true } ? (
-            ResolveExtractedArray<T, Core.Mode.Resolve<[TMode, { $hybrid: true }]>>
+            ResolveExtractedArray<T, Core.Mode.Resolve<[TMode, { unifyHybrid: true }]>>
         ) : ResolveSkippedArray<T, TMode>
     );
 
@@ -77,7 +77,7 @@ export namespace Unify {
         TMode extends { shallow: true } ? (
             T[number]
         ) : (
-            TMode extends { $hybrid: true } 
+            TMode extends { unifyHybrid: true } 
                 ? ResolveHybrid<T[number], TMode>
                 : ResolveArray<T[number], TMode>
         )
