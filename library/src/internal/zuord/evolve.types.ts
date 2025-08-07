cfe5c151be9cac0dfdf6dfa0a566ebdd3799ef03
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 03:27:02 +0300

    fix: specify TMode as Core.Mode.Field in ResolvePlain type for improved type safety

--- library/src/internal/zuord/evolve.types.ts
+++ library/src/internal/zuord/evolve.types.ts
@@ -1,7 +1,8 @@
 import { $Zuord } from ".";
+import { ZuordCore as Core } from "@zuord/core";
 
 
 export namespace Evolve {
-    export type ResolvePlain<TBase, TContent, TMode> 
+    export type ResolvePlain<TBase, TContent, TMode extends Core.Mode.Field> 
         = $Zuord.Integrate.Any<TBase, $Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
 }
