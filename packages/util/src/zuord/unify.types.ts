87255afecbf5a14c7681272e808bc95a4872762f
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 09:36:04 +0300

    fix: format Plain and Array type definitions for improved readability

--- packages/util/src/zuord/unify.types.ts
+++ packages/util/src/zuord/unify.types.ts
@@ -6,7 +6,9 @@ export namespace Unify {
     export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field> 
         = $ZuordUtil.Unify.ResolveHybrid<T, Core.Mode.Resolve<[{ unifyPlain: true, unifyArray: true }, TMode]>>;
 
-    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.Unify.ResolvePlain<T, TMode>;
+    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field> 
+        = $ZuordUtil.Unify.ResolvePlain<T, TMode>;
 
-    export type Array<T extends Type.Array, TMode extends Core.Mode.Field> = $ZuordUtil.Unify.ResolveArray<T, TMode>;
+    export type Array<T extends Type.Array, TMode extends Core.Mode.Field> 
+        = $ZuordUtil.Unify.ResolveArray<T, TMode>;
 }
