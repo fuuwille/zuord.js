60dd01aa0d11a76459fc4738d11d194ff5ceea13
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 14:52:20 +0300

    fix: export Keys type in Tuple namespace for improved accessibility

--- packages/util/src/internal/zuord/tuple.types.ts
+++ packages/util/src/internal/zuord/tuple.types.ts
@@ -1,3 +1,3 @@
 export namespace Tuple {
-    type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
+    export type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
 }
