b1de352c4cc867afb06444762d4c659c5f0a1b33
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 15:00:08 +0300

    feat: refine Unify type to allow broader type compatibility in Tuple namespace

--- packages/util/src/internal/zuord/tuple.types.ts
+++ packages/util/src/internal/zuord/tuple.types.ts
@@ -1,8 +1,10 @@
 export namespace Tuple {
     export type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
 
-    export type Unify<T extends Record<number, any>, Acc extends any[] =[]> = `${Acc['length']}` extends infer K ? (
-        K extends keyof T ? Tuple.Unify<T, [...Acc, T[K]]> : Acc
+    export type Unify<T, Acc = []> = Acc extends any[] ? (
+        `${Acc['length']}` extends infer K ? (
+            K extends keyof T ? Tuple.Unify<T, [...Acc, T[K]]> : Acc
+        ) : never
     ) : never;
 
     export type UnifyLike<T> = {
