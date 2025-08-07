006c60eaa7c244798446d7810341b544fd26fefd
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 13:34:45 +0300

    fix: refine DistributeHybrid type to ensure proper inference of T

--- packages/util/src/internal/zuord/unify.types.ts
+++ packages/util/src/internal/zuord/unify.types.ts
@@ -10,7 +10,7 @@ export namespace Unify {
     export type DistributeHybrid<T, TMode> = (
         | (Trait.Exclude<T, Type.Primitive> extends infer TExcluded ? Unify.HandleHybrid<TExcluded, TMode> : never)
         | (Trait.Extract<T, Type.Primitive> extends infer TExtracted ? TExtracted : never)
-    );
+    ) extends infer T ? T : never;
 
     export type HandleHybrid<T, TMode> = (
         Unify.ResolveHybrid<T, TMode> extends infer THybrid ? (
