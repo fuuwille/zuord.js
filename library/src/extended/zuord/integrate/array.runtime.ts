e95e49abe8b6b9b0bc0294ff3a220a3f1988e35e
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 03:42:27 +0300

    fix: update loose function parameters to use spread syntax for improved type handling

--- library/src/extended/zuord/integrate/array.runtime.ts
+++ library/src/extended/zuord/integrate/array.runtime.ts
@@ -15,11 +15,11 @@ const $ = ($base: Type.Array, $input: Type.Array, $mode: Partial<ZuordModeX.Inte
 
 // LOOSE
 
-export function loose <TBase extends Type.Array, TInput extends Type.Array> (base: TBase, input: TInput)
+export function loose <TBase extends Type.Array, TInput extends Type.Array> (base: [...TBase], input: [...TInput])
     : ZuordX.Integrate.Array.Loose<TBase, TInput>;
 
-export function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array>> (base: TBase, input: TInput, mode : TMode)
+export function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array>> (base: [...TBase], input: [...TInput], mode : TMode)
     : ZuordX.Integrate.Array.Loose<TBase, TInput, TMode>;
 
-export function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array>> (base: TBase, input: TInput, mode : TMode = {} as TMode)
+export function loose <TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array>> (base: [...TBase], input: [...TInput], mode : TMode = {} as TMode)
     : ZuordX.Integrate.Array.Loose<TBase, TInput, TMode> { return $(base, input, mode) as ZuordX.Integrate.Array.Loose<TBase, TInput, TMode>; }
