7258cb0d8a4e0dd7ec06247ef28eb829c0a2f3c4
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 14:53:57 +0300

    feat: add UnifyLike type to Tuple namespace for enhanced type manipulation

--- packages/util/src/internal/zuord/tuple.types.ts
+++ packages/util/src/internal/zuord/tuple.types.ts
@@ -1,3 +1,7 @@
 export namespace Tuple {
     export type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
+
+    export type UnifyLike<T> = {
+        [K in Tuple.Keys<T>]: T extends any ? (K extends keyof T ? T[K] : never) : never;
+    };
 }
