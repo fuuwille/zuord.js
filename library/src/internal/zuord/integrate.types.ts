b7d22640e0e520d1708c579618e1ba1bd3aab217
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 03:21:35 +0300

    fix: correct type reference in Any type for improved type accuracy

--- library/src/internal/zuord/integrate.types.ts
+++ library/src/internal/zuord/integrate.types.ts
@@ -2,15 +2,15 @@ import { ZuordType as Type } from "@zuord/type";
 
 export declare namespace Integrate {
     export type Any<TBase, TOverlay, TMode> = (
-        [TBase, TOverlay] extends [infer A extends Type.Plain, infer B extends Type.Plain] ? Integrate.Plain<A, B, TMode> :
+        [TBase, TOverlay] extends [infer A extends Type.Plain, infer B extends Type.Plain] ? Integrate.PlainRaw<A, B, TMode> :
         [TBase, TOverlay] extends [infer A extends Type.Array, infer B extends Type.Array] ? Array<A, B, TMode> : TOverlay
     );
 
-    export type Plain<TBase extends Type.Plain, TOverlay extends Type.Plain, TMode> = (
+    export type PlainRaw<TBase extends Type.Plain, TOverlay extends Type.Plain, TMode> = (
         (PlainOverrides<TBase, TOverlay, TMode> & PlainExtras<TBase, TOverlay>) extends infer TIntegrated ? ({
             -readonly [K in keyof TIntegrated]: TIntegrated[K];
         }) : never
-    ) extends infer TPlain extends Type.Plain ? TPlain : never;
+    );
 
     export type PlainOverrides<TBase extends Type.Plain, TOverlay extends Type.Plain, TMode> = ({
         [K in keyof TBase]: K extends keyof TOverlay ? (
