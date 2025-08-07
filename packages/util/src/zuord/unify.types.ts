35e2632efd05ba7e06632baca07ebb4a1e62b772
k4yr2 <kyrzkaya@gmail.com>
2025-08-07 16:50:33 +0300

    feat: update Hybrid, Plain, and Array types to set default TMode to an empty object

--- packages/util/src/zuord/unify.types.ts
+++ packages/util/src/zuord/unify.types.ts
@@ -3,12 +3,12 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
 export namespace Unify {
-    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field> 
+    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field = {}> 
         = $ZuordUtil.Unify.Hybrid<T, TMode>;
 
-    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field> 
+    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field = {}> 
         = $ZuordUtil.Unify.Plain<T, TMode>;
 
-    export type Array<T extends Type.Array, TMode extends Core.Mode.Field> 
+    export type Array<T extends Type.Array, TMode extends Core.Mode.Field = {}> 
         = $ZuordUtil.Unify.Array<T, TMode>;
 }
