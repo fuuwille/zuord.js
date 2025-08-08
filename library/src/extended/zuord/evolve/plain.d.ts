58f0dbe749f98e5c8acaebcf7a9d2ec8d932803d
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 22:21:42 +0300

    fix: add strict type to plainAPI for enhanced functionality

--- library/src/extended/zuord/evolve/plain.d.ts
+++ library/src/extended/zuord/evolve/plain.d.ts
@@ -1,7 +1,9 @@
 import type { restrict as $restrict } from "./plain.runtime";
+import type { strict as $strict } from "./plain.runtime";
 
 type plainAPI = {
     readonly restrict: typeof $restrict;
+    readonly strict: typeof $strict;
 }
 
 export declare const plain: plainAPI;
