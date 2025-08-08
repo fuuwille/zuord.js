4a0e7991e16f4c7f71d08f053fa486fc1ae0b38b
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 22:17:03 +0300

    fix: add KeysBatch type for batch processing in Strict namespace

--- packages/util/src/zuord/strict.types.ts
+++ packages/util/src/zuord/strict.types.ts
@@ -4,4 +4,7 @@ import { ZuordType } from "@zuord/type";
 export namespace Strict {
     export type Keys<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
         = $ZuordUtil.Strict.ResolveKeys<TBase, TContent>;
+
+    export type KeysBatch<TBase extends ZuordType.Plain, TContents extends ZuordType.PlainArray> 
+        = $ZuordUtil.Strict.ResolveKeysBatch<TBase, TContents> extends infer T extends ZuordType.PlainArray ? T : never
 }
