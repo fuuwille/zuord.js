3013121649d542f9c7e1db28e0f34ac492e414b6
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 22:14:14 +0300

    fix: refactor Keys type to utilize ResolveIncludedKeys and ResolveExcludedKeys for clarity in Strict namespace

--- packages/util/src/internal/zuord/strict.types.ts
+++ packages/util/src/internal/zuord/strict.types.ts
@@ -1,11 +1,8 @@
 import { ZuordType as Type } from "@zuord/type";
 
 export namespace Strict {
-  export type Keys<TBase, TInput> =  TBase extends Type.Plain ? (
-    { [K in Exclude<keyof TBase, keyof TInput>]: never } &
-    { [K in Exclude<keyof TInput, keyof TBase>]: never } &
-    { [K in keyof TInput & keyof TBase]: Keys<TBase[K], TInput[K]>
-    }
+  export type Keys<TBase, TInput> = TBase extends Type.Plain ? (
+    ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>
   ) : TInput;
 
   export type ResolveExcludedKeys<TBase, TInput> = (
