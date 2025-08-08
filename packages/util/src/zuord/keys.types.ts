67ead025b8b8a918f65d73a83306c71ea738dd0f
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 23:45:53 +0300

    feat: add Keys namespace with All, Required, and Optional types for enhanced type management

---
+++ packages/util/src/zuord/keys.types.ts
@@ -0,0 +1,9 @@
+import { $ZuordUtil as $Util } from "../internal";
+
+export namespace Keys {
+    export type All<T> = $Util.Keys.All<T>;
+
+    export type Required<T> = $Util.Keys.Required<T>;
+
+    export type Optional<T> = $Util.Keys.Optional<T>;
+}
