c43715a5ed7e00d630492e53cc7c64e2e8ccea54
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 22:19:31 +0300

    fix: add Strict type for enhanced batch processing in Plain namespace

--- library/src/extended/zuord/evolve/plain.types.ts
+++ library/src/extended/zuord/evolve/plain.types.ts
@@ -6,4 +6,7 @@ import { ZuordUtil as Util } from "@zuord/util";
 export namespace Plain {
     export type Restrict<TBase extends Type.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
         = $Zuord.Evolve.ResolvePlain<TBase, TContent, TMode>;
+
+    export type Strict<TBase extends Type.Plain, TContent extends Util.Strict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
+        = $Zuord.Evolve.ResolvePlain<TBase, TContent, TMode>;
 }
