1b53beca112817b4200080261c81df093668423e
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 21:42:53 +0300

    fix: consolidate zuordModeX imports for consistency in plain.types.ts

--- library/src/extended/zuord/integrate/plain.types.ts
+++ library/src/extended/zuord/integrate/plain.types.ts
@@ -1,12 +1,8 @@
-import { zuordModeX } from "zuord/extended";
 import type { $Zuord } from "zuord/internal";
-import type { ZuordModeX } from "zuord/extended";
+import type { zuordModeX, ZuordModeX } from "zuord/extended";
 import type { ZuordType as Type } from "@zuord/type";
 import type { ZuordCore as Core } from "@zuord/core";
 import type { ZuordUtil as Util } from "@zuord/util";
-
-
-//
 
 export namespace Plain {
     export type Loose<TBase extends Type.Plain, TInput extends Type.Plain, TMode extends Partial<ZuordModeX.Integrate.Plain> = {}> 
