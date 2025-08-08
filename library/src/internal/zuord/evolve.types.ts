cf7b2aae5995fac4a17d3e4ba8759b71bf41f261
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 22:28:13 +0300

    feat: enhance Evolve namespace with Plain type for improved type resolution

--- library/src/internal/zuord/evolve.types.ts
+++ library/src/internal/zuord/evolve.types.ts
@@ -1,8 +1,16 @@
+import { ZuordType } from "@zuord/type";
 import { $Zuord } from ".";
 import { ZuordCore as Core } from "@zuord/core";
+import { ZuordUtil } from "@zuord/util";
 
 
 export namespace Evolve {
+    export type Plain<TBase, TContent, TMode extends Core.Mode.Field> = (
+        [Evolve.ResolvePlain<TBase, TContent, TMode>] extends [infer TResolved extends ZuordType.Plain] ? (
+            ZuordUtil.Unify.Hybrid<TResolved, TMode>
+        ) : never
+    )
+
     export type ResolvePlain<TBase, TContent, TMode extends Core.Mode.Field> 
         = $Zuord.Integrate.ResolvePlain<TBase, $Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
 }
