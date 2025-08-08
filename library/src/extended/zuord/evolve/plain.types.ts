22ddcf4000df49afa1e45ccdb40dffb2be9ed66c
k4yr2 <kyrzkaya@gmail.com>
2025-08-08 21:51:26 +0300

    fix: remove duplicate import for clarity in plain.types.ts

--- library/src/extended/zuord/evolve/plain.types.ts
+++ library/src/extended/zuord/evolve/plain.types.ts
@@ -1,7 +1,7 @@
 import { $Zuord } from "zuord/internal";
+import { ZuordModeX } from "zuord/extended";
 import { ZuordType as Type } from "@zuord/type";
 import { ZuordUtil as Util } from "@zuord/util";
-import { ZuordModeX } from "zuord/extended";
 
 export namespace Plain {
     export type Restrict<TBase extends Type.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
