f1a75abc8a5731cf4288c77ee38ff5532e847a0a
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 13:35:15 +0300

    feat: add ToTuple type to Union namespace for enhanced type manipulation

--- packages/util/src/internal/zuord/union.types.ts
+++ packages/util/src/internal/zuord/union.types.ts
@@ -2,4 +2,6 @@ export namespace Union {
     export type ToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
     export type ToLast<U> = ToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
+
+    export type ToTuple<U, R extends any[] = []> = [U] extends [never]? R : ToTuple<Exclude<U, ToLast<U>>, [ToLast<U>, ...R]>;
 }
