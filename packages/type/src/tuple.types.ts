5efffd647b82e5f7f432b87fac321b109066580a
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 22:23:09 +0300

    fix: update TupleFirst type definition for consistency and clarity

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -1,6 +1,6 @@
 export type Tuple<T extends unknown = unknown> = readonly [T, ...T[]];
 
-export type TupleFirst<TFirst extends unknown = unknown, TRest extends unknown = unknown> = readonly [TFirst, ...TRest[]];
+export type TupleFirst<TFirst extends unknown = unknown, TRest extends unknown[] = unknown[]> = readonly [TFirst, ...TRest];
 
 export type TupleLast<TRest extends unknown[], TLast extends unknown> = readonly [...TRest, TLast];
 
