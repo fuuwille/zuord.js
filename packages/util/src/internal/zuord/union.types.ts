9a49a7423eda91d473886b3b3e57f8744fd2bf03
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 13:35:39 +0300

    fix: reference Union namespace in ToLast and ToTuple types for consistency

--- packages/util/src/internal/zuord/union.types.ts
+++ packages/util/src/internal/zuord/union.types.ts
@@ -1,7 +1,7 @@
 export namespace Union {
     export type ToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
-    export type ToLast<U> = ToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
+    export type ToLast<U> = Union.ToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
 
-    export type ToTuple<U, R extends any[] = []> = [U] extends [never]? R : ToTuple<Exclude<U, ToLast<U>>, [ToLast<U>, ...R]>;
+    export type ToTuple<U, R extends any[] = []> = [U] extends [never]? R : Union.ToTuple<Exclude<U, Union.ToLast<U>>, [Union.ToLast<U>, ...R]>;
 }
