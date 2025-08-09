2a24d75139fa579af50b22fbfe2edb719ad09b9d
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 19:06:49 +0300

    fix: remove union.types.ts and update alias.types.ts exports for consistency

--- packages/type/src/alias.types.ts
+++ packages/type/src/alias.types.ts
@@ -3,5 +3,4 @@ export type * from "./function.types";
 export type * from "./tuple.types";
 export type * from "./array.types";
 export type * from "./plain.types";
-export type * from "./union.types";
 export type * from "./primitive.types";
