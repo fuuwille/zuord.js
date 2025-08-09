bcb36dc1a1f81dd7959655d6fb2c619872ff39c9
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 22:31:03 +0300

    fix: rename PureTuple to TupleNest for clarity and consistency

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -4,4 +4,4 @@ export type TupleFirst<TFirst extends unknown = unknown, TRest extends unknown[]
 
 export type TupleLast<TRest extends unknown[] = unknown[], TLast extends unknown = unknown> = readonly [...TRest, TLast];
 
-export type PureTuple<T extends unknown = unknown> = readonly [T] | [T, ...[T, ...T[]]];
+export type TupleNest<T extends unknown = unknown> = readonly [T] | [T, ...[T, ...T[]]];
