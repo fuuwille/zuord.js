d1855c4a58dedcf0b1f7157d42ed472e3f688ae3
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 01:10:03 +0300

    feat: update tuple function to use a standardized type parameter for improved clarity

--- packages/type/src/zuord/tuple.runtime.ts
+++ packages/type/src/zuord/tuple.runtime.ts
@@ -1,6 +1,6 @@
-import { array } from "../array";
+import { array } from "./array";
 import type { ZuordType as Type } from ".";
 
-export function tuple<T extends unknown = unknown>(obj: unknown, checkT?: (item: unknown) => item is T): obj is Type.Tuple<T> {
-    return array(obj, checkT);
+export function tuple<T extends unknown = unknown>(obj: unknown, type?: { item?: (z: unknown) => z is T }): obj is Type.Tuple<T> {
+    return array(obj, type);
 }
