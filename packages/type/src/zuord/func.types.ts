151301a93068fb40687c18045b4ed0d5aebd8912
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 02:09:03 +0300

    fix: update Func type to use unknown[] for better type safety

--- packages/type/src/zuord/func.types.ts
+++ packages/type/src/zuord/func.types.ts
@@ -1,1 +1,1 @@
-export type Func<TArgs extends any[] = any[], TResult extends unknown = unknown> = (...args: TArgs) => TResult;
+export type Func<TArgs extends unknown[] = unknown[], TResult extends unknown = unknown> = (...args: TArgs) => TResult;
