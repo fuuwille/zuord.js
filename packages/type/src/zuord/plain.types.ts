b66ce92e31651d56817a78b3e6ee2e41959e698e
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 01:03:19 +0300

    feat: parameterize PlainArray and PlainTuple types for enhanced type flexibility

--- packages/type/src/zuord/plain.types.ts
+++ packages/type/src/zuord/plain.types.ts
@@ -4,6 +4,6 @@ export type Plain<K extends PropertyKey = PropertyKey, T extends unknown = unkno
   [key in K]: T;
 };
 
-export type PlainArray = ZuordType.Array<Plain>;
+export type PlainArray<K extends PropertyKey = PropertyKey, T extends unknown = unknown> = ZuordType.Array<Plain<K, T>>;
 
-export type PlainTuple = ZuordType.Tuple<Plain>;
+export type PlainTuple<K extends PropertyKey = PropertyKey, T extends unknown = unknown> = ZuordType.Tuple<Plain<K, T>>;
