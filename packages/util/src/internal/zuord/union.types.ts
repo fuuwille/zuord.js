0136c392ea3847ec6d79a58a3bae293476951c15
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 19:04:55 +0300

    fix: reorder IsUnion type definition for consistency

--- packages/util/src/internal/zuord/union.types.ts
+++ packages/util/src/internal/zuord/union.types.ts
@@ -1,4 +1,6 @@
 export namespace Union {
+    export type IsUnion<T, U = T> = T extends any ? ([U] extends [T] ? false : true) : never;
+
     export type ToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
     export type ToLast<U> = Union.ToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
