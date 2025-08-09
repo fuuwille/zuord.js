d4438f2ebbdcc3dfa125db8f37d8f8c699f6eff7
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 01:54:16 +0300

    fix: update type checks in Merge namespace to use TupleNest instead of PureTuple

--- library/src/internal/zuord/merge.types.ts
+++ library/src/internal/zuord/merge.types.ts
@@ -7,18 +7,18 @@ export declare namespace Merge {
     export type Plain<TContent, TMode extends Core.Mode.Field> = (
         Merge.ResolvePlain<TContent, TMode> extends infer TResolved extends Type.Plain ? (
             Util.Unify.Hybrid<TResolved, Core.Mode.Resolve<[TMode, { 
-                unifyPlain: TContent extends Type.PureTuple ? false : true
+                unifyPlain: TContent extends Type.TupleNest ? false : true
             }]>>
         ) : never
     )
 
     export type ResolvePlain<TContent, TMode extends Core.Mode.Field> = (
-        [TContent] extends [Type.PureTuple] ? BuildPlain<TContent, TMode> : 
+        [TContent] extends [Type.TupleNest] ? BuildPlain<TContent, TMode> : 
         [TContent] extends [Type.Array<infer TInfer extends Type.Plain>] ? Util.Only.Required<TInfer, TMode> : never
     );
 
     export type BuildPlain<TContent, TMode extends Core.Mode.Field> = (
-        TContent extends Type.TupleLast<infer TRest extends Type.Plain[], infer TLast extends Type.Plain> ? (
+        TContent extends Type.TupleLast<infer TLast extends Type.Plain ,infer TRest extends Type.Plain[]> ? (
             TRest["length"] extends 0 ? TLast : 
             TRest["length"] extends 1 ? $Zuord.Integrate.ResolvePlain<TRest[0], TLast, TMode> 
             : $Zuord.Integrate.ResolvePlain<BuildPlain<TRest, TMode>, TLast, TMode>
@@ -32,12 +32,12 @@ export declare namespace Merge {
     );
 
     export type ResolveArray<TContent, TMode extends Core.Mode.Field> = (
-        [TContent] extends [Type.PureTuple] ? BuildArray<Util.Mutable.Hybrid<TContent>, TMode> : 
+        [TContent] extends [Type.TupleNest] ? BuildArray<Util.Mutable.Hybrid<TContent>, TMode> : 
         [TContent] extends [Type.Array<infer TInfer extends Type.Array>] ? TInfer : never
     )
 
     export type BuildArray<TContent, TMode extends Core.Mode.Field> = (
-        TContent extends Type.TupleLast<infer TRest extends Type.Array[], infer TLast extends Type.Array> ? (
+        TContent extends Type.TupleLast<infer TLast extends Type.Array, infer TRest extends Type.Array[]> ? (
             TRest["length"] extends 0 ? TLast : 
             TRest["length"] extends 1 ? $Zuord.Integrate.ResolveArray<TRest[0], TLast, { concat: true }> : 
             $Zuord.Integrate.ResolveArray<BuildArray<TRest, TMode>, TLast, { concat: true }>
