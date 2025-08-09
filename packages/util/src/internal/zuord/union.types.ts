96d5374d899fb55a9686bb5013c141c41fcada44
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 13:25:20 +0300

    feat: add ToLast type to Union namespace for enhanced type manipulation

--- packages/util/src/internal/zuord/union.types.ts
+++ packages/util/src/internal/zuord/union.types.ts
@@ -1,3 +1,5 @@
 export namespace Union {
     export type ToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+
+    export type ToLast<U> = ToIntersection<U extends any ? () => U : never> extends () => infer L ? L : never;
 }
