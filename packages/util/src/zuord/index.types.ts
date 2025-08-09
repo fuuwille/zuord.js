40e66eddfc4b430e6da530abb2c61aba061611b7
k4yr2 <kyrzkaya@gmail.com>
2025-08-09 18:39:46 +0300

    feat: import Union type into ZuordUtil namespace for enhanced type accessibility

--- packages/util/src/zuord/index.types.ts
+++ packages/util/src/zuord/index.types.ts
@@ -1,6 +1,7 @@
 import { Keys as $Keys } from "./keys";
 import { Tuple as $Tuple } from "./tuple";
 import { Plain as $Plain } from "./plain";
+import { Union as $Union } from "./union";
 import { Only as $Only } from "./only";
 import { One as $One } from "./one";
 import { Unify as $Unify } from "./unify";
@@ -16,6 +17,7 @@ export declare namespace ZuordUtil {
     export import Keys = $Keys;
     export import Tuple = $Tuple;
     export import Plain = $Plain;
+    export import Union = $Union;
     export import Only = $Only;
     export import One = $One;
     export import Unify = $Unify;
