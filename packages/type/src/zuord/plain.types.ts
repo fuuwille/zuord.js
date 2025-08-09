e41de86991c114cf4d10af34505eab8dfbad2e57
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 01:11:41 +0300

    fix: consolidate Plain type definition for improved readability

--- packages/type/src/zuord/plain.types.ts
+++ packages/type/src/zuord/plain.types.ts
@@ -1,8 +1,6 @@
 import { ZuordType } from ".";
 
-export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = {
-  [key in TKey]: TValue;
-};
+export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = { [key in TKey]: TValue; };
 
 export type PlainArray<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Array<Plain<TKey, TValue>>;
 
