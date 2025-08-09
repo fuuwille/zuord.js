ac08a791c182551e0e3d005b4ecee078f2c05188
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 01:46:09 +0300

    feat: add primitiveArray function to validate arrays of primitive types

--- packages/type/src/zuord/primitive.runtime.ts
+++ packages/type/src/zuord/primitive.runtime.ts
@@ -1,5 +1,10 @@
+import { array } from "./array";
 import type { ZuordType as Type } from ".";
 
 export function primitive(obj: unknown): obj is Type.Primitive {
     return obj === null || (typeof obj !== "object" && typeof obj !== "function");
+}
+
+export function primitiveArray(obj: unknown): obj is Type.PrimitiveArray {
+    return array(obj, { item: primitive });
 }
