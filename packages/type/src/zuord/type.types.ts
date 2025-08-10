37ffff38174be5f8a4ef9dbcda0d989a98fc65f7
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:21:57 +0300

    feat: add Plain type definition to Type namespace in type.types.ts

--- packages/type/src/zuord/type.types.ts
+++ packages/type/src/zuord/type.types.ts
@@ -1,3 +1,5 @@
 export namespace Type {
     export type Primitive = string | number | boolean | bigint | symbol | null | undefined;
+
+    export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = { [key in TKey]: TValue; };
 }
