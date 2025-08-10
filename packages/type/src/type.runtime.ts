3eb64f96b4e2a9fe51d3aa6fbe5f30338b524a0b
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:26:21 +0300

    feat: implement plain type guard function in type.runtime.ts

--- packages/type/src/type.runtime.ts
+++ packages/type/src/type.runtime.ts
@@ -3,3 +3,20 @@ import { ZuordType as Type } from "./type.types";
 export function primitive(obj: unknown): obj is Type.Primitive {
     return obj === null || (typeof obj !== "object" && typeof obj !== "function");
 }
+
+export function plain<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is Type.Plain<TKey, TItem> {
+    if (typeof obj !== 'object' || obj === null) return false;
+
+    if (Object.getPrototypeOf(obj) !== Object.prototype) return false;
+
+    if(type) {
+        const entries = Object.entries(obj);
+
+        for (const [key, value] of entries) {
+            if (type.key && !type.key(key)) return false;
+            if (type.item && !type.item(value)) return false;
+        }
+    }
+
+    return true;
+}
