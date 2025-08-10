4852d9bbca82efc90acc8ab02bbb6113cbb7d700
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:38:19 +0300

    feat: import ZuordType in plain.types.ts for Array type definition

--- packages/type/src/plain.types.ts
+++ packages/type/src/plain.types.ts
@@ -1,3 +1,5 @@
+import { ZuordType as Type } from "./type.types";
+
 export namespace ZuordPlain {
-    
+    export type Array<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Array<Type.Plain<TKey, TValue>>;
 }
