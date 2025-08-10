ee832340e1c686f3bb4f6b8630d9ab1852ec2da6
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:25:31 +0300

    feat: rename Type namespace to ZuordType in type.types.ts and update imports in type.runtime.ts

--- packages/type/src/type.runtime.ts
+++ packages/type/src/type.runtime.ts
@@ -1,4 +1,4 @@
-import { Type } from "./type.types";
+import { ZuordType as Type } from "./type.types";
 
 export function primitive(obj: unknown): obj is Type.Primitive {
     return obj === null || (typeof obj !== "object" && typeof obj !== "function");
--- packages/type/src/type.types.ts
+++ packages/type/src/type.types.ts
@@ -1,4 +1,4 @@
-export namespace Type {
+export namespace ZuordType {
     export type Primitive = string | number | boolean | bigint | symbol | null | undefined;
 
     export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = { [key in TKey]: TValue; };
