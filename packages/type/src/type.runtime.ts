e69ca039b46c1ab1e8fdf55e327aa05d094af6a5
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:24:51 +0300

    feat: implement primitive type guard function in type.runtime.ts

---
+++ packages/type/src/type.runtime.ts
@@ -0,0 +1,5 @@
+import { Type } from "./type.types";
+
+export function primitive(obj: unknown): obj is Type.Primitive {
+    return obj === null || (typeof obj !== "object" && typeof obj !== "function");
+}
