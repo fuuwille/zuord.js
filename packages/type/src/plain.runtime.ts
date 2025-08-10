5a1db1a2fd87797d8b7d0cee2063297e0e05e21b
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:42:59 +0300

    refactor: rename functions to remove 'plain' prefix for consistency in plain.runtime.ts

--- packages/type/src/plain.runtime.ts
+++ packages/type/src/plain.runtime.ts
@@ -1,10 +1,10 @@
 import { zuordType, ZuordType } from "./type";
 import { ZuordPlain } from "./plain.types";
 
-export function plainArray<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is ZuordPlain.Array<TKey, TItem> {
+export function array<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is ZuordPlain.Array<TKey, TItem> {
     return zuordType.array(obj, { item: (item): item is ZuordType.Plain<TKey, TItem> => zuordType.plain(item, type) });
 }
 
-export function plainTuple<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is ZuordPlain.Tuple<TKey, TItem> {
+export function tuple<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is ZuordPlain.Tuple<TKey, TItem> {
     return zuordType.tuple(obj, { item: (item): item is ZuordType.Plain<TKey, TItem> => zuordType.plain(item, type) });
 }
