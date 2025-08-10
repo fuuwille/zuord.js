3c6a2d96d0fb4003662c4091968661fd1a06acb0
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 17:27:38 +0300

    fix: update import statement to use alias for ZuordType in plain.types.ts

--- packages/type/src/zuord/plain.types.ts
+++ packages/type/src/zuord/plain.types.ts
@@ -1,7 +1,7 @@
-import { ZuordType } from ".";
+import { ZuordType as Type } from ".";
 
 export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = { [key in TKey]: TValue; };
 
-export type PlainArray<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Array<Plain<TKey, TValue>>;
+export type PlainArray<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Array<Plain<TKey, TValue>>;
 
-export type PlainTuple<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Tuple<Plain<TKey, TValue>>;
+export type PlainTuple<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Tuple<Plain<TKey, TValue>>;
