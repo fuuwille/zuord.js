6cdd4905e92537f40f25b22e9e377d301603d4ca
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:33:01 +0300

    feat: add func type to zuordTypeAPI definition in type.d.ts

--- packages/type/src/type.d.ts
+++ packages/type/src/type.d.ts
@@ -2,10 +2,13 @@ import { primitive as $primitive } from "./type.runtime";
 import { plain as $plain } from "./type.runtime";
 import { array as $array } from "./type.runtime";
 import { tuple as $tuple } from "./type.runtime";
+import { func as $func } from "./type.runtime";
+
 
 type zuordTypeAPI = {
     primitive: typeof $primitive;
     plain: typeof $plain;
     array: typeof $array;
     tuple: typeof $tuple;
+    func: typeof $func;
 }
