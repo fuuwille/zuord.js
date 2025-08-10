3992fa12183e46a49438d4174aa2fdc723e0d1d7
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:48:54 +0300

    feat: add Empty type definition to Array namespace in array.types.ts

--- packages/type/src/array.types.ts
+++ packages/type/src/array.types.ts
@@ -2,4 +2,6 @@ import { ZuordType as Type } from "./type";
 
 export namespace Array {
     export type Nest<T extends unknown = unknown> = Type.Array<Type.Array<T>>;
+    
+    export type Empty = readonly [];
 }
