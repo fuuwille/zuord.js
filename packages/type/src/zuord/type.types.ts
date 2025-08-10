e310ecfbecfd8a8606d705e2739903b02ec425cf
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:22:32 +0300

    feat: add Array type definition to Type namespace in type.types.ts

--- packages/type/src/zuord/type.types.ts
+++ packages/type/src/zuord/type.types.ts
@@ -2,4 +2,6 @@ export namespace Type {
     export type Primitive = string | number | boolean | bigint | symbol | null | undefined;
 
     export type Plain<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = { [key in TKey]: TValue; };
+
+    export type Array<T extends unknown = unknown> = readonly T[];
 }
