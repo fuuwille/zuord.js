4920f296f7323485ad9b24b14d531b99cb076e79
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:38:54 +0300

    feat: add Tuple type definition to ZuordPlain namespace in plain.types.ts

--- packages/type/src/plain.types.ts
+++ packages/type/src/plain.types.ts
@@ -2,4 +2,6 @@ import { ZuordType as Type } from "./type.types";
 
 export namespace ZuordPlain {
     export type Array<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Array<Type.Plain<TKey, TValue>>;
+
+    export type Tuple<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Tuple<Type.Plain<TKey, TValue>>;
 }
