ef171ae498983498165dad50587cb1bf2cfcd8bb
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:32:20 +0300

    feat: add array type to zuordTypeAPI definition in type.d.ts

--- packages/type/src/type.d.ts
+++ packages/type/src/type.d.ts
@@ -1,7 +1,10 @@
 import { primitive as $primitive } from "./type.runtime";
 import { plain as $plain } from "./type.runtime";
+import { array as $array } from "./type.runtime";
+
 
 type zuordTypeAPI = {
     primitive: typeof $primitive;
     plain: typeof $plain;
+    array: typeof $array;
 }
