fae5997905132e18d23d43ed18f23b8eaa8d8185
k4yr2 <kyrzkaya@gmail.com>
2025-08-10 18:32:01 +0300

    feat: add plain type to zuordTypeAPI definition in type.d.ts

--- packages/type/src/type.d.ts
+++ packages/type/src/type.d.ts
@@ -1,5 +1,7 @@
 import { primitive as $primitive } from "./type.runtime";
+import { plain as $plain } from "./type.runtime";
 
 type zuordTypeAPI = {
     primitive: typeof $primitive;
+    plain: typeof $plain;
 }
