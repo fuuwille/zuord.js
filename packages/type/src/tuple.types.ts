9d7476af279fbaa8f952e8418bc9934d753f5396
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:12:55 +0300

    feat: add Nest type to ZuordTuple namespace

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -2,4 +2,6 @@ export namespace ZuordTuple {
     export type First<TFirst extends unknown = unknown, TRest extends unknown[] = unknown[]> = readonly [TFirst, ...TRest];
 
     export type Last<TLast extends unknown = unknown, TRest extends unknown[] = unknown[]> = readonly [...TRest, TLast];
+
+    export type Nest<TItem extends unknown = unknown> = readonly [TItem] | [TItem, ...[TItem, ...TItem[]]];
 }
