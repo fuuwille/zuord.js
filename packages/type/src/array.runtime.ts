ab522527136b01cf87d04ad080504c3515128c2e
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:04:13 +0300

    feat: add empty function for validating empty ZuordArray

--- packages/type/src/array.runtime.ts
+++ packages/type/src/array.runtime.ts
@@ -5,3 +5,7 @@ import { ZuordArray } from "./array.types";
 export function nest<T extends unknown = unknown>(obj: unknown, type?: { item?: (z: unknown) => z is T }): obj is ZuordArray.Nest<T> {
   return zuordType.array(obj, { item: (item): item is Type.Array<T> => zuordType.array(item, type) });
 }
+
+export function empty(obj: unknown): obj is ZuordArray.Empty {
+    return zuordType.array(obj) && obj.length === 0;
+}
