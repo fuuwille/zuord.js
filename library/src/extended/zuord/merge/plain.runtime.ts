57e9bd1472fe343b1627fc2ddd91df442b61f410
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:46:53 +0300

    refactor: update references from ZuordType to ZuordPlain in plain.runtime.ts

--- library/src/extended/zuord/merge/plain.runtime.ts
+++ library/src/extended/zuord/merge/plain.runtime.ts
@@ -1,13 +1,13 @@
 import { $zuord } from "zuord/internal";
 import { zuordModeX } from "zuord/extended";
-import { zuordType as type } from "@zuord/type";
+import { zuordPlain } from "@zuord/type";
 import { zuordCore as core } from "@zuord/core";
 import type { ZuordX } from "zuord/extended";
 import type { ZuordModeX } from "zuord/extended";
-import type { ZuordType as Type } from "@zuord/type";
+import type { ZuordPlain } from "@zuord/type";
 
-const $ = ($content: Type.PlainArray, $mode: Partial<ZuordModeX.Merge.Plain>) => {
-    if(!type.plainArray($content)) {
+const $ = ($content: ZuordPlain.Array, $mode: Partial<ZuordModeX.Merge.Plain>) => {
+    if(!zuordPlain.array($content)) {
         throw new TypeError("Integrate function expects the base to be a valid plain.");
     }
 
@@ -17,11 +17,11 @@ const $ = ($content: Type.PlainArray, $mode: Partial<ZuordModeX.Merge.Plain>) =>
 
 // LOOSE
 
-export function loose <TContent extends Type.PlainArray> (content: [...TContent])
+export function loose <TContent extends ZuordPlain.Array> (content: [...TContent])
     : ZuordX.Merge.Plain.Loose<TContent>;
 
-export function loose <TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain>> (content: [...TContent], mode: TMode)
+export function loose <TContent extends ZuordPlain.Array, TMode extends Partial<ZuordModeX.Merge.Plain>> (content: [...TContent], mode: TMode)
     : ZuordX.Merge.Plain.Loose<TContent, TMode>;
 
-export function loose <TContent extends Type.PlainArray, TMode extends Partial<ZuordModeX.Merge.Plain>> (content: [...TContent], mode: TMode = {} as TMode)
+export function loose <TContent extends ZuordPlain.Array, TMode extends Partial<ZuordModeX.Merge.Plain>> (content: [...TContent], mode: TMode = {} as TMode)
     : ZuordX.Merge.Plain.Loose<TContent, TMode> { return $(content, mode); }
