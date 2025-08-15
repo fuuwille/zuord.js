f14e7a4a96f6b6a0d8e12d650bab40268fc189c8
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:37:29 +0300

    refactor: standardize import aliases in plain.runtime.ts

--- library/src/extended/zuord/evolve/plain.runtime.ts
+++ library/src/extended/zuord/evolve/plain.runtime.ts
@@ -1,40 +1,40 @@
 import { $zuord } from "zuord/internal";
 import { zuordModeX } from "zuord/extended";
 import { zuordPlain } from "@zuord/type";
-import { zuordCore as core } from "@zuord/core";
+import { zuordCore } from "@zuord/core";
 import type { ZuordX } from "zuord/extended";
 import type { ZuordModeX } from "zuord/extended";
 import type { ZuordType, ZuordPlain } from "@zuord/type";
-import type { ZuordUtil as Util } from "@zuord/util";
+import type { ZuordUtil } from "@zuord/util";
 
 const $ = ($base: ZuordType.Plain, $content: ZuordPlain.Array, $mode: Partial<ZuordModeX.Evolve.Plain>) => {
     if(!zuordPlain.array($content)) {
         throw new TypeError("Integrate function expects the base to be a valid plain.");
     }
 
-    return $zuord.evolve.plain($base, $content, core.mode.resolve([zuordModeX.evolve.plain, $mode]));
+    return $zuord.evolve.plain($base, $content, zuordCore.mode.resolve([zuordModeX.evolve.plain, $mode]));
 }
 
 
 // LOOSE
 
-export function restrict <TBase extends ZuordType.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>> (base: TBase, content: [...TContent])
+export function restrict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Restrict.KeysBatch<TBase, TContent>> (base: TBase, content: [...TContent])
     : ZuordX.Evolve.Plain.Restrict<TBase, TContent>;
 
-export function restrict <TBase extends ZuordType.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode)
+export function restrict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode)
     : ZuordX.Evolve.Plain.Restrict<TBase, TContent, TMode>;
 
-export function restrict <TBase extends ZuordType.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode = {} as TMode)
+export function restrict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode = {} as TMode)
     : ZuordX.Evolve.Plain.Restrict<TBase, TContent, TMode> { return $(base, content, mode); }
 
 
 // STRICT
 
-export function strict <TBase extends ZuordType.Plain, TContent extends Util.Strict.KeysBatch<TBase, TContent>> (base: TBase, content: [...TContent])
+export function strict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Strict.KeysBatch<TBase, TContent>> (base: TBase, content: [...TContent])
     : ZuordX.Evolve.Plain.Strict<TBase, TContent>;
 
-export function strict <TBase extends ZuordType.Plain, TContent extends Util.Strict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode)
+export function strict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Strict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode)
     : ZuordX.Evolve.Plain.Strict<TBase, TContent, TMode>;
 
-export function strict <TBase extends ZuordType.Plain, TContent extends Util.Strict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode = {} as TMode)
+export function strict <TBase extends ZuordType.Plain, TContent extends ZuordUtil.Strict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (base: TBase, content: [...TContent], mode: TMode = {} as TMode)
     : ZuordX.Evolve.Plain.Strict<TBase, TContent, TMode> { return $(base, content, mode); }
