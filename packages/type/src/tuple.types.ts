3733f4a4a6f72f2899ff36333d08b5740535d780
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:12:39 +0300

    feat: add Last type to ZuordTuple namespace

--- packages/type/src/tuple.types.ts
+++ packages/type/src/tuple.types.ts
@@ -1,3 +1,5 @@
 export namespace ZuordTuple {
     export type First<TFirst extends unknown = unknown, TRest extends unknown[] = unknown[]> = readonly [TFirst, ...TRest];
+
+    export type Last<TLast extends unknown = unknown, TRest extends unknown[] = unknown[]> = readonly [...TRest, TLast];
 }
