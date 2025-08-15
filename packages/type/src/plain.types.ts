d391c9113e042c2e818423066086729c45897ae4
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:09:27 +0300

    fix: restore direct imports of ZuordType in plain.types.ts

--- packages/type/src/plain.types.ts
+++ packages/type/src/plain.types.ts
@@ -1,7 +1,7 @@
-import { ZuordType as Type } from "./type.types";
+import { ZuordType } from "./type.types";
 
 export namespace ZuordPlain {
-    export type Array<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Array<Type.Plain<TKey, TValue>>;
+    export type Array<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Array<ZuordType.Plain<TKey, TValue>>;
 
-    export type Tuple<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = Type.Tuple<Type.Plain<TKey, TValue>>;
+    export type Tuple<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Tuple<ZuordType.Plain<TKey, TValue>>;
 }
