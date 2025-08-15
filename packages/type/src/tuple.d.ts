a6828b62067cbd6d974c2c44afe1beec3f922c28
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:19:47 +0300

    feat: add nest function type definition to zuordTupleAPI

--- packages/type/src/tuple.d.ts
+++ packages/type/src/tuple.d.ts
@@ -1,7 +1,9 @@
 import { first as $first } from "./tuple.runtime";
 import { last as $last } from "./tuple.runtime";
+import { nest as $nest } from "./tuple.runtime";
 
 type zuordTupleAPI = {
     first: typeof $first;
     last: typeof $last;
+    nest: typeof $nest;
 }
