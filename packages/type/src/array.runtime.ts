2f327d5debfecb70b74928afccf413e6a30628b5
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:04:45 +0300

    fix: correct type imports and usage in array.runtime.ts

--- packages/type/src/array.runtime.ts
+++ packages/type/src/array.runtime.ts
@@ -1,9 +1,9 @@
-import { ZuordType as Type } from "./type";
 import { zuordType } from "./type";
-import { ZuordArray } from "./array.types";
+import type { ZuordType } from "./type";
+import type { ZuordArray } from "./array.types";
 
 export function nest<T extends unknown = unknown>(obj: unknown, type?: { item?: (z: unknown) => z is T }): obj is ZuordArray.Nest<T> {
-  return zuordType.array(obj, { item: (item): item is Type.Array<T> => zuordType.array(item, type) });
+  return zuordType.array(obj, { item: (item): item is ZuordType.Array<T> => zuordType.array(item, type) });
 }
 
 export function empty(obj: unknown): obj is ZuordArray.Empty {
