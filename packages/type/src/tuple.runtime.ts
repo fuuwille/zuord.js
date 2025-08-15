81459470ceea6361b1c17a144e76cbf20951340d
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:17:03 +0300

    feat: implement tupleFirst function for validating tuple structure

---
+++ packages/type/src/tuple.runtime.ts
@@ -0,0 +1,15 @@
+import { zuordType } from "./type";
+import type { ZuordTuple } from "./tuple.types";
+
+export function tupleFirst<TFirst extends unknown = unknown, TRest extends unknown[] = unknown[]>(obj: unknown, type?: { first?: (z: unknown) => z is TFirst, rest?: (z: unknown) => z is TRest }): obj is ZuordTuple.First<TFirst, TRest> {
+    if (!zuordType.tuple(obj)) return false;
+    if (obj.length < 1) return false;
+
+    if (type) {
+        const [first, ...rest] = obj;
+        if (type.first && !type.first(first)) return false;
+        if (type.rest && !type.rest(rest)) return false;
+    }
+
+    return true;
+}
