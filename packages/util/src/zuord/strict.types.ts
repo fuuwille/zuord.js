191d08f6a460d4cec3826060f6d1a87e91f41796
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:51:48 +0300

    refactor: update references from ZuordType.PlainArray to ZuordPlain.Array in strict.types.ts

--- packages/util/src/zuord/strict.types.ts
+++ packages/util/src/zuord/strict.types.ts
@@ -1,10 +1,10 @@
 import { $ZuordUtil } from "../internal";
-import { ZuordType } from "@zuord/type";
+import { ZuordType, ZuordPlain } from "@zuord/type";
 
 export namespace Strict {
     export type Keys<TBase extends ZuordType.Plain, TContent extends ZuordType.Plain> 
         = $ZuordUtil.Strict.ResolveKeys<TBase, TContent>;
 
-    export type KeysBatch<TBase extends ZuordType.Plain, TContents extends ZuordType.PlainArray> 
-        = $ZuordUtil.Strict.ResolveKeysBatch<TBase, TContents> extends infer T extends ZuordType.PlainArray ? T : never
+    export type KeysBatch<TBase extends ZuordType.Plain, TContents extends ZuordPlain.Array> 
+        = $ZuordUtil.Strict.ResolveKeysBatch<TBase, TContents> extends infer T extends ZuordPlain.Array ? T : never
 }
