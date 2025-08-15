2043a942c4a1396df05a6e338e6c9fe4dc82f829
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:21:25 +0300

    feat: add export for zuordTuple and ZuordTuple from tuple module

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,3 +1,4 @@
 export { zuordType, ZuordType } from "./type";
 export { zuordPlain, ZuordPlain } from "./plain";
 export { zuordArray, ZuordArray } from "./array";
+export { zuordTuple, ZuordTuple } from "./tuple";
