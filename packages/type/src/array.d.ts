9387a95be24691586b5aceb922f53a8906829215
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 01:06:40 +0300

    feat: add empty property to zuordArrayAPI type definition

--- packages/type/src/array.d.ts
+++ packages/type/src/array.d.ts
@@ -1,5 +1,9 @@
 import { nest as $nest } from "./array.runtime";
+import { empty as $empty } from "./array.runtime";
 
 type zuordArrayAPI = {
     nest: typeof $nest;
+    empty: typeof $empty;
 };
+
+export declare const zuordArray: zuordArrayAPI;
