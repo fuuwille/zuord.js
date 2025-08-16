3d618ee2aec9d426a92652509fe83436d8346304
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 04:34:28 +0300

    feat: restore type exports in index.d.ts for zuord types

---
+++ packages/type/src/index.d.ts
@@ -0,0 +1,4 @@
+export type { zuordType, ZuordType } from "./type";
+export type { zuordPlain, ZuordPlain } from "./plain";
+export type { zuordArray, ZuordArray } from "./array";
+export type { zuordTuple, ZuordTuple } from "./tuple";
--- packages/type/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { zuordType, ZuordType } from "./type";
-export { zuordPlain, ZuordPlain } from "./plain";
-export { zuordArray, ZuordArray } from "./array";
-export { zuordTuple, ZuordTuple } from "./tuple";
