b800f2a5f517f632a5d8e1cde7f6db403747b7ac
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 14:59:02 +0300

    feat: add concat method to flagsAPI type definition

--- packages/core/src/internal/zuord/flags.d.ts
+++ packages/core/src/internal/zuord/flags.d.ts
@@ -1,5 +1,7 @@
 import { shallow as $shallow } from "./flags.runtime";
+import { concat as $concat } from "./flags.runtime";
 
 type flagsAPI = {
     shallow: typeof $shallow;
-}
+    concat: typeof $concat;
+};
