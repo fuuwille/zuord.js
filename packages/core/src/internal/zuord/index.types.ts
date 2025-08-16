62492d0f82f3ab6a06cdcb5f964f3ced1aa37781
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 15:01:27 +0300

    feat: import Flags into $ZuordCore namespace in index.types.ts

--- packages/core/src/internal/zuord/index.types.ts
+++ packages/core/src/internal/zuord/index.types.ts
@@ -1,5 +1,8 @@
 import { Mode as $Mode } from "./mode.types";
+import { Flags as $Flags } from "./flags.types";
+
 
 export declare namespace $ZuordCore {
     export import Mode = $Mode;
+    export import Flags = $Flags;
 }
