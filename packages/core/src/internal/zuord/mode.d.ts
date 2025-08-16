8fcff949bfd8fd9ef0317f54100f144632112748
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 13:40:59 +0300

    feat: add resolve function to modeAPI type definition in mode.d.ts

--- packages/core/src/internal/zuord/mode.d.ts
+++ packages/core/src/internal/zuord/mode.d.ts
@@ -1,5 +1,7 @@
 import { flags as $flags } from "./mode.runtime";
+import { resolve as $resolve } from "./mode.runtime";
 
 type modeAPI = {
     flags: typeof $flags;
+    resolve: typeof $resolve;
 }
