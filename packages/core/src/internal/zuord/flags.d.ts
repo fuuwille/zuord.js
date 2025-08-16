a681a5668b60f3907eb90c4a8d62a3ad9e4c538e
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 14:59:40 +0300

    feat: add base method to flagsAPI type definition

--- packages/core/src/internal/zuord/flags.d.ts
+++ packages/core/src/internal/zuord/flags.d.ts
@@ -1,9 +1,11 @@
 import { shallow as $shallow } from "./flags.runtime";
 import { concat as $concat } from "./flags.runtime";
 import { unique as $unique } from "./flags.runtime";
+import { base as $base } from "./flags.runtime";
 
 type flagsAPI = {
     shallow: typeof $shallow;
     concat: typeof $concat;
     unique: typeof $unique;
+    base: typeof $base;
 };
