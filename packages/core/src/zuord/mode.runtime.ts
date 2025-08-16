7a4bbe3af4a2480d4aeac0b4532c7a11b4b1a9d6
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 16:18:25 +0300

    fix: remove unnecessary newline and ensure type assertion in resolve function

--- packages/core/src/zuord/mode.runtime.ts
+++ packages/core/src/zuord/mode.runtime.ts
@@ -1,7 +1,6 @@
 import { $zuordCore } from "../internal/zuord";
 import { Mode } from "./mode.types";
 
-
 export function flags <K extends string, const V extends boolean = false>(key: K, value?: V) 
     : Mode.Flags<K, V>
 
@@ -15,6 +14,6 @@ export function flags <K extends string, const V extends boolean = false>(key: K
     return $zuordCore.mode.flags(key, value ?? false) as Mode.Flags<K, V>;
 }
 
-export function resolve <const TModes extends Mode.Flags[]>(modes: TModes): Mode.Resolve<TModes> {
-    return $zuordCore.mode.resolve(modes);
+export function resolve <const TModes extends Mode.Flags[]>(modes: TModes) {
+    return $zuordCore.mode.resolve(modes) as Mode.Resolve<TModes>;
 };
