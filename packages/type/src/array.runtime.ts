64da78aac13aca97d512708ec5ffbc5f8c284f0c
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 03:19:16 +0300

    refactor: consolidate type imports in array.runtime.ts

--- packages/type/src/array.runtime.ts
+++ packages/type/src/array.runtime.ts
@@ -1,6 +1,5 @@
 import { zuordType } from "./type";
-import type { ZuordType } from "./type";
-import type { ZuordArray } from "./array.types";
+import type { ZuordType, ZuordArray } from ".";
 
 export function nest<T extends unknown = unknown>(obj: unknown, type?: { item?: (z: unknown) => z is T }): obj is ZuordArray.Nest<T> {
   return zuordType.array(obj, { item: (item): item is ZuordType.Array<T> => zuordType.array(item, type) });
