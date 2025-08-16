74cbb2d9a7fbaa66f1964938f551db9915361f9c
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 02:43:37 +0300

    feat: update mode.runtime.ts to use $ZuordCore types for flags and resolve functions

--- packages/core/src/zuord/mode.runtime.ts
+++ packages/core/src/zuord/mode.runtime.ts
@@ -1,19 +1,19 @@
 import { $zuordCore } from "../internal";
-import { Mode } from "./mode.types";
+import { ZuordCore } from ".";
 
 export function flags <K extends string, const V extends boolean = false>(key: K, value?: V) 
-    : Mode.Flags<K, V>
+    : ZuordCore.Mode.Flags<K, V>
 
 export function flags <K extends string, const V extends boolean = false>(key: K[], value?: V) 
-    : Mode.Flags<K, V>
+    : ZuordCore.Mode.Flags<K, V>
 
 export function flags <K extends string, const V extends boolean = false>(key: K | K[], value?: V) 
-    : Mode.Flags<K, V>;
+    : ZuordCore.Mode.Flags<K, V>;
 
 export function flags <K extends string, const V extends boolean = false>(key: K | K[], value?: V) {
-    return $zuordCore.mode.flags(key, value ?? false) as Mode.Flags<K, V>;
+    return $zuordCore.mode.flags(key, value ?? false) as ZuordCore.Mode.Flags<K, V>;
 }
 
-export function resolve <const TModes extends Mode.Flags[]>(modes: TModes) {
-    return $zuordCore.mode.resolve(modes) as Mode.Resolve<TModes>;
+export function resolve <const TModes extends ZuordCore.Mode.Flags[]>(modes: TModes) {
+    return $zuordCore.mode.resolve(modes) as ZuordCore.Mode.Resolve<TModes>;
 };
