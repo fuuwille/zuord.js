1f2aee50fe781bf86f825dc13518a2e12fd7dbdb
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 16:26:34 +0300

    feat: add unique method to flagsAPI type definition in flags.d.ts

--- packages/core/src/zuord/flags.d.ts
+++ packages/core/src/zuord/flags.d.ts
@@ -1,7 +1,9 @@
 import { shallow as $shallow } from "./flags.runtime";
 import { concat as $concat } from "./flags.runtime";
+import { unique as $unique } from "./flags.runtime";
 
 type flagsAPI = {
     shallow: typeof $shallow;
     concat: typeof $concat;
+    unique: typeof $unique;
 };
