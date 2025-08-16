ac090be02b1f8c69f7a71166e4e8475098f6162d
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 15:02:06 +0300

    feat: add flags to zuordCoreAPI type definition

--- packages/core/src/internal/zuord/index.d.ts
+++ packages/core/src/internal/zuord/index.d.ts
@@ -1,7 +1,9 @@
 import { mode as $mode } from "./index.runtime";
+import { flags as $flags } from "./index.runtime";
 
 type zuordCoreAPI = {
     mode: typeof $mode;
+    flags: typeof $flags;
 };
 
 export declare const zuordCore: zuordCoreAPI;
