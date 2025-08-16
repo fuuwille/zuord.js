5527f3ce8ccc59959e6c125e0d3bb242fe445e81
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 03:17:24 +0300

    refactor: change import statement for ZuordType to use type import

--- packages/type/src/plain.types.ts
+++ packages/type/src/plain.types.ts
@@ -1,4 +1,4 @@
-import { ZuordType } from ".";
+import type { ZuordType } from ".";
 
 export namespace ZuordPlain {
     export type Array<TKey extends PropertyKey = PropertyKey, TValue extends unknown = unknown> = ZuordType.Array<ZuordType.Plain<TKey, TValue>>;
