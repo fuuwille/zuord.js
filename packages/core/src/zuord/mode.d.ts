bc4467de0ac9feeb239ed3f6dece18c0d83a21cf
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 16:20:02 +0300

    fix: rename type zuordModeAPI to modeAPI for consistency

--- packages/core/src/zuord/mode.d.ts
+++ packages/core/src/zuord/mode.d.ts
@@ -1,7 +1,7 @@
 import { flags as $flags } from "./mode.runtime";
 import { resolve as $resolve } from "./mode.runtime";
 
-type zuordModeAPI = {
+type modeAPI = {
     flags: typeof $flags;
     resolve: typeof $resolve;
 };
