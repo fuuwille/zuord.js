e9207a6d15e4d54f8216f5d1893051368063f626
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 02:39:31 +0300

    refactor: remove unnecessary blank line in $ZuordCore namespace declaration

--- packages/core/src/internal/zuord/index.types.ts
+++ packages/core/src/internal/zuord/index.types.ts
@@ -1,7 +1,6 @@
 import { Mode as $Mode } from "./mode.types";
 import { Flags as $Flags } from "./flags.types";
 
-
 export declare namespace $ZuordCore {
     export import Mode = $Mode;
     export import Flags = $Flags;
