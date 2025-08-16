0c4dc5c2efbdd90a0390446c67902da4d0ffa827
k4yr2 <kyrzkaya@gmail.com>
2025-08-16 16:22:28 +0300

    feat: add flags type definitions for $ZuordCore in flags.types.ts

---
+++ packages/core/src/zuord/flags.types.ts
@@ -0,0 +1,11 @@
+import { $ZuordCore } from "../internal/zuord";
+
+export declare namespace Flags {
+    export type Shallow = $ZuordCore.Flags.Shallow;
+
+    export type Concat = $ZuordCore.Flags.Concat;
+
+    export type Unique = $ZuordCore.Flags.Unique;
+
+    export type Base = $ZuordCore.Flags.Base;
+}
