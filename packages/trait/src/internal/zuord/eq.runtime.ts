f5e9574c259d40b0d3212c44e085aa0cfe4277e6
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:44:16 +0300

    feat: add 'any' function for array element comparison in eq.runtime.ts

--- packages/trait/src/internal/zuord/eq.runtime.ts
+++ packages/trait/src/internal/zuord/eq.runtime.ts
@@ -1,3 +1,7 @@
 export function both(a: unknown, b: unknown): boolean {
     return a === b;
 }
+
+export function any(u1: unknown[], t2: unknown): boolean {
+    return u1.some((item) => both(item, t2));
+}
