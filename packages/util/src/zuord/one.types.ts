7de8127dc97563d597de4a4d7d7b170cb191156a
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:17:37 +0300

    fix: update All, Required, and Optional types to use Core.Mode.Flags instead of Core.Mode.Field

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -3,9 +3,9 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
 export namespace One {
-    export type All<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveAll<T, TMode>;
+    export type All<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveAll<T, TMode>;
 
-    export type Required<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveRequired<T, TMode>;
+    export type Required<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveRequired<T, TMode>;
 
-    export type Optional<T extends Type.Plain, TMode extends Core.Mode.Field> = $ZuordUtil.One.ResolveOptional<T, TMode>;
+    export type Optional<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveOptional<T, TMode>;
 }
