b6b949346de1e79cd421fb46e76d0c89c846dd18
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:16:22 +0300

    fix: update ResolveAll, ResolveRequired, and ResolveOptional types to use Core.Mode.Flags

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -4,14 +4,14 @@ import { ZuordCore as Core } from "@zuord/core";
 import { ZuordTrait as Trait } from "@zuord/trait";
 
 export namespace One {
-    export type ResolveAll<T, TMode extends Core.Mode.Field> = 
-    [T, Core.Mode.Resolve<[TMode, { "$one.all": true }]>] extends [infer TPlain extends Type.Plain, infer THybridMode extends Core.Mode.Field] ? (
+    export type ResolveAll<T, TMode extends Core.Mode.Flags> = 
+    [T, Core.Mode.Resolve<[TMode, { "$one.all": true }]>] extends [infer TPlain extends Type.Plain, infer THybridMode extends Core.Mode.Flags] ? (
         (ResolveRequired<TPlain, THybridMode> & ResolveOptional<TPlain, THybridMode>) extends infer TInfer ? ({
             [K in keyof TInfer]: One.ResolveAll<TInfer[K], TMode>;
         }) : never
     ) : T;
 
-    export type ResolveRequired<T, TMode extends Core.Mode.Field> = [Trait.Is<T, Type.Plain>] extends [true] ? {
+    export type ResolveRequired<T, TMode extends Core.Mode.Flags> = [Trait.Is<T, Type.Plain>] extends [true] ? {
         [K in $Util.Keys.Required<T>]: (
             [TMode ]extends [{ "$one.all": true }] ? (
                 One.ResolveAll<T[K], TMode>
@@ -19,7 +19,7 @@ export namespace One {
         )
     } : T;
 
-    export type ResolveOptional<T, TMode extends Core.Mode.Field> = [Trait.Is<T, Type.Plain>] extends [true] ? {
+    export type ResolveOptional<T, TMode extends Core.Mode.Flags> = [Trait.Is<T, Type.Plain>] extends [true] ? {
         [K in $Util.Keys.Optional<T>]?: T extends any ? (
             K extends keyof T ? (
                 [TMode] extends [{ "$one.all": true }] ? (
