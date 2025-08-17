0feffcbd0f064ca2e66f47083a447404bceea933
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 03:11:45 +0300

    fix: update import path for $ZuordCore in flags.types.ts

--- packages/core/src/zuord/flags.types.ts
+++ packages/core/src/zuord/flags.types.ts
@@ -1,4 +1,4 @@
-import { $ZuordCore } from "../internal";
+import { $ZuordCore } from "@zuord/core/internal";
 
 export declare namespace Flags {
     export type Shallow = $ZuordCore.Flags.Shallow;
