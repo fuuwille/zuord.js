fccfdd6d9fa3dca4832281faa395fd955ad944ed
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:18:22 +0300

    fix: update Hybrid, Plain, Tuple, and Array types to use Core.Mode.Flags instead of Core.Mode.Field

--- packages/util/src/zuord/unify.types.ts
+++ packages/util/src/zuord/unify.types.ts
@@ -3,15 +3,15 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
 export namespace Unify {
-    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Field = {}> 
+    export type Hybrid<T extends Type.Plain | Type.Array, TMode extends Core.Mode.Flags = {}> 
         = $ZuordUtil.Unify.Hybrid<T, TMode>;
 
-    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Field = {}> 
+    export type Plain<T extends Type.Plain, TMode extends Core.Mode.Flags = {}> 
         = $ZuordUtil.Unify.Plain<T, TMode>;
 
-    export type Tuple<T extends Type.Tuple, TMode extends Core.Mode.Field = {}> 
+    export type Tuple<T extends Type.Tuple, TMode extends Core.Mode.Flags = {}> 
         = $ZuordUtil.Unify.Tuple<T, TMode>;
 
-    export type Array<T extends Type.Array, TMode extends Core.Mode.Field = {}> 
+    export type Array<T extends Type.Array, TMode extends Core.Mode.Flags = {}> 
         = $ZuordUtil.Unify.Array<T, TMode>;
 }
