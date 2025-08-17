e3dfbfc3fb35f069d4818cd3446a41ffbccdc9c0
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:36:07 +0300

    feat: add Any type to Eq namespace in eq.types.ts

--- packages/trait/src/internal/zuord/eq.types.ts
+++ packages/trait/src/internal/zuord/eq.types.ts
@@ -1,3 +1,7 @@
 export declare namespace Eq {
     export type Both<T1, T2> = (<T>() => T extends T1 ? 0 : 1) extends (<T>() => T extends T2 ? 0 : 1) ? true : false;
+
+    export type Any<U1, T2> = U1 extends [infer T1, ...infer R1] ? (
+        [Eq.Both<T1, T2>] extends [true] ? true : Eq.Any<R1, T2>
+    ) : false;
 }
