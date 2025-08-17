e216ec824f25002ac0d502873ff051863ab2b13f
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 06:17:55 +0300

    fix: update Required type to use Core.Mode.Flags instead of Core.Mode.Field

--- packages/util/src/zuord/only.types.ts
+++ packages/util/src/zuord/only.types.ts
@@ -3,5 +3,5 @@ import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
 export namespace Only {
-    export type Required<T extends Type.Plain, TMode extends Core.Mode.Field> = $Util.Only.ResolveRequired<T, TMode>;
+    export type Required<T extends Type.Plain, TMode extends Core.Mode.Flags> = $Util.Only.ResolveRequired<T, TMode>;
 }
