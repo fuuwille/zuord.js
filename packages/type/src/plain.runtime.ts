51c65f7567f8c9ae9ae90b1ec6d8004585d568ea
k4yr2 <kyrzkaya@gmail.com>
2025-08-17 05:25:18 +0300

    fix: correct import path for ZuordType and ZuordPlain in plain.runtime.ts

--- packages/type/src/plain.runtime.ts
+++ packages/type/src/plain.runtime.ts
@@ -1,5 +1,5 @@
 import { zuordType } from "./type";
-import type { ZuordType, ZuordPlain } from ".";
+import type { ZuordType, ZuordPlain } from "@zuord/type";
 
 export function array<TKey extends PropertyKey, TItem extends unknown>(obj: unknown, type?: { key?: (z: unknown) => z is TKey, item?: (z: unknown) => z is TItem }) : obj is ZuordPlain.Array<TKey, TItem> {
     return zuordType.array(obj, { item: (item): item is ZuordType.Plain<TKey, TItem> => zuordType.plain(item, type) });
