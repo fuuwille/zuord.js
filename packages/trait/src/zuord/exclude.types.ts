800f1cd81ce37872751659653b7a591cb82a2078
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:46:19 +0300

    fix: correct type parameter name in Exclude.Is type definition

--- packages/trait/src/zuord/exclude.types.ts
+++ packages/trait/src/zuord/exclude.types.ts
@@ -1,7 +1,7 @@
 import { $ZuordTrait } from "@zuord/trait/internal";
 
 export declare namespace Exclude {
-    export type Is<TSource extends unknown, TExcluded extends unknown> = $ZuordTrait.Exclude.Is<TSource, TExcluded>;
+    export type Is<TSource extends unknown, TExclude extends unknown> = $ZuordTrait.Exclude.Is<TSource, TExclude>;
 
     export type IsEach<TSource extends unknown, TExcludes extends unknown[]> = $ZuordTrait.Exclude.IsEach<TSource, TExcludes>;
 }
