de4e2ade99627782aac008df0be3d04c2938708f
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:01:35 +0300

    feat: add Every type definition to Is namespace for enhanced type support

--- packages/trait/src/zuord/is.types.ts
+++ packages/trait/src/zuord/is.types.ts
@@ -4,4 +4,6 @@ export namespace Is {
     export type Base<TSource extends unknown, TBase extends unknown> = $ZuordTrait.Is.Base<TSource, TBase>;
 
     export type Any<TSources extends unknown[], TBase extends unknown> = $ZuordTrait.Is.Any<TSources, TBase>;
+
+    export type Every<TSources extends unknown[], TBase extends unknown> = $ZuordTrait.Is.Every<TSources, TBase>;
 }
