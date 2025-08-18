4b977be2cf6692d49ac7d917f21ae3d5a117a965
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:55:18 +0300

    feat: add Is type to Extract namespace for conditional type evaluation

--- packages/trait/src/internal/zuord/extract.types.ts
+++ packages/trait/src/internal/zuord/extract.types.ts
@@ -1,3 +1,7 @@
-export declare namespace Extract {
+import { $ZuordTrait } from ".";
 
+export declare namespace Extract {
+    export type Is<TSource, TBase> = TSource extends any ? (
+        [$ZuordTrait.Is.Base<TSource, TBase>] extends [true] ? TSource : never
+    ) : never;
 }
