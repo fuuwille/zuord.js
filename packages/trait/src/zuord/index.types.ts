824d21aa7f7ffc3a1943faaeb0817974680ba0a8
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:53:00 +0300

    feat: add Has type import to ZuordTrait namespace

--- packages/trait/src/zuord/index.types.ts
+++ packages/trait/src/zuord/index.types.ts
@@ -1,8 +1,9 @@
 import { Eq as $Eq } from "./eq.types";
 import { Is as $Is } from "./is.types";
-
+import { Has as $Has } from "./has.types";
 
 export declare namespace ZuordTrait {
     export import Eq = $Eq;
     export import Is = $Is;
+    export import Has = $Has;
 }
