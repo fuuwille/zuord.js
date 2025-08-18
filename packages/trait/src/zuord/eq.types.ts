1e60471ee729dbb6d50d95238274cdc3a3506841
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 12:52:58 +0300

    feat: add Every type definition to Eq namespace for enhanced type support

--- packages/trait/src/zuord/eq.types.ts
+++ packages/trait/src/zuord/eq.types.ts
@@ -5,4 +5,6 @@ export declare namespace Eq {
     export type Both<T1 extends unknown, T2 extends unknown> = $ZuordTrait.Eq.Both<T1, T2>;
 
     export type Any<U1 extends ZuordType.Tuple, T2 extends unknown> = $ZuordTrait.Eq.Any<U1, T2>;
+
+    export type Every<U1 extends ZuordType.Tuple, T2 extends unknown> = $ZuordTrait.Eq.Every<U1, T2>;
 }
