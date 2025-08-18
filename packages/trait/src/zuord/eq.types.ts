fec2e194d7fb61f800600f43771c94a99f3d7398
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 12:51:49 +0300

    fix: correct import statement for $ZuordTrait in eq.types.ts

--- packages/trait/src/zuord/eq.types.ts
+++ packages/trait/src/zuord/eq.types.ts
@@ -1,3 +1,5 @@
+import { $ZuordTrait } from "@zuord/trait/internal";
+
 export declare namespace Eq {
-    
+    export type Both<T1 extends unknown, T2 extends unknown> = $ZuordTrait.Eq.Both<T1, T2>;
 }
