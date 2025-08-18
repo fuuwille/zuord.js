346e3394634e44b13c7ca4ca1fbebd3d61885daf
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:28:59 +0300

    feat: define Base type in Is namespace for type comparison

--- packages/trait/src/internal/zuord/is.types.ts
+++ packages/trait/src/internal/zuord/is.types.ts
@@ -1,3 +1,5 @@
 export namespace Is {
-    
+    export type Base<TSource, TBase> = [TSource] extends [TBase] ? (
+        [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
+    ) : false;
 }
