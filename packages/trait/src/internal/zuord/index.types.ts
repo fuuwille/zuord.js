422e77a5c50f8d1c2037540cafa3b9bd12cc0d9f
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:57:07 +0300

    feat: add Extract import to $ZuordTrait namespace for type extraction functionality

--- packages/trait/src/internal/zuord/index.types.ts
+++ packages/trait/src/internal/zuord/index.types.ts
@@ -2,10 +2,12 @@ import { Eq as $Eq } from "./eq.types";
 import { Is as $Is } from "./is.types";
 import { Has as $Has } from "./has.types";
 import { Exclude as $Exclude } from "./exclude.types";
+import { Extract as $Extract } from "./extract.types";
 
 export namespace $ZuordTrait {
     export import Eq = $Eq;
     export import Is = $Is;
     export import Has = $Has;
     export import Exclude = $Exclude;
+    export import Extract = $Extract;
 }
