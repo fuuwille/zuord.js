b13dcef32e80729b1323ae7c4f27fb72bc4171c9
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:45:38 +0300

    feat: update Exclude namespace to include import statement

--- packages/trait/src/zuord/exclude.types.ts
+++ packages/trait/src/zuord/exclude.types.ts
@@ -1,3 +1,5 @@
+import { $ZuordTrait } from "@zuord/trait/internal";
+
 export declare namespace Exclude {
-    
+    export type Is<TSource extends unknown, TExcluded extends unknown> = $ZuordTrait.Exclude.Is<TSource, TExcluded>;
 }
