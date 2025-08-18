b2463ceb43f76877ad990e39deaf039275d834ab
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:07:59 +0300

    feat: add Every type definition to Has namespace

--- packages/trait/src/zuord/has.types.ts
+++ packages/trait/src/zuord/has.types.ts
@@ -4,4 +4,6 @@ export namespace Has {
     export type Base<TSource extends unknown, TBase extends unknown> = $ZuordTrait.Has.Base<TSource, TBase>;
 
     export type Any<TSources extends unknown[], TBase extends unknown> = $ZuordTrait.Has.Any<TSources, TBase>;
+
+    export type Every<TSources extends unknown[], TBase extends unknown> = $ZuordTrait.Has.Every<TSources, TBase>;
 }
