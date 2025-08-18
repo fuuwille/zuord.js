3f1d18858ba705e98f95d6d4b0749721642ec6ef
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:42:04 +0300

    feat: change Has namespace to declare for improved type visibility

--- packages/trait/src/internal/zuord/has.types.ts
+++ packages/trait/src/internal/zuord/has.types.ts
@@ -1,7 +1,7 @@
 import { $ZuordTrait } from ".";
 import { ZuordArray } from "@zuord/type";
 
-export namespace Has {
+export declare namespace Has {
     export type Base<TSource, TBase> = true extends (TSource extends any ? $ZuordTrait.Is.Base<TSource, TBase> : never) ? true : false;
 
     export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
