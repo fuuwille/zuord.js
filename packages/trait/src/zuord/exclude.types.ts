b9dc138ee02ae5809ec6dc88b6f1b57eb264d8d4
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:46:07 +0300

    feat: add IsEach type definition to Exclude namespace

--- packages/trait/src/zuord/exclude.types.ts
+++ packages/trait/src/zuord/exclude.types.ts
@@ -2,4 +2,6 @@ import { $ZuordTrait } from "@zuord/trait/internal";
 
 export declare namespace Exclude {
     export type Is<TSource extends unknown, TExcluded extends unknown> = $ZuordTrait.Exclude.Is<TSource, TExcluded>;
+
+    export type IsEach<TSource extends unknown, TExcludes extends unknown[]> = $ZuordTrait.Exclude.IsEach<TSource, TExcludes>;
 }
