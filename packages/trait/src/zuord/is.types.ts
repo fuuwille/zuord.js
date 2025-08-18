84e1a0718e9ddd2c6ce9e1be474d787ebd2d5a7f
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:01:04 +0300

    feat: import $ZuordTrait in Is namespace for type definition support

--- packages/trait/src/zuord/is.types.ts
+++ packages/trait/src/zuord/is.types.ts
@@ -1,3 +1,5 @@
+import { $ZuordTrait } from "../internal";
+
 export namespace Is {
-    
+    export type Base<TSource extends unknown, TBase extends unknown> = $ZuordTrait.Is.Base<TSource, TBase>;
 }
