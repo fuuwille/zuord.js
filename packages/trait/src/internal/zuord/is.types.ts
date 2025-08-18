af37c342322a4c7d625c12379e2fa40cea9dc357
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:30:11 +0300

    feat: add Any type for recursive type comparison in Is namespace

--- packages/trait/src/internal/zuord/is.types.ts
+++ packages/trait/src/internal/zuord/is.types.ts
@@ -2,4 +2,8 @@ export namespace Is {
     export type Base<TSource, TBase> = [TSource] extends [TBase] ? (
         [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
     ) : false;
+
+    export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
+        [Is.Base<TSource, TBase>] extends [true] ? true : Is.Any<TRestSources, TBase>
+    ) : false;
 }
