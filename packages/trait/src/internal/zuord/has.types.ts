63acaf7176bfbe6c5f904e784915223d0e2cd30a
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:49:43 +0300

    feat: add ZuordArray import and implement Every type in Has namespace

--- packages/trait/src/internal/zuord/has.types.ts
+++ packages/trait/src/internal/zuord/has.types.ts
@@ -1,9 +1,14 @@
 import { ZuordTrait } from ".";
+import { ZuordArray } from "@zuord/type";
 
 export namespace Has {
     export type Base<TSource, TBase> = true extends (TSource extends any ? ZuordTrait.Is.Base<TSource, TBase> : never) ? true : false;
 
     export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
         [Has.Base<TSource, TBase>] extends [true] ? true : Has.Any<TSourceRest, TBase>
+    ) : false;
+
+    export type Every<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+        [Has.Base<TSource, TBase>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : Has.Every<TSourceRest, TBase>) : false
     ) : false;
 }
