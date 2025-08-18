5cebc6e0ca39f3ede165d8256ab7aa233d02bfea
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:48:10 +0300

    feat: add IsEach type definition to Extract namespace

--- packages/trait/src/zuord/extract.types.ts
+++ packages/trait/src/zuord/extract.types.ts
@@ -2,4 +2,6 @@ import { $ZuordTrait } from "@zuord/trait/internal";
 
 export namespace Extract {
     export type Is<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Is<TSource, TExtract>;
+
+    export type IsEach<TSource extends unknown, TExtracts extends unknown[]> = $ZuordTrait.Extract.IsEach<TSource, TExtracts>;
 }
