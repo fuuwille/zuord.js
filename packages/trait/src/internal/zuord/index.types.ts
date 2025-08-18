829347d7724372c3f317d319094dc1bcf3768a15
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:53:41 +0300

    feat: add Exclude type import to $ZuordTrait namespace for enhanced type functionality

--- packages/trait/src/internal/zuord/index.types.ts
+++ packages/trait/src/internal/zuord/index.types.ts
@@ -1,9 +1,11 @@
 import { Eq as $Eq } from "./eq.types";
 import { Is as $Is } from "./is.types";
 import { Has as $Has } from "./has.types";
+import { Exclude as $Exclude } from "./exclude.types";
 
 export namespace $ZuordTrait {
     export import Eq = $Eq;
     export import Is = $Is;
     export import Has = $Has;
+    export import Exclude = $Exclude;
 }
