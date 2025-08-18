7ad39fe054702940071e6c96eb9c5fe960882b42
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:48:51 +0300

    feat: enhance Exclude namespace with Is type for improved type filtering

--- packages/trait/src/internal/zuord/exclude.types.ts
+++ packages/trait/src/internal/zuord/exclude.types.ts
@@ -1,3 +1,7 @@
+import { $ZuordTrait } from ".";
+
 export declare namespace Exclude {
-    
+    export type Is<TSource, TBase> = TSource extends any ? (
+        [$ZuordTrait.Is.Base<TSource, TBase>] extends [false] ? TSource : never
+    ) : never;
 }
