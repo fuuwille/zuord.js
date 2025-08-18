172b0d0d8d8f9f4bf083707a6f2503e2dea59dab
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:30:58 +0300

    feat: add Every type for type comparison in Is namespace

--- packages/trait/src/internal/zuord/is.types.ts
+++ packages/trait/src/internal/zuord/is.types.ts
@@ -1,3 +1,5 @@
+import { ZuordArray } from "@zuord/type";
+
 export namespace Is {
     export type Base<TSource, TBase> = [TSource] extends [TBase] ? (
         [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
@@ -5,5 +7,9 @@ export namespace Is {
 
     export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
         [Is.Base<TSource, TBase>] extends [true] ? true : Is.Any<TRestSources, TBase>
+    ) : false;
+
+    export type Every<TSources, TBase> = TSources extends [infer TSource, ...infer TRestSources] ? (
+        [Is.Base<TSource, TBase>] extends [true] ? (TRestSources extends ZuordArray.Empty ? true : Is.Every<TRestSources, TBase>) : false
     ) : false;
 }
