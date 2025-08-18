e0f24addbf7d7e9208f955b98ed763ba02f8d382
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:50:27 +0300

    feat: add IsEach type to Exclude namespace for recursive type filtering

--- packages/trait/src/internal/zuord/exclude.types.ts
+++ packages/trait/src/internal/zuord/exclude.types.ts
@@ -4,4 +4,8 @@ export declare namespace Exclude {
     export type Is<TSource, TBase> = TSource extends any ? (
         [$ZuordTrait.Is.Base<TSource, TBase>] extends [false] ? TSource : never
     ) : never;
+
+    export type IsEach<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
+        Exclude.IsEach<Exclude.Is<TSource, TBase>, TBaseRest>
+    ) : TSource;
 }
