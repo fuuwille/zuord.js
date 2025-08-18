b9e9e7dad52e8f7656eb3019433e95de843fe094
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 01:47:45 +0300

    fix: remove dts plugin from rollup configuration and simplify typescript plugin handling

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 
 export function createConfig({ input, tsconfig = null }) {
     return {
@@ -13,8 +12,7 @@ export function createConfig({ input, tsconfig = null }) {
         external: [/^@zuord\//],
         plugins: [
             resolve({ preferBuiltins: true }),
-            tsconfig && typescript({ tsconfig }),
-            !tsconfig && dts()
-        ].filter(Boolean),
+            typescript({ tsconfig })
+        ],
     };
 }
