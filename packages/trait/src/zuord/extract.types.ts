ff4cea998fab7d2e042ebf88cc16b166b3aeea06
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:47:58 +0300

    feat: import $ZuordTrait in Extract namespace

--- packages/trait/src/zuord/extract.types.ts
+++ packages/trait/src/zuord/extract.types.ts
@@ -1,3 +1,5 @@
+import { $ZuordTrait } from "@zuord/trait/internal";
+
 export namespace Extract {
-    
+    export type Is<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Is<TSource, TExtract>;
 }
