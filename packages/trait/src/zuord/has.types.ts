7032a0a9077e9bb5e0b8569469ee6e4cc1247829
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:07:38 +0300

    fix: adjust import statement for $ZuordTrait in Has namespace

--- packages/trait/src/zuord/has.types.ts
+++ packages/trait/src/zuord/has.types.ts
@@ -1,3 +1,5 @@
+import { $ZuordTrait } from "@zuord/trait/internal";
+
 export namespace Has {
-    
+    export type Base<TSource extends unknown, TBase extends unknown> = $ZuordTrait.Has.Base<TSource, TBase>;
 }
