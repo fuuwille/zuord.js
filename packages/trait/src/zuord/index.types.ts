c4f9adfe2f4743b6de945c9611977d0eb5173a18
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 13:52:49 +0300

    feat: add Is type import to ZuordTrait namespace

--- packages/trait/src/zuord/index.types.ts
+++ packages/trait/src/zuord/index.types.ts
@@ -1,5 +1,8 @@
 import { Eq as $Eq } from "./eq.types";
+import { Is as $Is } from "./is.types";
+
 
 export declare namespace ZuordTrait {
     export import Eq = $Eq;
+    export import Is = $Is;
 }
