e813b58b7e5d813dd0340f041f8659680b1457dc
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:49:25 +0300

    feat: add Any type to Has namespace in has.types.ts

--- packages/trait/src/internal/zuord/has.types.ts
+++ packages/trait/src/internal/zuord/has.types.ts
@@ -2,4 +2,8 @@ import { ZuordTrait } from ".";
 
 export namespace Has {
     export type Base<TSource, TBase> = true extends (TSource extends any ? ZuordTrait.Is.Base<TSource, TBase> : never) ? true : false;
+
+    export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+        [Has.Base<TSource, TBase>] extends [true] ? true : Has.Any<TSourceRest, TBase>
+    ) : false;
 }
