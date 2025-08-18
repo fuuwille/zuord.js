bfe7190a0693fb621be1846dd682d2106954ce44
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 11:55:35 +0300

    feat: add IsEach type to Extract namespace for recursive type evaluation

--- packages/trait/src/internal/zuord/extract.types.ts
+++ packages/trait/src/internal/zuord/extract.types.ts
@@ -4,4 +4,8 @@ export declare namespace Extract {
     export type Is<TSource, TBase> = TSource extends any ? (
         [$ZuordTrait.Is.Base<TSource, TBase>] extends [true] ? TSource : never
     ) : never;
+
+    export type IsEach<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
+        Extract.IsEach<Extract.Is<TSource, TBase>, TBaseRest>
+    ) : TSource;
 }
