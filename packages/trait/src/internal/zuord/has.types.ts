a484aa0a207836122537673513f72975d1410458
k4yr2 <kyrzkaya@gmail.com>
2025-08-18 10:50:37 +0300

    feat: rename ZuordTrait to $ZuordTrait and update related imports

--- packages/trait/src/internal/zuord/has.types.ts
+++ packages/trait/src/internal/zuord/has.types.ts
@@ -1,8 +1,8 @@
-import { ZuordTrait } from ".";
+import { $ZuordTrait } from ".";
 import { ZuordArray } from "@zuord/type";
 
 export namespace Has {
-    export type Base<TSource, TBase> = true extends (TSource extends any ? ZuordTrait.Is.Base<TSource, TBase> : never) ? true : false;
+    export type Base<TSource, TBase> = true extends (TSource extends any ? $ZuordTrait.Is.Base<TSource, TBase> : never) ? true : false;
 
     export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
         [Has.Base<TSource, TBase>] extends [true] ? true : Has.Any<TSourceRest, TBase>
