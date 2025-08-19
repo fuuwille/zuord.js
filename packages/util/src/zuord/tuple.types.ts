e14eb1d97059ebb632ff9a84b420344222ea3db3
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:05:48 +0300

    fix: correct import statement for $ZuordUtil in tuple.types.ts

--- packages/util/src/zuord/tuple.types.ts
+++ packages/util/src/zuord/tuple.types.ts
@@ -1,8 +1,8 @@
-import { $ZuordUtil as $Util } from "../internal";
+import type { $ZuordUtil } from "@zuord/util/internal";
 import { ZuordType as Type } from "@zuord/type";
 
 export namespace Tuple {
-    export type Keys<T extends Type.Tuple> = $Util.Tuple.Keys<T>;
+    export type Keys<T extends Type.Tuple> = $ZuordUtil.Tuple.Keys<T>;
 
-    export type Unify<T extends Type.Tuple> = $Util.Tuple.Unify<T>;
+    export type Unify<T extends Type.Tuple> = $ZuordUtil.Tuple.Unify<T>;
 }
