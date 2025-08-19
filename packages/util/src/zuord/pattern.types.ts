e554989ccf8cd93693b7b4bd39b6372796aa3746
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:07:34 +0300

    fix: change namespace export to declare for Pattern

--- packages/util/src/zuord/pattern.types.ts
+++ packages/util/src/zuord/pattern.types.ts
@@ -1,6 +1,6 @@
 import type { $ZuordUtil } from "@zuord/util/internal";
 import type { ZuordType } from "@zuord/type";
 
-export namespace Pattern {
+export declare namespace Pattern {
     export type Plain<T extends ZuordType.Plain> = $ZuordUtil.Pattern.PlainResolve<T>;
 }
