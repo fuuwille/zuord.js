f67698d9bf8b1b6ce2e309e255b0552197652bb9
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:13:13 +0300

    fix: update type imports and references in One namespace

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -1,11 +1,11 @@
 import type { $ZuordUtil } from "@zuord/util/internal";
-import { ZuordType as Type } from "@zuord/type";
-import { ZuordCore as Core } from "@zuord/core";
+import type { ZuordType } from "@zuord/type";
+import type { ZuordCore } from "@zuord/core";
 
 export namespace One {
-    export type All<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveAll<T, TMode>;
+    export type All<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.One.ResolveAll<T, TMode>;
 
-    export type Required<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveRequired<T, TMode>;
+    export type Required<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.One.ResolveRequired<T, TMode>;
 
-    export type Optional<T extends Type.Plain, TMode extends Core.Mode.Flags> = $ZuordUtil.One.ResolveOptional<T, TMode>;
+    export type Optional<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.One.ResolveOptional<T, TMode>;
 }
