89bc2f28821f05fc7e303b41dee243d0a92d8832
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:04:52 +0300

    fix: update import statement for $ZuordUtil to use correct path

--- packages/util/src/zuord/unify.types.ts
+++ packages/util/src/zuord/unify.types.ts
@@ -1,4 +1,4 @@
-import { $ZuordUtil } from "../internal";
+import type { $ZuordUtil } from "@zuord/util/internal";
 import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
