df7961574b8bf832dd10f884e35eb8e20646e9d7
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 04:43:46 +0300

    fix: update import paths in ZuordTrait to remove .types extension

--- packages/trait/src/zuord/index.ts
+++ packages/trait/src/zuord/index.ts
@@ -1,8 +1,8 @@
-import { Eq as $Eq } from "./eq.types";
-import { Is as $Is } from "./is.types";
-import { Has as $Has } from "./has.types";
-import { Extract as $Extract } from "./extract.types";
-import { Exclude as $Exclude } from "./exclude.types";
+import { Eq as $Eq } from "./eq";
+import { Is as $Is } from "./is";
+import { Has as $Has } from "./has";
+import { Extract as $Extract } from "./extract";
+import { Exclude as $Exclude } from "./exclude";
 
 export namespace ZuordTrait {
     export import Eq = $Eq;
