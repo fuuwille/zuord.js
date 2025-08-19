d441226cb0203831a2c52120d7ddb7bddf0dfb90
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:09:00 +0300

    fix: change namespace export to declare for Tuple

--- packages/util/src/zuord/tuple.types.ts
+++ packages/util/src/zuord/tuple.types.ts
@@ -1,7 +1,7 @@
 import type { $ZuordUtil } from "@zuord/util/internal";
 import type { ZuordType } from "@zuord/type";
 
-export namespace Tuple {
+export declare namespace Tuple {
     export type Keys<T extends ZuordType.Tuple> = $ZuordUtil.Tuple.Keys<T>;
 
     export type Unify<T extends ZuordType.Tuple> = $ZuordUtil.Tuple.Unify<T>;
