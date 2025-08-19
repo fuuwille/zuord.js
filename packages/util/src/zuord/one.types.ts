4db3f26360ac32c2b45b5c34dd838ef956351e6c
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:13:56 +0300

    fix: change namespace export to declare for One

--- packages/util/src/zuord/one.types.ts
+++ packages/util/src/zuord/one.types.ts
@@ -2,7 +2,7 @@ import type { $ZuordUtil } from "@zuord/util/internal";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordCore } from "@zuord/core";
 
-export namespace One {
+export declare namespace One {
     export type All<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.One.ResolveAll<T, TMode>;
 
     export type Required<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.One.ResolveRequired<T, TMode>;
