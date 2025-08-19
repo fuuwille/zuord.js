592dc98bf49717d6e50e3a4166096ec9fe4f0ee1
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 04:50:32 +0300

    fix: restore $ZuordTrait namespace and exports in index.types.ts

--- packages/trait/src/internal/zuord/index.ts
+++ packages/trait/src/internal/zuord/index.ts
@@ -1,13 +1,1 @@
-import { Eq as $Eq } from "./eq";
-import { Is as $Is } from "./is";
-import { Has as $Has } from "./has";
-import { Exclude as $Exclude } from "./exclude";
-import { Extract as $Extract } from "./extract";
-
-export namespace $ZuordTrait {
-    export import Eq = $Eq;
-    export import Is = $Is;
-    export import Has = $Has;
-    export import Exclude = $Exclude;
-    export import Extract = $Extract;
-}
+export { $ZuordTrait } from "./index.types";
---
+++ packages/trait/src/internal/zuord/index.types.ts
@@ -0,0 +1,13 @@
+import { Eq as $Eq } from "./eq";
+import { Is as $Is } from "./is";
+import { Has as $Has } from "./has";
+import { Exclude as $Exclude } from "./exclude";
+import { Extract as $Extract } from "./extract";
+
+export namespace $ZuordTrait {
+    export import Eq = $Eq;
+    export import Is = $Is;
+    export import Has = $Has;
+    export import Exclude = $Exclude;
+    export import Extract = $Extract;
+}
