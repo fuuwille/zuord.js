7afd088446291b73a86e3b5dd0a65435de9654ce
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:24:50 +0300

    fix: change namespace declaration for Pattern to use 'declare' keyword in pattern.types.ts

--- packages/util/src/internal/zuord/pattern.types.ts
+++ packages/util/src/internal/zuord/pattern.types.ts
@@ -1,6 +1,6 @@
 import type { ZuordType } from "@zuord/type";
 
-export namespace Pattern {
+export declare namespace Pattern {
     export type PlainResolve<T> = T extends ZuordType.Plain ? {
         [K in keyof T]?: true | PlainResolve<T[K]>
     } : never;
