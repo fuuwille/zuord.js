c4bc0034ae1eb1dd1ad6161162f3b83306983063
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:25:02 +0300

    fix: change namespace declaration for Only to use 'declare' keyword in only.types.ts

--- packages/util/src/internal/zuord/only.types.ts
+++ packages/util/src/internal/zuord/only.types.ts
@@ -1,6 +1,6 @@
 import type { ZuordType } from '@zuord/type';
 
-export namespace Only {
+export declare namespace Only {
     export type ResolveRequired<T, TMode> = [T] extends [ZuordType.Plain] ? { 
         [K in keyof T as {} extends Pick<T, K> ? never : K]: Exclude<T[K], undefined> extends infer TValue ? (
             TMode extends { shallo: true } ? (
