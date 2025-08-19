cf9152a1745a987d0b5797f49c130d573f467b08
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:06:00 +0300

    fix: update type import for ZuordType in tuple.types.ts for consistency

--- packages/util/src/zuord/tuple.types.ts
+++ packages/util/src/zuord/tuple.types.ts
@@ -1,8 +1,8 @@
 import type { $ZuordUtil } from "@zuord/util/internal";
-import { ZuordType as Type } from "@zuord/type";
+import type { ZuordType } from "@zuord/type";
 
 export namespace Tuple {
-    export type Keys<T extends Type.Tuple> = $ZuordUtil.Tuple.Keys<T>;
+    export type Keys<T extends ZuordType.Tuple> = $ZuordUtil.Tuple.Keys<T>;
 
-    export type Unify<T extends Type.Tuple> = $ZuordUtil.Tuple.Unify<T>;
+    export type Unify<T extends ZuordType.Tuple> = $ZuordUtil.Tuple.Unify<T>;
 }
