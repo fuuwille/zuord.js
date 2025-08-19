1f2ba9febf0abc8ae274799d6961a3a274c94644
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:14:12 +0300

    fix: update import statement for $ZuordUtil in Mutable namespace

--- packages/util/src/zuord/mutable.types.ts
+++ packages/util/src/zuord/mutable.types.ts
@@ -1,4 +1,4 @@
-import { $ZuordUtil } from "../internal";
+import type { $ZuordUtil } from "@zuord/util/internal";
 
 export namespace Mutable {
     export type Hybrid<T extends object> = $ZuordUtil.Mutable.Resolve<T>;
