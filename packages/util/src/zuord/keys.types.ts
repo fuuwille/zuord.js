35520155cd24cd007c2ba13b288619eeef129310
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:14:55 +0300

    fix: change namespace export to declare for Keys

--- packages/util/src/zuord/keys.types.ts
+++ packages/util/src/zuord/keys.types.ts
@@ -1,6 +1,6 @@
 import type { $ZuordUtil } from "@zuord/util/internal";
 
-export namespace Keys {
+export declare namespace Keys {
     export type All<T> = $ZuordUtil.Keys.All<T>;
 
     export type Required<T> = $ZuordUtil.Keys.Required<T>;
