5e3d81ebc89d76eb2d226573bfd7c9453cee6b02
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:10:27 +0300

    fix: change namespace export to declare for Only

--- packages/util/src/zuord/only.types.ts
+++ packages/util/src/zuord/only.types.ts
@@ -2,6 +2,6 @@ import { $ZuordUtil as $Util } from "../internal";
 import { ZuordType as Type } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 
-export namespace Only {
+export declare namespace Only {
     export type Required<T extends Type.Plain, TMode extends Core.Mode.Flags> = $Util.Only.ResolveRequired<T, TMode>;
 }
