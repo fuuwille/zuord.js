00de7781c4ac9adf7097d14e08aa2586a4789c42
k4yr2 <kyrzkaya@gmail.com>
2025-08-19 05:21:08 +0300

    fix: update import statement to use ZuordType instead of alias in restrict.types.ts

--- packages/util/src/internal/zuord/restrict.types.ts
+++ packages/util/src/internal/zuord/restrict.types.ts
@@ -1,19 +1,19 @@
-import { ZuordType as Type } from "@zuord/type";
+import type { ZuordType } from "@zuord/type";
 
 export namespace Restrict {
-    export type ResolveKeys<TBase, TInput> = TBase extends Type.Plain ? (
+    export type ResolveKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
         (ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>) extends infer T ? {
             [K in keyof T as [undefined] extends [T[K]] ? never : K]: T[K] extends never ? never : T[K];
         } : never
     ) : TInput;
 
-    export type ResolveIncludedKeys<TBase extends Type.Plain, TInput> = {
+    export type ResolveIncludedKeys<TBase extends ZuordType.Plain, TInput> = {
         [K in keyof TInput]: K extends keyof TBase
             ? ResolveKeys<TBase[K], TInput[K]>
             : never
     } 
 
-    export type ResolveExcludedKeys<TBase extends Type.Plain, TInput> = {
+    export type ResolveExcludedKeys<TBase extends ZuordType.Plain, TInput> = {
         [K in Exclude<keyof TBase, keyof TInput>]?: never
     };
 
