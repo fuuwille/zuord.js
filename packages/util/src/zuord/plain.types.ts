d81322d7337d7c0cd5ec00fa17a90e6a1db95028
k4yr2 <kyrzkaya@gmail.com>
2025-08-21 21:45:34 +0300

    fix: update import path in plain.types.ts for consistency

--- packages/util/src/zuord/plain.types.ts
+++ packages/util/src/zuord/plain.types.ts
@@ -1,4 +1,4 @@
-import type { $ZuordUtil } from "@zuord/util/internal";
+import type { $ZuordUtil } from "../internal";
 
 export declare namespace Plain {
     export type Unify<T> = $ZuordUtil.Plain.Unify<T>;
