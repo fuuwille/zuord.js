a34ee623148ac1e9d7c282516b91a002f295a675
k4yr2 <kyrzkaya@gmail.com>
2025-08-21 21:45:45 +0300

    fix: update import path in restrict.types.ts for consistency

--- packages/util/src/zuord/restrict.types.ts
+++ packages/util/src/zuord/restrict.types.ts
@@ -1,4 +1,4 @@
-import type { $ZuordUtil } from "@zuord/util/internal";
+import type { $ZuordUtil } from "../internal";
 import type { ZuordType, ZuordPlain } from "@zuord/type";
 
 export declare namespace Restrict {
