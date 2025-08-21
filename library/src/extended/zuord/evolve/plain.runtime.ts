c6e504d66ee76de732b9b0a9dfbbeb50b79a1a27
k4yr2 <kyrzkaya@gmail.com>
2025-08-21 21:50:19 +0300

    fix: update import statements for consistency in plain.runtime.ts

--- library/src/extended/zuord/evolve/plain.runtime.ts
+++ library/src/extended/zuord/evolve/plain.runtime.ts
@@ -1,9 +1,8 @@
-import { $zuord } from "zuord/internal";
+import { $zuord } from "../../../internal";
 import { zuordModeX } from "../../mode";
 import { zuordPlain } from "@zuord/type";
 import { zuordCore } from "@zuord/core";
-import type { ZuordX } from "zuord/extended";
-import type { ZuordModeX } from "zuord/extended";
+import type { ZuordX, ZuordModeX } from "../../";
 import type { ZuordType, ZuordPlain } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
