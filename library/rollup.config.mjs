b6e6bb34110e582757e23f30d389c0baa9c07d94
k4yr2 <kyrzkaya@gmail.com>
2025-08-21 05:07:53 +0300

    fix: add external dependencies to rollup configuration

--- library/rollup.config.mjs
+++ library/rollup.config.mjs
@@ -3,4 +3,5 @@ import { createConfig } from '../rollup.config.mjs';
 export default createConfig({
     input: 'src/index.ts',
     tsconfig: './tsconfig-dist.json',
+    external: ["@zuord/type", "@zuord/core", "@zuord/trait", "@zuord/util"]
 });
