5e90fee95fdd12fda0be4315b219eaeb4374dd24
k4yr2 <kyrzkaya@gmail.com>
2025-08-21 23:52:42 +0300

    fix: simplify input configuration in rollup.config.mjs

--- library/rollup.config.mjs
+++ library/rollup.config.mjs
@@ -1,6 +1,6 @@
 import { createConfig } from '../rollup.config.mjs';
 
 export default createConfig({
-    input: ['src/index', 'src/internal/index', 'src/extended/index'],
+    input: 'src/index',
     tsconfig: './tsconfig-dist.json',
 });
