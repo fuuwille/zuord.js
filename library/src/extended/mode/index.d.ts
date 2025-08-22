1b7dc6137d19a28356e01e8524671c385bbf8d72
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:31:51 +0300

    feat: add pick type to zuordModeXAPI in index.d.ts

--- library/src/extended/mode/index.d.ts
+++ library/src/extended/mode/index.d.ts
@@ -1,11 +1,13 @@
 import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
 import { evolve as $evolve } from "./index.runtime";
+import { pick as $pick } from "./index.runtime";
 
 type zuordModeXAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
     evolve: typeof $evolve;
+    pick: typeof $pick;
 }
 
 export declare const zuordModeX: zuordModeXAPI;
