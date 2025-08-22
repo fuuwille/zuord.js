7c1e985d927338e7dd7d36c7efd899ab0f0d5921
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:37:08 +0300

    feat: add _TMode parameter to Loose type in plain.types.ts

--- library/src/extended/zuord/pick/plain.types.ts
+++ library/src/extended/zuord/pick/plain.types.ts
@@ -1,7 +1,8 @@
 import { $Zuord } from "../../../internal";
+import { ZuordModeX } from "../../mode";
 import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
 
 export declare namespace Plain {
-    export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>> = $Zuord.Pick.Plain<T, P>;
+    export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>, _TMode extends Partial<ZuordModeX.Evolve.Plain>> = $Zuord.Pick.Plain<T, P>;
 }
