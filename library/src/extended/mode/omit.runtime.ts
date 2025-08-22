78cbd4735e60b1d37891eb284a4c1cf4cbe5f079
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:54:48 +0300

    feat: implement plain resolution in omit.runtime.ts

---
+++ library/src/extended/mode/omit.runtime.ts
@@ -0,0 +1,4 @@
+import { zuordCore } from "@zuord/core";
+import type { ZuordModeX } from ".";
+
+export const plain = zuordCore.mode.resolve([zuordCore.flags.base]) satisfies ZuordModeX.Omit.Plain;
