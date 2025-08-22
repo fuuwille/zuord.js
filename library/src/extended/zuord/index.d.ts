a11addfb6c06d3c0f80dcbd279576446d7af3693
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 18:06:35 +0300

    feat: add omit type to zuordXAPI in index.d.ts

--- library/src/extended/zuord/index.d.ts
+++ library/src/extended/zuord/index.d.ts
@@ -2,12 +2,14 @@ import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
 import { evolve as $evolve } from "./index.runtime";
 import { pick as $pick } from "./index.runtime";
+import { omit as $omit } from "./index.runtime";
 
 type zuordXAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
     evolve: typeof $evolve;
     pick: typeof $pick;
+    omit: typeof $omit;
 }
 
 export declare const zuordX: zuordXAPI;
