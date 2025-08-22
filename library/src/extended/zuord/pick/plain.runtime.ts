bdc4ecbf0331bbcbef5e25e2c3e5334afc114f7f
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 18:00:32 +0300

    refactor: simplify $ function by removing unnecessary validation

--- library/src/extended/zuord/pick/plain.runtime.ts
+++ library/src/extended/zuord/pick/plain.runtime.ts
@@ -1,17 +1,12 @@
 import { $zuord } from "../../../internal";
 import { zuordModeX } from "../../mode";
-import { zuordPlain } from "@zuord/type";
 import { zuordCore } from "@zuord/core";
 import type { ZuordX, ZuordModeX } from "../../";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
-const $ = ($obj: ZuordType.Plain, $content: ZuordUtil.Pattern.Plain<ZuordType.Plain>, $mode: Partial<ZuordModeX.Pick.Plain>) => {
-    if(!zuordPlain.array($content)) {
-        throw new TypeError("Integrate function expects the base to be a valid plain.");
-    }
-
-    return $zuord.pick.plain($obj, $content, zuordCore.mode.resolve([zuordModeX.pick.plain, $mode]));
+const $ = ($obj: ZuordType.Plain, $pattern: ZuordUtil.Pattern.Plain<ZuordType.Plain>, $mode: Partial<ZuordModeX.Pick.Plain>) => {
+    return $zuord.pick.plain($obj, $pattern, zuordCore.mode.resolve([zuordModeX.pick.plain, $mode]));
 }
 
 
