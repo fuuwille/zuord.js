849c47abfde7d3bb6d8cb6c6be4cd43e33633d8d
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:31:09 +0300

    feat: import Pick from pick module in ZuordModeX namespace

--- library/src/extended/mode/index.types.ts
+++ library/src/extended/mode/index.types.ts
@@ -1,9 +1,11 @@
 import { Integrate as $Integrate } from "./integrate";
 import { Merge as $Merge } from "./merge";
 import { Evolve as $Evolve } from "./evolve";
+import { Pick as $Pick } from "./pick";
 
 export declare namespace ZuordModeX {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
     export import Evolve = $Evolve;
+    export import Pick = $Pick;
 }
