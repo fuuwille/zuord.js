90792c0240979d7e6f1d3187d23d4eae4d3cf829
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:52:57 +0300

    feat: add Omit import to ZuordModeX namespace in index.types.ts

--- library/src/extended/mode/index.types.ts
+++ library/src/extended/mode/index.types.ts
@@ -2,10 +2,12 @@ import { Integrate as $Integrate } from "./integrate";
 import { Merge as $Merge } from "./merge";
 import { Evolve as $Evolve } from "./evolve";
 import { Pick as $Pick } from "./pick";
+import { Omit as $Omit } from "./omit";
 
 export declare namespace ZuordModeX {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
     export import Evolve = $Evolve;
     export import Pick = $Pick;
+    export import Omit = $Omit;
 }
