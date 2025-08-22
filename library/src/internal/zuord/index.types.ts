cb85f555de67dae8118547fead32efa0d4924f05
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:19:47 +0300

    feat: add Pick type import to $Zuord namespace

--- library/src/internal/zuord/index.types.ts
+++ library/src/internal/zuord/index.types.ts
@@ -1,9 +1,11 @@
 import { Integrate as $Integrate } from "./integrate.types";
 import { Merge as $Merge } from "./merge.types";
 import { Evolve as $Evolve } from "./evolve.types";
+import { Pick as $Pick } from "./pick.types";
 
 export namespace $Zuord {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
     export import Evolve = $Evolve;
+    export import Pick = $Pick;
 }
