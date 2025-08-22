327f1d77b44eb68096078a7ae820a0159b7e6051
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:42:59 +0300

    feat: add pick type to zuordXAPI in index.d.ts

--- library/src/extended/zuord/index.d.ts
+++ library/src/extended/zuord/index.d.ts
@@ -1,11 +1,13 @@
 import { integrate as $integrate } from "./index.runtime";
 import { merge as $merge } from "./index.runtime";
 import { evolve as $evolve } from "./index.runtime";
+import { pick as $pick } from "./index.runtime";
 
 type zuordXAPI = {
     integrate: typeof $integrate;
     merge: typeof $merge;
     evolve: typeof $evolve;
+    pick: typeof $pick;
 }
 
 export declare const zuordX: zuordXAPI;
