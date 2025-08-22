e266f1da1e0651ce83feb950e9d708e35c545d7e
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:38:28 +0300

    feat: update imports and enhance loose function signatures for better type support

--- library/src/extended/zuord/pick/plain.runtime.ts
+++ library/src/extended/zuord/pick/plain.runtime.ts
@@ -2,7 +2,7 @@ import { $zuord } from "../../../internal";
 import { zuordModeX } from "../../mode";
 import { zuordPlain } from "@zuord/type";
 import { zuordCore } from "@zuord/core";
-import type { ZuordModeX } from "../../";
+import type { ZuordX, ZuordModeX } from "../../";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
@@ -13,3 +13,15 @@ const $ = ($obj: ZuordType.Plain, $content: ZuordUtil.Pattern.Plain<ZuordType.Pl
 
     return $zuord.pick.plain($obj, $content, zuordCore.mode.resolve([zuordModeX.pick.plain, $mode]));
 }
+
+
+// LOOSE
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>> (base: TTarget, pattern: TPattern)
+    : ZuordX.Pick.Plain.Loose<TTarget, TPattern>;
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Pick.Plain>> (base: TTarget, pattern: TPattern, mode: TMode)
+    : ZuordX.Pick.Plain.Loose<TTarget, TPattern, TMode>;
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Pick.Plain>> (base: TTarget, pattern: TPattern, mode: TMode = {} as TMode)
+    : ZuordX.Pick.Plain.Loose<TTarget, TPattern, TMode> { return $(base, pattern, mode); }
