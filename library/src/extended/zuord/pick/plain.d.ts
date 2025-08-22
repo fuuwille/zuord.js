007704123686d899e5c8f5574ae07d272dad7b99
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 17:41:26 +0300

    feat: define plainAPI type and export plain constant from plain.d.ts

--- library/src/extended/zuord/pick/plain.d.ts
+++ library/src/extended/zuord/pick/plain.d.ts
@@ -1,1 +1,9 @@
+import { loose as $loose } from "./plain.runtime";
+
+type plainAPI = {
+    loose: typeof $loose;
+}
+
+export declare const plain: plainAPI;
+
 export { Plain } from "./plain.types";
