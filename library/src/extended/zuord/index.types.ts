f39121cebf6f67f1ac95f29701a9b265dec33b7a
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 18:06:08 +0300

    feat: export Omit type in ZuordX namespace

--- library/src/extended/zuord/index.types.ts
+++ library/src/extended/zuord/index.types.ts
@@ -2,10 +2,13 @@ import { Integrate as $Integrate } from "./integrate";
 import { Merge as $Merge } from "./merge";
 import { Evolve as $Evolve } from "./evolve";
 import { Pick as $Pick } from "./pick";
+import { Omit as $Omit } from "./omit";
+
 
 export namespace ZuordX {
     export import Integrate = $Integrate;
     export import Merge = $Merge;
     export import Evolve = $Evolve;
     export import Pick = $Pick;
+    export import Omit = $Omit;
 }
