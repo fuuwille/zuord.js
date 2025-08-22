a8343e4329d79a63c185b5468a56b038cec1fc6d
k4yr2 <kyrzkaya@gmail.com>
2025-08-22 18:02:05 +0300

    feat: enhance loose function signatures for improved type handling

--- library/src/extended/zuord/omit/plain.runtime.ts
+++ library/src/extended/zuord/omit/plain.runtime.ts
@@ -1,10 +1,21 @@
 import { $zuord } from "../../../internal";
 import { zuordModeX } from "../../mode";
 import { zuordCore } from "@zuord/core";
-import type { ZuordModeX } from "../../";
+import type { ZuordModeX, ZuordX } from "../../";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
 const $ = ($target: ZuordType.Plain, $pattern: ZuordUtil.Pattern.Plain<ZuordType.Plain>, $mode: Partial<ZuordModeX.Omit.Plain>) => {
     return $zuord.omit.plain($target, $pattern, zuordCore.mode.resolve([zuordModeX.omit.plain, $mode]));
 }
+
+// LOOSE
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>> (base: TTarget, pattern: TPattern)
+    : ZuordX.Omit.Plain.Loose<TTarget, TPattern>;
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Omit.Plain>> (base: TTarget, pattern: TPattern, mode: TMode)
+    : ZuordX.Omit.Plain.Loose<TTarget, TPattern, TMode>;
+
+export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Omit.Plain>> (base: TTarget, pattern: TPattern, mode: TMode = {} as TMode)
+    : ZuordX.Omit.Plain.Loose<TTarget, TPattern, TMode> { return $(base, pattern, mode); }
