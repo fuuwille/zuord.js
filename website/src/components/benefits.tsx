b62f70458056cc099c5e0a24ea5157b471a050c9
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 05:33:05 +0300

    feat: refactor Benefits component to separate card distribution logic

--- website/src/components/benefits.tsx
+++ website/src/components/benefits.tsx
@@ -1,13 +1,18 @@
 import style from '../css/modules/benefits.module.scss';
 import BenefitCard, { BenefitCardProps } from './benefitCard';
 
-const Benefits: React.FC<BenefitsProps> = ({ cards }) => (
-    <div className={style['benefits']}>
-        {cards.map((card, index) => (
-            <BenefitCard key={index} {...card} />
-        ))}
-    </div>
-);
+const Benefits: React.FC<BenefitsProps> = ({ cards }) => { 
+    const leftCards = []; const rightCards = [];
+    cards.forEach((card, index) => (index % 2 ? leftCards : rightCards).push(card));
+
+    return (
+        <div className={style['benefits']}>
+            {cards.map((card, index) => (
+                <BenefitCard key={index} {...card} />
+            ))}
+        </div>
+    );
+};
 
 export type BenefitsProps = {
     cards: BenefitCardProps[];
