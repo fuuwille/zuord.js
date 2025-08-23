61443d89fbef1422e1598da135ccf54c2dc5b179
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 08:01:05 +0300

    feat: add new benefit card for zero runtime overhead

--- website/src/data/benefits.ts
+++ website/src/data/benefits.ts
@@ -2,5 +2,9 @@ export const benefitCards = [
     {
         title: "Runtime & Type Harmony",
         description: "Zuord provides a fully synchronized API for both runtime behavior and compile-time types.",
+    },
+    {
+        title: "Zero runtime overhead",
+        description: "Zuord aims to provide a compiler experience that perfectly aligns with runtime behavior, without any runtime overhead."
     }
 ]
