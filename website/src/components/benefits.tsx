9b057c59f242766cfe465e2013b4683769e99d8a
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 05:15:58 +0300

    feat: create Benefits component with initial structure

---
+++ website/src/components/benefits.tsx
@@ -0,0 +1,9 @@
+import style from '../css/modules/benefits.module.scss';
+
+const Benefits: React.FC = () => (
+    <div className={style['benefits']}>
+        Benefits
+    </div>
+);
+
+export default Benefits;
