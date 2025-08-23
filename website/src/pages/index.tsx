154a0270467eed490b1529e8c1b273a223800c8c
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 03:25:11 +0300

    feat: add initial Home page layout in index.tsx

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+
+export default function Home() {
+  return (
+    <Layout title="Zuord">
+      H
+    </Layout>
+  );
+}
