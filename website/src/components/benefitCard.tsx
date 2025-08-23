1e09e0be29c94b75ac91896a8ede09aac703f2e2
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 03:30:12 +0300

    feat: add BenefitCard component for displaying benefits

---
+++ website/src/components/benefitCard.tsx
@@ -0,0 +1,8 @@
+export const BenefitCard = () => {
+    return (
+        <div className="benefit-card">
+            <div className="title">Benefit Title</div>
+            <div className="description">Benefit description goes here.</div>
+        </div>
+    );
+};
