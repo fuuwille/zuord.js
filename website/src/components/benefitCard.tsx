6f65864421032766e9614038a0c5bda0c0050713
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 03:33:42 +0300

    feat: refactor BenefitCard component to accept props for title and description

--- website/src/components/benefitCard.tsx
+++ website/src/components/benefitCard.tsx
@@ -1,8 +1,14 @@
-export const BenefitCard = () => {
-    return (
-        <div className="benefit-card">
-            <div className="title">Benefit Title</div>
-            <div className="description">Benefit description goes here.</div>
-        </div>
-    );
+export const BenefitCard: React.FC<BenefitCardProps> = ({
+  title = "Benefit Title",
+  description = "Benefit description goes here.",
+})  => (
+    <div className="benefit-card">
+        <div className="title">{title}</div>
+        <div className="description">{description}</div>
+    </div>
+);
+
+export type BenefitCardProps = {
+  title?: string;
+  description?: string;
 };
