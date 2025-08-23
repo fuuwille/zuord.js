b45852fd0e2b3c1c4c0fd26b9518160d1de0332c
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 03:50:49 +0300

    fix: add description prop to BenefitCard component in Home page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,7 +5,9 @@ import BenefitCard from '../components/benefitCard';
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <BenefitCard title={"Runtime & Type Harmony"}/>
+      <BenefitCard 
+        title="Runtime & Type Harmony"
+        description="Zuord provides a fully synchronized API for both runtime behavior and compile-time types." />
     </Layout>
   );
 }
