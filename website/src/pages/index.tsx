0e517b608c0fa467d2a3757530aede7428bc4da8
k4yr2 <kyrzkaya@gmail.com>
2025-08-23 03:42:19 +0300

    fix: update title prop in BenefitCard component on Home page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,7 +5,7 @@ import BenefitCard from '../components/benefitCard';
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <BenefitCard title={"Harmony Between Runtime and Type"}/>
+      <BenefitCard title={"Runtime & Type Harmony"}/>
     </Layout>
   );
 }
