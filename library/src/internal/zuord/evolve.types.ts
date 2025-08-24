e31d6ef04a7d5d229d3f6e9aedb49a144a522506
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:56:54 +0300

    refactor: streamline Evolve namespace by exporting types directly for improved clarity

--- library/src/internal/zuord/evolve.types.ts
+++ library/src/internal/zuord/evolve.types.ts
@@ -3,14 +3,11 @@ import { ZuordType } from "@zuord/type";
 import { ZuordCore as Core } from "@zuord/core";
 import { ZuordUtil } from "@zuord/util";
 
+export type Plain<TBase, TContent, TMode extends Core.Mode.Flags> = (
+    [ResolvePlain<TBase, TContent, TMode>] extends [infer TResolved extends ZuordType.Plain] ? (
+        ZuordUtil.Unify.Hybrid<TResolved, TMode>
+    ) : never
+)
 
-export declare namespace Evolve {
-    export type Plain<TBase, TContent, TMode extends Core.Mode.Flags> = (
-        [Evolve.ResolvePlain<TBase, TContent, TMode>] extends [infer TResolved extends ZuordType.Plain] ? (
-            ZuordUtil.Unify.Hybrid<TResolved, TMode>
-        ) : never
-    )
-
-    export type ResolvePlain<TBase, TContent, TMode extends Core.Mode.Flags> 
-        = $Zuord.Integrate.ResolvePlain<TBase, $Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
-}
+export type ResolvePlain<TBase, TContent, TMode extends Core.Mode.Flags> 
+    = $Zuord.Integrate.ResolvePlain<TBase, $Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
