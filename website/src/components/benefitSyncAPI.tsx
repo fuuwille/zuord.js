a99eeb4db30d291988684c38b9402027651da06d
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 05:34:48 +0300

    fix: correct className syntax for runtime and type sections in BenefitSyncAPI component

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -4,15 +4,15 @@ import clsx from 'clsx';
 const BenefitSyncAPI: React.FC = ({
 }) => (
     <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
-        <div className={style.runtime}>
-            <div className={style.import}>{"import { zuord } from 'zuord';"}</div>
-            <div className={style.navbar}>
+        <div className={style['runtime']}>
+            <div className={style['import']}>{"import { zuord } from 'zuord';"}</div>
+            <div className={style['navbar']}>
 
             </div>
         </div>
-        <div className={style.type}>
-            <div className={style.import}>{"import { Zuord } from 'zuord';"}</div>
-            <div className={style.navbar}>
+        <div className={style['type']}>
+            <div className={style['import']}>{"import { Zuord } from 'zuord';"}</div>
+            <div className={style['navbar']}>
 
             </div>
         </div>
