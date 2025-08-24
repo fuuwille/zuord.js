69296b8e3c676371f53c6f95de2555425107e158
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:33:48 +0300

    refactor: simplify One namespace by removing unnecessary declarations for improved clarity

--- packages/util/src/internal/zuord/one.types.ts
+++ packages/util/src/internal/zuord/one.types.ts
@@ -3,29 +3,27 @@ import type { ZuordType } from "@zuord/type";
 import type { ZuordCore } from "@zuord/core";
 import type { ZuordTrait } from "@zuord/trait";
 
-export declare namespace One {
-    export type ResolveAll<T, TMode extends ZuordCore.Mode.Flags> = 
-    [T, ZuordCore.Mode.Resolve<[TMode, { "$one.all": true }]>] extends [infer TPlain extends ZuordType.Plain, infer THybridMode extends ZuordCore.Mode.Flags] ? (
-        (ResolveRequired<TPlain, THybridMode> & ResolveOptional<TPlain, THybridMode>) extends infer TInfer ? ({
-            [K in keyof TInfer]: One.ResolveAll<TInfer[K], TMode>;
-        }) : never
-    ) : T;
+export type ResolveAll<T, TMode extends ZuordCore.Mode.Flags> = 
+[T, ZuordCore.Mode.Resolve<[TMode, { "$one.all": true }]>] extends [infer TPlain extends ZuordType.Plain, infer THybridMode extends ZuordCore.Mode.Flags] ? (
+    (ResolveRequired<TPlain, THybridMode> & ResolveOptional<TPlain, THybridMode>) extends infer TInfer ? ({
+        [K in keyof TInfer]: ResolveAll<TInfer[K], TMode>;
+    }) : never
+) : T;
 
-    export type ResolveRequired<T, TMode extends ZuordCore.Mode.Flags> = [ZuordTrait.Is.Base<T, ZuordType.Plain>] extends [true] ? {
-        [K in $ZuordUtil.Keys.Required<T>]: (
-            [TMode ]extends [{ "$one.all": true }] ? (
-                One.ResolveAll<T[K], TMode>
-            ) : One.ResolveRequired<T[K], TMode>
-        )
-    } : T;
+export type ResolveRequired<T, TMode extends ZuordCore.Mode.Flags> = [ZuordTrait.Is.Base<T, ZuordType.Plain>] extends [true] ? {
+    [K in $ZuordUtil.Keys.Required<T>]: (
+        [TMode ]extends [{ "$one.all": true }] ? (
+            ResolveAll<T[K], TMode>
+        ) : ResolveRequired<T[K], TMode>
+    )
+} : T;
 
-    export type ResolveOptional<T, TMode extends ZuordCore.Mode.Flags> = [ZuordTrait.Is.Base<T, ZuordType.Plain>] extends [true] ? {
-        [K in $ZuordUtil.Keys.Optional<T>]?: T extends any ? (
-            K extends keyof T ? (
-                [TMode] extends [{ "$one.all": true }] ? (
-                    One.ResolveAll<T[K], TMode>
-                ) : One.ResolveOptional<T[K], TMode>
-            ) : never
+export type ResolveOptional<T, TMode extends ZuordCore.Mode.Flags> = [ZuordTrait.Is.Base<T, ZuordType.Plain>] extends [true] ? {
+    [K in $ZuordUtil.Keys.Optional<T>]?: T extends any ? (
+        K extends keyof T ? (
+            [TMode] extends [{ "$one.all": true }] ? (
+                ResolveAll<T[K], TMode>
+            ) : ResolveOptional<T[K], TMode>
         ) : never
-    } : T;
-}
+    ) : never
+} : T;
