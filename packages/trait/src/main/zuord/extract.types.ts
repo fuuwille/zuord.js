57eb0f3a30e2708e31bc9dc41cfec2b422a2172b
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:22:26 +0300

    refactor: simplify Extract types by removing unnecessary namespace for improved clarity

--- packages/trait/src/main/zuord/extract.types.ts
+++ packages/trait/src/main/zuord/extract.types.ts
@@ -1,11 +1,9 @@
 import { $ZuordTrait } from "../../internal";
 
-export namespace Extract {
-    export type Is<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Is<TSource, TExtract>;
+export type Is<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Is<TSource, TExtract>;
 
-    export type IsEach<TSource extends unknown, TExtracts extends unknown[]> = $ZuordTrait.Extract.IsEach<TSource, TExtracts>;
+export type IsEach<TSource extends unknown, TExtracts extends unknown[]> = $ZuordTrait.Extract.IsEach<TSource, TExtracts>;
 
-    export type Eq<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Eq<TSource, TExtract>;
+export type Eq<TSource extends unknown, TExtract extends unknown> = $ZuordTrait.Extract.Eq<TSource, TExtract>;
 
-    export type EqEach<TSource extends unknown, TExtracts extends unknown[]> = $ZuordTrait.Extract.EqEach<TSource, TExtracts>;
-}
+export type EqEach<TSource extends unknown, TExtracts extends unknown[]> = $ZuordTrait.Extract.EqEach<TSource, TExtracts>;
