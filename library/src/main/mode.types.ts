8556c6c5263af16cf79d5efa93c2c168d4d93db8
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 13:11:05 +0300

    refactor: simplify exports in mode.types.ts by removing namespace for improved clarity

--- library/src/main/mode.types.ts
+++ library/src/main/mode.types.ts
@@ -1,17 +1,15 @@
 import { ZuordModeX } from "../extended";
 
-export namespace ZuordMode {
-    export import Integrate = ZuordModeX.Integrate.Plain;
+export import Integrate = ZuordModeX.Integrate.Plain;
 
-    export import IntegrateArray = ZuordModeX.Integrate.Array;
+export import IntegrateArray = ZuordModeX.Integrate.Array;
 
-    export import Merge = ZuordModeX.Merge.Plain;
+export import Merge = ZuordModeX.Merge.Plain;
 
-    export import MergeArray = ZuordModeX.Merge.Array;
+export import MergeArray = ZuordModeX.Merge.Array;
 
-    export import Evolve = ZuordModeX.Evolve.Plain;
+export import Evolve = ZuordModeX.Evolve.Plain;
 
-    export import Pick = ZuordModeX.Pick.Plain;
+export import Pick = ZuordModeX.Pick.Plain;
 
-    export import Omit = ZuordModeX.Omit.Plain;
-}
+export import Omit = ZuordModeX.Omit.Plain;
