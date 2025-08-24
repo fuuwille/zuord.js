fa87a6c7a1147781acf893bfc7f1e07054a5ae90
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:35:22 +0300

    refactor: simplify Restrict namespace by removing unnecessary declarations for improved clarity

--- packages/util/src/internal/zuord/restrict.types.ts
+++ packages/util/src/internal/zuord/restrict.types.ts
@@ -1,23 +1,21 @@
 import type { ZuordType } from "@zuord/type";
 
-export declare namespace Restrict {
-    export type ResolveKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
-        (ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>) extends infer T ? {
-            [K in keyof T as [undefined] extends [T[K]] ? never : K]: T[K] extends never ? never : T[K];
-        } : never
-    ) : TInput;
+export type ResolveKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
+    (ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>) extends infer T ? {
+        [K in keyof T as [undefined] extends [T[K]] ? never : K]: T[K] extends never ? never : T[K];
+    } : never
+) : TInput;
 
-    export type ResolveIncludedKeys<TBase extends ZuordType.Plain, TInput> = {
-        [K in keyof TInput]: K extends keyof TBase
-            ? ResolveKeys<TBase[K], TInput[K]>
-            : never
-    } 
+export type ResolveIncludedKeys<TBase extends ZuordType.Plain, TInput> = {
+    [K in keyof TInput]: K extends keyof TBase
+        ? ResolveKeys<TBase[K], TInput[K]>
+        : never
+} 
 
-    export type ResolveExcludedKeys<TBase extends ZuordType.Plain, TInput> = {
-        [K in Exclude<keyof TBase, keyof TInput>]?: never
-    };
+export type ResolveExcludedKeys<TBase extends ZuordType.Plain, TInput> = {
+    [K in Exclude<keyof TBase, keyof TInput>]?: never
+};
 
-    export type ResolveKeysBatch<TBase, TInputs extends readonly unknown[]> = {
-        [I in keyof TInputs]: ResolveKeys<TBase, TInputs[I]>;
-    };
-}
+export type ResolveKeysBatch<TBase, TInputs extends readonly unknown[]> = {
+    [I in keyof TInputs]: ResolveKeys<TBase, TInputs[I]>;
+};
