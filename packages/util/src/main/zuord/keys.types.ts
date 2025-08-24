1752ce473a58991cff1c6cf115e4b8aaccb99eb1
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:41:33 +0300

    refactor: simplify Keys namespace by exporting types directly for improved clarity

--- packages/util/src/main/zuord/keys.types.ts
+++ packages/util/src/main/zuord/keys.types.ts
@@ -1,9 +1,7 @@
 import type { $ZuordUtil } from "../../internal";
 
-export declare namespace Keys {
-    export type All<T> = $ZuordUtil.Keys.All<T>;
+export type All<T> = $ZuordUtil.Keys.All<T>;
 
-    export type Required<T> = $ZuordUtil.Keys.Required<T>;
+export type Required<T> = $ZuordUtil.Keys.Required<T>;
 
-    export type Optional<T> = $ZuordUtil.Keys.Optional<T>;
-}
+export type Optional<T> = $ZuordUtil.Keys.Optional<T>;
