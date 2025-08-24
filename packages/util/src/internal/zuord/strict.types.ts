87b09ba80b503d5aec5efd4637b4d519e5ceee51
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:35:44 +0300

    refactor: simplify Strict namespace by removing unnecessary declarations for improved clarity

--- packages/util/src/internal/zuord/strict.types.ts
+++ packages/util/src/internal/zuord/strict.types.ts
@@ -1,27 +1,25 @@
 import type { ZuordType } from "@zuord/type";
 
-export declare namespace Strict {
-  export type ResolveKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
-    ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>
-  ) : TInput;
+export type ResolveKeys<TBase, TInput> = TBase extends ZuordType.Plain ? (
+  ResolveIncludedKeys<TBase, TInput> & ResolveExcludedKeys<TBase, TInput>
+) : TInput;
 
-  export type ResolveIncludedKeys<TBase, TInput> = { 
-    [K in keyof TInput & keyof TBase]: ResolveKeys<TBase[K], TInput[K]>
-  }
-
-  export type ResolveExcludedKeys<TBase, TInput> = (
-    ResolveExcludedBaseKeys<TBase, TInput> & ResolveExcludedInputKeys<TBase, TInput>
-  )
-
-  export type ResolveExcludedBaseKeys<TBase, TInput> = { 
-    [K in Exclude<keyof TBase, keyof TInput>]: never
-  };
-
-  export type ResolveExcludedInputKeys<TBase, TInput> = { 
-    [K in Exclude<keyof TInput, keyof TBase>]: never
-  };
-
-  export type ResolveKeysBatch<TBase, TInputs extends readonly unknown[]> = {
-    [I in keyof TInputs]: ResolveKeys<TBase, TInputs[I]>;
-  };
+export type ResolveIncludedKeys<TBase, TInput> = { 
+  [K in keyof TInput & keyof TBase]: ResolveKeys<TBase[K], TInput[K]>
 }
+
+export type ResolveExcludedKeys<TBase, TInput> = (
+  ResolveExcludedBaseKeys<TBase, TInput> & ResolveExcludedInputKeys<TBase, TInput>
+)
+
+export type ResolveExcludedBaseKeys<TBase, TInput> = { 
+  [K in Exclude<keyof TBase, keyof TInput>]: never
+};
+
+export type ResolveExcludedInputKeys<TBase, TInput> = { 
+  [K in Exclude<keyof TInput, keyof TBase>]: never
+};
+
+export type ResolveKeysBatch<TBase, TInputs extends readonly unknown[]> = {
+  [I in keyof TInputs]: ResolveKeys<TBase, TInputs[I]>;
+};
