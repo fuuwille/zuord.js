d6f0d9c1b87b8f3e4aab07c15d4d66c5362202d8
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 10:55:46 +0300

    refactor: streamline flags type exports by removing unnecessary namespace

--- packages/core/src/main/zuord/flags.types.ts
+++ packages/core/src/main/zuord/flags.types.ts
@@ -1,11 +1,9 @@
 import type { $ZuordCore } from "../../internal";
 
-export declare namespace Flags {
-    export type Shallow = $ZuordCore.Flags.Shallow;
+export type Shallow = $ZuordCore.Flags.Shallow;
 
-    export type Concat = $ZuordCore.Flags.Concat;
+export type Concat = $ZuordCore.Flags.Concat;
 
-    export type Unique = $ZuordCore.Flags.Unique;
+export type Unique = $ZuordCore.Flags.Unique;
 
-    export type Base = $ZuordCore.Flags.Base;
-}
+export type Base = $ZuordCore.Flags.Base;
