027630bba5e70d6e0d8b5b74dbca3bb7a8a94f98
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:36:05 +0300

    refactor: simplify Tuple namespace by removing unnecessary declarations for improved clarity

--- packages/util/src/internal/zuord/tuple.types.ts
+++ packages/util/src/internal/zuord/tuple.types.ts
@@ -1,15 +1,13 @@
-export declare namespace Tuple {
-    export type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
+export type Keys<T> = T extends any ? Exclude<keyof T, keyof any[]> : never;
 
-    export type Unify<T> = ResolveUnify<ExtractUnify<T>>;
+export type Unify<T> = ResolveUnify<ExtractUnify<T>>;
 
-    export type ResolveUnify<T, Acc = []> = Acc extends any[] ? (
-        `${Acc['length']}` extends infer K ? (
-            K extends keyof T ? Tuple.ResolveUnify<T, [...Acc, T[K]]> : Acc
-        ) : never
-    ) : never;
+export type ResolveUnify<T, Acc = []> = Acc extends any[] ? (
+    `${Acc['length']}` extends infer K ? (
+        K extends keyof T ? ResolveUnify<T, [...Acc, T[K]]> : Acc
+    ) : never
+) : never;
 
-    export type ExtractUnify<T> = {
-        [K in Tuple.Keys<T>]: T extends any ? (K extends keyof T ? T[K] : never) : never;
-    };
-}
+export type ExtractUnify<T> = {
+    [K in Keys<T>]: T extends any ? (K extends keyof T ? T[K] : never) : never;
+};
