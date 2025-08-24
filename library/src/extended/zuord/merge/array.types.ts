5ddc9201e49c30da4bfb24840286e98685ce9661
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 12:42:14 +0300

    refactor: simplify Array types by removing namespace and directly exporting Loose type for improved clarity

--- library/src/extended/zuord/merge/array.types.ts
+++ library/src/extended/zuord/merge/array.types.ts
@@ -2,7 +2,5 @@ import { $Zuord } from "../../../internal";
 import { ZuordModeX } from "../../";
 import { ZuordType as Type } from "@zuord/type";
 
-export namespace Array {
-    export type Loose<TContent extends Type.Array<Type.Array>, TMode extends Partial<ZuordModeX.Merge.Array> = {}> 
-        = $Zuord.Merge.Array<TContent, TMode>;
-}
+export type Loose<TContent extends Type.Array<Type.Array>, TMode extends Partial<ZuordModeX.Merge.Array> = {}> 
+    = $Zuord.Merge.Array<TContent, TMode>;
