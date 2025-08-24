3a5ee23081e84a7e67c76293e84f4a62dcc04fc6
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 12:47:24 +0300

    refactor: remove namespace from Loose type export for improved clarity

--- library/src/extended/zuord/pick/plain.types.ts
+++ library/src/extended/zuord/pick/plain.types.ts
@@ -3,6 +3,4 @@ import { ZuordModeX } from "../../mode";
 import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
 
-export declare namespace Plain {
-    export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>, _TMode extends Partial<ZuordModeX.Pick.Plain> = {}> = $Zuord.Pick.Plain<T, P>;
-}
+export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>, _TMode extends Partial<ZuordModeX.Pick.Plain> = {}> = $Zuord.Pick.Plain<T, P>;
