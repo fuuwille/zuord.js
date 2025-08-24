42dad2dd73c4cb16a1155a7a4850d15032fae181
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:42:38 +0300

    refactor: simplify Only namespace by exporting types directly for improved clarity

--- packages/util/src/main/zuord/only.types.ts
+++ packages/util/src/main/zuord/only.types.ts
@@ -2,6 +2,4 @@ import type { $ZuordUtil } from "../../internal";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordCore } from "@zuord/core";
 
-export declare namespace Only {
-    export type Required<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.Only.ResolveRequired<T, TMode>;
-}
+export type Required<T extends ZuordType.Plain, TMode extends ZuordCore.Mode.Flags> = $ZuordUtil.Only.ResolveRequired<T, TMode>;
