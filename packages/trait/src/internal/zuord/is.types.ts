2ad8b3771f0870ca2117ac6b1b587b0c381a4541
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:19:23 +0300

    refactor: remove unnecessary namespace from Is types for improved clarity

--- packages/trait/src/internal/zuord/is.types.ts
+++ packages/trait/src/internal/zuord/is.types.ts
@@ -1,39 +1,37 @@
 import { ZuordArray } from "@zuord/type";
 
-export declare namespace Is {
-    export type Base<TSource, TBase> = [TSource] extends [TBase] ? (
-        [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
-    ) : false;
+export type Base<TSource, TBase> = [TSource] extends [TBase] ? (
+    [TSource] extends [never] ? ([TBase] extends [never] ? true : false) : true
+) : false;
 
-    export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Base<TSource, TBase>] extends [true] ? true : Is.Any<TSourceRest, TBase>
-    ) : false;
+export type Any<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Base<TSource, TBase>] extends [true] ? true : Any<TSourceRest, TBase>
+) : false;
 
-    export type Every<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Base<TSource, TBase>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : Is.Every<TSourceRest, TBase>) : false
-    ) : false;
+export type Every<TSources, TBase> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Base<TSource, TBase>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : Every<TSourceRest, TBase>) : false
+) : false;
 
-    export type Some<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
-        [Is.Base<TSource, TBase>] extends [true] ? true : Is.Some<TSource, TBaseRest>
-    ) : false;
+export type Some<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
+    [Base<TSource, TBase>] extends [true] ? true : Some<TSource, TBaseRest>
+) : false;
 
-    export type Each<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
-        [Is.Base<TSource, TBase>] extends [true] ? (TBaseRest extends ZuordArray.Empty ? true : Is.Each<TSource, TBaseRest>) : false
-    ) : false;
+export type Each<TSource, TBases> = TBases extends [infer TBase, ...infer TBaseRest] ? (
+    [Base<TSource, TBase>] extends [true] ? (TBaseRest extends ZuordArray.Empty ? true : Each<TSource, TBaseRest>) : false
+) : false;
 
-    export type AnySome<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Some<TSource, TBases>] extends [true] ? true : Is.AnySome<TSourceRest, TBases>
-    ) : false;
+export type AnySome<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Some<TSource, TBases>] extends [true] ? true : AnySome<TSourceRest, TBases>
+) : false;
 
-    export type AnyEach<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Each<TSource, TBases>] extends [true] ? true : Is.AnyEach<TSourceRest, TBases>
-    ) : false;
+export type AnyEach<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Each<TSource, TBases>] extends [true] ? true : AnyEach<TSourceRest, TBases>
+) : false;
 
-    export type EverySome<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Some<TSource, TBases>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : Is.EverySome<TSourceRest, TBases>) : false
-    ) : false;
+export type EverySome<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Some<TSource, TBases>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : EverySome<TSourceRest, TBases>) : false
+) : false;
 
-    export type EveryEach<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
-        [Is.Each<TSource, TBases>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : Is.EveryEach<TSourceRest, TBases>) : false
-    ) : false;
-}
+export type EveryEach<TSources, TBases> = TSources extends [infer TSource, ...infer TSourceRest] ? (
+    [Each<TSource, TBases>] extends [true] ? (TSourceRest extends ZuordArray.Empty ? true : EveryEach<TSourceRest, TBases>) : false
+) : false;
