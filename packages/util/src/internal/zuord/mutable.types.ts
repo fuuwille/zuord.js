73733788c5e8925413d3bd7b9e9d32acba0b961e
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:33:23 +0300

    refactor: simplify Mutable types by removing unnecessary namespace for improved clarity

--- packages/util/src/internal/zuord/mutable.types.ts
+++ packages/util/src/internal/zuord/mutable.types.ts
@@ -1,20 +1,18 @@
 import type { ZuordType } from "@zuord/type";
  
-export declare namespace Mutable {
-    export type Resolve<T> = (
-        T extends ZuordType.Plain | ZuordType.Tuple ? (
-            StructResolve<T>
-        ) :
-        T extends ZuordType.Array ? ( 
-            ArrayResolve<T>
-        ) : T
-    )
+export type Resolve<T> = (
+    T extends ZuordType.Plain | ZuordType.Tuple ? (
+        StructResolve<T>
+    ) :
+    T extends ZuordType.Array ? ( 
+        ArrayResolve<T>
+    ) : T
+)
 
-    export type StructResolve<T> = ({ 
-        -readonly [K in keyof T]: Mutable.Resolve<T[K]> 
-    }) extends infer V ? V : never;
+export type StructResolve<T> = ({ 
+    -readonly [K in keyof T]: Resolve<T[K]> 
+}) extends infer V ? V : never;
 
-    export type ArrayResolve<T> = (
-        T extends ZuordType.Array<infer U> ?  Mutable.Resolve<U>[] : never
-    );
-}
+export type ArrayResolve<T> = (
+    T extends ZuordType.Array<infer U> ?  Resolve<U>[] : never
+);
