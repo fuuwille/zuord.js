047faf7179431a65127244d59bcb808c5cf44e05
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 10:56:37 +0300

    refactor: simplify type imports and remove unnecessary namespace in index.types.ts

--- packages/core/src/main/zuord/index.types.ts
+++ packages/core/src/main/zuord/index.types.ts
@@ -1,7 +1,2 @@
-import { Mode as $Mode } from "./mode.types";
-import { Flags as $Flags } from "./flags.types";
-
-export declare namespace ZuordCore {
-    export import Mode = $Mode;
-    export import Flags = $Flags;
-}
+import { Mode } from "./mode";
+import { Flags } from "./flags";
