f5281913a5aa8e480b3234224954ee6e4935610c
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 12:45:49 +0300

    refactor: simplify Plain types by removing namespace and directly exporting Loose type for improved clarity

--- library/src/extended/zuord/omit/plain.types.ts
+++ library/src/extended/zuord/omit/plain.types.ts
@@ -3,6 +3,4 @@ import { ZuordModeX } from "../../mode";
 import { ZuordType } from "@zuord/type";
 import { ZuordUtil } from "@zuord/util";
 
-export declare namespace Plain {
-    export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>, _TMode extends Partial<ZuordModeX.Omit.Plain> = {}> = $Zuord.Omit.Plain<T, P>;
-}
+export type Loose<T extends ZuordType.Plain, P extends ZuordUtil.Pattern.Plain<T>, _TMode extends Partial<ZuordModeX.Omit.Plain> = {}> = $Zuord.Omit.Plain<T, P>;
