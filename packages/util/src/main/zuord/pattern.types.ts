1e8266d323715bfa8448e3f4ef35442b6b703c4b
k4yr2 <kyrzkaya@gmail.com>
2025-08-24 11:42:56 +0300

    refactor: simplify Pattern namespace by exporting types directly for improved clarity

--- packages/util/src/main/zuord/pattern.types.ts
+++ packages/util/src/main/zuord/pattern.types.ts
@@ -1,6 +1,4 @@
 import type { $ZuordUtil } from "../../internal";
 import type { ZuordType } from "@zuord/type";
 
-export declare namespace Pattern {
-    export type Plain<T extends ZuordType.Plain> = $ZuordUtil.Pattern.PlainResolve<T>;
-}
+export type Plain<T extends ZuordType.Plain> = $ZuordUtil.Pattern.PlainResolve<T>;
