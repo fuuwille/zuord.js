b916b4ffef3120fc55d8e672be02bdd234553c97
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 18:39:31 +0300

    refactor: add CodeTokenProps interface for enhanced token handling

--- website/src/data/code.tsx
+++ website/src/data/code.tsx
@@ -5,6 +5,10 @@ export interface CodeTokenData {
 
 export type CodeTokenWrapper = React.FC<CodeTokenData>;
 
+export interface CodeTokenProps extends CodeTokenData {
+    Wrapper?: CodeTokenWrapper
+}
+
 export type CodeTokenPredicate = (token: CodeTokenData) => boolean;
 
 export type CodeTokenModifier = {
