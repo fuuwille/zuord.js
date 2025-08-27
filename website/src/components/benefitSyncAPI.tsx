fbffad64cf0350de09c13a226997fc8fe3b79df7
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 12:56:07 +0300

    refactor: remove unnecessary line breaks for improved readability

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -9,14 +9,12 @@ const BenefitSyncAPI: React.FC = ({
     <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
         <div className={style['runtime']}>
           <CodeBlock code={`import { zuord } from 'zuord';`} />
-
           <div className={style['navbar']}>
 
           </div>
         </div>
         <div className={style['type']}>
           <CodeBlock code={`import { Zuord } from 'zuord';`} />
-
           <div className={style['navbar']}>
 
           </div>
