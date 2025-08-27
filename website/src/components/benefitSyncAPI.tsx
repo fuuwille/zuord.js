6e28318170651a8f2fcbadfcef32aba00df40ac6
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 20:09:41 +0300

    refactor: standardize button labels in navbar for consistency

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -8,13 +8,22 @@ const BenefitSyncAPI: React.FC = () => (
     <div className={style['runtime']}>
       <CodeBlock code={`import { zuord } from 'zuord';`} modifiers={runtimeImportModifiers} />
       <div className={style['navbar']}>
-
+        <div className={style['button']}>integrate</div>
+        <div className={style['button']}>merge</div>
+        <div className={style['button']}>evolve</div>
+        <div className={style['button']}>pick</div>
+        <div className={style['button']}>omit</div>
       </div>
     </div>
+
     <div className={style['type']}>
       <CodeBlock code={`import { Zuord } from 'zuord';`} />
       <div className={style['navbar']}>
-
+        <div className={style['button']}>Integrate</div>
+        <div className={style['button']}>Merge</div>
+        <div className={style['button']}>Evolve</div>
+        <div className={style['button']}>Pick</div>
+        <div className={style['button']}>Omit</div>
       </div>
     </div>
   </div>
