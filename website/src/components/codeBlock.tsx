4467bad7ba33aded5b335cde958964ffc4d624a8
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 18:13:26 +0300

    refactor: simplify CodeBlock component structure and styling

--- website/src/components/codeBlock.tsx
+++ website/src/components/codeBlock.tsx
@@ -4,38 +4,29 @@ import "prism-themes/themes/prism-vsc-dark-plus.css";
 import { tokenText } from "@site/src/utils/tokenText";
 
 export const CodeBlock: React.FC<CodeBlockProps> = ({ code, style }) => {
-  const tokens = Prism.tokenize(code, Prism.languages.ts);
+    const tokens = Prism.tokenize(code, Prism.languages.ts);
+    const blockStyle: Partial<React.CSSProperties> = { background: "transparent", fontSize: "14px", padding: 3, margin: 3, userSelect: "none", ...style };
 
-  return (
-    <pre
-      className="code-block"
-      style={{
-        background: "transparent",
-        fontSize: "14px",
-        padding: 2,
-        margin: 2,
-        userSelect: "none",
-        ...style,
-      }}
-    >
-      {tokens.map((token, i) => {
-        const { type, text } = typeof token === "string" 
-            ? { type: "", text: token }
-            : { type: token.type, text: tokenText(token.content) };
+    return (
+        <pre className="code-block" style={blockStyle}>
+        {tokens.map((token, i) => {
+            const { type, text } = typeof token === "string" 
+                ? { type: "", text: token }
+                : { type: token.type, text: tokenText(token.content) };
 
-        return (
-          <span
-            key={i}
-            className={`token ${type}`}
-            data-token-index={i}
-            data-token-type={type}
-          >
-            {text}
-          </span>
-        );
-      })}
-    </pre>
-  );
+            return (
+            <span
+                key={i}
+                className={`token ${type}`}
+                data-token-index={i}
+                data-token-type={type}
+            >
+                {text}
+            </span>
+            );
+        })}
+        </pre>
+    );
 };
 
 
