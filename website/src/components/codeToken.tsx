cd579631b832537f3cbb80fa2acec8ee3ddb7c7e
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 18:31:42 +0300

    refactor: update CodeTokenProps to use CodeTokenWrapper and pass type prop to Wrapper

--- website/src/components/codeToken.tsx
+++ website/src/components/codeToken.tsx
@@ -1,14 +1,14 @@
 import clsx from "clsx";
-import { CodeTokenData } from "@site/src/data/code";
+import { CodeTokenData, CodeTokenWrapper } from "@site/src/data/code";
 
 export const CodeToken: React.FC<CodeTokenProps> = ({ text, type, Wrapper }) => {
     return (
         <span className={clsx(`token ${type}`)}>
-            {Wrapper ? <Wrapper text={text}/> : text}
+            {Wrapper ? <Wrapper text={text} type={type} /> : text}
         </span>
     );
 };
 
 export interface CodeTokenProps extends CodeTokenData {
-    Wrapper?: React.FC<{ text: string}>;
+    Wrapper?: CodeTokenWrapper
 }
