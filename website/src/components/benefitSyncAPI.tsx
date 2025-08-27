b44cea2b1cc05cb213da066b9733c0f2dca3870c
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 12:48:11 +0300

    refactor: restructure BenefitSyncAPI component to use CodeBlock for code display

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -1,14 +1,18 @@
 import style from '@site/src/css/modules/benefitSyncAPI.module.scss';
 import clsx from 'clsx';
+import { CodeBlock } from './codeBlock';
 
 const BenefitSyncAPI: React.FC = ({
-}) => (
+}) => { 
+  
+  return(
     <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
         <div className={style['runtime']}>
-            <div className={style['import']}>{"import { zuord } from 'zuord';"}</div>
-            <div className={style['navbar']}>
+          <CodeBlock code={`import { zuord } from 'zuord'`} />
 
-            </div>
+          <div className={style['navbar']}>
+
+          </div>
         </div>
         <div className={style['type']}>
             <div className={style['import']}>{"import { Zuord } from 'zuord';"}</div>
@@ -17,6 +21,6 @@ const BenefitSyncAPI: React.FC = ({
             </div>
         </div>
     </div>
-);
+)};
 
 export default BenefitSyncAPI;
