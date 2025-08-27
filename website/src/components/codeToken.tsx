17eca5eb1cc7c74bc03097d10fce7e29420c66e3
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 18:11:18 +0300

    refactor: enhance token handling in CodeToken component

--- website/src/components/codeToken.tsx
+++ website/src/components/codeToken.tsx
@@ -1,10 +1,14 @@
 import clsx from "clsx";
+import { tokenText } from "../utils/tokenText";
 
 export const CodeToken: React.FC<CodeTokenProps> = ({ token }) => {
-
+    const { type, text } = typeof token === "string" 
+        ? { type: "", text: token }
+        : { type: token.type, text: tokenText(token.content) };
+        
     return (
         <span className={clsx('token')}>
-            
+
         </span>
     );
 };
