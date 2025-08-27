0ba55742de3b70ebdf48764a978b8b020008c8a0
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 13:05:32 +0300

    refactor: simplify BenefitSyncAPI component structure for improved readability

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -2,24 +2,21 @@ import style from '@site/src/css/modules/benefitSyncAPI.module.scss';
 import clsx from 'clsx';
 import { CodeBlock } from './codeBlock';
 
-const BenefitSyncAPI: React.FC = ({
-}) => { 
-  
-  return(
-    <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
-        <div className={style['runtime']}>
-          <CodeBlock code={`import { zuord } from 'zuord';`} />
-          <div className={style['navbar']}>
+const BenefitSyncAPI: React.ReactNode = (  
+  <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
+    <div className={style['runtime']}>
+      <CodeBlock code={`import { zuord } from 'zuord';`} />
+      <div className={style['navbar']}>
 
-          </div>
-        </div>
-        <div className={style['type']}>
-          <CodeBlock code={`import { Zuord } from 'zuord';`} />
-          <div className={style['navbar']}>
-
-          </div>
-        </div>
+      </div>
     </div>
-)};
+    <div className={style['type']}>
+      <CodeBlock code={`import { Zuord } from 'zuord';`} />
+      <div className={style['navbar']}>
+
+      </div>
+    </div>
+  </div>
+);
 
 export default BenefitSyncAPI;
