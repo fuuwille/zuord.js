3a2f5a52222278ed58ee84fddc1cc7fe54cecbe6
k4yr2 <kyrzkaya@gmail.com>
2025-08-27 18:46:04 +0300

    refactor: add modifiers to CodeBlock for enhanced token rendering

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -1,11 +1,12 @@
 import style from '@site/src/css/modules/benefitSyncAPI.module.scss';
 import clsx from 'clsx';
 import { CodeBlock } from './codeBlock';
+import { CodeTokenModifier } from '@site/src/data/code';
 
 const BenefitSyncAPI: React.FC = () => (  
   <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
     <div className={style['runtime']}>
-      <CodeBlock code={`import { zuord } from 'zuord';`} />
+      <CodeBlock code={`import { zuord } from 'zuord';`} modifiers={runtimeImportModifiers} />
       <div className={style['navbar']}>
 
       </div>
@@ -20,3 +21,10 @@ const BenefitSyncAPI: React.FC = () => (
 );
 
 export default BenefitSyncAPI;
+
+export const runtimeImportModifiers: CodeTokenModifier[] = [
+  {
+    predicate: (data) => data.text.trim() === "zuord",
+    props: { Wrapper: ({ text }) => <span style={{ backgroundColor: 'yellow' }}>{text}</span> }
+  }
+]
