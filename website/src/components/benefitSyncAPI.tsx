4f3b557ed334dcf27515cccdbca5199e471d2019
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 00:05:08 +0300

    refactor: reorganize imports and navbar buttons for improved structure and readability

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -5,27 +5,22 @@ import { CodeTokenModifier } from '@site/src/data/code';
 
 const BenefitSyncAPI: React.FC = () => (  
   <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
-    <div className={style['runtime']}>
+    <div className={style['imports']}>
       <CodeBlock code={`import { zuord } from 'zuord';`} modifiers={runtimeImportModifiers} />
+      <CodeBlock code={`import { Zuord } from 'zuord';`} />
+    </div>
       <div className={style['navbar']}>
         <div className={style['button']}>integrate</div>
         <div className={style['button']}>merge</div>
         <div className={style['button']}>evolve</div>
         <div className={style['button']}>pick</div>
         <div className={style['button']}>omit</div>
-      </div>
-    </div>
-
-    <div className={style['type']}>
-      <CodeBlock code={`import { Zuord } from 'zuord';`} />
-      <div className={style['navbar']}>
         <div className={style['button']}>Integrate</div>
         <div className={style['button']}>Merge</div>
         <div className={style['button']}>Evolve</div>
         <div className={style['button']}>Pick</div>
         <div className={style['button']}>Omit</div>
       </div>
-    </div>
   </div>
 );
 
