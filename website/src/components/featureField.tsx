ccabc51b90cc7e37228e56dfbe8b10b4bb93698d
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 19:51:41 +0300

    feat: implement FeatureField component with tooltip functionality

---
+++ website/src/components/featureField.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Tooltip } from '@mui/material';
+import clsx from 'clsx';
+import style from '@site/src/css/modules/featureField.module.scss';
+
+const FeatureField: React.FC<{ text: string }> = ({ text }) => {
+    const [hovered, setHovered] = useState(false);
+    const [tooltip, setTooltip] = useState(false);
+    const focused = hovered || tooltip;
+
+    let type, passiveText = `.${text}`, activeText;
+
+    if(text[0] === text[0].toLowerCase()) {
+        type = 'runtime';
+        activeText = `${text}()`;
+    }
+    else {
+        type = 'type';
+        activeText = `${text}<>`;
+    }
+
+    return (
+        <Tooltip 
+            title={passiveText} 
+            onOpen={() => setTooltip(true)} 
+            onClose={() => setTooltip(false)}
+            enterDelay={200}
+            enterNextDelay={200}
+            leaveDelay={100}
+        >
+        <div 
+            className={clsx(style['box'], style[type], focused ? style['focused'] : null)}  
+            onMouseEnter={() => setHovered(true)} 
+            onMouseLeave={() => setHovered(false)}
+        >
+            <span className={style['layout']}>
+                <span className={style['text']}>{passiveText}</span>
+            </span>
+            <span className={style['visual']}>
+            <span className={clsx(style['text'], style['passive'])}>{passiveText}</span>
+            <span className={clsx(style['text'], style['active'])}>{activeText}</span>
+            </span>
+        </div>
+        </Tooltip>
+    )
+}
