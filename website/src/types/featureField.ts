7ce931988b9ba7b00a9f880b2dc7a8c742266cc4
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 20:31:07 +0300

    refactor: add FeatureFieldProps interface to define properties for FeatureField component

---
+++ website/src/types/featureField.ts
@@ -0,0 +1,9 @@
+export interface FeatureFieldProps {
+    layoutText: string;
+    passiveText: string;
+    activeText: string;
+    className?: string;
+    enterDelay?: number;
+    leaveDelay?: number;
+    enterNextDelay?: number;
+}
