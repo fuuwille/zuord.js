df3ffa6ce32bf96a6e4492d38ca54f8fb752e7ef
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 20:53:39 +0300

    feat: update FeatureMonitor component to use FeatureMonitorProps for props definition

--- website/src/components/featureMonitor.tsx
+++ website/src/components/featureMonitor.tsx
@@ -1,12 +1,14 @@
 import style from '@site/src/css/modules/featureMonitor.module.scss';
 import clsx from "clsx";
+import { FeatureMonitorProps } from '@site/src/types/featureMonitor';
 
-export const FeatureMonitor: React.FC = ({ 
+export const FeatureMonitor: React.FC<FeatureMonitorProps> = ({ 
+    title = "Feature Monitor"
 }) => {
     return (
         <div className={clsx('feature-monitor', style['feature-monitor'])}>
             <div className={clsx(style['title'])}>
-
+                {title}
             </div>
             <div className={clsx(style['content'])}>
                 <div className={clsx(style['view'])}>
