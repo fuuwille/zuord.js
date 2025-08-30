c16c0f0c0334234a9f7940c40afd0816f5ad902d
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 20:38:36 +0300

    refactor: add tooltip prop to FeatureFieldProps interface for improved usability

--- website/src/types/featureField.ts
+++ website/src/types/featureField.ts
@@ -1,8 +1,9 @@
 export interface FeatureFieldProps {
-    tooltip?: React.ReactNode;
     layoutText: string;
     passiveText: string;
     activeText: string;
+    
+    monitor?: React.ReactNode;
     className?: string;
     enterDelay?: number;
     leaveDelay?: number;
