e508beae1ead513c46d83ea250327c9a797fd1b8
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 19:55:25 +0300

    refactor: remove unused type variable from FeatureField component

--- website/src/components/featureField.tsx
+++ website/src/components/featureField.tsx
@@ -11,11 +11,9 @@ export const FeatureField: React.FC<FeatureFieldProps> = ({ text, className }) =
     let type, passiveText = `.${text}`, activeText;
 
     if(text[0] === text[0].toLowerCase()) {
-        type = 'runtime';
         activeText = `${text}()`;
     }
     else {
-        type = 'type';
         activeText = `${text}<>`;
     }
 
@@ -28,19 +26,19 @@ export const FeatureField: React.FC<FeatureFieldProps> = ({ text, className }) =
             enterNextDelay={200}
             leaveDelay={100}
         >
-        <div 
-            className={clsx(style['feature-field'], style[type], focused ? style['focused'] : null, className)}  
-            onMouseEnter={() => setHovered(true)} 
-            onMouseLeave={() => setHovered(false)}
-        >
-            <span className={style['layout']}>
-                <span className={style['text']}>{passiveText}</span>
-            </span>
-            <span className={style['visual']}>
-            <span className={clsx(style['text'], style['passive'])}>{passiveText}</span>
-            <span className={clsx(style['text'], style['active'])}>{activeText}</span>
-            </span>
-        </div>
+            <div 
+                className={clsx(style['feature-field'], focused ? style['focused'] : null, className)}  
+                onMouseEnter={() => setHovered(true)} 
+                onMouseLeave={() => setHovered(false)}
+            >
+                <span className={style['layout']}>
+                    <span className={style['text']}>{passiveText}</span>
+                </span>
+                <span className={style['visual']}>
+                    <span className={clsx(style['text'], style['passive'])}>{passiveText}</span>
+                    <span className={clsx(style['text'], style['active'])}>{activeText}</span>
+                </span>
+            </div>
         </Tooltip>
     )
 }
