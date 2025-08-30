c4bddcd86da9ef8178a94954aa511ce97b3b9358
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 10:21:28 +0300

    refactor: update BenefitSyncAPI import to named export for consistency

--- website/src/components/benefitSyncAPI.tsx
+++ website/src/components/benefitSyncAPI.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import { CodeBlock } from './codeBlock';
 import { CodeTokenModifier } from '@site/src/data/code';
 
-const BenefitSyncAPI: React.FC = () => (  
+export const BenefitSyncAPI: React.FC = () => (  
   <div className={clsx('benefit-sync-api', style['benefit-sync-api'])}>
     <div className={style['imports']}>
       <CodeBlock code={`import { zuord } from 'zuord';`} modifiers={runtimeImportModifiers} />
@@ -23,8 +23,6 @@ const BenefitSyncAPI: React.FC = () => (
       </div>
   </div>
 );
-
-export default BenefitSyncAPI;
 
 export const runtimeImportModifiers: CodeTokenModifier[] = [
   {
