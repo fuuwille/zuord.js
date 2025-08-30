672f6883fd01c0d5556a62379c03a1d877dfafc5
k4yr2 <kyrzkaya@gmail.com>
2025-08-30 20:48:23 +0300

    refactor: remove static title and description from FeatureMonitor component

--- website/src/components/featureMonitor.tsx
+++ website/src/components/featureMonitor.tsx
@@ -5,8 +5,9 @@ export const FeatureMonitor: React.FC = ({
 }) => {
     return (
         <div className={clsx('feature-monitor', style['feature-monitor'])}>
-            <h2>Feature Monitor</h2>
-            <p>Monitor your features in real-time.</p>
+            <div className={clsx(style['title'])}>
+
+            </div>
         </div>
     );
 }
