043e2bba0275cf11aab6d4e75513951d6c623017
k4yr2 <kyrzkaya@gmail.com>
2025-08-31 10:50:51 +0300

    refactor: replace createConfig function with rollupConfig for simplified configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import tsconfigPaths from "rollup-plugin-tsconfig-paths";
+import glob from 'fast-glob';
 
-export function createConfig({ input, tsconfig }) {
-    return {
-        input,
-        output: {
-            dir: 'dist',
-            format: 'esm',
-            sourcemap: true,
-            preserveModules: true,
-        },
-        external: [/node_modules/, /^@?zuord\//],
-        plugins: [
-            resolve({
-                extensions: ['.js', '.ts']
-            }),
-            tsconfigPaths({ tsConfigPath: tsconfig }),
-            typescript({ tsconfig }),
-        ],
-    };
+export const rollupConfig = {
+    input: await glob(['src/**/*.ts', '!src/**/*.types.ts', '!**/*.d.ts']),
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        sourcemap: true,
+        preserveModules: true,
+    },
+    external: [/node_modules/, /^@?zuord\//],
+    plugins: [
+        resolve({
+            extensions: ['.js', '.ts']
+        }),
+        tsconfigPaths({ tsConfigPath: './tsconfig-dist.json' }),
+        typescript({ tsconfig: './tsconfig-dist.json' }),
+    ],
 }
