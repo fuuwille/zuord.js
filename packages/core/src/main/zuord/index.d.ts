7293a86ccc25b1d5d1c693a1330e9f807a9f2b76
k4yr2 <kyrzkaya@gmail.com>
2025-08-31 06:20:09 +0300

    fix: remove file extensions from imports and exports in index.d.ts for consistency

--- packages/core/src/main/zuord/index.d.ts
+++ packages/core/src/main/zuord/index.d.ts
@@ -1,5 +1,5 @@
-import { mode as $mode } from "./index.runtime.js";
-import { flags as $flags } from "./index.runtime.js";
+import { mode as $mode } from "./index.runtime";
+import { flags as $flags } from "./index.runtime";
 
 type zuordCoreAPI = {
     mode: typeof $mode;
@@ -8,4 +8,4 @@ type zuordCoreAPI = {
 
 export declare const zuordCore: zuordCoreAPI;
 
-export * as ZuordCore from "./index.types.js";
+export * as ZuordCore from "./index.types";
