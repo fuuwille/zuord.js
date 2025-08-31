e4a7f85eded1b3d82c5917c594abebb427113d8e
k4yr2 <kyrzkaya@gmail.com>
2025-08-31 09:20:42 +0300

    fix: remove file extensions from exports in index files for consistency

--- packages/type/src/main/index.d.ts
+++ packages/type/src/main/index.d.ts
@@ -1,4 +1,8 @@
-export { zuordType, ZuordType } from "./type.js";
-export { zuordPlain, ZuordPlain } from "./plain.js";
-export { zuordArray, ZuordArray } from "./array.js";
-export { zuordTuple, ZuordTuple } from "./tuple.js";
+export { zuordType } from "./type";
+export { zuordPlain } from "./plain";
+export { zuordArray } from "./array";
+export { zuordTuple } from "./tuple";
+export type { ZuordType } from "./type";
+export type { ZuordPlain } from "./plain";
+export type { ZuordArray } from "./array";
+export type { ZuordTuple } from "./tuple";
