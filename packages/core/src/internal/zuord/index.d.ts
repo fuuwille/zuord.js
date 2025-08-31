fb308c1ad9fc7a92dda2dc9b0992eb947419cd85
k4yr2 <kyrzkaya@gmail.com>
2025-08-31 03:00:28 +0300

    fix: update import and export paths to include file extensions for consistency

--- packages/core/src/internal/zuord/index.d.ts
+++ packages/core/src/internal/zuord/index.d.ts
@@ -1,5 +1,5 @@
-import { mode as $mode } from "./index.runtime";
-import { flags as $flags } from "./index.runtime";
+import { mode as $mode } from "./index.runtime.js";
+import { flags as $flags } from "./index.runtime.js";
 
 type $zuordCoreAPI = {
     mode: typeof $mode;
@@ -8,4 +8,4 @@ type $zuordCoreAPI = {
 
 export declare const $zuordCore: $zuordCoreAPI;
 
-export * as $ZuordCore from "./index.types";
+export * as $ZuordCore from "./index.types.js";
