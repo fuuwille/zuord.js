0698d05559028d5df86e3c6e12041840b5b5af10
k4yr2 <kyrzkaya@gmail.com>
2025-08-31 09:15:54 +0300

    fix: separate value and type exports for clarity in index file

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,4 +1,8 @@
-export { zuordType, ZuordType } from "./main";
-export { zuordPlain, ZuordPlain } from "./main";
-export { zuordArray, ZuordArray } from "./main";
-export { zuordTuple, ZuordTuple } from "./main";
+export { zuordType } from "./main";
+export { zuordPlain } from "./main";
+export { zuordArray } from "./main";
+export { zuordTuple } from "./main";
+export type { ZuordType } from "./main";
+export type { ZuordPlain } from "./main";
+export type { ZuordArray } from "./main";
+export type { ZuordTuple } from "./main";
