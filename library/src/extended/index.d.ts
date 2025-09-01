1a9510aec1e0756ce06057e8d3ad2f4edc6d027b
k4yr2 <kyrzkaya@gmail.com>
2025-09-01 06:50:18 +0300

    feat: restore index.ts with exports for zuordX, zuordModeX and their types

--- library/src/extended/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { zuordX, ZuordX } from "./zuord";
-export { zuordModeX, ZuordModeX } from "./mode";
---
+++ library/src/extended/index.ts
@@ -0,0 +1,4 @@
+export { zuordX } from "./zuord";
+export { zuordModeX } from "./mode";
+export type { ZuordX } from "./zuord";
+export type { ZuordModeX } from "./mode";
