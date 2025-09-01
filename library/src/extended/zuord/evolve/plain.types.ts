94d2bcb86006886d1fcc6bb14d2bc1036a06bd1a
k4yr2 <kyrzkaya@gmail.com>
2025-09-01 06:54:37 +0300

    fix: change imports to use type imports in plain.types.ts

--- library/src/extended/zuord/evolve/plain.types.ts
+++ library/src/extended/zuord/evolve/plain.types.ts
@@ -1,7 +1,7 @@
-import { $Zuord } from "../../../internal";
-import { ZuordModeX } from "../../";
-import { ZuordType as Type } from "@zuord/type";
-import { ZuordUtil as Util } from "@zuord/util";
+import type { $Zuord } from "../../../internal";
+import type { ZuordModeX } from "../../";
+import type { ZuordType as Type } from "@zuord/type";
+import type { ZuordUtil as Util } from "@zuord/util";
 
 export type Restrict<TBase extends Type.Plain, TContent extends Util.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
     = $Zuord.Evolve.Plain<TBase, TContent, TMode>;
