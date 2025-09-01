1e08108c4fdd726994ff8c13ac375d9bfa34edd4
k4yr2 <kyrzkaya@gmail.com>
2025-09-01 06:59:43 +0300

    fix: change imports to use type imports in plain.types.ts

--- library/src/extended/zuord/merge/plain.types.ts
+++ library/src/extended/zuord/merge/plain.types.ts
@@ -1,6 +1,6 @@
-import { $Zuord } from "../../../internal";
-import { ZuordModeX } from "../../";
-import { ZuordPlain } from "@zuord/type";
+import type { $Zuord } from "../../../internal";
+import type { ZuordModeX } from "../../";
+import type { ZuordPlain } from "@zuord/type";
 
 export type Loose<TContent extends ZuordPlain.Array, TMode extends Partial<ZuordModeX.Merge.Plain> = {}> 
     = $Zuord.Merge.Plain<TContent, TMode>;
