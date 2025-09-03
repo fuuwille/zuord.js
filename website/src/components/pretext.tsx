d55af0c4bdc033a3eaa2e54a25be9db3be550952
k4yr2 <kyrzkaya@gmail.com>
2025-09-03 20:18:22 +0300

    feat: replace Prism.js with Shiki for improved syntax highlighting in Pretext component

--- website/src/components/pretext.tsx
+++ website/src/components/pretext.tsx
@@ -1,17 +1,23 @@
 import style from '@site/src/css/modules/pretext.module.scss';
-import Prism from "prismjs";
-import "prismjs/components/prism-typescript"; // TS desteği için
-import "prism-themes/themes/prism-vsc-dark-plus.css";
-import { tokenText } from "@site/src/utils/tokenText";
 import clsx from "clsx";
 import { PretextProps } from '@site/src/types/pretext';
 import { zuordX } from 'zuord';
+import { codeToTokens, createHighlighterCoreSync, createJavaScriptRegexEngine, ThemedToken } from 'shiki';
+import { useEffect, useState } from 'react';
+import ts from '@shikijs/langs/typescript'
+import darkPlus from '@shikijs/themes/dark-plus'
+
+const shiki = createHighlighterCoreSync({
+  themes: [darkPlus],
+  langs: [ts],
+  engine: createJavaScriptRegexEngine()
+})
 
 export const Pretext: React.FC<PretextProps> = ($props) => {
     const props = zuordX.integrate.plain.loose({
         text: '',
         modifiers: [],
-        language: Prism.languages.ts,
+        language: 'typescript',
         design: {
             selectable: true,
             style: {},
@@ -19,25 +25,24 @@ export const Pretext: React.FC<PretextProps> = ($props) => {
     }, $props);
 
 
-    const tokens = Prism.tokenize(props.text, props.language);
+    const { tokens } = shiki.codeToTokens(props.text, { lang: 'ts', theme: 'dark-plus' });
+
 
     return (
         <pre className={clsx('pretext', style['pretext'])} style={{ userSelect: props.design.selectable ? 'text' : 'none', ...props.design.style }}>
-            {tokens.map((token, i) => {
-                const data = typeof token === "string" 
-                    ? { content: token, type: "" }
-                    : { content: tokenText(token.content), type: token.type };
-
-                const modifier = props.modifiers.find(modifier => modifier.predicate(data))
-
-                const { type, content, Node } = { ...data, ...(modifier?.props ?? {}) };
-
-                return (
-                    <span className={clsx(`token ${type}`)}>
-                        {Node ? <Node content={content} type={type} /> : content}
-                    </span>
-                )
-            })}
+            {tokens.map((line, i) => (
+                <div key={i}>
+                    {line.map((token, j) => (
+                        <span
+                            key={j}
+                            style={{ color: token.color }}
+                            className={`token ${token.explanation?.[0]?.scopes.at(-1)?.scopeName ?? ''}`}
+                        >
+                            {token.content}
+                        </span>
+                    ))}
+                </div>
+            ))}
         </pre>
     );
 };
