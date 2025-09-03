1c4c2f2778624da0b5ea85cc2c0198b0f8664f46
k4yr2 <kyrzkaya@gmail.com>
2025-09-04 01:51:33 +0300

    feat: add lightPlus theme to highlighter for improved styling options

--- website/src/utils/pretext.ts
+++ website/src/utils/pretext.ts
@@ -1,9 +1,10 @@
-import ts from '@shikijs/langs/typescript'
-import darkPlus from '@shikijs/themes/dark-plus'
+import ts from '@shikijs/langs/typescript';
+import darkPlus from '@shikijs/themes/dark-plus';
+import lightPlus from '@shikijs/themes/light-plus';
 import { createHighlighterCoreSync, createJavaScriptRegexEngine } from 'shiki';
 
 export const highlighter = createHighlighterCoreSync({
-    themes: [darkPlus],
+    themes: [darkPlus, lightPlus],
     langs: [ts],
     engine: createJavaScriptRegexEngine()
 })
