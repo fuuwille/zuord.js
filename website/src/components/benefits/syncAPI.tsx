706717a4697ecea7bc3035c80938aedaeaf091f5
k4yr2 <kyrzkaya@gmail.com>
2025-09-03 14:00:37 +0300

    refactor: remove unused runtime import modifiers from BenefitsSyncAPI component

--- website/src/components/benefits/syncAPI.tsx
+++ website/src/components/benefits/syncAPI.tsx
@@ -1,14 +1,13 @@
 import style from '@site/src/css/modules/benefits/syncAPI.module.scss';
 import clsx from 'clsx';
 import { Pretext } from '@site/src/components/pretext';
-import { PretextTokenModifier } from '@site/src/types/pretext';
 import { Showcase } from '@site/src/components//showcase';
 import { syncAPIControl } from '@site/src/utils/benefits/syncAPI';
 
 export const BenefitsSyncAPI: React.FC = () => (  
   <div className={clsx('benefits-sync-api', style['benefits-sync-api'])}>
     <div className={style['imports']}>
-      <Pretext text={`import { zuord } from 'zuord';`} modifiers={runtimeImportModifiers} design={{ selectable: false }}/>
+      <Pretext text={`import { zuord } from 'zuord';`} design={{ selectable: false }}/>
       <Pretext text={`import { Zuord } from 'zuord';`} design={{ selectable: false }}/>
     </div>
       <Showcase container={{
@@ -20,11 +19,4 @@ export const BenefitsSyncAPI: React.FC = () => (
       }} design={{ className: style['showcase'] }}/>
   </div>
 );
-
-export const runtimeImportModifiers: PretextTokenModifier[] = [
-  {
-    predicate: (data) => data.content.trim() === "zuord",
-    props: { Node: ({ content }) => <span className={style['runtime-import-zuord']}>{content}</span> }
-  }
-]
 
