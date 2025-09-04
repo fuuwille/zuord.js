8107dfd7b3daa03af0fb831a8e6637c734e4bbf8
k4yr2 <kyrzkaya@gmail.com>
2025-09-04 08:06:15 +0300

    feat: wrap layout component with ThemeProvider for theme integration

--- website/src/theme/Layout/index.tsx
+++ website/src/theme/Layout/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '@site/src/data/theme';
 
 export default function Layout(props) {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <OriginalLayout {...props} />
-        </>
+        </ThemeProvider>
     );
 }
