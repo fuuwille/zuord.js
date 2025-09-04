9eeb7bb4aa87a388a9f77f5bf6c86c128d486784
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 01:30:38 +0300

    fix: add modifiers to Pretext components for improved token handling

--- website/src/components/benefits/syncAPI.tsx
+++ website/src/components/benefits/syncAPI.tsx
@@ -4,12 +4,13 @@ import { Pretext } from '@site/src/components/pretext';
 import { Showcase } from '@site/src/components//showcase';
 import { syncAPIControl } from '@site/src/utils/benefits/syncAPI';
 import { controlData } from '@site/src/data/benefits/syncAPI';
+import { tokenModifier } from '@site/src/utils/pretext';
 
 export const BenefitsSyncAPI: React.FC = () => (  
   <div className={clsx('benefits-sync-api', style['benefits-sync-api'])}>
     <div className={style['imports']}>
-      <Pretext text={`import { zuord } from 'zuord';`} design={{ selectable: false, preWrap: false }}/>
-      <Pretext text={`import { Zuord } from 'zuord';`} design={{ selectable: false, preWrap: false }}/>
+      <Pretext text={`import { zuord } from 'zuord';`} design={{ selectable: false, preWrap: false }} modifiers={[tokenModifier.const("zuord")]}/>
+      <Pretext text={`import { Zuord } from 'zuord';`} design={{ selectable: false, preWrap: false }} modifiers={[tokenModifier.type("Zuord")]}/>
     </div>
       <Showcase container={{
         controls: [
