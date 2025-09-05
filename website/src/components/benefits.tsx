f5a840a70cc2d9a856839aa65e62436655651640
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 18:16:59 +0300

    refactor: update animated token modifiers for improved styling

--- website/src/components/benefits.tsx
+++ website/src/components/benefits.tsx
@@ -52,8 +52,8 @@ export const BenefitsBody = {
                 modifiers={[
                     tokenModifier.const("zuord"),
                     tokenModifier.func("integrate", '(...)'),
-                    tokenModifier.animated("integrate", ["integrate", <div style={{ color: 'red' }}>anything</div>, "merge", "evolve", "pick", "omit"]),
-                    tokenModifier.animated("Integrate", ["Integrate",  <div style={{ color: 'red' }}>Anything</div>,"Merge", "Evolve", "Pick", "Omit"]),
+                    tokenModifier.animated("integrate", [<div style={{ fontStyle: "italic" }}>a function</div>, "integrate", "merge", "evolve", "pick", "omit"]),
+                    tokenModifier.animated("Integrate", [<div style={{ fontStyle: "italic" }}>It's Type</div>, "Integrate", "Merge", "Evolve", "Pick", "Omit"]),
                 ]}
             />
         </div>
