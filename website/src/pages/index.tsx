bec783e2527fe79ee4fc61a65ea25e32a7e41cb7
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 13:15:49 +0300

    refactor: adjust grid sizes for syncAPI and zeroCostRI for improved layout consistency

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,15 +7,15 @@ import { Grid } from '@mui/material';
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <Grid container margin={2} spacing={2} display={{ xs: 'none', lg: 'flex' }}>
-        <Grid size={{ lg: 8, xl: 6 }}>
+      <Grid container margin={2} spacing={2} display={{ xs: 'none', xl: 'flex' }}>
+        <Grid size={{ lg: 6, xl: 6 }}>
           <Benefits {...benefitsBody.syncAPI} />
         </Grid>
-        <Grid size={{ lg: 4, xl: 6 }}>
+        <Grid size={{ lg: 6, xl: 6 }}>
           <Benefits {...benefitsBody.zeroCostRI} />
         </Grid>
       </Grid>
-      <Grid container margin={2} spacing={2} display={{ xs: 'flex', lg: 'none' }}>
+      <Grid container margin={2} spacing={2} display={{ xs: 'flex', xl: 'none' }}>
         <Grid size={12}>
           <Benefits {...benefitsBody.syncAPI} />
         </Grid>
