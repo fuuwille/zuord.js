6091ffda5c184142aec65b732d6a269d0af4db4c
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 03:33:16 +0300

    refactor: restructure Home component layout for improved organization

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import { Benefits } from '@site/src/components/benefits';
 import { benefits } from '@site/src/data/benefits';
+import { Grid } from '@mui/material';
 
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <Benefits {...benefits.syncAPI} />
+      <Grid container>
+        <Grid size={8}>
+          <Benefits {...benefits.syncAPI} />
+        </Grid>
+        <Grid size={4}>
+          <Benefits {...benefits.zeroRuntime} />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
