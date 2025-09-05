99c1c60dc5cdd97666f90829382095d3bbc9e211
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 03:04:21 +0300

    fix: refactor benefits data structure for improved clarity and usage in components

--- website/src/data/benefits.tsx
+++ website/src/data/benefits.tsx
@@ -1,17 +1,17 @@
 import { BenefitsSyncAPI } from "@site/src/components/benefits/syncAPI";
 
-export const benefitCards = [
-    {
+export const benefits = {
+    syncAPI: {
         head: {
             title: "Runtime & Type Harmony",
             description: "Experience APIs that are in sync between runtime and type level",
         },
         content: <BenefitsSyncAPI />
     },
-    {
+    zeroRuntime: {
         head: {
             title: "Zero runtime overhead",
             description: "Zuord aims to provide a compiler experience that perfectly aligns with runtime behavior, without any runtime overhead."
         }
     }
-]
+}
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import { Benefits } from '@site/src/components/benefits';
-import { benefitCards } from '@site/src/data/benefits';
+import { benefits } from '@site/src/data/benefits';
 
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <Benefits cards={benefitCards} />
+      <Benefits {...benefits.syncAPI} />
     </Layout>
   );
 }
