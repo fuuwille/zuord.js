5ce5912b8467dee3fe99356634b0a915ef1d4ea0
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 12:48:47 +0300

    refactor: update title for syncAPI to improve clarity

--- website/src/data/benefits.tsx
+++ website/src/data/benefits.tsx
@@ -3,7 +3,7 @@ import { BenefitsBody } from "@site/src/components/benefits";
 export const benefitsBody = {
     syncAPI: {
         head: {
-            title: "Runtime & Type Harmony",
+            title: "Sync Runtime & Type APIs",
             description: "Experience APIs that are in sync between runtime and type level",
         },
         content: <BenefitsBody.SyncAPI />
