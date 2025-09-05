a4cfb0af985c4e7ac998cfefdec255264be8efde
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 22:35:55 +0300

    refactor: adjust Grid structure for better readability and maintainability

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,12 +7,14 @@ import { Grid } from '@mui/material';
 export default function Home() {
   return (
     <Layout title="Zuord">
-      <Grid container margin={2} spacing={2}  alignItems="flex-end">
-        <Grid size={{ xs: 12, xl: 6 }}>
-          <Benefits {...benefitsBody.syncAPI} />
-        </Grid>
-        <Grid size={{ xs: 12, xl: 6 }}>
-          <Benefits {...benefitsBody.zeroCostRT} />
+      <Grid container margin={2} spacing={2}>
+        <Grid size={12} container alignItems="flex-end">
+          <Grid size={{ xs: 12, xl: 6 }}>
+            <Benefits {...benefitsBody.syncAPI} />
+          </Grid>
+          <Grid size={{ xs: 12, xl: 6 }}>
+            <Benefits {...benefitsBody.zeroCostRT} />
+          </Grid>
         </Grid>
       </Grid>
     </Layout>
