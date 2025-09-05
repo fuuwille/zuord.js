d453360086dd22c6089f6b1d1593e028f396161e
k4yr2 <kyrzkaya@gmail.com>
2025-09-06 01:41:06 +0300

    refactor: restructure benefits data to enhance integration and inspector components

--- website/src/data/benefits.tsx
+++ website/src/data/benefits.tsx
@@ -1,4 +1,8 @@
 import { BenefitsBody } from "@site/src/components/benefits";
+import { Pretext } from "@site/src/components/pretext";
+import { ShowcaseInspectorHead, ShowcaseInspectorBody } from "@site/src/components/showcase";
+import { tokenModifier } from "@site/src/utils/pretext";
+import { code, modifiers } from "./benefits/syncAPI";
 
 export const benefitsBody = {
     syncAPI: {
@@ -14,5 +18,19 @@ export const benefitsBody = {
             description: "Return types fully managed by the compiler, with zero runtime cost.",
         },
         content: <BenefitsBody.ZeroCostRT />
+    }
+}
+
+export const benefits = {
+    syncAPI: {
+        control: {
+            integrate: {
+                text: "integrate",
+                inspector: {
+                    head: <ShowcaseInspectorHead.Detailed content={<Pretext text={`zuord.integrate(base, input)`} modifiers={[tokenModifier.const("zuord")]} />} />,
+                    body: <ShowcaseInspectorBody.Trial example={<Pretext text={code.integrate} modifiers={modifiers.integrate} />} value={null} inference={null} />
+                }
+            }
+        }
     }
 }
