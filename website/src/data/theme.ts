d7bd7f27ac77bd0a0db4c4a4a948da02181ce61c
k4yr2 <kyrzkaya@gmail.com>
2025-09-05 18:29:25 +0300

    refactor: add breakpoints configuration to theme

--- website/src/data/theme.ts
+++ website/src/data/theme.ts
@@ -1,6 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 768,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
     components: {
         MuiTooltip: {
             styleOverrides: {
