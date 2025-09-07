eee6391e3785b8fdc57ab7b88c94af7748f908fb
k4yr2 <kyrzkaya@gmail.com>
2025-09-07 12:47:17 +0300

    refactor: implement pick API and types for enhanced integration

---
+++ library/src/internal/mode/pick.d.ts
@@ -0,0 +1,9 @@
+import type { plain as $plain } from "./pick.runtime";
+
+type pickAPI = {
+    plain: typeof $plain;
+}
+
+export declare const pick: pickAPI;
+
+export type * as Pick from "./pick.types";
---
+++ library/src/internal/mode/pick.types.ts
@@ -0,0 +1,3 @@
+import type { ZuordCore as Core } from "@zuord/core";
+
+export type Plain = Core.Mode.Resolve<[Core.Flags.Base]>;
