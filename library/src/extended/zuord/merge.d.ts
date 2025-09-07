5377d0adb8fa95ae5d3f3ce53de3196f1c764552
k4yr2 <kyrzkaya@gmail.com>
2025-09-07 12:35:39 +0300

    refactor: add array type to mergeAPI for enhanced functionality

--- library/src/extended/zuord/merge.d.ts
+++ library/src/extended/zuord/merge.d.ts
@@ -1,7 +1,9 @@
 import type { loose } from "./merge.runtime";
+import type { array } from "./merge.runtime";
 
 type mergeAPI = {
     readonly loose: typeof loose;
+    readonly array: typeof array;
 }
 
 export declare const merge: mergeAPI;
