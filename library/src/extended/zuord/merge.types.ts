0acb194c84805563330c3ad0bea865884e8a6fa0
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 05:57:58 +0300

    refactor: update type parameter naming for consistency in merge.types.ts

--- library/src/extended/zuord/merge.types.ts
+++ library/src/extended/zuord/merge.types.ts
@@ -3,8 +3,8 @@ import type { zuordModeX, ZuordModeX } from "../mode";
 import type { ZuordType, ZuordPlain } from "@zuord/type";
 import { ZuordCore } from "@zuord/core";
 
-export type Loose<TContent extends ZuordPlain.Array, TMode extends Partial<ZuordModeX.Merge.Loose> = {}> 
-    = $Zuord.Merge.Plain<TContent, ZuordCore.ModeResolve<[typeof zuordModeX.merge.loose, TMode]>>;
+export type Loose<TContents extends ZuordPlain.Array, TMode extends Partial<ZuordModeX.Merge.Loose> = {}> 
+    = $Zuord.Merge.Plain<TContents, ZuordCore.ModeResolve<[typeof zuordModeX.merge.loose, TMode]>>;
 
-export type Array<TContent extends ZuordType.Array<ZuordType.Array>, TMode extends Partial<ZuordModeX.Merge.Array> = {}> 
-    = $Zuord.Merge.Array<TContent, ZuordCore.ModeResolve<[typeof zuordModeX.merge.array, TMode]>>;
+export type Array<TContents extends ZuordType.Array<ZuordType.Array>, TMode extends Partial<ZuordModeX.Merge.Array> = {}> 
+    = $Zuord.Merge.Array<TContents, ZuordCore.ModeResolve<[typeof zuordModeX.merge.array, TMode]>>;
