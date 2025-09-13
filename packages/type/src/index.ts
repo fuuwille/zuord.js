d32af05f9cd42cf697c24d5326e0c3444964b40d
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 16:18:51 +0300

    refactor: simplify exports in index.ts by consolidating zuordType, zuordPlain, zuordArray, and zuordTuple

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,8 +1,2 @@
-export { zuordType } from "./main";
-export { zuordPlain } from "./main";
-export { zuordArray } from "./main";
-export { zuordTuple } from "./main";
-export type { ZuordType } from "./main";
-export type { ZuordPlain } from "./main";
-export type { ZuordArray } from "./main";
-export type { ZuordTuple } from "./main";
+export * from "./main";
+export type * from "./main";
