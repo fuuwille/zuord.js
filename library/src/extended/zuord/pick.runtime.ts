ac2727ef38c78d3dd20e28e79c9452ecf92ace8b
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 04:37:07 +0300

    refactor: update type parameters in loose functions for consistency

--- library/src/extended/zuord/pick.runtime.ts
+++ library/src/extended/zuord/pick.runtime.ts
@@ -1,22 +1,17 @@
 import { $zuord } from "../../internal";
 import { zuordModeX } from "../mode";
-import { zuordCore } from "@zuord/core";
 import type { ZuordX, ZuordModeX } from "../../";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
-const $plain = ($target: ZuordType.Plain, $pattern: ZuordUtil.Pattern.Plain<ZuordType.Plain>, $mode: Partial<ZuordModeX.Pick.Plain>) => {
-    return $zuord.pick.plain($target, $pattern, zuordCore.mode.resolve([zuordModeX.pick.plain, $mode]));
-}
-
 
 // LOOSE
 
-export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>> (base: TTarget, pattern: TPattern)
-    : ZuordX.Pick.Loose<TTarget, TPattern>;
+export function loose <TSource extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TSource>> (source: TSource, pattern: TPattern)
+    : ZuordX.Pick.Loose<TSource, TPattern>;
 
-export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Pick.Plain>> (base: TTarget, pattern: TPattern, mode: TMode)
-    : ZuordX.Pick.Loose<TTarget, TPattern, TMode>;
+export function loose <TSource extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TSource>, TMode extends Partial<ZuordModeX.Pick.Loose>> (source: TSource, pattern: TPattern, mode: TMode)
+    : ZuordX.Pick.Loose<TSource, TPattern, TMode>;
 
-export function loose <TTarget extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TTarget>, TMode extends Partial<ZuordModeX.Pick.Plain>> (base: TTarget, pattern: TPattern, mode: TMode = {} as TMode)
-    : ZuordX.Pick.Loose<TTarget, TPattern, TMode> { return $plain(base, pattern, mode); }
+export function loose <TSource extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TSource>, TMode extends Partial<ZuordModeX.Pick.Loose>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
+    : ZuordX.Pick.Loose<TSource, TPattern, TMode> { return $zuord.pick.plain(source, pattern, [zuordModeX.pick.loose, mode]); }
