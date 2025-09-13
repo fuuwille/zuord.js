89e70930b619d9ba69d458b62c4ec3cf8790fdba
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 05:38:51 +0300

    refactor: standardize type parameters in ResolveAny, Plain, and ResolvePlain types

--- library/src/internal/zuord/integrate.types.ts
+++ library/src/internal/zuord/integrate.types.ts
@@ -1,50 +1,49 @@
 import type { ZuordType, ZuordTuple } from "@zuord/type";
 import type { ZuordCore as Core } from "@zuord/core";
 import type { ZuordUtil as Util } from "@zuord/util";
+import { $ZuordMode } from "../mode";
 
-export type ResolveAny<TBase, TOverlay, TMode> = (
-    [TBase, TOverlay] extends [infer A extends ZuordType.Plain, infer B extends ZuordType.Plain] ? ResolvePlain<A, B, TMode> :
-    [TBase, TOverlay] extends [infer A extends ZuordType.Array, infer B extends ZuordType.Array] ? ResolveArray<A, B, TMode> : TOverlay
+export type ResolveAny<TSource, TContent, TMode> = (
+    [TSource, TContent] extends [infer A extends ZuordType.Plain, infer B extends ZuordType.Plain] ? ResolvePlain<A, B, TMode> :
+    [TSource, TContent] extends [infer A extends ZuordType.Array, infer B extends ZuordType.Array] ? ResolveArray<A, B, TMode> : TContent
 );
 
-export type Plain<TBase, TOverlay, TMode> = (
-    [TMode] extends [infer TMode extends Core.ModeRecord] ? (
-        ResolvePlain<TBase, TOverlay, TMode> extends infer TPlain extends ZuordType.Plain ? (
-            Util.Unify.Plain<TPlain, Core.ModeResolve<[TMode, { unifyArray: true }]>>
-        ) : never
+export type Plain<TSource, TContent, TMode extends $ZuordMode.Integrate.Plain> = (
+    ResolvePlain<TSource, TContent, TMode> extends infer TPlain extends ZuordType.Plain ? (
+        Util.Unify.Plain<TPlain, Core.ModeResolve<[TMode, { unifyArray: true }]>>
     ) : never
 );
 
-export type ResolvePlain<TBase, TOverlay, TMode> = (
-    (ResolvePlainOverrides<TBase, TOverlay, TMode> & ResolvePlainExtras<TBase, TOverlay>) extends infer TIntegrated ? ({
+export type ResolvePlain<TSource, TContent, TMode> = (
+    (ResolvePlainOverrides<TSource, TContent, TMode> & ResolvePlainExtras<TSource, TContent>) extends infer TIntegrated ? ({
         -readonly [K in keyof TIntegrated]: TIntegrated[K];
     }) : never
 );
 
-export type ResolvePlainOverrides<TBase, TOverlay, TMode> = ({
-    [K in keyof TBase]: K extends keyof TOverlay ? (
+export type ResolvePlainOverrides<TSource, TContent, TMode> = ({
+    [K in keyof TSource]: K extends keyof TContent ? (
         TMode extends { shallow: true } ? (
-            TOverlay[K]
-        ) : ResolveAny<TBase[K], TOverlay[K], TMode>
-    ) : TBase[K];
+            TContent[K]
+        ) : ResolveAny<TSource[K], TContent[K], TMode>
+    ) : TSource[K];
 });
 
-export type ResolvePlainExtras<TBase, TOverlay> = ({
-    [K in keyof TOverlay as K extends keyof TBase ? never : K]: TOverlay[K];
+export type ResolvePlainExtras<TSource, TContent> = ({
+    [K in keyof TContent as K extends keyof TSource ? never : K]: TContent[K];
 });
 
-export type Array<TBase, TOverlay, TMode> = (
+export type Array<TSource, TContent, TMode> = (
     [TMode] extends [infer TMode extends Core.ModeRecord] ? (
-        ResolveArray<TBase, TOverlay, { concat: true }> extends infer TArray extends ZuordType.Array ? (
+        ResolveArray<TSource, TContent, { concat: true }> extends infer TArray extends ZuordType.Array ? (
             Util.Unify.Array<TArray, TMode>
         ) : never
     ) : never
 );
 
-export type ResolveArray<TBase, TOverlay, TMode> = (
+export type ResolveArray<TSource, TContent, TMode> = (
     TMode extends { concat: true } ? (
-        [TBase, TOverlay] extends [infer A extends ZuordTuple.Nest, infer B extends ZuordTuple.Nest] ? (
+        [TSource, TContent] extends [infer A extends ZuordTuple.Nest, infer B extends ZuordTuple.Nest] ? (
             [...A, ...B]
-        ) : TBase | TOverlay
-    ) : TOverlay
+        ) : TSource | TContent
+    ) : TContent
 );
