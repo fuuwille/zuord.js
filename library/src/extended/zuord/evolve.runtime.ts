8aa79bc500cb417f772074bdcf479558aad9efd1
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 04:33:54 +0300

    refactor: update type parameters in restrict functions for consistency

--- library/src/extended/zuord/evolve.runtime.ts
+++ library/src/extended/zuord/evolve.runtime.ts
@@ -1,18 +1,8 @@
 import { $zuord } from "../../internal";
 import { zuordModeX } from "../mode";
-import { zuordPlain } from "@zuord/type";
-import { zuordCore } from "@zuord/core";
 import type { ZuordX, ZuordModeX } from "../../";
-import type { ZuordType, ZuordPlain } from "@zuord/type";
+import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
-
-const $plain = ($source: ZuordType.Plain, $patches: ZuordPlain.Array, $mode: Partial<ZuordModeX.Evolve.Plain>) => {
-    if(!zuordPlain.array($patches)) {
-        throw new TypeError("Integrate function expects the base to be a valid plain.");
-    }
-
-    return $zuord.evolve.plain($source, $patches, zuordCore.mode.resolve([zuordModeX.evolve.plain, $mode]));
-}
 
 
 // LOOSE
@@ -20,8 +10,8 @@ const $plain = ($source: ZuordType.Plain, $patches: ZuordPlain.Array, $mode: Par
 export function restrict <TSource extends ZuordType.Plain, TPatches extends ZuordUtil.Restrict.KeysBatch<TSource, TPatches>> (source: TSource, patches: [...TPatches])
     : ZuordX.Evolve.Restrict<TSource, TPatches>;
 
-export function restrict <TSource extends ZuordType.Plain, TPatches extends ZuordUtil.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (source: TSource, patches: [...TPatches], mode: TMode)
+export function restrict <TSource extends ZuordType.Plain, TPatches extends ZuordUtil.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Restrict>> (source: TSource, patches: [...TPatches], mode: TMode)
     : ZuordX.Evolve.Restrict<TSource, TPatches, TMode>;
 
-export function restrict <TSource extends ZuordType.Plain, TPatches extends ZuordUtil.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Plain>> (source: TSource, patches: [...TPatches], mode: TMode = {} as TMode)
-    : ZuordX.Evolve.Restrict<TSource, TPatches, TMode> { return $plain(source, patches, mode); }
+export function restrict <TSource extends ZuordType.Plain, TPatches extends ZuordUtil.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Restrict>> (source: TSource, patches: [...TPatches], mode: TMode = {} as TMode)
+    : ZuordX.Evolve.Restrict<TSource, TPatches, TMode> { return $zuord.evolve.plain(source, patches, [zuordModeX.evolve.restrict, mode]); }
--- library/src/extended/zuord/evolve.types.ts
+++ library/src/extended/zuord/evolve.types.ts
@@ -3,8 +3,5 @@ import type { ZuordModeX } from "../";
 import type { ZuordType as Type } from "@zuord/type";
 import type { ZuordUtil as Util } from "@zuord/util";
 
-export type Restrict<TSource extends Type.Plain, TPatches extends Util.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
-    = $Zuord.Evolve.Plain<TSource, TPatches, TMode>;
-
-export type Strict<TSource extends Type.Plain, TPatches extends Util.Strict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Plain> = {}> 
+export type Restrict<TSource extends Type.Plain, TPatches extends Util.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ZuordModeX.Evolve.Restrict> = {}> 
     = $Zuord.Evolve.Plain<TSource, TPatches, TMode>;
