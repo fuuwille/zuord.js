d578ccf5ee774df1bf6f26010f36862c8e8018b4
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 14:46:36 +0300

    refactor: replace array mode handling with zuordCore.modeResolve in omit and pick functions

--- library/src/extended/zuord/pick.runtime.ts
+++ library/src/extended/zuord/pick.runtime.ts
@@ -1,5 +1,6 @@
 import { $zuord } from "../../internal";
 import { zuordModeX } from "../mode";
+import { zuordCore } from "@zuord/core";
 import type { ZuordX, ZuordModeX } from "../../";
 import type { ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
@@ -14,4 +15,4 @@ export function loose <TSource extends ZuordType.Plain, TPattern extends ZuordUt
     : ZuordX.Pick.Loose<TSource, TPattern, TMode>;
 
 export function loose <TSource extends ZuordType.Plain, TPattern extends ZuordUtil.Pattern.Plain<TSource>, TMode extends Partial<ZuordModeX.Pick.Loose>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
-    : ZuordX.Pick.Loose<TSource, TPattern, TMode> { return $zuord.pick.plain(source, pattern, [zuordModeX.pick.loose, mode]); }
+    : ZuordX.Pick.Loose<TSource, TPattern, TMode> { return $zuord.pick.plain(source, pattern, zuordCore.modeResolve([zuordModeX.pick.loose, mode])); }
