5d46ccb8b3bf2a58914b4707c882a6b16bd39db7
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 06:03:01 +0300

    refactor: update type constraint for Plain and ResolvePlain types to use Core.ModeRecord

--- library/src/internal/zuord/evolve.types.ts
+++ library/src/internal/zuord/evolve.types.ts
@@ -1,13 +1,15 @@
 import type { $Zuord } from ".";
 import type { ZuordType } from "@zuord/type";
-import type { ZuordCore as Core } from "@zuord/core";
+import type { ZuordCore } from "@zuord/core";
 import type { ZuordUtil } from "@zuord/util";
 
-export type Plain<TBase, TContent, TMode extends Core.Mode.Flags> = (
-    [ResolvePlain<TBase, TContent, TMode>] extends [infer TResolved extends ZuordType.Plain] ? (
-        ZuordUtil.Unify.Hybrid<TResolved, TMode>
+export type Plain<TBase, TContent, TMode> = (
+    [TMode] extends [ZuordCore.ModeRecord] ? (
+        [ResolvePlain<TBase, TContent, TMode>] extends [infer TResolved extends ZuordType.Plain] ? (
+            ZuordUtil.Unify.Hybrid<TResolved, TMode>
+        ) : never
     ) : never
 )
 
-export type ResolvePlain<TBase, TContent, TMode extends Core.Mode.Flags> 
+export type ResolvePlain<TBase, TContent, TMode extends ZuordCore.ModeRecord> 
     = $Zuord.Integrate.ResolvePlain<TBase, $Zuord.Merge.ResolvePlain<TContent, TMode>, TMode>;
