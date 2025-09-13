8d9809096f61b222d8b888bd44165fd64830b10b
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 06:00:49 +0300

    refactor: replace internal function calls with direct resolve calls in evolve.runtime.ts

--- library/src/internal/zuord/evolve.runtime.ts
+++ library/src/internal/zuord/evolve.runtime.ts
@@ -1,14 +1,14 @@
 import { integrate } from "./integrate";
 import { merge } from "./merge";
 
-const $ = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode, integrate: (base: any, input: any, mode: any) => any, merge: (content: any, mode: any) => any) => {
-    return integrate(base, merge(content, mode), mode);
+export const plain = <TSource, TContents, TMode>(source : TSource, contents: TContents, mode: TMode) => {
+    return resolve(source, contents, mode, integrate.plain, merge.plain);
 }
 
-export const plain = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode) => {
-    return $(base, content, mode, integrate.plain, merge.plain);
+export const array = <TSource, TContents, TMode>(source : TSource, contents: TContents, mode: TMode) => {
+    return resolve(source, contents, mode, integrate.array, merge.array);
 }
 
-export const array = <TBase, TContent, TMode>(base : TBase, content: TContent, mode: TMode) => {
-    return $(base, content, mode, integrate.array, merge.array);
+export const resolve = <TSource, TContents, TMode>(source : TSource, contents: TContents, mode: TMode, integrate: (source: any, content: any, mode: any) => any, merge: (contents: any, mode: any) => any) => {
+    return integrate(source, merge(contents, mode), mode);
 }
