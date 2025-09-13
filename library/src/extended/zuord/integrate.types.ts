68f3452df8b182a927fdf8e1b635614f5bc370e6
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 03:19:53 +0300

    refactor: update type parameter names in integrate.types.ts for consistency

--- library/src/extended/zuord/integrate.types.ts
+++ library/src/extended/zuord/integrate.types.ts
@@ -4,11 +4,11 @@ import type { ZuordType } from "@zuord/type";
 import type { ZuordCore } from "@zuord/core";
 import type { ZuordUtil } from "@zuord/util";
 
-export type Loose<TBase extends ZuordType.Plain, TInput extends ZuordType.Plain, TMode extends Partial<ZuordModeX.Integrate.Loose> = {}> 
-    = $Zuord.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.loose, TMode]>>;
+export type Loose<TSource extends ZuordType.Plain, TContent extends ZuordType.Plain, TMode extends Partial<ZuordModeX.Integrate.Loose> = {}> 
+    = $Zuord.Integrate.Plain<TSource, TContent, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.loose, TMode]>>;
 
-export type Restrict<TBase extends ZuordType.Plain, TInput extends ZuordUtil.Restrict.Keys<TBase, TInput>, TMode extends Partial<ZuordModeX.Integrate.Restrict> = {}> 
-    = $Zuord.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.restrict, TMode]>>;
+export type Restrict<TSource extends ZuordType.Plain, TPatch extends ZuordUtil.Restrict.Keys<TSource, TPatch>, TMode extends Partial<ZuordModeX.Integrate.Restrict> = {}> 
+    = $Zuord.Integrate.Plain<TSource, TPatch, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.restrict, TMode]>>;
 
-export type Array<TBase extends ZuordType.Array, TInput extends ZuordType.Array, TMode extends Partial<ZuordModeX.Integrate.Array> = {}> 
-    = $Zuord.Integrate.Array<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.array, TMode]>>;
+export type Array<TSource extends ZuordType.Array, TContent extends ZuordType.Array, TMode extends Partial<ZuordModeX.Integrate.Array> = {}> 
+    = $Zuord.Integrate.Array<TSource, TContent, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.array, TMode]>>;
