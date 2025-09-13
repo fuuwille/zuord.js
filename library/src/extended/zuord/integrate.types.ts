20ca8a39ca2daef5c9d2223bd9d0e2cb721b1899
k4yr2 <kyrzkaya@gmail.com>
2025-09-13 03:18:20 +0300

    refactor: remove aliasing of types in integrate.types.ts for clarity

--- library/src/extended/zuord/integrate.types.ts
+++ library/src/extended/zuord/integrate.types.ts
@@ -1,14 +1,14 @@
 import type { $Zuord } from "../../internal";
 import type { zuordModeX, ZuordModeX } from "../../";
-import type { ZuordType as Type } from "@zuord/type";
-import type { ZuordCore as Core } from "@zuord/core";
-import type { ZuordUtil as Util } from "@zuord/util";
+import type { ZuordType } from "@zuord/type";
+import type { ZuordCore } from "@zuord/core";
+import type { ZuordUtil } from "@zuord/util";
 
-export type Loose<TBase extends Type.Plain, TInput extends Type.Plain, TMode extends Partial<ZuordModeX.Integrate.Loose> = {}> 
-    = $Zuord.Integrate.Plain<TBase, TInput, Core.ModeResolve<[typeof zuordModeX.integrate.loose, TMode]>>;
+export type Loose<TBase extends ZuordType.Plain, TInput extends ZuordType.Plain, TMode extends Partial<ZuordModeX.Integrate.Loose> = {}> 
+    = $Zuord.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.loose, TMode]>>;
 
-export type Restrict<TBase extends Type.Plain, TInput extends Util.Restrict.Keys<TBase, TInput>, TMode extends Partial<ZuordModeX.Integrate.Restrict> = {}> 
-    = $Zuord.Integrate.Plain<TBase, TInput, Core.ModeResolve<[typeof zuordModeX.integrate.restrict, TMode]>>;
+export type Restrict<TBase extends ZuordType.Plain, TInput extends ZuordUtil.Restrict.Keys<TBase, TInput>, TMode extends Partial<ZuordModeX.Integrate.Restrict> = {}> 
+    = $Zuord.Integrate.Plain<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.restrict, TMode]>>;
 
-export type Array<TBase extends Type.Array, TInput extends Type.Array, TMode extends Partial<ZuordModeX.Integrate.Array> = {}> 
-    = $Zuord.Integrate.Array<TBase, TInput, Core.ModeResolve<[typeof zuordModeX.integrate.array, TMode]>>;
+export type Array<TBase extends ZuordType.Array, TInput extends ZuordType.Array, TMode extends Partial<ZuordModeX.Integrate.Array> = {}> 
+    = $Zuord.Integrate.Array<TBase, TInput, ZuordCore.ModeResolve<[typeof zuordModeX.integrate.array, TMode]>>;
