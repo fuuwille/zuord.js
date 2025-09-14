2b6ab970ca498c653103e46ad90d2b266aec9b67
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 14:01:09 +0300

    feat: add style prop to Pretext for improved customization in DocsapiHead

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -3,25 +3,25 @@ import { Pretext } from "@site/src/components/pretext"
 
 export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-    <div style={{ display: 'inline' }}>
-        <div
-            style={{
-                display: 'inline-block',
-                minWidth: 'max-content',
-                verticalAlign: 'top',
-            }}
-        >
-            <Pretext {...props.pretext} />
+        <div style={{ display: 'inline' }}>
+            <div
+                style={{
+                    display: 'inline-block',
+                    minWidth: 'max-content',
+                    verticalAlign: 'top',
+                }}
+            >
+                <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} />
+            </div>
+            <div
+                style={{
+                    display: 'inline',
+                    whiteSpace: 'normal',
+                    verticalAlign: 'top',
+                }}
+            >
+                {props.description}
+            </div>
         </div>
-        <div
-            style={{
-                display: 'inline',
-                whiteSpace: 'normal',
-                verticalAlign: 'top',
-            }}
-        >
-            {props.description}
-        </div>
-    </div>
     )
 }
