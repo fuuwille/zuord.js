7f16b7c8175d501d4e44f62a919ed8f136beade2
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 03:38:27 +0300

    refactor: update merge function signatures and implementation

--- library/src/main/zuord.runtime.ts
+++ library/src/main/zuord.runtime.ts
@@ -3,7 +3,7 @@ import { zuordX } from "../extended";
 import { $zuord } from "../internal";
 import { zuordCore } from "@zuord/core";
 import type { Zuord, ZuordMode } from ".";
-import type { ZuordType } from "@zuord/type";
+import type { ZuordPlain, ZuordType } from "@zuord/type";
 import type { ZuordUtil } from "@zuord/util";
 
 export function integrate <TSource extends ZuordType.Plain, TPatch extends ZuordUtil.Restrict.Keys<TSource, TPatch>> (source: TSource, patch: TPatch)
@@ -15,7 +15,14 @@ export function integrate <TSource extends ZuordType.Plain, TPatch extends Zuord
 export function integrate <TSource extends ZuordType.Plain, TPatch extends ZuordUtil.Restrict.Keys<TSource, TPatch>, TMode extends Partial<ZuordMode.Merge>> (source: TSource, patch: TPatch, mode: TMode = {} as TMode)
     : Zuord.Integrate<TSource, TPatch> { return $zuord.integrate.plain(source, patch, zuordCore.modeResolve([zuordMode.integrate, mode])) as Zuord.Integrate<TSource, TPatch, TMode>; }
 
-export const merge = zuordX.merge.loose;
+export function merge <TContents extends ZuordPlain.Array> (contents: [...TContents])
+    : Zuord.Merge<TContents>;
+
+export function merge <TContents extends ZuordPlain.Array, TMode extends Partial<ZuordMode.Merge>> (contents: [...TContents], mode: TMode)
+    : Zuord.Merge<TContents, TMode>;
+
+export function merge <TContents extends ZuordPlain.Array, TMode extends Partial<ZuordMode.Merge>> (contents: [...TContents], mode: TMode = {} as TMode)
+    : Zuord.Merge<TContents, TMode> { return $zuord.merge.plain(contents, zuordCore.modeResolve([zuordMode.merge, mode])); }
 
 export const evolve = zuordX.evolve.restrict;
 
