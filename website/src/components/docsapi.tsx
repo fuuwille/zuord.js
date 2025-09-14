8fda96eb52fed887aeaf5a3993a4187c5bc8b717
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 14:37:26 +0300

    feat: rename DocsapiHead to DocsapiDefinition and update associated styles for consistency

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -2,17 +2,17 @@ import style from '@site/src/css/modules/docsapi.module.scss';
 import { PretextProps } from "@site/src/types/pretext"
 import { Pretext } from "@site/src/components/pretext"
 
-export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
+export const DocsapiDefinition : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div className={style['docsapi-head']}>
+        <div className={style['docsapi-definition']}>
             <div className={style['content']}>
-                <div className={style['tag']}>
+                <div className={style['head']}>
                     <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} design={{ selectable: true }} />
                 </div>
                 <div className={style['separator']}>
                     :
                 </div>
-                <div className={style['description']}>
+                <div className={style['label']}>
                     {props.description}
                 </div>
             </div>
