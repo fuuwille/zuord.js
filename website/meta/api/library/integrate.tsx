6cd6d9b84b585cc28c6d3f383f8f0ea6162d7f4c
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 08:08:19 +0300

    feat: format integrate function signatures for improved readability

--- website/meta/api/library/integrate.tsx
+++ website/meta/api/library/integrate.tsx
@@ -2,8 +2,10 @@ import { getTokens } from "@site/src/utils/pretext"
 
 export const signature = {
     runtime: [
-`function integrate <TSource, TPatch>(source: TSource, patch: TPatch): Zuord.Integrate<TSource, TPatch>`,
-`function integrate <TSource, TPatch, TMode>(source: TSource, patch: TPatch, mode: TMode): Zuord.Integrate<TSource, TPatch, TMode>`
+`function integrate <TSource, TPatch>(source: TSource, patch: TPatch) 
+    : Zuord.Integrate<TSource, TPatch>`,
+`function integrate <TSource, TPatch, TMode>(source: TSource, patch: TPatch, mode: TMode)
+    : Zuord.Integrate<TSource, TPatch, TMode>`
     ],
     type: [
 `type Integrate<TSource, TPatch>`,
