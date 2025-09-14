e33bd1de20ba2b6ccba5dbc3a9d6bcd827de6061
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 14:18:39 +0300

    feat: update DocsapiHead styling for improved layout and visual consistency

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -3,12 +3,16 @@ import { Pretext } from "@site/src/components/pretext"
 
 export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div  style={{ display: 'flex', alignItems: 'center', gap: '8px', margin: '16px' }}>
+        <div  style={{ display: 'flex', alignItems: 'center', gap: '8px', margin: '16px 0' }}>
             <div style={{ display: 'inline' }}>
                 <div
                     style={{
+                        padding: '0 6px',
+                        marginRight: '6px',
+                        border: '1px solid #ccc',
                         display: 'inline-block',
                         minWidth: 'max-content',
+                        borderRadius: '8px',
                     }}
                 >
                     <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} />
