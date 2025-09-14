40206c8c8e19ad56e4720baab439c56a83e1cd36
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 12:13:39 +0300

    feat: extend DocsapiHead component to include description prop and enhance layout

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -1,10 +1,11 @@
 import { PretextProps } from "../types/pretext"
 import { Pretext } from "./pretext"
 
-export const DocsapiHead : React.FC<{ pretext: PretextProps }> = (props) => {
+export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div>
-            <Pretext {...props.pretext} />
+        <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+            <Pretext {...props.pretext} /> :
+            <p>{props.description}</p>
         </div>
     )
 }
