4e9fa2a91debbf2814290ab7ed2f9db826bc6a80
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 14:25:11 +0300

    feat: refactor DocsapiHead component to use CSS modules for improved styling

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -1,30 +1,15 @@
+import style from '@site/src/css/modules/docsapi.module.scss';
 import { PretextProps } from "@site/src/types/pretext"
 import { Pretext } from "@site/src/components/pretext"
 
 export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div  style={{ display: 'flex', alignItems: 'center', gap: '8px', margin: '16px 0' }}>
-            <div style={{ display: 'inline' }}>
-                <div
-                    style={{
-                        padding: '0 6px',
-                        marginRight: '6px',
-                        border: '1px solid #ccc',
-                        display: 'inline-block',
-                        minWidth: 'max-content',
-                        borderRadius: '8px',
-                    }}
-                >
+        <div className={style['docsapi-head']}>
+            <div className={style['content']}>
+                <div className={style['tag']}>
                     <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} />
                 </div>
-                <div
-                    style={{
-                        display: 'inline',
-                        whiteSpace: 'normal',
-                        verticalAlign: 'top',
-                        lineHeight: '20px'
-                    }}
-                >
+                <div className={style['description']}>
                     : {props.description}
                 </div>
             </div>
