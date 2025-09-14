7b55b4adfe4ad1d3f1985ce77ad53578f5d2697f
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 08:09:53 +0300

    feat: update integrate function signatures and token extraction for improved clarity

--- website/meta/api/library/integrate.tsx
+++ website/meta/api/library/integrate.tsx
@@ -2,12 +2,14 @@ import { getTokens } from "@site/src/utils/pretext"
 
 export const signature = {
     runtime: [
+`zuord.integrate(source, patch, mode?)`,
 `function integrate <TSource, TPatch>(source: TSource, patch: TPatch) 
     : Zuord.Integrate<TSource, TPatch>`,
 `function integrate <TSource, TPatch, TMode>(source: TSource, patch: TPatch, mode: TMode)
     : Zuord.Integrate<TSource, TPatch, TMode>`
     ],
     type: [
+`Zuord.Integrate<TSource, TPatch, TMode?>`,
 `type Integrate<TSource, TPatch>`,
 `type Integrate<TSource, TPatch, TMode>`
     ]
@@ -15,11 +17,11 @@ export const signature = {
 
 export const tokens = {
     runtime: [
-        getTokens(signature.runtime[0]),
-        getTokens(signature.runtime[1])
+        getTokens(signature.runtime[1]),
+        getTokens(signature.runtime[2])
     ],
     type: [
-        getTokens(signature.type[0]),
-        getTokens(signature.type[1])
+        getTokens(signature.type[1]),
+        getTokens(signature.type[2])
     ]
 }
