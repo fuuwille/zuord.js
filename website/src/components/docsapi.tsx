a15afccdea7c1a9636609c939e0463f5f7d2f0b9
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 13:52:14 +0300

    feat: refactor DocsapiHead layout for improved structure and styling

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -1,11 +1,27 @@
-import { PretextProps } from "../types/pretext"
-import { Pretext } from "./pretext"
+import { PretextProps } from "@site/src/types/pretext"
+import { Pretext } from "@site/src/components/pretext"
 
 export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
-            <Pretext {...props.pretext} /> :
-            <p>{props.description}</p>
+    <div style={{ display: 'inline' }}>
+        <div
+            style={{
+                display: 'inline-block',
+                minWidth: 'max-content',
+                verticalAlign: 'top',
+            }}
+        >
+            <Pretext {...props.pretext} />
         </div>
+        <div
+            style={{
+                display: 'inline',
+                whiteSpace: 'normal',
+                verticalAlign: 'top',
+            }}
+        >
+            {props.description}
+        </div>
+    </div>
     )
 }
