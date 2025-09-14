07305495350c7aa29ea47e6051984eca405f74b6
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 14:13:38 +0300

    feat: update DocsapiHead layout for improved styling and structure

--- website/src/components/docsapi.tsx
+++ website/src/components/docsapi.tsx
@@ -3,24 +3,26 @@ import { Pretext } from "@site/src/components/pretext"
 
 export const DocsapiHead : React.FC<{ pretext: PretextProps, description: string }> = (props) => {
     return (
-        <div style={{ display: 'inline' }}>
-            <div
-                style={{
-                    display: 'inline-block',
-                    minWidth: 'max-content',
-                    verticalAlign: 'top',
-                }}
-            >
-                <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} />
-            </div>
-            <div
-                style={{
-                    display: 'inline',
-                    whiteSpace: 'normal',
-                    verticalAlign: 'top',
-                }}
-            >
-                {props.description}
+        <div  style={{ display: 'flex', alignItems: 'center', gap: '8px', margin: '16px' }}>
+            <div style={{ display: 'inline' }}>
+                <div
+                    style={{
+                        display: 'inline-block',
+                        minWidth: 'max-content',
+                    }}
+                >
+                    <Pretext {...props.pretext} style={{ padding: 0, margin: 0 }} />
+                </div>
+                <div
+                    style={{
+                        display: 'inline',
+                        whiteSpace: 'normal',
+                        verticalAlign: 'top',
+                        lineHeight: '20px'
+                    }}
+                >
+                    : {props.description}
+                </div>
             </div>
         </div>
     )
