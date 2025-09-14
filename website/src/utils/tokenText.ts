364a93ab270ac8c991f54e1971346a087d447312
k4yr2 <kyrzkaya@gmail.com>
2025-09-14 12:06:20 +0300

    feat: remove unused tokenText utility function

--- website/src/utils/tokenText.ts
+++
@@ -1,9 +0,0 @@
-export const tokenText = (token: string | Prism.Token | (string | Prism.Token)[]) : string => {
-    if (typeof token === "string") return token;
-
-    if (Array.isArray(token)) {
-        return token.map((c, i) => tokenText(c)).join('');
-    }
-
-    return tokenText(token.content);
-};
