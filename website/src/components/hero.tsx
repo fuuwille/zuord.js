a23d95e7c8918ee8bb786e27eb5b42b546166fca
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 00:21:27 +0300

    feat: add Hero component with introductory text

---
+++ website/src/components/hero.tsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+    return (
+        <div>
+            <h1>Zuord</h1>
+            <p>Runtime & Type Sync APIs within TS-first Packages</p>
+        </div>
+    );
+};
