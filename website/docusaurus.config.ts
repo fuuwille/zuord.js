4197e9ee7fac6ad9bb434fb0c65447468f73a0be
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 00:26:40 +0300

    feat: replace custom.css with custom.scss for improved styling

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
           onUntruncatedBlogPosts: 'warn',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: './src/css/custom.scss',
         },
       } satisfies Preset.Options,
     ],
