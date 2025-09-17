0bced35b04bb9e68f825df3e4c34df53cc7ad9c3
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 02:32:08 +0300

    fix: refactor hero section layout and add wave animation for enhanced visual appeal

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -9,14 +9,17 @@ export default function Home() {
     <Layout title="Zuord">
 
       <Box className="hero" flexDirection={{ xs: 'column', md: 'row' }}>
-        <Box className="title">Zuord</Box>
-        <Box className="divider" display={{ xs: 'none', md: 'block' }}>/</Box>
-        <Box className="description"
-          display={{ xs: 'block', sm: 'inline-flex' }} 
-          fontSize={{ xs: '1rem', sm: '1.25rem' }}
-        >
-          <Box>Synchronous Runtime & Type APIs</Box>
-          <Box>within TS-first Packages</Box>
+        <Box className="wave"></Box>
+        <Box className="content">
+          <Box className="title">Zuord</Box>
+          <Box className="divider" display={{ xs: 'none', md: 'block' }}>/</Box>
+          <Box className="description"
+            display={{ xs: 'block', sm: 'inline-flex' }} 
+            fontSize={{ xs: '1rem', sm: '1.25rem' }}
+          >
+            <Box>Synchronous Runtime & Type APIs</Box>
+            <Box>within TS-first Packages</Box>
+          </Box>
         </Box>
       </Box>
 
