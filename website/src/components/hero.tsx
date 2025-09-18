c35da8c6f12d0875e838f936f1e18b5ff52bb10c
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 03:09:37 +0300

    fix: remove wave animation progress calculation and set fixed animation delay

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -2,21 +2,9 @@
 import { Box } from "@mui/material";
 
 export const Hero = () => {
-    const duration = 60;
-
-    // Saniyeyi client-side hemen hesapla, SSR'da 0 olarak bırak
-    const seconds =
-        typeof window !== "undefined"
-        ? new Date().getSeconds() + new Date().getMinutes() * 60
-        : 0;
-
-    const progress = (seconds % duration) / duration;
-
     return (
         <Box className="hero" flexDirection={{ xs: 'column', md: 'row' }}>
-            <Box className="wave"
-                style={{ animationDelay: `-${progress * duration}s` }}
-            />
+            <Box className="wave" />
             <Box className="content">
                 <Box className="title">Zuord</Box>
                 <Box className="divider" display={{ xs: 'none', md: 'block' }}>/</Box>
