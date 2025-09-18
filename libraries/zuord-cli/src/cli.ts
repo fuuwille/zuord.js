724d61baf21b8ec7d5f04579d35c7c2aba2eb37a
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 19:17:19 +0300

    fix: update CLI script to set working directory for command execution

--- libraries/zuord-cli/src/cli.ts
+++ libraries/zuord-cli/src/cli.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { exec } from 'child_process';
+import path from 'path';
+
+const rootDir = path.resolve('./');
 
 const command = 'npx rimraf dist && npx rollup -c && npx cpy "src/**/*{.js,.d.ts}" dist';
 
-console.log(`Running: ${command}`);
+console.log(`Building Zuord Package...`);
 
-const child = exec(command);
+const child = exec(command, { cwd: rootDir });
 
 child.stdout?.pipe(process.stdout);
 child.stderr?.pipe(process.stderr);
