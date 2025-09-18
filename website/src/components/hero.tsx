51277c64863e60a14b3d41178393ad776c12b428
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 03:33:12 +0300

    fix: adjust hero component layout and center title text for better presentation

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -3,9 +3,9 @@ import { Box } from "@mui/material";
 
 export const Hero = () => {
     return (
-        <Box className={style['hero']} flexDirection={{ xs: 'column', md: 'row' }}>
+        <Box className={style['hero']}>
             <Box className={style['wave']} />
-            <Box className={style['content']}>
+            <Box className={style['content']} flexDirection={{ xs: 'column', md: 'row' }}>
                 <Box className={style['title']}>Zuord</Box>
                 <Box className={style['divider']} display={{ xs: 'none', md: 'block' }}>/</Box>
                 <Box className={style['description']}
