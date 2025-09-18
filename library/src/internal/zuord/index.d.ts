18def6cf6f27fcb4e3dcc065eb94bc4b8307c781
k4yr2 <kyrzkaya@gmail.com>
2025-09-18 18:22:30 +0300

    feat: add definePackage to $zuordAPI and update exports in index.runtime.ts

--- library/src/internal/zuord/index.d.ts
+++ library/src/internal/zuord/index.d.ts
@@ -1,8 +1,10 @@
 import type { mode as $mode } from "./index.runtime";
 import type { modeRecord as $modeRecord } from "./index.runtime";
 import type { modeResolve as $modeResolve } from "./index.runtime";
+import type { definePackage as $definePackage } from "./index.runtime";
 
 type $zuordAPI = {
+    definePackage: typeof $definePackage;
     mode: typeof $mode;
     modeRecord: typeof $modeRecord;
     modeResolve: typeof $modeResolve;
--- library/src/internal/zuord/index.runtime.ts
+++ library/src/internal/zuord/index.runtime.ts
@@ -1,2 +1,3 @@
 export { mode } from "./mode";
 export * from "./mode_.runtime";
+export * from "./package_.runtime";
