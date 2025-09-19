df99c15200d604ee6000febc53cd610b6d4d3019
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 20:40:27 +0300

    refactor: add JSDoc comments for ZuordTags, ZuordTypeTags, and ZuordRuntimeTags interfaces

--- libraries/zuord-extractor/src/api/internal/zuordTags.types.ts
+++ libraries/zuord-extractor/src/api/internal/zuordTags.types.ts
@@ -1,11 +1,20 @@
+/**
+ * @zuordID tags
+ */
 export interface ZuordTags {
 
 }
 
+/**
+ * @zuordID tags-type
+ */
 export interface ZuordTypeTags extends ZuordTags {
     id: string;
 }
 
+/**
+ * @zuordID tags-runtime
+ */
 export interface ZuordRuntimeTags extends ZuordTags {
     type: string;
 }
