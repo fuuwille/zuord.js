9f2a488e40cc8df3331ec65e53ec1a2160f10281
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 21:58:37 +0300

    refactor: remove unnecessary JSDoc comments from tags.types.ts

--- libraries/zuord-extractor/src/api/internal/tags.types.ts
+++ libraries/zuord-extractor/src/api/internal/tags.types.ts
@@ -1,20 +1,11 @@
-/**
- * @zuordID tags
- */
 export interface Tags {
 
 }
 
-/**
- * @zuordID type-tags
- */
 export interface TypeTags extends Tags {
     id: string;
 }
 
-/**
- * @zuordID runtime-tags
- */
 export interface RuntimeTags extends Tags {
     type: string;
 }
