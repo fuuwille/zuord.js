1b0203eb6c6d8a6d26c5fd465f8d047131486dbf
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 03:46:48 +0300

    refactor: move build logic to separate build.ts file and simplify program.ts

--- libraries/zuord/src/cli/program.ts
+++ libraries/zuord/src/cli/program.ts
@@ -1,14 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { join } from "path";
-import { execSync } from "child_process";
-import { fileURLToPath } from "url";
-import { env } from "./env";
-
-const tsconfigPath = join(process.cwd(), "tsconfig.dist.json");
-
-const rollupConfigUrl = import.meta.resolve("zuord/meta/rollup.config.mjs");
-const rollupConfigPath = fileURLToPath(rollupConfigUrl);
+import build from "./build";
 
 const program = new Command();
 
@@ -21,47 +13,6 @@ program
 .command("build")
 .description("Build the zuord package")
 .option("--library", "Build as library")
-.action(async (options) => {
-    const cwd = process.cwd();
-    const packagePath = join(cwd, "zuord-package.ts");
-
-    console.info("Building zuord package...");
-    let pkg;
-
-    try {
-        pkg = await import(packagePath);
-
-        if(options.library) {
-            console.error("❌ Build cannot continue in library mode");
-            process.exit(1);
-        }
-
-    } catch (err) {
-        if(options.library) {
-            console.info("Build will continue as library mode...")
-        }
-        else {
-            console.error("Import hatası:", err);
-            process.exit(1);
-        }
-    }
-
-    const typeOnly = !!pkg?.default?.typeOnly;
-
-    let command = typeOnly
-        ? `npx rimraf dist && npx tsc --project "${tsconfigPath}" && npx cpy 'src/**/*.d.ts' dist`
-        : `npx rimraf dist && npx rollup -c "${rollupConfigPath}" && npx cpy "src/**/*{.js,.d.ts}" dist`;
-
-    env.tsconfig(() => {
-        try {
-            execSync(command, { stdio: "inherit" });
-            console.log(typeOnly ? "✅ Package built as Type-Only." : "✅ Package built as Synchronous.");
-        } catch (error: any) {
-            console.error(`❌ Error: ${error.message}`);
-            process.exit(1);
-        }
-    });
-
-});
+.action(build);
 
 program.parse(process.argv);
