e5ac9c7884c0def2414b545ed9eaf222c002a997
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 01:38:20 +0300

    refactor: remove unnecessary @zuordID annotations from type definitions

--- packages/produce/src/main/produce.runtime.ts
+++ packages/produce/src/main/produce.runtime.ts
@@ -7,31 +7,18 @@ import type { TypeUtil } from "@zuord/util";
 
 /**
  * Produces a new object with the given patch integrated into the source
- * @zuordID integrate
  */
 export function integrate <TSource extends FundType.Plain, TPatch extends TypeUtil.Restrict.Keys<TSource, TPatch>, TMode extends Partial<ProduceMode.Merge>> (source: TSource, patch: TPatch, mode: TMode = {} as TMode)
     : Produce.Integrate<TSource, TPatch> { return $produce.integrate.plain(source, patch, zuord.modeResolve([produceMode.integrate, mode])) as Produce.Integrate<TSource, TPatch, TMode>; }
 
-/**
- * @zuordID merge
- */
 export function merge <TContents extends PlainType.Array, TMode extends Partial<ProduceMode.Merge>> (contents: [...TContents], mode: TMode = {} as TMode)
     : Produce.Merge<TContents, TMode> { return $produce.merge.plain(contents, zuord.modeResolve([produceMode.merge, mode])); }
 
-/**
- * @zuordID evolve
- */
 export function evolve <TSource extends FundType.Plain, TPatches extends TypeUtil.Restrict.KeysBatch<TSource, TPatches>, TMode extends Partial<ProduceMode.Evolve>> (source: TSource, patches: [...TPatches], mode: TMode = {} as TMode)
     : Produce.Evolve<TSource, TPatches, TMode> { return $produce.evolve.plain(source, patches, zuord.modeResolve([produceMode.evolve, mode])); }
 
-/**
- * @zuordID pick
- */
 export function pick <TSource extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TSource>, TMode extends Partial<ProduceMode.Pick>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
     : Produce.Pick<TSource, TPattern, TMode> { return $produce.pick.plain(source, pattern, zuord.modeResolve([produceMode.pick, mode])); }
 
-/**
- * @zuordID omit
- */
 export function omit <TSource extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TSource>, TMode extends Partial<ProduceMode.Omit>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
     : Produce.Omit<TSource, TPattern, TMode> { return $produce.omit.plain(source, pattern, zuord.modeResolve([produceMode.omit, mode])); }
--- packages/produce/src/main/produce.types.ts
+++ packages/produce/src/main/produce.types.ts
@@ -5,31 +5,18 @@ import { TypeUtil } from "@zuord/util";
 
 /**
  * Produces a new type with the given patch integrated into the source.
- * @zuordID integrate
  */
 export type Integrate<TBase extends FundType.Plain, TInput extends TypeUtil.Restrict.Keys<TBase, TInput>, TMode extends Partial<ProduceMode.Integrate> = {}>
     = ProduceX.Integrate.Restrict<TBase, TInput, TMode>;
 
-/**
- * @zuordID merge
- */
 export type Merge<TContent extends PlainType.Array, TMode extends Partial<ProduceMode.Merge> = {}> 
     = ProduceX.Merge.Loose<TContent, TMode>;
 
-/**
- * @zuordID evolve
- */
 export type Evolve<TBase extends FundType.Plain, TContent extends TypeUtil.Restrict.KeysBatch<TBase, TContent>, TMode extends Partial<ProduceMode.Evolve> = {}> 
     = ProduceX.Evolve.Restrict<TBase, TContent, TMode>;
 
-/**
- * @zuordID pick
- */
 export type Pick<TTarget extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TTarget>, TMode extends Partial<ProduceMode.Pick> = {}>
     = ProduceX.Pick.Loose<TTarget, TPattern, TMode>;
 
-/**
- * @zuordID omit
- */
 export type Omit<TTarget extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TTarget>, TMode extends Partial<ProduceMode.Omit> = {}> 
     = ProduceX.Omit.Loose<TTarget, TPattern, TMode>;
