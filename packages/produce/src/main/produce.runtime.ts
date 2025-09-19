7c6a9f586f9f1294c071d636e23185b7e43b6338
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 14:04:00 +0300

    refactor: add missing JSDoc comment for integrate function

--- packages/produce/src/main/produce.runtime.ts
+++ packages/produce/src/main/produce.runtime.ts
@@ -5,6 +5,9 @@ import type { Produce, ProduceMode } from ".";
 import type { PlainType, FundType } from "@zuord/type";
 import type { TypeUtil } from "@zuord/util";
 
+/**
+ * Produces a new object with the given patch integrated into the source 
+ */
 export function integrate <TSource extends FundType.Plain, TPatch extends TypeUtil.Restrict.Keys<TSource, TPatch>, TMode extends Partial<ProduceMode.Merge>> (source: TSource, patch: TPatch, mode: TMode = {} as TMode)
     : Produce.Integrate<TSource, TPatch> { return $produce.integrate.plain(source, patch, zuord.modeResolve([produceMode.integrate, mode])) as Produce.Integrate<TSource, TPatch, TMode>; }
 
@@ -16,6 +19,6 @@ export function evolve <TSource extends FundType.Plain, TPatches extends TypeUti
 
 export function pick <TSource extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TSource>, TMode extends Partial<ProduceMode.Pick>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
     : Produce.Pick<TSource, TPattern, TMode> { return $produce.pick.plain(source, pattern, zuord.modeResolve([produceMode.pick, mode])); }
-    
+
 export function omit <TSource extends FundType.Plain, TPattern extends TypeUtil.Pattern.Plain<TSource>, TMode extends Partial<ProduceMode.Omit>> (source: TSource, pattern: TPattern, mode: TMode = {} as TMode)
     : Produce.Omit<TSource, TPattern, TMode> { return $produce.omit.plain(source, pattern, zuord.modeResolve([produceMode.omit, mode])); }
