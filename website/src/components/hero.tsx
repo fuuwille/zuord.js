2d02ca66d04740caf879d8549723f875356366c1
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 01:35:51 +0300

    fix: update text in Hero component to correct terminology from 'APIs' to 'Modules'

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -12,7 +12,7 @@ export const Hero = () => {
                     display={{ xs: 'block', sm: 'inline-flex' }} 
                     fontSize={{ xs: '16px', sm: '20px' }}
                 >
-                    <Box>Synchronous Model & Variants APIs</Box>
+                    <Box>Synchronous Model & Variants Modules</Box>
                     <Box>within TS-first Packages</Box>
                 </Box>
             </Box>
