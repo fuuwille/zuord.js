67848529568715bce3624a2170fd8f6d035de245
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 21:50:52 +0300

    style: format RuntimeNode type definition for improved readability

--- libraries/zuord-extractor/src/api/internal/node.types.ts
+++ libraries/zuord-extractor/src/api/internal/node.types.ts
@@ -1,12 +1,12 @@
 import { FunctionDeclaration, ArrowFunction, FunctionExpression, VariableDeclaration, ExportAssignment, ObjectLiteralExpression, InterfaceDeclaration, TypeAliasDeclaration } from "ts-morph";
 
 export type RuntimeNode =
-  | FunctionDeclaration
-  | ArrowFunction
-  | FunctionExpression
-  | VariableDeclaration
-  | ExportAssignment
-  | ObjectLiteralExpression;
+    | FunctionDeclaration
+    | ArrowFunction
+    | FunctionExpression
+    | VariableDeclaration
+    | ExportAssignment
+    | ObjectLiteralExpression;
 
 export type TypeNode =
     | InterfaceDeclaration
