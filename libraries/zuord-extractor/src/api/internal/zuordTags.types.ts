16b5d54e94f399927a1d4672d186af99bdee20b6
k4yr2 <kyrzkaya@gmail.com>
2025-09-19 20:38:44 +0300

    feat: add ZuordTypeTags interface extending ZuordTags with id property

--- libraries/zuord-extractor/src/api/internal/zuordTags.types.ts
+++ libraries/zuord-extractor/src/api/internal/zuordTags.types.ts
@@ -2,6 +2,10 @@ export interface ZuordTags {
 
 }
 
+export interface ZuordTypeTags extends ZuordTags {
+    id: string;
+}
+
 export interface ZuordRuntimeTags extends ZuordTags {
     type: string;
 }
