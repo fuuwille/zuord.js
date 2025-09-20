998c27129cc584174eec55dff9e0da28c55256bb
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 18:34:39 +0300

    refactor: add 'others' property to ModuleFile interface for improved structure

--- libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
@@ -1,8 +1,10 @@
+import { Node } from "ts-morph";
 import { ModuleMember, ModuleModelMember } from "./moduleMember.model";
 
 export interface ModuleFile {
     kind: ModuleFileKind;
     members: ModuleMember[];
+    others: Node[];
 };
 
 export interface ModuleModelFile extends ModuleFile {
