1bc08e8205a4407d7c17472c6fddfb3cf2fdc314
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 13:53:10 +0300

    refactor: standardize node type parameter from 'TSNode' to 'Node' in module node variant functions

--- libraries/zuord-extractor/src/api/internal/moduleNode.variants.ts
+++ libraries/zuord-extractor/src/api/internal/moduleNode.variants.ts
@@ -1,34 +1,34 @@
-import { Node as TSNode, SyntaxKind } from "ts-morph";
+import { Node, SyntaxKind } from "ts-morph";
 import { ModuleNode, ModuleModelNode, ModuleTypeNode, ModuleInterfaceNode, ModuleEnumNode, ModuleVariantNode, ModuleFunctionNode, ModuleVariableNode } from "./moduleNode.model";
 
-export const isModuleNode = (node: TSNode): node is ModuleNode => {
+export const isModuleNode = (node: Node): node is ModuleNode => {
     return isModuleModelNode(node) || isModuleVariantNode(node);
 }
 
-export const isModuleModelNode = (node: TSNode): node is ModuleModelNode => {
+export const isModuleModelNode = (node: Node): node is ModuleModelNode => {
     return isModuleTypeNode(node) || isModuleInterfaceNode(node) || isModuleEnumNode(node);
 }
 
-export const isModuleTypeNode = (node: TSNode): node is ModuleTypeNode => {
+export const isModuleTypeNode = (node: Node): node is ModuleTypeNode => {
     return node.getKind() === SyntaxKind.TypeAliasDeclaration;
 }
 
-export const isModuleInterfaceNode = (node: TSNode): node is ModuleInterfaceNode => {
+export const isModuleInterfaceNode = (node: Node): node is ModuleInterfaceNode => {
     return node.getKind() === SyntaxKind.InterfaceDeclaration;
 }
 
-export const isModuleEnumNode = (node: TSNode): node is ModuleEnumNode => {
+export const isModuleEnumNode = (node: Node): node is ModuleEnumNode => {
     return node.getKind() === SyntaxKind.EnumDeclaration;
 }
 
-export const isModuleVariantNode = (node: TSNode): node is ModuleVariantNode => {
+export const isModuleVariantNode = (node: Node): node is ModuleVariantNode => {
     return isModuleFunctionNode(node) || isModuleVariableNode(node);
 }
 
-export const isModuleFunctionNode = (node: TSNode): node is ModuleFunctionNode => {
+export const isModuleFunctionNode = (node: Node): node is ModuleFunctionNode => {
     return node.getKind() === SyntaxKind.FunctionDeclaration;
 }
 
-export const isModuleVariableNode = (node: TSNode): node is ModuleVariableNode => {
+export const isModuleVariableNode = (node: Node): node is ModuleVariableNode => {
     return node.getKind() === SyntaxKind.VariableStatement;
 }
