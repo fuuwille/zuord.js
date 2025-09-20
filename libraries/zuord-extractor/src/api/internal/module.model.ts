e546769d1b3419932208f6fa6c59ee109efb7799
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 18:17:59 +0300

    refactor: remove unused ModuleType enum from module.model.ts

--- libraries/zuord-extractor/src/api/internal/module.model.ts
+++ libraries/zuord-extractor/src/api/internal/module.model.ts
@@ -3,8 +3,3 @@ import { ModuleModelFile } from "./moduleFile.model";
 export type Module = {
     modelFile: ModuleModelFile;
 }
-
-export enum ModuleType {
-    Model = "model",
-    Variants = "variants"
-}
