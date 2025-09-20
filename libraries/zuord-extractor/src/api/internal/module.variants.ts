407e7229a49e2828756abda48abe20dbfaa1401d
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:25:33 +0300

    refactor: enhance extractModule function by restructuring file path handling and node extraction

--- libraries/zuord-extractor/src/api/internal/module.variants.ts
+++ libraries/zuord-extractor/src/api/internal/module.variants.ts
@@ -1,11 +1,29 @@
+import path from "path";
+import { Project } from "ts-morph";
 import { Module, ModuleType } from "./module.model";
 import { ModuleNode } from "./moduleNode.model";
+import { extractModuleNode } from "./moduleNode.variants";
 
 export const extractModule = ($dir: string, $name: string, $type: ModuleType): Module => {
     const dir = $dir;
     const name = $name;
     const type = $type;
-    const nodes = [] as ModuleNode[];
+
+    const fileName = $type === ModuleType.Model 
+        ? `${$name}.model.ts` 
+        : `${$name}.variants.ts`;
+
+    const fullPath = path.join($dir, fileName);
+
+    const project = new Project();
+    const sourceFile = project.addSourceFileAtPath(fullPath);
+
+    const nodes : ModuleNode[] = [];
+    sourceFile.forEachChild((node) => {
+        const moduleNode = extractModuleNode(node);
+        nodes.push(moduleNode);
+    });
+
 
     return {
         dir,
