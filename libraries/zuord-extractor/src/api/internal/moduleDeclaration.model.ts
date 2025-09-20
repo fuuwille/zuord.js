f96be4327f30a8fc7b56037185f86eac149bf94b
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 11:22:11 +0300

    refactor: implement module declaration types and variant checks for improved type handling

---
+++ libraries/zuord-extractor/src/api/internal/moduleDeclaration.model.ts
@@ -0,0 +1,24 @@
+import { FunctionDeclaration, VariableDeclaration, InterfaceDeclaration, TypeAliasDeclaration, EnumDeclaration } from "ts-morph";
+
+export type ModuleDeclaration = 
+    | ModuleModelDeclaration
+    | ModuleVariantDeclaration;
+
+export type ModuleModelDeclaration =
+    | ModuleTypeDeclaration
+    | ModuleInterfaceDeclaration
+    | ModuleEnumDeclaration;
+
+export type ModuleTypeDeclaration = TypeAliasDeclaration;
+
+export type ModuleInterfaceDeclaration = InterfaceDeclaration;
+
+export type ModuleEnumDeclaration = EnumDeclaration;
+
+export type ModuleVariantDeclaration =
+    | ModuleFunctionDeclaration
+    | ModuleVariableDeclaration;
+
+export type ModuleFunctionDeclaration = FunctionDeclaration;
+
+export type ModuleVariableDeclaration = VariableDeclaration;
---
+++ libraries/zuord-extractor/src/api/internal/moduleDeclaration.variants.ts
@@ -0,0 +1,34 @@
+import { Node as TSNode, SyntaxKind } from "ts-morph";
+import { ModuleDeclaration, ModuleModelDeclaration, ModuleTypeDeclaration, ModuleInterfaceDeclaration, ModuleVariantDeclaration, ModuleFunctionDeclaration, ModuleVariableDeclaration, ModuleEnumDeclaration } from "./moduleDeclaration.model";
+
+export const isModuleDeclaration = (node: TSNode): node is ModuleDeclaration => {
+    return isModuleModelDeclaration(node) || isModuleVariantDeclaration(node);
+}
+
+export const isModuleTypeDeclaration = (node: TSNode): node is ModuleTypeDeclaration => {
+    return node.getKind() === SyntaxKind.TypeAliasDeclaration;
+}
+
+export const isModuleInterfaceDeclaration = (node: TSNode): node is ModuleInterfaceDeclaration => {
+    return node.getKind() === SyntaxKind.InterfaceDeclaration;
+}
+
+export const isModuleEnumDeclaration = (node: TSNode): node is ModuleEnumDeclaration => {
+    return node.getKind() === SyntaxKind.EnumDeclaration;
+}
+
+export const isModuleModelDeclaration = (node: TSNode): node is ModuleModelDeclaration => {
+    return isModuleTypeDeclaration(node) || isModuleInterfaceDeclaration(node);
+}
+
+export const isModuleFunctionDeclaration = (node: TSNode): node is ModuleFunctionDeclaration => {
+    return node.getKind() === SyntaxKind.FunctionDeclaration;
+}
+
+export const isModuleVariableDeclaration = (node: TSNode): node is ModuleVariableDeclaration => {
+    return node.getKind() === SyntaxKind.VariableDeclaration;
+}
+
+export const isModuleVariantDeclaration = (node: TSNode): node is ModuleVariantDeclaration => {
+    return isModuleFunctionDeclaration(node) || isModuleVariableDeclaration(node);
+}
