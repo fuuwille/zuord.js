af126a62370622f9bd02b7879004568c122834a7
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:11:33 +0300

    refactor: simplify Module and extractModule structure by removing unused interfaces and properties

--- libraries/zuord-extractor/src/api/internal/module.model.ts
+++ libraries/zuord-extractor/src/api/internal/module.model.ts
@@ -1,20 +1,6 @@
-import { Node } from "ts-morph";
+import { ModuleNode } from "./moduleNode.model";
 
 export type Module = {
-    models: ModuleModel[];
-    variants: ModuleVariant[];
-}
-
-export interface ModuleElement {
-    node: Node;
     name: string;
-}
-
-export interface ModuleModel extends ModuleElement {
-    type: "type" | "interface";
-    variants: ModuleVariant[];
-}
-
-export interface ModuleVariant extends ModuleElement {
-    type: "function" | "value";
+    nodes: ModuleNode[];
 }
--- libraries/zuord-extractor/src/api/internal/module.variants.ts
+++ libraries/zuord-extractor/src/api/internal/module.variants.ts
@@ -1,34 +1,8 @@
-import * as fs from "fs";
-import * as path from "path";
-import { Project } from "ts-morph";
-import { Module, ModuleModel } from "./module.model";
-import { isModelNode } from "./node.variants";
+import { Module } from "./module.model";
 
 export const extractModule = (dir: string, name: string): Module => {
-    const modelPath = path.join(dir, `${name}.model.ts`);
-    const variantsPath = path.join(dir, `${name}.variant.ts`);
-
-    const modelText = fs.readFileSync(modelPath, "utf-8");
-    const variantsText = fs.readFileSync(variantsPath, "utf-8");
-
-    const project = new Project();
-    
-    const modelSource = project.createSourceFile(modelPath, modelText, { overwrite: true });
-    const variantsSource = project.createSourceFile(variantsPath, variantsText, { overwrite: true });
-
-    const models: ModuleModel[] = [];
-
-    modelSource.forEachChild((node) => {
-        if (isModelNode(node)) {
-            const modelName = node.getName?.() || "Unnamed";
-            models.push({
-                node,
-                type: "type",
-                name: modelName,
-                variants: [],
-            });
-        }
-    });
-
-    return { models, variants: [] };
+    return {
+        name,
+        nodes: [],
+    };
 };
