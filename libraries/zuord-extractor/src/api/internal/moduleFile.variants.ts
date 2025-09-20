cb930aa07d108cab0c9ee29e23d985b4afc9f8cd
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 18:08:27 +0300

    refactor: update initializeModuleFile to accept SourceFile for member extraction

--- libraries/zuord-extractor/src/api/internal/moduleFile.variants.ts
+++ libraries/zuord-extractor/src/api/internal/moduleFile.variants.ts
@@ -1,24 +1,25 @@
 import path from "path";
-import { Project } from "ts-morph";
+import { Project, SourceFile } from "ts-morph";
 import { ModuleFile, ModuleModelFile, ModuleFileKind } from "./moduleFile.model";
 import { ModuleMember, ModuleModelMember } from "./moduleMember.model";
 
-export const initializeModuleFile = ($dir: string, $name: string, $kind: ModuleFileKind, $solve: (project: Project) => ModuleMember[]) : ModuleFile => {
+export const initializeModuleFile = ($dir: string, $name: string, $kind: ModuleFileKind, $solve: (file: SourceFile) => ModuleMember[]) : ModuleFile => {
     const kind = $kind;
     const fileName = `${$name}.${$kind.toLowerCase()}.ts`;
     const filePath = path.join($dir, fileName);
 
     const project = new Project();
     const sourceFile = project.addSourceFileAtPath(filePath);
+    const members = $solve(sourceFile);
 
     return {
         kind,
-        members: []
+        members
     };
 };
 
 export const extractModuleModelFile = ($dir: string, $name: string) : ModuleModelFile => {
-    return initializeModuleFile($dir, $name, ModuleFileKind.Model, (project) => {
+    return initializeModuleFile($dir, $name, ModuleFileKind.Model, (file) => {
         const members : ModuleModelMember[] = [];
         return members;
     }) as ModuleModelFile;
