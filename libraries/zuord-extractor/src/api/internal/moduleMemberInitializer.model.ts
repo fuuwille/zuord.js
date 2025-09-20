9ad2a0ef1c67b0ae01e34724b824507d2c099db7
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 21:14:11 +0300

    refactor: move kind property and ModuleMemberInitializerKind enum to the end of ModuleMemberInitializer interface

--- libraries/zuord-extractor/src/api/internal/moduleMemberInitializer.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleMemberInitializer.model.ts
@@ -2,4 +2,10 @@ import { Node } from "ts-morph";
 
 export interface ModuleMemberInitializer {
     node: Node;
+    kind: ModuleMemberInitializerKind;
+}
+
+export enum ModuleMemberInitializerKind {
+    Value = "value",
+    Function = "function"
 }
