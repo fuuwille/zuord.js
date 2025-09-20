5e7c796cbb774adb78fb3fe2c8c5628cdbdabf68
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:02:29 +0300

    feat: update extractModuleNode to use ModuleNodeCategory and correct kind assignment

--- libraries/zuord-extractor/src/api/internal/moduleNode.variants.ts
+++ libraries/zuord-extractor/src/api/internal/moduleNode.variants.ts
@@ -1,12 +1,14 @@
 import { Node } from "ts-morph";
-import { ModuleNode, ModuleNodeKind } from "./moduleNode.model";
+import { ModuleNode, ModuleNodeKind, ModuleNodeCategory } from "./moduleNode.model";
 
 export const extractModuleNode = (node: Node) : ModuleNode => {
     const source = node;
-    const kind = ModuleNodeKind.Model;
+    const kind = ModuleNodeKind.Type;
+    const category = ModuleNodeCategory.Model;
 
     return {
         source,
         kind,
+        category,   
     };
 };
