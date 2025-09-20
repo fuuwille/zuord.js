27c61aa04d8c4260f295659830babd94b5ed4f3e
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 22:50:12 +0300

    refactor: simplify initializeModuleMember logic and update variant member extraction

--- libraries/zuord-extractor/src/api/internal/moduleMember.variants.ts
+++ libraries/zuord-extractor/src/api/internal/moduleMember.variants.ts
@@ -1,45 +1,41 @@
 import { Node, SyntaxKind, VariableStatement } from "ts-morph";
 import { ModuleModelMember, ModuleMemberKind, ModuleMember, ModuleVariantMember, ModuleMemberSlot } from "./moduleMember.model";
-import { ModuleMemberNode } from "./moduleMemberNode.model";
 import { isModuleMemberEnumNode, isModuleMemberFunctionNode, isModuleMemberTypeNode, isModuleMemberVariableNode, isModuleMemberInterfaceNode, isModuleMemberModelNode, isModuleMemberVariantNode } from "./moduleMemberNode.variants";
 
 export const initializeModuleMember = (
-    node: Node, resolve: (node: Node) => node is ModuleMemberNode
+    node: Node, resolve: (member: ModuleMember) => void
 ) : ModuleMember | null => {
-    
-    if(!resolve(node)) {
-        return null;
-    }
 
     const moduleMember = {
         node,
         kind: getModuleMemberKind(node)!
     } as ModuleMember;
 
-    if(moduleMember.node instanceof VariableStatement) {
-        const declaration = moduleMember.node.getDeclarations()[0];
-        const initializer = declaration.getInitializer();
-
-        if (
-            initializer?.isKind(SyntaxKind.ArrowFunction) ||
-            initializer?.isKind(SyntaxKind.FunctionExpression)
-        ) {
-            moduleMember.slot = ModuleMemberSlot.Function;
-        }
-        else {
-            moduleMember.slot = ModuleMemberSlot.Value;
-        }
-    }
-
+    resolve(moduleMember);
     return moduleMember;
 }
 
 export const extractModuleModelMember = (node: Node) : ModuleModelMember | null => {
-    return initializeModuleMember(node, isModuleMemberModelNode) as ModuleModelMember;
+    return initializeModuleMember(node, () => {}) as ModuleModelMember;
 };
 
 export const extractModuleVariantMember = (node: Node) : ModuleVariantMember | null => {
-    return initializeModuleMember(node, isModuleMemberVariantNode) as ModuleVariantMember;
+    return initializeModuleMember(node, (member) => {
+        if(member.node instanceof VariableStatement) {
+            const declaration = member.node.getDeclarations()[0];
+            const initializer = declaration.getInitializer();
+
+            if (
+                initializer?.isKind(SyntaxKind.ArrowFunction) ||
+                initializer?.isKind(SyntaxKind.FunctionExpression)
+            ) {
+                member.slot = ModuleMemberSlot.Function;
+            }
+            else {
+                member.slot = ModuleMemberSlot.Value;
+            }
+        }
+    }) as ModuleVariantMember;
 }
 
 export const getModuleMemberKind = (node: Node): ModuleMemberKind | undefined => {
