c83118ce39c4bb05442fc63c8a5f4b09feed7c8a
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:02:02 +0300

    feat: add category property to ModuleNode type and define ModuleNodeCategory enum

--- libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
@@ -3,12 +3,15 @@ import { Node } from "ts-morph"
 export type ModuleNode = {
     source: Node;
     kind: ModuleNodeKind;
+    category: ModuleNodeCategory;
 };
 
 export enum ModuleNodeKind {
     Type = 1 << 1, Interface = 1 << 2, Enum = 1 << 3,
-    Model = ModuleNodeKind.Type | ModuleNodeKind.Interface | ModuleNodeKind.Enum,
-
     Function = 1 << 4, Variable = 1 << 5,
-    Variant = ModuleNodeKind.Function | ModuleNodeKind.Variable,
+}
+
+export enum ModuleNodeCategory {
+    Model = 1 << 1,
+    Variant = 1 << 2,
 }
