3db2381589e91a754b310c588480ce43c2b4c261
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 19:57:52 +0300

    refactor: change invalids property type in ModuleFile from ModuleMember[] to Node[] for improved type accuracy

--- libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
@@ -1,9 +1,10 @@
+import { Node } from "ts-morph";
 import { ModuleMember, ModuleModelMember, ModuleVariantMember } from "./moduleMember.model";
 
 export interface ModuleFile {
     kind: ModuleFileKind;
     members: ModuleMember[];
-    invalids: ModuleMember[];
+    invalids: Node[];
 };
 
 export interface ModuleModelFile extends ModuleFile {
