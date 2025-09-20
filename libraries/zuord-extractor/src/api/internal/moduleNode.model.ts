a52e5a0cc93579bd8926abc77bb53aba04652361
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:03:55 +0300

    feat: add Unknown value to ModuleNodeKind and ModuleNodeCategory enums

--- libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
@@ -7,11 +7,13 @@ export type ModuleNode = {
 };
 
 export enum ModuleNodeKind {
+    Unknown = 0,
     Type = 1 << 1, Interface = 1 << 2, Enum = 1 << 3,
     Function = 1 << 4, Variable = 1 << 5,
 }
 
 export enum ModuleNodeCategory {
+    Unknown = 0,
     Model = 1 << 1,
     Variant = 1 << 2,
 }
