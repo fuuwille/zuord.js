d2dffbf4707c3d6699a8ba2639b149f5e660cc2e
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 09:48:12 +0300

    feat: add kind property to ModuleNode type and define ModuleNodeKind enum

--- libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleNode.model.ts
@@ -2,4 +2,13 @@ import { Node } from "ts-morph"
 
 export type ModuleNode = {
     source: Node;
+    kind: ModuleNodeKind;
 };
+
+export enum ModuleNodeKind {
+    Type = 1 << 1, Interface = 1 << 2, Enum = 1 << 3,
+    Model = ModuleNodeKind.Type | ModuleNodeKind.Interface | ModuleNodeKind.Enum,
+
+    Function = 1 << 4, Variable = 1 << 5,
+    Variant = ModuleNodeKind.Function | ModuleNodeKind.Variable,
+}
