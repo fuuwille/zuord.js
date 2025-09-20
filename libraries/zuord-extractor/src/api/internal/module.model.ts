49a046b7facf6e072e48c8ab98440b1d2b45f43d
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:13:24 +0300

    feat: add dir property to Module type and update extractModule function

--- libraries/zuord-extractor/src/api/internal/module.model.ts
+++ libraries/zuord-extractor/src/api/internal/module.model.ts
@@ -1,6 +1,7 @@
 import { ModuleNode } from "./moduleNode.model";
 
 export type Module = {
+    dir: string;
     name: string;
     type: ModuleType;
     nodes: ModuleNode[];
--- libraries/zuord-extractor/src/api/internal/module.variants.ts
+++ libraries/zuord-extractor/src/api/internal/module.variants.ts
@@ -2,6 +2,7 @@ import { Module, ModuleType } from "./module.model";
 
 export const extractModule = (dir: string, name: string, type: ModuleType): Module => {
     return {
+        dir,
         name,
         type,
         nodes: [],
