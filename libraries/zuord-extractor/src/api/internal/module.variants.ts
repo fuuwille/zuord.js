0e88020a73fcd83691681d1df6f2ee490121efa2
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 10:14:23 +0300

    refactor: update extractModule function to use destructured parameters and improve readability

--- libraries/zuord-extractor/src/api/internal/module.variants.ts
+++ libraries/zuord-extractor/src/api/internal/module.variants.ts
@@ -1,10 +1,16 @@
 import { Module, ModuleType } from "./module.model";
+import { ModuleNode } from "./moduleNode.model";
 
-export const extractModule = (dir: string, name: string, type: ModuleType): Module => {
+export const extractModule = ($dir: string, $name: string, $type: ModuleType): Module => {
+    const dir = $dir;
+    const name = $name;
+    const type = $type;
+    const nodes = [] as ModuleNode[];
+
     return {
         dir,
         name,
         type,
-        nodes: [],
+        nodes,
     };
 };
