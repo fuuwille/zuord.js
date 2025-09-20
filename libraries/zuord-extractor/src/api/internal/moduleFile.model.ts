dc15bb9f6b382fe43168041255cefafcc060787d
k4yr2 <kyrzkaya@gmail.com>
2025-09-20 17:03:35 +0300

    refactor: add members property to ModuleModelFile interface for improved structure

--- libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleFile.model.ts
@@ -1,9 +1,11 @@
+import { ModuleModelMember } from "./moduleMember.model";
+
 export interface ModuleFile {
     kind: ModuleFileKind;
 };
 
 export interface ModuleModelFile extends ModuleFile {
-
+    members: ModuleModelMember[];
 }
 
 export interface ModuleVariantsFile extends ModuleFile {
