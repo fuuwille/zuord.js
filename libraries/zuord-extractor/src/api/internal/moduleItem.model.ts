a453e03cdbf8094fdec1e481e596d0e4b2aa7a13
k4yr2 <kyrzkaya@gmail.com>
2025-09-21 09:55:12 +0300

    feat: add ModuleModelItem and ModuleVariantItem interfaces for module member representation

--- libraries/zuord-extractor/src/api/internal/moduleItem.model.ts
+++ libraries/zuord-extractor/src/api/internal/moduleItem.model.ts
@@ -1,5 +1,13 @@
-import { ModuleMember } from "./moduleMember.model";
+import { ModuleMember, ModuleModelMember, ModuleVariantMember } from "./moduleMember.model";
 
 export interface ModuleItem {
     member: ModuleMember
+}
+
+export interface ModuleModelItem {
+    member: ModuleModelMember
+}
+
+export interface ModuleVariantItem {
+    member: ModuleVariantMember
 }
