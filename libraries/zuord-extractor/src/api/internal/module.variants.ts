3db6c36ac8eb583fe49ec80a665f65b666ef433c
k4yr2 <kyrzkaya@gmail.com>
2025-09-21 13:47:03 +0300

    feat: add error handling to extractModule function by conditionally removing empty errors array

--- libraries/zuord-extractor/src/api/internal/module.variants.ts
+++ libraries/zuord-extractor/src/api/internal/module.variants.ts
@@ -5,8 +5,13 @@ import { extractModuleFileIfExists } from "./moduleFile.variants";
 export const extractModule = (dir: string, name: string): Module => {
     const module = {
         modelFile: extractModuleFileIfExists(dir, name, ModuleMode.Model) ?? null,
-        variantsFile: extractModuleFileIfExists(dir, name, ModuleMode.Variants) ?? null
+        variantsFile: extractModuleFileIfExists(dir, name, ModuleMode.Variants) ?? null,
+        errors: []
     } as Module;
+
+    if(module.errors!.length == 0) {
+        delete module.errors;
+    }
 
     return module;
 };
