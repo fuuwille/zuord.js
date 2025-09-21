36c750cc20f016e17b8f6107c583f1a014a1c40f
k4yr2 <kyrzkaya@gmail.com>
2025-09-21 03:46:36 +0300

    refactor: add ModuleNode type to enhance module representation

--- libraries/zuord-extractor/src/api/internal/module.model.ts
+++ libraries/zuord-extractor/src/api/internal/module.model.ts
@@ -1,6 +1,9 @@
+import { Node } from "ts-morph";
 import { ModuleModelFile, ModuleVariantsFile } from "./moduleFile.model";
 
 export type Module = {
     modelFile: ModuleModelFile | null;
     variantsFile: ModuleVariantsFile | null;
 }
+
+export type ModuleNode = Node;
