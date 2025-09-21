4945eec9df332d3ee910faa0b446f775a720805a
k4yr2 <kyrzkaya@gmail.com>
2025-09-21 13:46:07 +0300

    feat: add optional errors property to Module type for improved error handling

--- libraries/zuord-extractor/src/api/internal/module.model.ts
+++ libraries/zuord-extractor/src/api/internal/module.model.ts
@@ -3,6 +3,7 @@ import { ModuleModelFile, ModuleVariantsFile } from "./moduleFile.model";
 export type Module = {
     modelFile: ModuleModelFile | null;
     variantsFile: ModuleVariantsFile | null;
+    errors?: string[];
 }
 
 export enum ModuleMode {
