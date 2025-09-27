6466a1f7597a3f933c9c56dca63445f57ed1af63
k4yr2 <kyrzkaya@gmail.com>
2025-09-27 13:39:18 +0300

    feat: enhance extractVariantID to support variable nodes extraction

--- libraries/zuord-extractor/src/api/internal/~variantID.variants.ts
+++ libraries/zuord-extractor/src/api/internal/~variantID.variants.ts
@@ -1,12 +1,24 @@
-import { ModuleFunctionNode, ModuleVariantLikeNode } from "./moduleNode.type";
-import { isModuleFunctionNode } from "./moduleNode.variants";
+import { ModuleFunctionNode, ModuleVariableNode, ModuleVariantLikeNode } from "./moduleNode.type";
+import { isModuleFunctionNode, isModuleVariableNode } from "./moduleNode.variants";
 
 export const extractVariantID = (node: ModuleVariantLikeNode): string | undefined => {
     if(isModuleFunctionNode(node)) {
         return extractVariantIDAsFunction(node);
     }
+
+    if(isModuleVariableNode(node)) {
+        return extractVariantIDAsVariable(node);
+    }
 }
 
 export const extractVariantIDAsFunction = (node: ModuleFunctionNode): string | undefined => {
     return node.getName();
+}
+
+export const extractVariantIDAsVariable = (node: ModuleVariableNode): string | undefined => {
+    const declaration = node.getDeclarations()[0];
+
+    if(declaration) {
+        return declaration.getName();
+    }
 }
