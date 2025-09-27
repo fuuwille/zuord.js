04445f22c80579f1f8e0c0399e3d02261f982ed4
k4yr2 <kyrzkaya@gmail.com>
2025-09-27 21:04:58 +0300

    feat: implement provideCodeLenses method in CodelensProvider for dynamic code lens generation

--- libraries/zuord-analyzer/src/codelens.ts
+++ libraries/zuord-analyzer/src/codelens.ts
@@ -1,11 +1,40 @@
 import * as vscode from "vscode";
+import explorer from "./explorer";
+import { Node } from "ts-morph";
 
 export class CodelensProvider implements vscode.CodeLensProvider {
+
     public provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
-        return [];
+        const codelenses: vscode.CodeLens[] = [];
+
+        const explorerModule = explorer.getModule();
+        const module = explorerModule?.module;
+
+        if(module) {
+            const file = module.typeFile;
+
+            if(file) {
+                file.members.forEach(member => {
+                    const node = member.node;
+                    const range = nodeToRange(node);
+                    const codelens = new vscode.CodeLens(range);
+                    codelenses.push(codelens);
+                });
+            }
+        }
+
+        return codelenses;
     }
 }
 
 const codelens = new CodelensProvider();
 
 export default codelens;
+
+function nodeToRange(node: Node): vscode.Range {
+    const start = node.getStartLineNumber() - 1; // VSCode 0 tabanlı
+    const startChar = node.getStart() - node.getPos(); // veya node.getStartLinePos()
+    const end = node.getEndLineNumber() - 1;
+    const endChar = node.getEnd() - node.getStart(); // approximate
+    return new vscode.Range(start, startChar, end, endChar);
+}
