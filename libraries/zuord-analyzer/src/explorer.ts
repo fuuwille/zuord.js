e99278e18d92f679394c6906b6a7721b6a53131e
k4yr2 <kyrzkaya@gmail.com>
2025-09-27 17:54:49 +0300

    feat: implement getWorkspaceFolder method in ExplorerProvider class

--- libraries/zuord-analyzer/src/explorer.ts
+++ libraries/zuord-analyzer/src/explorer.ts
@@ -1,5 +1,16 @@
-export class ExplorerProvider {
+import vscode from "vscode";
 
+export class ExplorerProvider {
+    getWorkspaceFolder(): vscode.WorkspaceFolder | undefined {
+        const activeEditor = vscode.window.activeTextEditor;
+
+        if (activeEditor) {
+            const uri = activeEditor.document.uri;
+            return vscode.workspace.getWorkspaceFolder(uri);
+        }
+
+        return undefined;
+    }
 }
 
 export const explorer = new ExplorerProvider();
