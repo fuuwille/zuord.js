071e2b94edc2c911dd935d1843040dbfaa271a13
k4yr2 <kyrzkaya@gmail.com>
2025-09-27 15:42:50 +0300

    fix: remove getTypeIDFromStr function and simplify getTypeIDFromSymbol implementation

--- libraries/zuord-extractor/src/api/internal/~typeID.variants.ts
+++ libraries/zuord-extractor/src/api/internal/~typeID.variants.ts
@@ -8,14 +8,5 @@ export const getTypeID = (type? : Type): string | undefined => {
 }
 
 export const getTypeIDFromSymbol = (symbol?: Symbol): string | undefined => {
-    return getTypeIDFromStr(symbol?.getName());
-}
-
-export const getTypeIDFromStr = (name?: string): string | undefined => {
-    if(!name) return undefined;
-
-    return name
-        .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
-        .replace(/([A-Z]+)([A-Z][a-z])/g, '$1-$2')
-        .toLowerCase();
+    return symbol?.getName();
 }
