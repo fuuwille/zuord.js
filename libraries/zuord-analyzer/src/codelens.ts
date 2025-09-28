0e7d1ba5028194ac14e59bcf405c08df7ffd74c7
k4yr2 <kyrzkaya@gmail.com>
2025-09-28 23:02:38 +0300

    refactor: add event emitter for code lens changes in CodelensProvider

--- libraries/zuord-analyzer/src/codelens.ts
+++ libraries/zuord-analyzer/src/codelens.ts
@@ -6,6 +6,13 @@ import { getKind, nodeToRange } from "./utils";
 
 export class CodelensProvider implements vscode.CodeLensProvider {
 
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+    public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+
+    public refreshCodeLenses(): void {
+        this._onDidChangeCodeLenses.fire();
+    }
+
     public provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
         const codelenses: vscode.CodeLens[] = [];
 
