a8181bf5325ecb0acc16d7f0f0297d760e7aa629
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 02:50:34 +0300

    refactor: implement extractRef and extractVariantLikeRef functions in moduleRef.variants.ts

--- libraries/zuord-extractor/src/internal/moduleRef.variants.ts
+++ libraries/zuord-extractor/src/internal/moduleRef.variants.ts
@@ -1,5 +1,48 @@
-import { ModuleTypeLikeRef } from "./moduleRef.type";
+import { Type, ts, Identifier } from "ts-morph";
+import { ModuleDefinitionLikeNode, ModuleFunctionLikeNode, ModuleVariantLikeNode } from "./moduleNode.type";
+import { isModuleFunctionLikeNode, isModuleVariantLikeNode } from "./moduleNode.variants";
+import { ModuleFunctionLikeRef, ModuleRef, ModuleVariantLikeRef } from "./moduleRef.type";
 
-export const extractTypeRef = (): ModuleTypeLikeRef | undefined => {
-    
-};
+export const extractRef = (node: ModuleDefinitionLikeNode): ModuleRef | undefined => {
+    if(isModuleVariantLikeNode(node)) {
+        return extractVariantLikeRef(node);
+    }
+}
+
+export const extractVariantLikeRef = (node: ModuleVariantLikeNode): ModuleVariantLikeRef | undefined => {
+    if(isModuleFunctionLikeNode(node)) {
+        return extractFunctionLikeRef(node);
+    }
+}
+
+
+export const extractFunctionLikeRef = (node: ModuleFunctionLikeNode): ModuleFunctionLikeRef | undefined => {
+    const returnTypeNode = node.getReturnTypeNode();
+    var returnType;
+
+    if(returnTypeNode) {
+        if(ts.isTypePredicateNode(returnTypeNode.compilerNode)) {
+            const targetTypeNode = returnTypeNode.compilerNode.type;
+            if (!targetTypeNode) return undefined;
+
+            const morphNode = node.getSourceFile().getDescendantAtPos(targetTypeNode.pos)! as unknown as Identifier;
+            if (!morphNode) return undefined;
+
+            returnType = morphNode.getType();
+        }
+
+        if(!returnType) {
+            returnType  = returnTypeNode.getType();
+        }
+
+        const constraint = returnType.getConstraint();
+
+        if(constraint) {
+            returnType = constraint;
+        }
+    }
+
+    return {
+        returnType
+    }
+}
