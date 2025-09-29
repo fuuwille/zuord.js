1a22c76aff4eb93bb8ff1214e49aee42d9578bf0
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 15:53:41 +0300

    refactor: add ModuleSchemaLikeMember, ModuleTypeMember, ModuleInterfaceMember, and ModuleEnumMember classes for enhanced module member representation

--- libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
+++ libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
@@ -1,4 +1,4 @@
-import { ModuleESMLikeNode, ModuleExportDefaultNode, ModuleExportNode, ModuleImportNode, ModuleNode } from "./moduleNode.tschema";
+import { ModuleEnumNode, ModuleESMLikeNode, ModuleExportDefaultNode, ModuleExportNode, ModuleImportNode, ModuleInterfaceNode, ModuleNode, ModuleSchemaLikeNode, ModuleTypeNode } from "./moduleNode.tschema";
 
 export abstract class ModuleMember {
     #node: ModuleNode
@@ -55,6 +55,46 @@ export class ModuleExportDefaultMember extends ModuleESMLikeMember {
 
     public get node(): ModuleExportDefaultNode {
         return super.node as ModuleExportDefaultNode;
+    }
+}
+
+export abstract class ModuleSchemaLikeMember extends ModuleMember {
+    public constructor(node: ModuleSchemaLikeNode, kind: ModuleMemberKind) {
+        super(node, kind);
+    }
+
+    public get node(): ModuleSchemaLikeNode {
+        return super.node as ModuleSchemaLikeNode;
+    }
+}
+
+export class ModuleTypeMember extends ModuleMember {
+    public constructor(node: ModuleTypeNode, kind: ModuleMemberKind) {
+        super(node, kind);
+    }
+
+    public get node(): ModuleTypeNode {
+        return super.node as ModuleTypeNode;
+    }
+}
+
+export class ModuleInterfaceMember extends ModuleMember {
+    public constructor(node: ModuleInterfaceNode, kind: ModuleMemberKind) {
+        super(node, kind);
+    }
+
+    public get node(): ModuleInterfaceNode {
+        return super.node as ModuleInterfaceNode;
+    }
+}
+
+export class ModuleEnumMember extends ModuleMember {
+    public constructor(node: ModuleEnumNode, kind: ModuleMemberKind) {
+        super(node, kind);
+    }
+
+    public get node(): ModuleEnumNode {
+        return super.node as ModuleEnumNode;
     }
 }
 
