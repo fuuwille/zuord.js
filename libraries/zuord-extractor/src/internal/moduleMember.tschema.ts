d35113858d4e32b68eea490bf311106a688af5e8
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 15:43:42 +0300

    refactor: implement constructor and getter for ModuleMember class

--- libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
+++ libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
@@ -1,5 +1,13 @@
 class ModuleMember {
+    #kind: ModuleMemberKind;
 
+    public constructor(kind: ModuleMemberKind) {
+        this.#kind = kind;
+    }
+
+    public get kind(): ModuleMemberKind {
+        return this.#kind;
+    }
 }
 
 enum ModuleMemberKind {
