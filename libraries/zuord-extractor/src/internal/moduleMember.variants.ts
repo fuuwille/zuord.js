ef19dfd95ba4054f2da586890e4ead37643e0550
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 17:09:36 +0300

    refactor: consolidate imports and add createModuleClassMember function for module member creation

--- libraries/zuord-extractor/src/internal/moduleMember.variants.ts
+++ libraries/zuord-extractor/src/internal/moduleMember.variants.ts
@@ -1,5 +1,5 @@
-import { ModuleExportDefaultMember, ModuleExportMember, ModuleImportMember } from "./moduleMember.tschema";
-import { ModuleExportDefaultNode, ModuleExportNode, ModuleImportNode } from "./moduleNode.tschema";
+import { ModuleClassMember, ModuleEnumMember, ModuleExportDefaultMember, ModuleExportMember, ModuleImportMember, ModuleInterfaceMember, ModuleTypeMember } from "./moduleMember.tschema";
+import { ModuleClassNode, ModuleEnumNode, ModuleExportDefaultNode, ModuleExportNode, ModuleImportNode, ModuleInterfaceNode, ModuleTypeNode } from "./moduleNode.tschema";
 
 export const createModuleImportMember = (node: ModuleImportNode): ModuleImportMember => {
     return new ModuleImportMember(node);
@@ -11,4 +11,20 @@ export const createModuleExportMember = (node: ModuleExportNode): ModuleExportMe
 
 export const createModuleExportDefaultMember = (node: ModuleExportDefaultNode): ModuleExportDefaultMember => {
     return new ModuleExportDefaultMember(node);
+}
+
+export const createModuleTypeMember = (node: ModuleTypeNode): ModuleTypeMember => {
+    return new ModuleTypeMember(node);
+}
+
+export const createModuleInterfaceMember = (node: ModuleInterfaceNode): ModuleInterfaceMember => {
+    return new ModuleInterfaceMember(node);
+}
+
+export const createModuleEnumMember = (node: ModuleEnumNode): ModuleEnumMember => {
+    return new ModuleEnumMember(node);
+}
+
+export const createModuleClassMember = (node: ModuleClassNode): ModuleClassMember => {
+    return new ModuleClassMember(node);
 }
