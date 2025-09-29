0731c627b6189556fda079d5b82345f226acec30
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 09:42:21 +0300

    refactor: simplify extractFunctionLikeRef by removing unused functions and consolidating return structure

--- libraries/zuord-extractor/src/internal/moduleRef.variants.ts
+++ libraries/zuord-extractor/src/internal/moduleRef.variants.ts
@@ -1,8 +1,8 @@
 import { ts, Identifier, TypeNode } from "ts-morph";
 import { ModuleNode, ModuleFunctionLikeNode, ModuleVariantLikeNode, ModuleVariableNode } from "./moduleNode.type";
 import { isModuleFunctionLikeNode, isModuleFunctionNode, isModuleVariableNode, isModuleVariantLikeNode } from "./moduleNode.variants";
-import { ModuleFunctionRef, ModuleRef, ModuleRefTypeDef, ModuleVariableRef, ModuleVariantLikeRef } from "./moduleRef.type";
-import { getTypeName, isPrimitiveType } from "./~type.variants";
+import { ModuleFunctionRef, ModuleRef, ModuleVariableRef, ModuleVariantLikeRef } from "./moduleRef.type";
+import { getFunctionPredicateType, getTypeName } from "./~type.variants";
 
 export const extractRef = (node: ModuleNode): ModuleRef | undefined => {
     if(isModuleVariantLikeNode(node)) {
@@ -35,58 +35,11 @@ export const extractVariableRef = (node: ModuleVariableNode): ModuleVariableRef 
 }
 
 export const extractFunctionLikeRef = (node: ModuleFunctionLikeNode): ModuleFunctionRef => {
+    const typeNode = node.getReturnTypeNode();
+    const paramTypeNode = node.getParameters()[0]?.getTypeNode();
+
     return {
-        type: {
-            return: getFunctionReturnType(node),
-            parameter: getFunctionParameterType(node)
-        }
+        typeNode,
+        paramTypeNode
     }
-}
-
-export const getFunctionReturnType = (node: ModuleFunctionLikeNode): ModuleRefTypeDef => {
-    const returnNode = node.getReturnTypeNode();
-
-    var returnType = getFunctionPredicateType(node, returnNode);
-
-    if(!returnType && returnNode) {
-        returnType  = returnNode.getType();
-    }
-
-    const constraint = returnType?.getConstraint();
-
-    if(constraint) {
-        returnType = constraint;
-    }
-
-    return returnType ?? null;
-}
-
-export const getFunctionPredicateType = (node: ModuleFunctionLikeNode, typeNode?: TypeNode): ModuleRefTypeDef => {
-    typeNode ??= node.getReturnTypeNode();
-
-    if(typeNode && ts.isTypePredicateNode(typeNode.compilerNode)) {
-        const targetTypeNode = typeNode.compilerNode.type;
-        if (!targetTypeNode) return null;
-
-        const morphNode = node.getSourceFile().getDescendantAtPos(targetTypeNode.pos)! as unknown as Identifier;
-        if (!morphNode) return null;
-
-        return morphNode.getType();
-    }
-
-    return null;
-}
-
-export const getFunctionParameterType = (node: ModuleFunctionLikeNode): ModuleRefTypeDef => {
-    const parameter = node.getParameters()[0];
-
-    if(parameter) {
-        return parameter.getType();
-    }
-
-    return null;
-}
-
-export const getTypeDefName = (typeDef: ModuleRefTypeDef): string | undefined => {
-    return getTypeName(typeDef ?? undefined);
 }
