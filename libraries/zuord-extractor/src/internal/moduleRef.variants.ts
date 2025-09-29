d642db60fbca1964eaf0ebd610263bc11b55868f
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 09:53:09 +0300

    refactor: streamline variable reference extraction and improve type handling

--- libraries/zuord-extractor/src/internal/moduleRef.variants.ts
+++ libraries/zuord-extractor/src/internal/moduleRef.variants.ts
@@ -1,8 +1,6 @@
-import { ts, Identifier, TypeNode } from "ts-morph";
 import { ModuleNode, ModuleFunctionLikeNode, ModuleVariantLikeNode, ModuleVariableNode } from "./moduleNode.type";
 import { isModuleFunctionLikeNode, isModuleFunctionNode, isModuleVariableNode, isModuleVariantLikeNode } from "./moduleNode.variants";
-import { ModuleFunctionRef, ModuleRef, ModuleVariableRef, ModuleVariantLikeRef } from "./moduleRef.type";
-import { getFunctionPredicateType, getTypeName } from "./~type.variants";
+import { ModuleFunctionLikeRef, ModuleFunctionRef, ModuleRef, ModuleVariableRef, ModuleVariantLikeRef } from "./moduleRef.type";
 
 export const extractRef = (node: ModuleNode): ModuleRef | undefined => {
     if(isModuleVariantLikeNode(node)) {
@@ -16,29 +14,36 @@ export const extractVariantLikeRef = (node: ModuleVariantLikeNode): ModuleVarian
     }
 
     if(isModuleFunctionNode(node)) {
-        return extractFunctionLikeRef(node);
+        return extractFunctionLikeRef(node) as ModuleFunctionRef;
     }
 }
 
 export const extractVariableRef = (node: ModuleVariableNode): ModuleVariableRef | undefined => {
     const declaration = node.getDeclarations()[0];
+    var initializer;
 
     if(declaration) {
-        const initializer = declaration.getInitializer();
+        const initializerNode = declaration.getInitializer();
 
-        if(initializer) {
-            if(isModuleFunctionLikeNode(initializer)) {
-                return extractFunctionLikeRef(initializer);
+        if(initializerNode) {
+            if(isModuleFunctionLikeNode(initializerNode)) {
+                initializer = extractFunctionLikeRef(initializerNode);
             }
         }
     }
+
+    return {
+        node,
+        initializer
+    }
 }
 
-export const extractFunctionLikeRef = (node: ModuleFunctionLikeNode): ModuleFunctionRef => {
+export const extractFunctionLikeRef = (node: ModuleFunctionLikeNode): ModuleFunctionLikeRef => {
     const typeNode = node.getReturnTypeNode();
     const paramTypeNode = node.getParameters()[0]?.getTypeNode();
 
     return {
+        node,
         typeNode,
         paramTypeNode
     }
