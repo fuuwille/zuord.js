a2d3bf29a2a2a3b1413309019dcf0efafa2c1327
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 15:42:52 +0300

    refactor: add ModuleMember class and ModuleMemberKind enum for module member representation

---
+++ libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
@@ -0,0 +1,15 @@
+class ModuleMember {
+
+}
+
+enum ModuleMemberKind {
+    Unknown = "unknown",
+    Import = "import",
+    Export = "export",
+    ExportDefault = "export-default",
+    Type = "type",
+    Interface = "interface",
+    Enum = "enum",
+    Function = "function",
+    Variable = "variable",
+}
