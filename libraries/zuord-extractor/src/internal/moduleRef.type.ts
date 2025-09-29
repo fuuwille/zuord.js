30ce05462773b84c1d0d627e20dee060ef9b7bab
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 09:48:59 +0300

    refactor: update module reference interfaces to ensure consistent node definitions

--- libraries/zuord-extractor/src/internal/moduleRef.type.ts
+++ libraries/zuord-extractor/src/internal/moduleRef.type.ts
@@ -1,22 +1,34 @@
 import { TypeNode } from "ts-morph";
+import { ModuleFunctionLikeNode, ModuleFunctionNode, ModuleNode, ModuleTypeLikeNode, ModuleVariableNode, ModuleVariantLikeNode } from "./moduleNode.type";
 
 export interface ModuleRef {
+    node: ModuleNode,
     typeNode?: TypeNode;
 };
 
 export interface ModuleTypeLikeRef extends ModuleRef {
+    node: ModuleTypeLikeNode;
     typeNode?: TypeNode;
 }
 
 export interface ModuleVariantLikeRef extends ModuleRef {
+    node: ModuleVariantLikeNode;
     typeNode?: TypeNode;
 }
 
 export interface ModuleVariableRef extends ModuleVariantLikeRef {
+    node: ModuleVariableNode;
     typeNode?: TypeNode;
 }
 
-export interface ModuleFunctionRef extends ModuleVariantLikeRef {
+export interface ModuleFunctionLikeRef extends ModuleRef {
+    node: ModuleFunctionLikeNode;
     typeNode?: TypeNode;
     paramTypeNode?: TypeNode;
+}
+
+
+export interface ModuleFunctionRef extends ModuleVariantLikeRef, ModuleFunctionLikeRef {
+    node: ModuleFunctionNode;
+    typeNode?: TypeNode;
 }
