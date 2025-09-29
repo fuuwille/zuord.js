30f5587d5dbb3f20fe3face42f55f4d77f045808
k4yr2 <kyrzkaya@gmail.com>
2025-09-29 05:32:43 +0300

    refactor: redefine ModuleRefType to include ModuleTypeLikeRefType and ModuleVariantLikeRefType

--- libraries/zuord-extractor/src/internal/moduleRef.type.ts
+++ libraries/zuord-extractor/src/internal/moduleRef.type.ts
@@ -4,4 +4,18 @@ export type ModuleRef = {
     type: ModuleRefType;
 };
 
-export type ModuleRefType = Type;
+export type ModuleRefType = 
+    | ModuleTypeLikeRefType
+    | ModuleVariantLikeRefType;
+
+export type ModuleTypeLikeRefType = {
+    identifier: ModuleRefType;
+};
+
+export type ModuleVariantLikeRefType =
+    | ModuleFunctionLikeRefType;
+
+export type ModuleFunctionLikeRefType = {
+    return?: Type;
+    parameter?: Type;
+}
