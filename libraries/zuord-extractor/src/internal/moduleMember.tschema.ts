1491f4399b3aad127a0aa78163ddaaba7b78119a
k4yr2 <kyrzkaya@gmail.com>
2025-09-30 05:26:54 +0300

    refactor: convert ModuleMember classes to interfaces for simplification

--- libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
+++ libraries/zuord-extractor/src/internal/moduleMember.tschema.ts
@@ -1,176 +1,68 @@
 import { Identifier, TypeNode } from "ts-morph";
 import { ModuleEnumNode, ModuleESMNode, ModuleExportDefaultNode, ModuleExportNode, ModuleFunctionNode, ModuleImportNode, ModuleInterfaceNode, ModuleNode, ModuleSchemaNode, ModuleTypeNode, ModuleVariableNode, ModuleVariantNode } from "./moduleNode.tschema";
-import { getModuleMemberNameNode } from "./moduleMember.variants";
 
-export abstract class ModuleMember {
-    #node: ModuleNode
-    #nameNode: Identifier | undefined | null;
-
-    public constructor(node: ModuleNode) {
-        this.#node = node;
-    }
-
-    public get node(): ModuleNode {
-        return this.#node;
-    }
-
-    public get nameNode(): Identifier | null {
-        if(this.#nameNode == undefined) {
-            this.#nameNode = getModuleMemberNameNode(this);
-        }
-
-        return this.#nameNode;
-    }
-
-    public abstract get kind(): ModuleMemberKind;
+export interface ModuleBaseMember {
+    node: ModuleNode;
+    kind: ModuleMemberKind;
+    nameNode?: Identifier | null;
 }
 
-export abstract class ModuleESMMember extends ModuleMember {
-    public constructor(node: ModuleESMNode) {
-        super(node);
-    }
-
-    public get node(): ModuleESMNode {
-        return super.node as ModuleESMNode;
-    }
+// ESM Members
+export interface ModuleESMMember extends ModuleBaseMember {
+    node: ModuleESMNode;
+    kind: ModuleMemberKind.Import | ModuleMemberKind.Export | ModuleMemberKind.ExportDefault;
 }
 
-export class ModuleImportMember extends ModuleESMMember {
-    public constructor(node: ModuleImportNode) {
-        super(node);
-    }
-
-    public get node(): ModuleImportNode {
-        return super.node as ModuleImportNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Import;
-    }
+export interface ModuleImportMember extends ModuleESMMember {
+    node: ModuleImportNode;
+    kind: ModuleMemberKind.Import;
 }
 
-export class ModuleExportMember extends ModuleESMMember {
-    public constructor(node: ModuleExportNode) {
-        super(node);
-    }
-
-    public get node(): ModuleExportNode {
-        return super.node as ModuleExportNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Export;
-    }
+export interface ModuleExportMember extends ModuleESMMember {
+    node: ModuleExportNode;
+    kind: ModuleMemberKind.Export;
 }
 
-export class ModuleExportDefaultMember extends ModuleESMMember {
-    public constructor(node: ModuleExportDefaultNode) {
-        super(node);
-    }
-
-    public get node(): ModuleExportDefaultNode {
-        return super.node as ModuleExportDefaultNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.ExportDefault;
-    }
+export interface ModuleExportDefaultMember extends ModuleESMMember {
+    node: ModuleExportDefaultNode;
+    kind: ModuleMemberKind.ExportDefault;
 }
 
-export abstract class ModuleSchemaMember extends ModuleMember {
-    public constructor(node: ModuleSchemaNode) {
-        super(node);
-    }
-
-    public get node(): ModuleSchemaNode {
-        return super.node as ModuleSchemaNode;
-    }
+// Schema Members
+export interface ModuleSchemaMember extends ModuleBaseMember {
+    node: ModuleSchemaNode;
+    kind: ModuleMemberKind.Type | ModuleMemberKind.Interface | ModuleMemberKind.Enum;
 }
 
-export class ModuleTypeMember extends ModuleSchemaMember {
-    public constructor(node: ModuleTypeNode) {
-        super(node);
-    }
-
-    public get node(): ModuleTypeNode {
-        return super.node as ModuleTypeNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Type;
-    }
+export interface ModuleTypeMember extends ModuleSchemaMember {
+    node: ModuleTypeNode;
+    kind: ModuleMemberKind.Type;
 }
 
-export class ModuleInterfaceMember extends ModuleSchemaMember {
-    public constructor(node: ModuleInterfaceNode) {
-        super(node);
-    }
-
-    public get node(): ModuleInterfaceNode {
-        return super.node as ModuleInterfaceNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Interface;
-    }
+export interface ModuleInterfaceMember extends ModuleSchemaMember {
+    node: ModuleInterfaceNode;
+    kind: ModuleMemberKind.Interface;
 }
 
-export class ModuleEnumMember extends ModuleSchemaMember {
-    public constructor(node: ModuleEnumNode) {
-        super(node);
-    }
-
-    public get node(): ModuleEnumNode {
-        return super.node as ModuleEnumNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Enum;
-    }
+export interface ModuleEnumMember extends ModuleSchemaMember {
+    node: ModuleEnumNode;
+    kind: ModuleMemberKind.Enum;
 }
 
-export abstract class ModuleVariantMember extends ModuleMember implements ModuleHasTypeNodeMember {
-    #typeNode: TypeNode | null = null;
-
-    public constructor(node: ModuleVariantNode) {
-        super(node);
-    }
-
-    public get node(): ModuleVariantNode {
-        return super.node as ModuleVariantNode;
-    }
-
-    public get typeNode(): TypeNode | null {
-        return this.#typeNode;
-    }
+// Variant Members
+export interface ModuleVariantMember extends ModuleBaseMember, ModuleHasTypeNodeMember {
+    node: ModuleVariantNode;
+    kind: ModuleMemberKind.Variable | ModuleMemberKind.Function;
 }
 
-export class ModuleVariableMember extends ModuleVariantMember {
-    public constructor(node: ModuleVariableNode) {
-        super(node);
-    }
-
-    public get node(): ModuleVariableNode {
-        return super.node as ModuleVariableNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Variable;
-    }
+export interface ModuleVariableMember extends ModuleVariantMember {
+    node: ModuleVariableNode;
+    kind: ModuleMemberKind.Variable;
 }
 
-export class ModuleFunctionMember extends ModuleVariantMember {
-    public constructor(node: ModuleFunctionNode) {
-        super(node);
-    }
-
-    public get node(): ModuleFunctionNode {
-        return super.node as ModuleFunctionNode;
-    }
-
-    public get kind(): ModuleMemberKind {
-        return ModuleMemberKind.Function;
-    }
+export interface ModuleFunctionMember extends ModuleVariantMember {
+    node: ModuleFunctionNode;
+    kind: ModuleMemberKind.Function;
 }
 
 export interface ModuleHasTypeNodeMember {
