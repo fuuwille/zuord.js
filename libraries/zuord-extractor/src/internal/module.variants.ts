5b9b4d554d886e6e7d7b0f5d733bac92eb6dedbf
k4yr2 <kyrzkaya@gmail.com>
2025-09-30 14:20:43 +0300

    refactor: utilize getModuleMemberNameNode to set nameNode in schemaContents for improved consistency

--- libraries/zuord-extractor/src/internal/module.variants.ts
+++ libraries/zuord-extractor/src/internal/module.variants.ts
@@ -3,7 +3,7 @@ import { Module } from "./module.tschema";
 import { ModuleMode } from "./module.tschema";
 import { extractModuleFileAtPath, extractModuleSchemaFile, extractModuleVariantsFile } from "./moduleFile.variants";
 import { createModuleTypeContent, createModuleVariantContent } from "./moduleContent.variants";
-import { isModuleSchemaMember, isModuleVariableMember, isModuleVariantMember } from "./moduleMember.variants";
+import { getModuleMemberNameNode, isModuleSchemaMember, isModuleVariableMember, isModuleVariantMember } from "./moduleMember.variants";
 import { ModuleSchemaFile, ModuleVariantsFile } from "./moduleFile.tschema";
 
 export const updateModule = (module: Module) => {
@@ -47,9 +47,9 @@ export const updateModule = (module: Module) => {
     if(module.schemaContents.length > 0) {
         for(const schemaContent of module.schemaContents) {
             const member = schemaContent.member;
-            const name = member.nameNode?.getText();
+            member.nameNode = getModuleMemberNameNode(member);
 
-            schemaContent.name = name;
+            schemaContent.name = member.nameNode?.getText();
         }
     }
 };
