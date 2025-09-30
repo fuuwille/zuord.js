67dbe3015bcd6760ec17a108229ad14f19ae6f01
k4yr2 <kyrzkaya@gmail.com>
2025-09-30 14:53:44 +0300

    refactor: add updateModuleSchemaContentName function to handle name assignment for schema content

--- libraries/zuord-extractor/src/internal/moduleContent.variants.ts
+++ libraries/zuord-extractor/src/internal/moduleContent.variants.ts
@@ -1,6 +1,7 @@
 import { Module } from "./module.tschema";
 import { ModuleContent, ModuleContentKind, ModuleSchemaContent, ModuleVariantContent } from "./moduleContent.tschema";
 import { ModuleSchemaMember, ModuleVariantMember } from "./moduleMember.tschema";
+import { updateModuleMemberNameNode } from "./moduleMember.variants";
 
 export const isModuleSchemaContent = (content: ModuleContent): content is ModuleSchemaContent => {
     return content.kind === ModuleContentKind.Schema;
@@ -32,4 +33,13 @@ export const createModuleVariantContent = (
         member,
         kind: ModuleContentKind.Variant
     };
+};
+
+//
+
+export const updateModuleSchemaContentName = (content: ModuleSchemaContent) => {
+    if(content.name == undefined) {
+        updateModuleMemberNameNode(content.member);
+        content.name = content.member.nameNode?.getText();
+    }
 };
