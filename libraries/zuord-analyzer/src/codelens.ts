26b694e8c245a9f74f647364ae7e6d202767daa0
k4yr2 <kyrzkaya@gmail.com>
2025-10-01 09:03:00 +0300

    feat: enhance diagnostics handling in CodelensProvider for better error reporting

--- libraries/zuord-analyzer/src/codelens.ts
+++ libraries/zuord-analyzer/src/codelens.ts
@@ -6,9 +6,13 @@ import { getKind, nodeToRange } from "./utils";
 
 export class CodelensProvider implements vscode.CodeLensProvider {
 
+    private diagnosticsCollection = vscode.languages.createDiagnosticCollection('customErrors');
+
     public provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
         const codelenses: vscode.CodeLens[] = [];
+        this.diagnosticsCollection.set(document.uri, []);
 
+        const allDiagnostics: vscode.Diagnostic[] = [];
         const explorerModule = explorer.getModule();
         const name = path.basename(document.uri.path);
         const kind = getKind(name);
@@ -23,11 +27,12 @@ export class CodelensProvider implements vscode.CodeLensProvider {
                     const node = schema.member.node;
                     const variantsCount = schema.variants?.length ?? 0;
 
-                    const range = nodeToRange(node);
+                    const range = nodeToRange(node, document);
                     const codelens = new vscode.CodeLens(range, {
                         title: "Have " + variantsCount + " variants",
                         command: ""
                     });
+                    
                     codelenses.push(codelens);
                 });
             }
@@ -35,16 +40,29 @@ export class CodelensProvider implements vscode.CodeLensProvider {
                 module.variantContents.forEach(variant => {
                     const member = variant.member;
                     const node = member.node;
-                    const range = nodeToRange(node);
+                    const range = nodeToRange(node, document);
                     const schema = variant.schema;
 
                     const codelens = new vscode.CodeLens(range, {
                         title: (schema?.name) ?? "no type",
                         command: ""
                     });
+
+                    variant.diagnostics?.forEach(diagnostic => {
+                        const diagRange = nodeToRange(diagnostic.node, document);
+                        const diag = new vscode.Diagnostic(
+                            diagRange,
+                            diagnostic.message,
+                            diagnostic.level === "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning
+                        );
+
+                        allDiagnostics.push(diag);
+                    });
                     codelenses.push(codelens);
                 });
             }
+
+            this.diagnosticsCollection.set(document.uri, allDiagnostics);
         }
 
         return codelenses;
