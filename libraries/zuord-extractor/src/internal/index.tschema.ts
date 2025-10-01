dfe544512bf462a716e000408cdf9c22f9571555
k4yr2 <kyrzkaya@gmail.com>
2025-10-02 02:42:39 +0300

    refactor: add missing exports for ModuleContent and ModuleDiagnostic in index files

--- libraries/zuord-extractor/src/internal/index.tschema.ts
+++ libraries/zuord-extractor/src/internal/index.tschema.ts
@@ -1,4 +1,6 @@
 export type * from "./module.tschema";
+export type { ModuleContent } from "./moduleContent";
+export type { ModuleDiagnostic } from "./moduleDiagnostic";
 export type { ModuleFile } from "./moduleFile";
 export type { ModuleMember } from "./moduleMember";
 export type { ModuleNode } from "./moduleNode";
--- libraries/zuord-extractor/src/internal/index.variants.ts
+++ libraries/zuord-extractor/src/internal/index.variants.ts
@@ -1,4 +1,6 @@
 export * from "./module.variants";
+export { moduleContent } from "./moduleContent";
+export { moduleDiagnostic } from "./moduleDiagnostic";
 export { moduleFile } from "./moduleFile";
 export { moduleMember } from "./moduleMember";
 export { moduleNode } from "./moduleNode";
