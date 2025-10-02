aeb55691045f0a15aa01dd185fdbcf6c8aa53a1e
k4yr2 <kyrzkaya@gmail.com>
2025-10-02 13:25:35 +0300

    feat: add diagnostic level exports for improved module diagnostics

---
+++ libraries/zuord-extractor/src/internal/moduleDiagnosticLevel.tvariants.ts
@@ -0,0 +1,7 @@
+import { ModuleDiagnosticLevel } from "./moduleDiagnosticLevel";
+
+export const Info: ModuleDiagnosticLevel.Info = "info";
+
+export const Warning: ModuleDiagnosticLevel.Warning = "warning";
+
+export const Error: ModuleDiagnosticLevel.Error = "error";
