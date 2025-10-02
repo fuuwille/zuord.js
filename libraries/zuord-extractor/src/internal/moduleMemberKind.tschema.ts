93d1d1344b09bcae0c826243d01f0b59ed3f19fc
k4yr2 <kyrzkaya@gmail.com>
2025-10-02 09:06:26 +0300

    feat: add module member types to moduleMemberKind.tschema.ts

---
+++ libraries/zuord-extractor/src/internal/moduleMemberKind.tschema.ts
@@ -0,0 +1,21 @@
+export type Unknown = "unknown";
+
+export type Import = "import";
+
+export type Export = "export";
+
+export type ExportDefault = "exportDefault";
+
+export type Type = "type";
+
+export type Interface = "interface";
+
+export type Variable = "variable";
+
+export type Function = "function";
+
+export type Value = "value";
+
+export type ArrowFunction = "arrowFunction";
+
+export type FunctionExpression = "functionExpression";
