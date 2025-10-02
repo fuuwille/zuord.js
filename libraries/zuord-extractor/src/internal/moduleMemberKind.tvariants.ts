2238772b2718e0d6972d0eb77b39d3c3fdcccbce
k4yr2 <kyrzkaya@gmail.com>
2025-10-02 09:06:39 +0300

    feat: add module member types to moduleMemberKind.tvariants.ts

---
+++ libraries/zuord-extractor/src/internal/moduleMemberKind.tvariants.ts
@@ -0,0 +1,25 @@
+import { ModuleMemberKind } from "./moduleMemberKind";
+
+//
+
+export const Unknown: ModuleMemberKind.Unknown = "unknown";
+
+export const Import: ModuleMemberKind.Import = "import";
+
+export const Export: ModuleMemberKind.Export = "export";
+
+export const ExportDefault: ModuleMemberKind.ExportDefault = "exportDefault";
+
+export const Type: ModuleMemberKind.Type = "type";
+
+export const Interface: ModuleMemberKind.Interface = "interface";
+
+export const Variable: ModuleMemberKind.Variable = "variable";
+
+export const Function: ModuleMemberKind.Function = "function";
+
+export const Value: ModuleMemberKind.Value = "value";
+
+export const ArrowFunction: ModuleMemberKind.ArrowFunction = "arrowFunction";
+
+export const FunctionExpression: ModuleMemberKind.FunctionExpression = "functionExpression";
