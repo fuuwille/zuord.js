5c6015a9fe2036131120270dd7eecdafb906baa8
k4yr2 <kyrzkaya@gmail.com>
2025-10-03 13:18:56 +0300

    refactor: replace tschema and tvariants with new zschema and zvariants files

--- libraries/zuord-extractor/src/internal/moduleDiagnostic.ts
+++ libraries/zuord-extractor/src/internal/moduleDiagnostic.ts
@@ -1,2 +1,2 @@
-export * as ModuleDiagnostic from "./moduleDiagnostic.tschema";
-export * as moduleDiagnostic from "./moduleDiagnostic.tvariants";
+export * as ModuleDiagnostic from "./moduleDiagnostic.zschema";
+export * as moduleDiagnostic from "./moduleDiagnostic.zvariants";
