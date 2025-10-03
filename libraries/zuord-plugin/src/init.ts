bebde019ba572e4afa0042265fe5765f14f33159
k4yr2 <kyrzkaya@gmail.com>
2025-10-04 01:01:40 +0300

    refactor: restructure handleScriptSnapshot function and improve script snapshot handling

--- libraries/zuord-plugin/src/init.ts
+++ libraries/zuord-plugin/src/init.ts
@@ -1,38 +1,17 @@
-// @ts-nocheck
 const utility = require("./utility");
 const caseAnything = require("case-anything");
 
+// @ts-ignore
 export = function (modules) {
     const typescript = modules.typescript;
 
+    // @ts-ignore
     function create(info) {
         const oldLS = info.languageService;
         const host = info.languageServiceHost;
 
-        const oldGetScriptSnapshot = host.getScriptSnapshot?.bind(host);
-
         host.getScriptSnapshot = (fileName: string) => {
-            const snapshot = oldGetScriptSnapshot?.(fileName);
-
-            if (snapshot && fileName.endsWith(".ts")) {
-                let virtualImports = "";
-
-                const baseName = utility.getBaseName(fileName) || '';
-
-                if (utility.isZVariantsFile(fileName)) {
-                    virtualImports += `\nimport * as ZSchema from './${baseName}.zs';`;
-                }
-                else if (utility.isZSchemaFile(fileName)) {
-                    virtualImports += `\nimport * as zvariants from './${baseName}.zv';`;
-                }
-                
-                let text = snapshot.getText(0, snapshot.getLength());
-                const combined = text + virtualImports;
-
-                return typescript.ScriptSnapshot.fromString(combined);
-            }
-
-            return snapshot;
+            handleScriptSnapshot(host.getScriptSnapshot?.bind(host), fileName);
         };
 
         const oldGetScriptKind = host.getScriptKind?.bind(host);
@@ -48,7 +27,30 @@ export = function (modules) {
         return oldLS;
     }
 
+    // @ts-ignore
+    function handleScriptSnapshot(origin, fileName: string) {
+        const snapshot = origin?.(fileName);
+
+        if (snapshot && fileName.endsWith(".ts")) {
+            let virtualImports = "";
+
+            const baseName = utility.getBaseName(fileName) || '';
+
+            if (utility.isZVariantsFile(fileName)) {
+                virtualImports += `\nimport * as ZSchema from './${baseName}.zs';`;
+            }
+            else if (utility.isZSchemaFile(fileName)) {
+                virtualImports += `\nimport * as zvariants from './${baseName}.zv';`;
+            }
+            
+            let text = snapshot.getText(0, snapshot.getLength());
+            const combined = text + virtualImports;
+
+            return typescript.ScriptSnapshot.fromString(combined);
+        }
+
+        return snapshot;
+    }
+
     return { create };
 }
-
-export function handleScriptSnapshot()
