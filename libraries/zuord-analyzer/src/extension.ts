b89f222d098cff319f9da733231b40b6298cd6d8
k4yr2 <kyrzkaya@gmail.com>
2025-10-03 14:22:29 +0300

    refactor: update file associations for .zs and .zv extensions to use TypeScript

--- libraries/zuord-analyzer/src/extension.ts
+++ libraries/zuord-analyzer/src/extension.ts
@@ -2,6 +2,15 @@ import vscode from 'vscode';
 import codelens from './codelens';
 
 export function activate(context: vscode.ExtensionContext) {
+    vscode.workspace.getConfiguration().update(
+        "files.associations",
+        {
+            "*.zs": "typescript",
+            "*.zv": "typescript"
+        },
+        vscode.ConfigurationTarget.Global
+    );
+
     context.subscriptions.push(
         vscode.languages.registerCodeLensProvider(
             { scheme: "file", language: "typescript" },
