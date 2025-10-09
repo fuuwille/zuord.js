451db0b2ec443ee8c0bc7b6ebec6c860dbc3a207
k4yr2 <kyrzkaya@gmail.com>
2025-10-09 04:59:38 +0300

    feat: add parent property to PackageDirectory type definition

--- libraries/zuord-extractor/temp/packageDirectory.zschema.ts
+++ libraries/zuord-extractor/temp/packageDirectory.zschema.ts
@@ -3,6 +3,7 @@ import { PackageModule } from "./packageModule.zschema";
 
 export type PackageDirectory = {
     package: Package;
+    parent: PackageDirectory | null;
     name: string;
     directories: PackageDirectory[];
     modules: PackageModule[];
