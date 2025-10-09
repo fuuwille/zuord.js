a6a4f008947014ce62e43a491b4220db12cedd99
k4yr2 <kyrzkaya@gmail.com>
2025-10-09 04:58:17 +0300

    feat: add package property to PackageDirectory type definition

--- libraries/zuord-extractor/temp/packageDirectory.zschema.ts
+++ libraries/zuord-extractor/temp/packageDirectory.zschema.ts
@@ -1,6 +1,8 @@
+import { Package } from "./package.zschema";
 import { PackageModule } from "./packageModule.zschema";
 
 export type PackageDirectory = {
+    package: Package;
     name: string;
     directories: PackageDirectory[];
     modules: PackageModule[];
