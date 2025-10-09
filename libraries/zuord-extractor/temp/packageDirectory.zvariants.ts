2b816481fe4be8098d012b2de60ade5128b188c6
k4yr2 <kyrzkaya@gmail.com>
2025-10-09 05:01:46 +0300

    feat: implement extractDirectory function for Package and PackageDirectory types

---
+++ libraries/zuord-extractor/temp/packageDirectory.zvariants.ts
@@ -0,0 +1,23 @@
+import * as ZSchema from "./packageDirectory.zschema";
+import { Package } from "./package.zschema";
+import { PackageDirectory } from "./packageDirectory.zschema";
+
+export const extractDirectory = (parent : Package | PackageDirectory, name : string) : ZSchema.PackageDirectory => {
+    if("path" in parent) {
+        return {
+            package: parent,
+            parent: null,
+            name,
+            directories: [],
+            modules: []
+        };
+    } else {
+        return {
+            package: parent.package,
+            parent,
+            name,
+            directories: [],
+            modules: []
+        };
+    }
+}
