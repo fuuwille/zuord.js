2812665a94c0da4e5e3390b2532ed83375e71667
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:21:39 +0300

    feat: add ModuleType enum and restructure Module interface

--- libraries/zuord-stash/src/module.ts
+++ libraries/zuord-stash/src/module.ts
@@ -1,5 +1,11 @@
 import { ModuleDocument } from "./moduleDocument";
 
 export interface Module {
+    type: ModuleType;
     document: ModuleDocument;
+}
+
+export enum ModuleType {
+    Source,
+    Dist
 }
