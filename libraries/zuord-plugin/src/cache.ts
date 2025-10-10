55625e9848c75c77543bd29a8b4e0bc3321751a0
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 10:55:59 +0300

    fix: add source property to ModuleEntry type in cache.ts

--- libraries/zuord-plugin/src/cache.ts
+++ libraries/zuord-plugin/src/cache.ts
@@ -1,6 +1,8 @@
 import ts from "typescript";
+import { $ZuordExtractor as ze } from "zuord-extractor";
 
 export type ModuleEntry = {
     dirty: boolean;
     snapshot: ts.IScriptSnapshot | undefined;
+    source: ze.Module | undefined;
 }
