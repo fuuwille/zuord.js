951944354fdd9a6f3a5da5cd238d1c746952e501
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:03:05 +0300

    feat: update ModuleFile type to allow undefined for snapshot and source properties

--- libraries/zuord-stash/src/moduleFile.ts
+++ libraries/zuord-stash/src/moduleFile.ts
@@ -1,7 +1,9 @@
 import ts from "typescript";
 import { ModuleDocument } from "./moduleDocument";
+import { $ZuordExtractor as ZE } from "zuord-extractor";
 
 export type ModuleFile = {
     document: ModuleDocument;
-    snapshot: ts.IScriptSnapshot;
+    snapshot: ts.IScriptSnapshot | undefined;
+    source: ZE.ModuleFile | undefined;
 }
