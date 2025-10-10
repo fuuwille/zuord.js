6e52cd50e9234f5b89da6574c83d8156f375ba63
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:22:59 +0300

    feat: add source property to Module interface for ZE.Module support

--- libraries/zuord-stash/src/module.ts
+++ libraries/zuord-stash/src/module.ts
@@ -1,8 +1,10 @@
 import { ModuleDocument } from "./moduleDocument";
+import { $ZuordExtractor as ZE } from "zuord-extractor";
 
 export interface Module {
     type: ModuleType;
     document: ModuleDocument;
+    source: ZE.Module | undefined;
 }
 
 export enum ModuleType {
