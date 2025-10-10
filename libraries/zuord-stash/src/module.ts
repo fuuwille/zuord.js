e38c0932aadf9c00f0acdb05d1cf5eaf7c447387
k4yr2 <kyrzkaya@gmail.com>
2025-10-11 01:01:51 +0300

    refactor: restructure Module interface to include ModuleInfo type

--- libraries/zuord-stash/src/module.ts
+++ libraries/zuord-stash/src/module.ts
@@ -2,10 +2,14 @@ import { ModuleDocument } from "./moduleDocument";
 import { $ZuordExtractor as ZE } from "zuord-extractor";
 
 export interface Module {
-    name: string;
-    type: ModuleType;
+    info: ModuleInfo;
     document: ModuleDocument;
     source: ZE.Module | undefined;
+}
+
+export type ModuleInfo = {
+    name: string;
+    type: ModuleType;
 }
 
 export enum ModuleType {
