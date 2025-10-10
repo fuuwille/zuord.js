19ba382767fe43e7cf3b58e2508de794ef351b16
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:16:22 +0300

    feat: refactor ModuleFile interface to use a symbol for dirty property

--- libraries/zuord-stash/src/moduleFile.ts
+++ libraries/zuord-stash/src/moduleFile.ts
@@ -2,12 +2,11 @@ import ts from "typescript";
 import { Module } from "./module";
 import { $ZuordExtractor as ZE } from "zuord-extractor";
 
+export const dirtySymbol = Symbol("dirty");
+
 export interface ModuleFile {
+    [dirtySymbol]: boolean;
     module: Module;
     snapshot: ts.IScriptSnapshot | undefined;
     source: ZE.ModuleFile | undefined;
-}
-
-export interface StoredModuleFile extends ModuleFile {
-    dirty: boolean;
 }
