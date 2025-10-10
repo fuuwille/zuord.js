b265b1b4f9aa001bf52d4bd4a38d1b8d7a74cd05
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:05:44 +0300

    feat: change ModuleFile type from type to interface for better extensibility

--- libraries/zuord-stash/src/moduleFile.ts
+++ libraries/zuord-stash/src/moduleFile.ts
@@ -2,8 +2,12 @@ import ts from "typescript";
 import { ModuleDocument } from "./moduleDocument";
 import { $ZuordExtractor as ZE } from "zuord-extractor";
 
-export type ModuleFile = {
+export interface ModuleFile {
     document: ModuleDocument;
     snapshot: ts.IScriptSnapshot | undefined;
     source: ZE.ModuleFile | undefined;
+}
+
+export interface StoredModuleFile extends ModuleFile {
+    dirt: boolean;
 }
