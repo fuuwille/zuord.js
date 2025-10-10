1c94c0afafdfad0202dfc5d0c664003c652a13e1
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:12:26 +0300

    feat: add properties to Module interface for main, schema, and variants files

--- libraries/zuord-stash/src/module.ts
+++ libraries/zuord-stash/src/module.ts
@@ -1,3 +1,7 @@
+import { ModuleFile } from "./moduleFile";
+
 export interface Module {
-    
+    mainFile: ModuleFile | undefined;
+    schemaFile: ModuleFile | undefined;
+    variantsFile: ModuleFile | undefined;
 }
