4a2317ac09b2aa9f7fe677f5d00e9a0c1f16a3dd
k4yr2 <kyrzkaya@gmail.com>
2025-10-10 16:23:50 +0300

    feat: add name property to Module interface

--- libraries/zuord-stash/src/module.ts
+++ libraries/zuord-stash/src/module.ts
@@ -2,6 +2,7 @@ import { ModuleDocument } from "./moduleDocument";
 import { $ZuordExtractor as ZE } from "zuord-extractor";
 
 export interface Module {
+    name: string;
     type: ModuleType;
     document: ModuleDocument;
     source: ZE.Module | undefined;
