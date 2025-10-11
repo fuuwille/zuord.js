dcc9e65afbc08be84078181894a414f5da6c00a3
k4yr2 <kyrzkaya@gmail.com>
2025-10-11 10:56:58 +0300

    refactor: improve getProjectPath method to use a variable for last found path

--- libraries/zuord-stash/src/stash.ts
+++ libraries/zuord-stash/src/stash.ts
@@ -25,16 +25,18 @@ export class Stash {
         if (!regex.path.exec(path)) return undefined;
 
         const segments = path.split("/").filter(Boolean);
+
         let lookingPath = PATH.isAbsolute(path) ? PATH.sep : "";
+        let lastFoundPath: string | undefined = undefined;
 
         for (const segment of segments) {
             lookingPath = PATH.join(lookingPath, segment);
 
             if (FS.existsSync(PATH.join(lookingPath, "zuord.json"))) {
-                return lookingPath;
+                lastFoundPath = lookingPath;
             }
         }
 
-        return undefined;
+        return lastFoundPath;
     }
 }
