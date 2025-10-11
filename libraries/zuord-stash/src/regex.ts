c9b1e0c8a05d939628734e8265d90e7e2f140bb2
k4yr2 <kyrzkaya@gmail.com>
2025-10-11 10:41:34 +0300

    feat: add regex for path segments extraction

--- libraries/zuord-stash/src/regex.ts
+++ libraries/zuord-stash/src/regex.ts
@@ -1,3 +1,5 @@
 export const path = /^\/(?:[a-zA-Z._-]+\/)*[a-zA-Z._-]*$/;
 
+export const pathSegments = /[\/\\]([a-zA-Z0-9_-]+?)(?=[\/\\]|$)/g;
+
 export const moduleName = /^(?:[\/\\][a-zA-Z0-9_-]+)+\/((?!.*\.\.)[a-zA-Z0-9_.-]+?)(\.(?:zschema\.ts|zvariants\.ts|ts|tzs|tzv))$/;
