b8b6f7ee4d8189903d715771e28c39d806e35845
k4yr2 <kyrzkaya@gmail.com>
2025-10-12 06:58:33 +0300

    feat: add regex pattern for valid project file names

--- libraries/zuord-stash/src/regex.ts
+++ libraries/zuord-stash/src/regex.ts
@@ -2,3 +2,8 @@
  * Matches a valid project path, starting with a '/' and containing segments of letters, numbers, dots, underscores, or hyphens.
  */
 export const path = /^\/(?:[\w.-]+\/)*[\w.-]*$/;
+
+/**
+ * Matches a valid project file name, consisting of a name and an extension.
+ */
+export const fileName = /^([\w-]+)\.(ts|tzs|tzv|zschema.ts|zvariants.ts)$/;
