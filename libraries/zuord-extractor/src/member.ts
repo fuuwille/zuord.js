7b815dc257d3b5113007d79005ebd9ad7e810884
k4yr2 <kyrzkaya@gmail.com>
2025-10-13 10:23:05 +0300

    refactor: simplify MemberKind enum by removing bitwise operations

--- libraries/zuord-extractor/src/member.ts
+++ libraries/zuord-extractor/src/member.ts
@@ -3,31 +3,15 @@
 export { default as MemberBase } from "./member.tzs";
 
 export enum MemberKind {
-    Unknown = 0,
-    Import = 1 << 0,
-    Export = 1 << 1,
-    ExportDefault = 1 << 2,
-    Type = 1 << 3,
-    Interface = 1 << 4,
-    Variable = 1 << 5,
-    Function = 1 << 6,
-    Value = 1 << 7,
-    ArrowFunction = 1 << 8,
-    FunctionExpression = 1 << 9,
-
-    //
-
-    FunctionAlt = ArrowFunction | FunctionExpression,
-    FunctionLike = Function | FunctionAlt,
-    InitializerLike = Value | ArrowFunction | FunctionExpression,
-    VariantOnly = Variable | Function,
-    SchemaOnly = Type | Interface,
-    DefinitionOnly = SchemaOnly | VariantOnly,
-    ExportLike = Export | ExportDefault,
-    ESMLike = Import | Export | ExportDefault,
-    GlobalLike = ESMLike,
-    VariantLike = GlobalLike | VariantOnly,
-    SchemaLike = GlobalLike | SchemaOnly,
-    DefinitionLike = GlobalLike | DefinitionOnly,
-    KnownLike = ESMLike | DefinitionOnly | InitializerLike,
+    Unknown,
+    Import,
+    Export,
+    ExportDefault,
+    Type,
+    Interface,
+    Variable,
+    Function,
+    Value,
+    ArrowFunction,
+    FunctionExpression
 }
