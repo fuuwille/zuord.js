dc678e0f79b01d4fc9e0e60b74a86925816a351f
k4yr2 <kyrzkaya@gmail.com>
2025-10-13 18:15:50 +0300

    refactor: update FileType enum to use bitwise operations for Schema and Variants

--- libraries/zuord-extractor/src/file.ts
+++ libraries/zuord-extractor/src/file.ts
@@ -3,9 +3,12 @@
 export { default as FileBase } from "./file.tzs";
 
 export enum FileType {
-    Main = "main",
-    Schema = "schema",
-    Variants = "variants"
+    TS = 0,
+    TV = 1 << 1,
+    TZS = 1 << 2,
+    TZV = 1 << 3,
+    Schema = FileType.TS | FileType.TZS,
+    Variants = FileType.TS | FileType.TZV
 }
 
 export { File as FileSpec, file as fileSpec };
