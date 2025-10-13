125b337d2b6875d0f6f32b47cdb93621adc3321d
k4yr2 <kyrzkaya@gmail.com>
2025-10-13 22:41:57 +0300

    feat: implement Module class with private name property and getter

---
+++ libraries/zuord-extractor/src/madule.ts
@@ -0,0 +1,13 @@
+export class Module {
+    #name: string;
+
+    constructor(name: string) {
+        this.#name = name;
+    }
+
+    //
+
+    get name(): string {
+        return this.#name;
+    }
+}
