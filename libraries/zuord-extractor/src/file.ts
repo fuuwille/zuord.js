519babc6a7830137de78030ce09ca23a8ba8c160
k4yr2 <kyrzkaya@gmail.com>
2025-10-13 18:18:00 +0300

    refactor: update FileType enum values and adjust related type usages in Schema and Variants

--- libraries/zuord-extractor/src/file.ts
+++ libraries/zuord-extractor/src/file.ts
@@ -3,12 +3,10 @@
 export { default as FileBase } from "./file.tzs";
 
 export enum FileType {
-    TS = 0,
-    TV = 1 << 1,
-    TZS = 1 << 2,
-    TZV = 1 << 3,
-    Schema = FileType.TS | FileType.TZS,
-    Variants = FileType.TS | FileType.TZV
+    TS = "ts",
+    TV = "tv",
+    TZS = "tzs",
+    TZV = "tzv"
 }
 
 export { File as FileSpec, file as fileSpec };
