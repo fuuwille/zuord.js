f94c38b5000efbe0bb77183a9d9356015ee2444d
k4yr2 <kyrzkaya@gmail.com>
2025-10-18 21:33:00 +0300

    refactor: update MemberInitializerLikeType and add MemberFunctionAltType for improved member type categorization

--- libraries/zuord-extractor/src/member.ts
+++ libraries/zuord-extractor/src/member.ts
@@ -56,3 +56,7 @@ export type MemberInitializerLikeType =
     | MemberType.Value
     | MemberType.ArrowFunction
     | MemberType.FunctionExpression;
+
+export type MemberFunctionAltType =
+    | MemberType.ArrowFunction
+    | MemberType.FunctionExpression;
